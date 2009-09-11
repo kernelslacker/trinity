ab11285134e9a7685c5b2b5710a903d2a5439433
Dave Jones <davej@redhat.com>
2009-09-11 14:41:56 -0400

    Fix parameters in test script

--- test-multi.sh
+++ test-multi.sh
@@ -5,9 +5,10 @@ if [ ! -d tmp ]; then
 fi
 cd tmp
 
-for i in `seq 1 20`;
+for i in `seq 1 10`
 do
-	../scrashme -z &
-	../scrashme -r &
-	../scrashme -r -i &
+	../scrashme --mode=rotate -z
+	../scrashme --mode=rotate -k
+	../scrashme --mode=random &
+	../scrashme --mode=random -i &
 done
