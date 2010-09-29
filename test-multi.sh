941c9c4efba66ea91e32bd0e857e22dae48f9bc0
Dave Jones <davej@redhat.com>
2010-09-28 23:50:46 -0400

    Move the bounded runs outside the loop

--- test-multi.sh
+++ test-multi.sh
@@ -5,10 +5,12 @@ if [ ! -d tmp ]; then
 fi
 cd tmp
 
+../scrashme --mode=rotate -z
+../scrashme --mode=rotate -k
+../scrashme --mode=rotate -u
+
 for i in `seq 1 10`
 do
-	../scrashme --mode=rotate -z &
-	../scrashme --mode=rotate -k &
 	../scrashme --mode=random &
 	../scrashme --mode=random -i &
 done
