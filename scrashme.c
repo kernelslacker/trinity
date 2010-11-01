7f203bcd446a77e402a3f991c34e27b72a0241b3
Dave Jones <davej@redhat.com>
2010-11-01 14:59:19 -0400

    always print out the pre/post sanitised options

--- scrashme.c
+++ scrashme.c
@@ -171,14 +171,15 @@ static long mkcall(int call)
 	}
 
 	if (intelligence == 1) {
+		printf("\n\tSanitising options.\n\tBefore:\t"
+		"(0x%lx,0x%lx,0x%lx,0x%lx,0x%lx,0x%lx)\n", a1, a2, a3, a4, a5, a6);
+
 		generic_sanitise(call, &a1, &a2, &a3, &a4, &a5, &a6);
-		if (syscalls[call].sanitise) {
-#if 1
-			printf("\n\tSanitising options.\n\tBefore:\t");
-			printf("(0x%lx,0x%lx,0x%lx,0x%lx,0x%lx,0x%lx)\n\tAfter:\t", a1, a2, a3, a4, a5, a6);
-#endif
+		if (syscalls[call].sanitise)
 			syscalls[call].sanitise(&a1, &a2, &a3, &a4, &a5, &a6);
-		}
+
+		printf("\tAfter:\t"
+		"(0x%lx,0x%lx,0x%lx,0x%lx,0x%lx,0x%lx)\n", a1, a2, a3, a4, a5, a6);
 	}
 
 	if (syscalls[call].num_args == 1)
