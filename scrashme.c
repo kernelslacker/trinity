15b1087c7235cb965836dfbdfccf17745593c10e
Dave Jones <davej@redhat.com>
2010-11-02 11:56:14 -0400

    merge the two post sanitised register printing routines.

--- scrashme.c
+++ scrashme.c
@@ -162,77 +162,62 @@ static long mkcall(int call)
 
 		printf("\t After:\t" WHITE "(");
 		if (syscalls[call].num_args >= 1) {
+			if (syscalls[call].arg1name)
+				printf("%s=", syscalls[call].arg1name);
 			if (olda1==a1)
 				printf(WHITE "0x%lx", a1);
 			else
 				printf(CYAN "0x%lx", a1);
 		}
 		if (syscalls[call].num_args >= 2) {
+			printf(", ");
+			if (syscalls[call].arg2name)
+				printf("%s=", syscalls[call].arg2name);
 			if (olda2==a2)
-				printf(WHITE ", 0x%lx", a2);
+				printf(WHITE "0x%lx", a2);
 			else
-				printf(CYAN ", 0x%lx", a2);
+				printf(CYAN "0x%lx", a2);
 		}
 		if (syscalls[call].num_args >= 3) {
+			printf(", ");
+			if (syscalls[call].arg3name)
+				printf("%s=", syscalls[call].arg3name);
 			if (olda3==a3)
-				printf(WHITE ", 0x%lx", a3);
+				printf(WHITE "0x%lx", a3);
 			else
-				printf(CYAN ", 0x%lx", a3);
+				printf(CYAN "0x%lx", a3);
 		}
 		if (syscalls[call].num_args >= 4) {
+			printf(", ");
+			if (syscalls[call].arg4name)
+				printf("%s=", syscalls[call].arg4name);
 			if (olda4==a4)
-				printf(WHITE ", 0x%lx", a4);
+				printf(WHITE "0x%lx", a4);
 			else
-				printf(CYAN ", 0x%lx", a4);
+				printf(CYAN "0x%lx", a4);
 		}
 		if (syscalls[call].num_args >= 5) {
+			printf(", ");
+			if (syscalls[call].arg5name)
+				printf("%s=", syscalls[call].arg5name);
 			if (olda5==a5)
-				printf(WHITE ", 0x%lx", a5);
+				printf(WHITE "0x%lx", a5);
 			else
-				printf(CYAN ", 0x%lx", a5);
+				printf(CYAN "0x%lx", a5);
 		}
 		if (syscalls[call].num_args == 6) {
+			printf(", ");
+			if (syscalls[call].arg6name)
+				printf("%s=", syscalls[call].arg6name);
 			if (olda6==a6)
-				printf(WHITE ", 0x%lx", a6);
+				printf(WHITE "0x%lx", a6);
 			else
-				printf(CYAN ", 0x%lx", a6);
+				printf(CYAN "0x%lx", a6);
 		}
-		printf(WHITE ")\n");
+		printf(WHITE ")");
 	}
 
-	printf("\t");
-
-	if (syscalls[call].num_args >= 1) {
-		if (syscalls[call].arg1name)
-			printf("%s=", syscalls[call].arg1name);
-		printf("0x%lx, ", a1);
-	}
-	if (syscalls[call].num_args > 1) {
-		if (syscalls[call].arg2name)
-			printf("%s=", syscalls[call].arg2name);
-		printf("0x%lx, ", a2);
-	}
-	if (syscalls[call].num_args > 2) {
-		if (syscalls[call].arg3name)
-			printf("%s=", syscalls[call].arg3name);
-		printf("0x%lx", a3);
-	}
-	if (syscalls[call].num_args > 3) {
-		if (syscalls[call].arg4name)
-			printf("%s=", syscalls[call].arg4name);
-		printf("0x%lx", a4);
-	}
-	if (syscalls[call].num_args > 4) {
-		if (syscalls[call].arg5name)
-			printf("%s=", syscalls[call].arg5name);
-		printf("0x%lx", a5);
-	}
-	if (syscalls[call].num_args > 5) {
-		if (syscalls[call].arg6name)
-			printf("%s=", syscalls[call].arg6name);
-		printf("0x%lx", a6);
-	}
-
+	printf(" ");
 
 
 
