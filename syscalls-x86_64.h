f1d13e22b8a7a79dae60c824e5a6bad21f2b791a
Dave Jones <davej@redhat.com>
2010-11-03 11:12:55 -0400

    Revert "revert this for now"
    This reverts commit b005219e2d61d18b3998e79ed0405a57692cb755.

--- syscalls-x86_64.h
+++ syscalls-x86_64.h
@@ -3496,6 +3496,7 @@ struct syscalltable syscalls_x86_64[NR_SYSCALLS+1] = {
 		.arg4type = ARG_FD,
 		.arg5name = "pathname",
 		.arg5type = ARG_ADDRESS,
+		.sanitise = sanitise_fanotify_mark,
 	},
 	/*-----------------------------------------------------------------------------------------------
 	  #302
