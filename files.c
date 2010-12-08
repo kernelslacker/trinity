0fbb1374e8dfa4538e1a1834a644c82bccfed1f3
Dave Jones <davej@redhat.com>
2010-12-07 22:50:14 -0500

    cache the successful socket opens.

--- files.c
+++ files.c
@@ -104,20 +104,29 @@ static void open_fds(char *dir)
 static int spin=0;
 static char spinner[]="-\\|/";
 
+static char *cachefilename="scrashme.socketcache";
+
 #define TYPE_MAX 128
 #define PROTO_MAX 256
-static void open_sockets()
+static void generate_sockets()
 {
 	int fd;
+	int cachefile;
 	int nr_to_create = 500;
-	int domain, type, protocol;
+	unsigned int domain, type, protocol;
+	unsigned int buffer[3];
 
 	if (fd_idx < 500)
 		nr_to_create = fd_idx;
 	if (nr_to_create < 100)
 		nr_to_create = 100;
 
-	printf("Creating sockets.\n");
+	cachefile = creat(cachefilename, S_IWUSR|S_IRUSR);
+	if (cachefile < 0) {
+		printf("Couldn't open cachefile for writing! (%s)\n",
+			strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 
 	for (domain = 0; domain < PF_MAX; domain++) {
 		for (type = 0; type < TYPE_MAX; type++) {
@@ -125,8 +134,8 @@ static void open_sockets()
 				printf("%c (%d sockets created. needed:%d) [domain:%d type:%d proto:%d]    \r",
 					spinner[spin++], socks, nr_to_create-socks,
 					domain, type, protocol);
-				if (spin==4)
-					spin=0;
+				if (spin == 4)
+					spin = 0;
 				fd = socket(domain, type, protocol);
 				if (fd > -1) {
 					socket_fds[socks] = fd;
@@ -134,15 +143,63 @@ static void open_sockets()
 						socks+fd_idx, domain, type, protocol);
 					socks++;
 
+					buffer[0] = domain;
+					buffer[1] = type;
+					buffer[2] = protocol;
+					write(cachefile, &buffer, sizeof(int) * 3);
+
 					if (socks == nr_to_create)
 						goto done;
-				}
+				} else
+					writelog_nosync("FAIL = domain:%i type:%i protocol:%i\n",
+						domain, type, protocol);
 			}
 		}
 	}
 done:
-
+	close(cachefile);
 	printf("\ncreated %d sockets\n", socks);
+	writelog("created %d sockets\n\n", socks);
+}
+
+static void open_sockets()
+{
+	int cachefile;
+	unsigned int domain, type, protocol;
+	unsigned int buffer[3];
+	int bytesread=-1;
+	int fd;
+
+	cachefile = open(cachefilename, O_RDONLY);
+	if (cachefile < 0) {
+		printf("Couldn't find socket cachefile. Regenerating.\n");
+		generate_sockets();
+		return;
+	}
+
+	while (bytesread != 0) {
+		bytesread = read(cachefile, buffer, sizeof(int) * 3);
+		if (bytesread == 0)
+			break;
+
+		domain = buffer[0];
+		type = buffer[1];
+		protocol = buffer[2];
+
+		fd = socket(domain, type, protocol);
+		if (fd < 0) {
+			printf("Cachefile is stale. Need to regenerate.\n");
+			unlink(cachefilename);
+			generate_sockets();
+		}
+		socket_fds[socks] = fd;
+		writelog_nosync("fd[%i] = domain:%i type:%i protocol:%i\n",
+			socks+fd_idx, domain, type, protocol);
+		socks++;
+	}
+	printf("(%d sockets created based on info from socket cachefile.)\n", socks);
+
+	close(cachefile);
 }
 
 static int pipes[2];
