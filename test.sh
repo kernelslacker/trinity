fefcf664f9d84c7ac4075508e58256cf9f8900ea
Dave Jones <davej@redhat.com>
2010-12-14 18:29:41 -0500

    test script improvements.
    - cpu binding
    - separate logfile per instance.

--- test.sh
+++ test.sh
@@ -5,18 +5,14 @@ if [ ! -d tmp ]; then
 fi
 cd tmp
 
-../scrashme --mode=rotate -z
-../scrashme --mode=rotate -k
-../scrashme --mode=rotate -u
-../scrashme --mode=rotate -z -i
-../scrashme --mode=rotate -k -i
-../scrashme --mode=rotate -u -i
+../scrashme --mode=rotate --logfile=../scrashme-cpu0.log -z -i
+../scrashme --mode=rotate --logfile=../scrashme-cpu0.log -k -i
+../scrashme --mode=rotate --logfile=../scrashme-cpu0.log -u -i
 
 NR_CPUS=`grep ^processor /proc/cpuinfo | /usr/bin/wc -l`
-NR_CPUS=$(($NR_CPUS+1))
+NR_CPUS=$(($NR_CPUS-1))
 
-for i in `seq 1 $NR_CPUS`
+for i in `seq 0 $NR_CPUS`
 do
-	../scrashme --mode=random &
-	../scrashme --mode=random -i &
+	taskset -c $i ../scrashme --mode=random --logfile=../scrashme-cpu$i.log -i &
 done
