c5540d8b8d7ca4e3cd6ff2ce377266c4dad63f99
Dave Jones <davej@redhat.com>
2010-12-14 15:21:33 -0500

    annotate fanotify flags

--- syscalls-x86_64.h
+++ syscalls-x86_64.h
@@ -8,6 +8,7 @@
 
 #include <fcntl.h>
 #include <signal.h>
+#include <linux/fanotify.h>
 
 #include <asm/mman.h>
 
--- syscalls/fanotify_mark.h
+++ syscalls/fanotify_mark.h
@@ -8,6 +8,12 @@
 	.arg1name = "fanotify_fd",
 	.arg1type = ARG_FD,
 	.arg2name = "flags",
+	.arg2type = ARG_LIST,
+	.arg2list = {
+		.num = 8,
+		.values = { FAN_MARK_ADD, FAN_MARK_REMOVE, FAN_MARK_DONT_FOLLOW, FAN_MARK_ONLYDIR,
+			    FAN_MARK_MOUNT, FAN_MARK_IGNORED_MASK, FAN_MARK_IGNORED_SURV_MODIFY, FAN_MARK_FLUSH },
+	},
 	.arg3name = "mask",
 	.arg4name = "dfd",
 	.arg4type = ARG_FD,
@@ -15,3 +21,5 @@
 	.arg5type = ARG_ADDRESS,
 	.sanitise = sanitise_fanotify_mark,
 },
+
+
