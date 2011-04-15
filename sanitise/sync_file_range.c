f0406296e494c4d18b49346565c913d5d3e16567
Dave Jones <davej@redhat.com>
2011-04-15 19:26:02 -0400

    rewrite sync_file_range sanity
    this still always fails, but so did the old code, and that was
    a lot slower. Need to figure out which path it keeps getting -EINVAL from.

--- sanitise/sync_file_range.c
+++ sanitise/sync_file_range.c
@@ -1,35 +1,34 @@
 #define _GNU_SOURCE
 #include <fcntl.h>
+#include <stdlib.h>
+
 #include "trinity.h"
 #include "sanitise.h"
+#include "arch.h"
 
 /*
  * asmlinkage long sys_sync_file_range(int fd, loff_t offset, loff_t nbytes, unsigned int flags)
- * flags must be part of VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| SYNC_FILE_RANGE_WAIT_AFTER)
  */
 
-#define VALID_SFR_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE|SYNC_FILE_RANGE_WAIT_AFTER)
-
-void sanitise_sync_file_range(__unused__ unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, __unused__ unsigned long *a5, __unused__ unsigned long *a6)
+void sanitise_sync_file_range(__unused__ unsigned long *fd,
+	unsigned long *offset,
+	__unused__ unsigned long *nbytes,
+	__unused__ unsigned long *a4,
+	__unused__ unsigned long *a5,
+	__unused__ unsigned long *a6)
 {
+	unsigned long endbyte;
 
-retry_flags:
-	if (*a4 & ~VALID_SFR_FLAGS) {
-		*a4 = rand64() & VALID_SFR_FLAGS;
-		printf("retrying flags\n");
-		goto retry_flags;
-	}
+retry:
+	*offset = rand() & 0xfffffff;
+	*nbytes = rand() & 0xfffffff;
 
-retry_offset:
-	if ((signed long)*a2 < 0) {
-		*a2 = rand64();
-		printf("retrying offset\n");
-		goto retry_offset;
-	}
+	endbyte = *offset + *nbytes;
 
-	if ((signed long)*a2+(signed long)*a3 < 0)
-		goto retry_offset;
+	if (endbyte < *offset)
+		goto retry;
 
-	if (*a2+*a3 < *a2)
-		goto retry_offset;
+	if (*offset >= (0x100000000ULL << PAGE_SHIFT))
+		goto retry;
+
 }
