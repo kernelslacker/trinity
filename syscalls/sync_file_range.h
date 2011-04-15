a0b0c17000c59719e5c07bbbec60189075e1b3fa
Dave Jones <davej@redhat.com>
2011-04-15 19:26:02 -0400

    rewrite sync_file_range sanity
    this still always fails, but so did the old code, and that was
    a lot slower. Need to figure out which path it keeps getting -EINVAL from.

--- syscalls/sync_file_range.h
+++ syscalls/sync_file_range.h
@@ -1,6 +1,9 @@
 /*
  * SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes, unsigned int flags)
  */
+
+#include <fcntl.h>
+
 {
 	.name = "sync_file_range",
 	.num_args = 4,
@@ -11,4 +14,9 @@
 	.arg3name = "nbytes",
 	.arg3type = ARG_LEN,
 	.arg4name = "flags",
+	.arg4type = ARG_LIST,
+        .arg4list = {
+		.num = 3,
+		.values = { SYNC_FILE_RANGE_WAIT_BEFORE, SYNC_FILE_RANGE_WRITE, SYNC_FILE_RANGE_WAIT_AFTER },
+        },
 },
