39812a29a24a40fdd1277c726e807f7970506d35
Dave Jones <davej@redhat.com>
2011-04-15 23:34:49 -0400

    sync_file_range2 flags annotation

--- syscalls/sync_file_range2.h
+++ syscalls/sync_file_range2.h
@@ -9,8 +9,12 @@
         .arg1name = "fd",
         .arg1type = ARG_FD,
         .arg2name = "flags",
+	.arg2type = ARG_LIST,
+        .arg2list = {
+		.num = 3,
+		.values = { SYNC_FILE_RANGE_WAIT_BEFORE, SYNC_FILE_RANGE_WRITE, SYNC_FILE_RANGE_WAIT_AFTER },
+        },
         .arg3name = "offset",
         .arg4name = "nbytes",
         .arg4type = ARG_LEN,
 },
-
