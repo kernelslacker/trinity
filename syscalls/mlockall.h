ae070b7af9634e71b7437879e0a119db71798608
Dave Jones <davej@redhat.com>
2011-04-15 22:44:07 -0400

    annotate flags for mlockall

--- syscalls/mlockall.h
+++ syscalls/mlockall.h
@@ -1,8 +1,17 @@
 /*
  * SYSCALL_DEFINE1(mlockall, int, flags)
  */
+
+#define MCL_CURRENT     1
+#define MCL_FUTURE      2
+
 {
 	.name = "mlockall",
 	.num_args = 1,
 	.arg1name = "flags",
+	.arg1type = ARG_LIST,
+	.arg1list = {
+		.num = 2,
+		.values = { MCL_CURRENT, MCL_FUTURE },
+	},
 },
