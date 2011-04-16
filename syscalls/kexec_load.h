bf1a3fcdf5a9db55c0a29cfb6d378e8e9a8ac2e3
Dave Jones <davej@redhat.com>
2011-04-15 22:30:15 -0400

    annotate kexec_load flags
    (completeness, this is a root-only syscall)

--- syscalls/kexec_load.h
+++ syscalls/kexec_load.h
@@ -2,6 +2,10 @@
  * SYSCALL_DEFINE4(kexec_load, unsigned long, entry, unsigned long, nr_segments,
 	struct kexec_segment __user *, segments, unsigned long, flags)
  */
+
+#define KEXEC_ON_CRASH          0x00000001
+#define KEXEC_PRESERVE_CONTEXT  0x00000002
+
 {
 	.name = "kexec_load",
 	.num_args = 4,
@@ -13,4 +17,9 @@
 	.arg3name = "segments",
 	.arg3type = ARG_ADDRESS,
 	.arg4name = "flags",
+	.arg4type = ARG_LIST,
+	.arg4list = {
+		.num = 2,
+		.values = { KEXEC_ON_CRASH, KEXEC_PRESERVE_CONTEXT },
+	},
 },
