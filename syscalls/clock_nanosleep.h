a2286e842dbbd6d13dc76cf673880a95fe90421a
Dave Jones <davej@redhat.com>
2011-04-15 21:25:17 -0400

    annotate clock_nanosleep flags

--- syscalls/clock_nanosleep.h
+++ syscalls/clock_nanosleep.h
@@ -3,11 +3,24 @@
 	const struct timespec __user *, rqtp,
 	struct timespec __user *, rmtp)
  */
+
+#include <time.h>
+
 {
 	.name = "clock_nanosleep",
 	.num_args = 4,
 	.arg1name = "which_clock",
+	.arg1type = ARG_LIST,
+	.arg1list = {
+		.num = 3,
+		.values = { CLOCK_REALTIME, CLOCK_MONOTONIC, CLOCK_PROCESS_CPUTIME_ID },
+	},
 	.arg2name = "flags",
+	.arg2type = ARG_LIST,
+	.arg2list = {
+		.num = 1,
+		.values = { TIMER_ABSTIME },
+	},
 	.arg3name = "rqtp",
 	.arg3type = ARG_ADDRESS,
 	.arg4name = "rmtp",
