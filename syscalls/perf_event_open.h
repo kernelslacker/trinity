038d778ba625c0455f63eeba451e9380903a1731
Dave Jones <davej@redhat.com>
2011-04-15 23:05:00 -0400

    flags annotation for perf_event_open

--- syscalls/perf_event_open.h
+++ syscalls/perf_event_open.h
@@ -3,6 +3,10 @@
 	 struct perf_event_attr __user *, attr_uptr,
 	 pid_t, pid, int, cpu, int, group_fd, unsigned long, flags)
  */
+#define PERF_FLAG_FD_NO_GROUP   (1U << 0)
+#define PERF_FLAG_FD_OUTPUT     (1U << 1)
+#define PERF_FLAG_PID_CGROUP    (1U << 2) /* pid=cgroup id, per-cpu mode only */
+
 {
 	.name = "perf_event_open",
 	.num_args = 5,
@@ -14,4 +18,9 @@
 	.arg4name = "group_fd",
 	.arg4type = ARG_FD,
 	.arg5name = "flags",
+	.arg5type = ARG_LIST,
+	.arg5list = {
+		.num = 3,
+		.values = { PERF_FLAG_FD_NO_GROUP, PERF_FLAG_FD_OUTPUT, PERF_FLAG_PID_CGROUP },
+	},
 },
