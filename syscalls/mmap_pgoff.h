7f1ba967f9802a3341389f33dd96ed1f05fcf708
Dave Jones <davej@redhat.com>
2011-04-15 22:46:49 -0400

    annotate mmap_pgoff

--- syscalls/mmap_pgoff.h
+++ syscalls/mmap_pgoff.h
@@ -3,6 +3,22 @@
                  unsigned long, prot, unsigned long, flags,
                  unsigned long, fd, unsigned long, pgoff)
  */
+#ifndef MAP_UNINITIALIZED
+#define MAP_UNINITIALIZED 0x4000000
+#endif
+
+#ifndef PROT_SEM
+#define PROT_SEM 0x8
+#endif
+
+#ifndef MAP_HUGETLB
+#define MAP_HUGETLB 0x40000
+#endif
+
+#ifndef MAP_STACK
+#define MAP_STACK 0x20000
+#endif
+
 {
 	.name = "mmap_pgoff",
 	.num_args = 6,
@@ -11,8 +27,22 @@
 	.arg2name = "len",
 	.arg2type = ARG_LEN,
 	.arg3name = "prot",
+	.arg3type = ARG_LIST,
+	.arg3list = {
+		.num = 4,
+		.values = { PROT_READ, PROT_WRITE, PROT_EXEC, PROT_SEM },
+	},
 	.arg4name = "flags",
+	.arg4type = ARG_LIST,
+	.arg4list = {
+		.num = 14,
+		.values = { MAP_SHARED, MAP_PRIVATE, MAP_FIXED, MAP_ANONYMOUS,
+			    MAP_GROWSDOWN, MAP_DENYWRITE, MAP_EXECUTABLE, MAP_LOCKED,
+			    MAP_NORESERVE, MAP_POPULATE, MAP_NONBLOCK, MAP_STACK,
+			    MAP_HUGETLB, MAP_UNINITIALIZED },
+	},
 	.arg5name = "fd",
 	.arg5type = ARG_FD,
 	.arg6name = "pgoff",
+	.arg6type = ARG_LEN,
 },
