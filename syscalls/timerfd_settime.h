996ed757458dc829f71863566505feff470a6d9c
Dave Jones <davej@redhat.com>
2011-04-15 23:41:47 -0400

    flags annotation for timerfd_settime

--- syscalls/timerfd_settime.h
+++ syscalls/timerfd_settime.h
@@ -3,12 +3,20 @@
 	 const struct itimerspec __user *, utmr,
 	 struct itimerspec __user *, otmr)
  */
+#define TFD_CLOEXEC 02000000
+#define TFD_NONBLOCK 04000
+
 {
 	.name = "timerfd_settime",
 	.num_args = 4,
 	.arg1name = "ufd",
 	.arg1type = ARG_FD,
 	.arg2name = "flags",
+	.arg2type = ARG_LIST,
+	.arg2list = {
+		.num = 2,
+		.values = { TFD_NONBLOCK, TFD_CLOEXEC },
+	},
 	.arg3name = "utmr",
 	.arg3type = ARG_ADDRESS,
 	.arg4name = "otmr",
