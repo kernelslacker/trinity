9f985c602c18828323481c14632e2553f059d9ec
Dave Jones <davej@redhat.com>
2011-04-15 23:00:24 -0400

    openat flags annotation

--- syscalls/openat.h
+++ syscalls/openat.h
@@ -1,6 +1,9 @@
 /*
  * SYSCALL_DEFINE4(openat, int, dfd, const char __user *, filename, int, flags, int, mode)
  */
+
+#include <fcntl.h>
+
 {
 	.name = "openat",
 	.num_args = 4,
@@ -9,5 +12,16 @@
 	.arg2name = "filename",
 	.arg2type = ARG_ADDRESS,
 	.arg3name = "flags",
+	.arg3type = ARG_LIST,
+	.arg3list = {
+		.num = 19,
+		.values = { O_RDONLY, O_WRONLY, O_RDWR,
+				O_CREAT, O_EXCL, O_NOCTTY,
+				O_TRUNC, O_APPEND, O_NONBLOCK,
+				O_SYNC, O_ASYNC,
+				O_DIRECTORY, O_NOFOLLOW, O_CLOEXEC,
+				O_DIRECT, O_NOATIME, O_PATH,
+				O_DSYNC, O_LARGEFILE },
+	},
 	.arg4name = "mode",
 },
