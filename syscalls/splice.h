8e9273b6638bf5be04fb7f33c2f65595b6a386f1
Dave Jones <davej@redhat.com>
2011-04-15 23:31:49 -0400

    flags annotation for splice

--- syscalls/splice.h
+++ syscalls/splice.h
@@ -3,6 +3,13 @@
 	int, fd_out, loff_t __user *, off_out,
 	size_t, len, unsigned int, flags)
  */
+# define SPLICE_F_MOVE          1       /* Move pages instead of copying.  */
+# define SPLICE_F_NONBLOCK      2       /* Don't block on the pipe splicing
+                                           (but we may still block on the fd
+                                           we splice from/to).  */
+# define SPLICE_F_MORE          4       /* Expect more data.  */
+# define SPLICE_F_GIFT          8       /* Pages passed in are a gift.  */
+
 {
 	.name = "splice",
 	.num_args = 6,
@@ -17,4 +24,9 @@
 	.arg5name = "len",
 	.arg5type = ARG_LEN,
 	.arg6name = "flags",
+	.arg4type = ARG_LIST,
+	.arg4list = {
+		.num = 4,
+		.values = { SPLICE_F_MOVE, SPLICE_F_NONBLOCK, SPLICE_F_MORE, SPLICE_F_GIFT },
+	},
 },
