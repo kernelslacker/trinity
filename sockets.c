073005857c050fec6cab50e61525d2d6dfd77a1a
Dave Jones <davej@redhat.com>
2011-04-20 18:23:31 -0400

    remove some unneeded includes

--- sockets.c
+++ sockets.c
@@ -3,10 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <dirent.h>
 #include <errno.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/socket.h>
 
 #include "trinity.h"
