580fa2c655a42386264e2f0867a510d447999ee8
Dave Jones <davej@redhat.com>
2011-05-13 14:33:34 -0400

    move set_robust_list sanity into syscall def

--- sanitise.h
+++ sanitise.h
@@ -11,7 +11,6 @@ void sanitise_writev(unsigned long *a1, unsigned long *a2, unsigned long *a3, un
 void sanitise_tee(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
 void sanitise_sync_file_range(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
 void sanitise_vmsplice(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
-void sanitise_set_robust_list(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
 void sanitise_sendto(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
 
 void sanitise_ioctl_sg_io(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
--- syscalls/set_robust_list.c
+++ syscalls/set_robust_list.c
@@ -1,8 +1,20 @@
 /*
  * SYSCALL_DEFINE2(set_robust_list, struct robust_list_head __user *, head, size_t, len)
  */
+#include <linux/futex.h>
 #include "trinity.h"
 #include "sanitise.h"
+
+static void sanitise_set_robust_list(
+	__unused__ unsigned long *a1,
+	unsigned long *len,
+	__unused__ unsigned long *a3,
+	__unused__ unsigned long *a4,
+	__unused__ unsigned long *a5,
+	__unused__ unsigned long *a6)
+{
+	*len = sizeof(struct robust_list_head);
+}
 
 struct syscall syscall_set_robust_list = {
 	.name = "set_robust_list",
