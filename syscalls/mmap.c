2664d441b973fcd2c3b1286e151e1bee4207be34
Dave Jones <davej@redhat.com>
2011-05-13 14:19:18 -0400

    move mmap sanitise into syscall def

--- syscalls/mmap.c
+++ syscalls/mmap.c
@@ -23,7 +23,18 @@
 
 #include "trinity.h"
 #include "sanitise.h"
+#include "arch.h"
 
+void sanitise_mmap(
+	__unused__ unsigned long *a1,
+	__unused__ unsigned long *a2,
+	__unused__ unsigned long *a3,
+	__unused__ unsigned long *a4,
+	__unused__ unsigned long *a5,
+	unsigned long *offset)
+{
+	*offset &= PAGE_MASK;
+}
 struct syscall syscall_mmap = {
 	.name = "mmap",
 	.num_args = 6,
