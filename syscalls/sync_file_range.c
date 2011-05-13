d243f6332faa1f336f9b4e96a9b5220263c5e23d
Dave Jones <davej@redhat.com>
2011-05-13 14:34:58 -0400

    move sync_file_range sanity into syscall def

--- syscalls/sync_file_range.c
+++ syscalls/sync_file_range.c
@@ -1,10 +1,37 @@
 /*
  * SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes, unsigned int flags)
  */
+#define _GNU_SOURCE
 #include <linux/fs.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 #include "trinity.h"
 #include "sanitise.h"
+#include "arch.h"
+
+static void sanitise_sync_file_range(__unused__ unsigned long *fd,
+	unsigned long *offset,
+	__unused__ unsigned long *nbytes,
+	__unused__ unsigned long *a4,
+	__unused__ unsigned long *a5,
+	__unused__ unsigned long *a6)
+{
+	unsigned long endbyte;
+
+retry:
+	*offset = rand() & 0xfffffff;
+	*nbytes = rand() & 0xfffffff;
+
+	endbyte = *offset + *nbytes;
+
+	if (endbyte < *offset)
+		goto retry;
+
+	if (*offset >= (0x100000000ULL << PAGE_SHIFT))
+		goto retry;
+
+}
 
 struct syscall syscall_sync_file_range = {
 	.name = "sync_file_range",
