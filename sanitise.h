e5d63048ad38b4f2bcc31f4272cabba88dbb0ab5
Dave Jones <davej@redhat.com>
2011-05-13 14:20:34 -0400

    move mprotect sanitise into syscall def

--- sanitise.h
+++ sanitise.h
@@ -6,7 +6,6 @@ void sanitise_write(unsigned long *a1, unsigned long *a2, unsigned long *a3, uns
 void sanitise_close(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
 void sanitise_newfstat(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
 void sanitise_mmap(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
-void sanitise_mprotect(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
 void sanitise_rt_sigaction(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
 void sanitise_rt_sigprocmask(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
 void sanitise_pread64(unsigned long *a1, unsigned long *a2, unsigned long *a3, unsigned long *a4, unsigned long *a5, unsigned long *a6);
--- syscalls/mprotect.c
+++ syscalls/mprotect.c
@@ -1,10 +1,37 @@
 /*
- * SYSCALL_DEFINE3(mprotect, unsigned long, start, size_t, len, unsigned long, prot)
+ * sys_mprotect(unsigned long start, size_t len, unsigned long prot)
  */
 #include <asm/mman.h>
 
 #include "trinity.h"
 #include "sanitise.h"
+
+static void sanitise_mprotect(
+		unsigned long *start,
+		unsigned long *len,
+		__unused__ unsigned long *prot,
+		__unused__ unsigned long *a4,
+		__unused__ unsigned long *a5,
+		__unused__ unsigned long *a6)
+{
+	unsigned long end;
+	unsigned long mask = ~(page_size-1);
+
+	*start &= mask;
+
+retry_end:
+	end = *start + *len;
+	if (*len == 0) {
+		*len = rand64();
+		goto retry_end;
+	}
+
+	/* End must be after start */
+	if (end <= *start) {
+		*len = rand64();
+		goto retry_end;
+	}
+}
 
 struct syscall syscall_mprotect = {
 	.name = "mprotect",
