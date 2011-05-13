4bf43b77702ba304778254a39753a041bdff653b
Dave Jones <davej@redhat.com>
2011-05-13 14:29:19 -0400

    move rt_sigaction sanity into syscall def

--- syscalls/rt_sigaction.c
+++ syscalls/rt_sigaction.c
@@ -5,9 +5,27 @@
 	size_t, sigsetsize)
  */
 #include <signal.h>
+#include <stdlib.h>
 
 #include "trinity.h"
 #include "sanitise.h"
+
+void sanitise_rt_sigaction(
+		__unused__ unsigned long *a1,
+		__unused__ unsigned long *a2,
+		__unused__ unsigned long *a3,
+		unsigned long *a4,
+		__unused__ unsigned long *a5,
+		__unused__ unsigned long *a6)
+{
+	if ((rand() % 2) == 0)
+		*a2 = 0;
+
+	if ((rand() % 2) == 0)
+		*a3 = 0;
+
+	*a4 = sizeof(sigset_t);
+}
 
 struct syscall syscall_rt_sigaction = {
 	.name = "rt_sigaction",
