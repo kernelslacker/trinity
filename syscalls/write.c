c700bfdf979f077c629989d67f63e1f51a8aefef
Dave Jones <davej@redhat.com>
2011-05-13 14:43:13 -0400

    move writre sanity into syscall def

--- syscalls/write.c
+++ syscalls/write.c
@@ -1,9 +1,38 @@
 /*
  * SYSCALL_DEFINE3(write, unsigned int, fd, const char __user *, buf, size_t, count)
  */
-
+#include <stdlib.h>
 #include "trinity.h"
 #include "sanitise.h"
+
+/*
+ * asmlinkage ssize_t sys_write(unsigned int fd, char __user * buf, size_t count)
+ */
+static void sanitise_write(
+		__unused__ unsigned long *a1,
+		unsigned long *a2,
+		unsigned long *a3,
+		__unused__ unsigned long *a4,
+		__unused__ unsigned long *a5,
+		__unused__ unsigned long *a6)
+{
+	unsigned long newsize = *a3 & 0xffff;
+	void *newbuffer;
+
+retry:
+	newbuffer = malloc(newsize);
+	if (newbuffer == NULL) {
+		newsize >>= 1;
+		goto retry;
+	}
+
+	free(filebuffer);
+	filebuffer = newbuffer;
+	filebuffersize = newsize;
+
+	*a2 = (unsigned long) filebuffer;
+	*a3 = newsize;
+}
 
 struct syscall syscall_write = {
 	.name = "write",
