5c3a8885164d5ea4bd7a978b19a5fc657a9f2304
Dave Jones <davej@redhat.com>
2011-05-13 14:30:11 -0400

    move rt_sigprocmask sanity into syscall def

--- syscalls/rt_sigprocmask.c
+++ syscalls/rt_sigprocmask.c
@@ -2,8 +2,20 @@
  * SYSCALL_DEFINE4(rt_sigprocmask, int, how, sigset_t __user *, set,
 	sigset_t __user *, oset, size_t, sigsetsize)
  */
+#include <signal.h>
 #include "trinity.h"
 #include "sanitise.h"
+
+static void sanitise_rt_sigprocmask(
+		__unused__ unsigned long *a1,
+		__unused__ unsigned long *a2,
+		__unused__ unsigned long *a3,
+		unsigned long *a4,
+		__unused__ unsigned long *a5,
+		__unused__ unsigned long *a6)
+{
+	*a4 = sizeof(sigset_t);
+}
 
 struct syscall syscall_rt_sigprocmask = {
 	.name = "rt_sigprocmask",
