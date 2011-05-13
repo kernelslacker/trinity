48dae14b1f9da79f87b2b0ed51cc36984aa441a1
Dave Jones <davej@redhat.com>
2011-05-13 14:22:39 -0400

    move pread64 sanity into syscall def

--- syscalls/pread64.c
+++ syscalls/pread64.c
@@ -4,6 +4,22 @@
 #include "trinity.h"
 #include "sanitise.h"
 
+static void sanitise_pread64(
+		__unused__ unsigned long *a1,
+		__unused__ unsigned long *a2,
+		__unused__ unsigned long *a3,
+		unsigned long *a4,
+		__unused__ unsigned long *a5,
+		__unused__ unsigned long *a6)
+{
+
+retry_pos:
+	if ((int)*a4 < 0) {
+		*a4 = rand64();
+		goto retry_pos;
+	}
+}
+
 struct syscall syscall_pread64 = {
 	.name = "pread64",
 	.num_args = 4,
