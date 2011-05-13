bea6400fada62ae3ee5dd829ace896f9016370ac
Dave Jones <davej@redhat.com>
2011-05-13 14:32:32 -0400

    move sendto sanity into syscall def

--- syscalls/sendto.c
+++ syscalls/sendto.c
@@ -30,6 +30,16 @@
 #include "trinity.h"
 #include "sanitise.h"
 
+void sanitise_sendto(__unused__ unsigned long *fd,
+	__unused__ unsigned long *buff,
+	__unused__ unsigned long *len,
+	__unused__ unsigned long *flags,
+	__unused__ unsigned long *addr,
+	unsigned long *addr_len)
+{
+	*addr_len %= 128;	// MAX_SOCK_ADDR
+}
+
 struct syscall syscall_sendto = {
 	.name = "sendto",
 	.num_args = 6,
