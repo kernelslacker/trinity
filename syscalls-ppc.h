930c32cb8ccc17c6fcc293d38bfa0fb6e9b639f4
Dave Jones <davej@redhat.com>
2011-07-11 18:53:17 -0400

    fix obvious build failure on ppc

--- syscalls-ppc.h
+++ syscalls-ppc.h
@@ -1,7 +1,7 @@
 /* Syscalls from arch/powerpc/include/asm/systbl.h as of 2.6.31rc2 */
 
-#include "trinity },
-#include "sanitise },
+#include "trinity.h"
+#include "sanitise.h"
 
 #include <fcntl.h>
 #include <signal.h>
