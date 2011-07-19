f5ea61f84e17aec9082b3325b8ad29c6077502b4
Dave Jones <davej@redhat.com>
2011-07-19 19:36:03 -0400

    socketcall annotation

--- syscalls/socketcall.c
+++ syscalls/socketcall.c
@@ -8,6 +8,9 @@ struct syscall syscall_socketcall = {
 	.name = "socketcall",
 	.num_args = 2,
 	.arg1name = "call",
+	.arg1type = ARG_RANGE,
+	.low1range= 1,
+	.hi1range = 20,	/* SYS_SENDMMSG */
 	.arg2name = "args",
 	.arg2type = ARG_ADDRESS,
 };
