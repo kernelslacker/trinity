af3ff7418945b1055fa3012e394da096d3344ed6
Dave Jones <davej@redhat.com>
2011-07-28 15:18:30 -0400

    update syscall list for x86-64 to match current Linus' tree.

---
+++ syscalls/setns.c
@@ -0,0 +1,14 @@
+/*
+ * SYSCALL_DEFINE2(setns, int, fd, int, nstype)
+ */
+
+#include "trinity.h"
+#include "sanitise.h"
+
+struct syscall syscall_setns= {
+	.name = "setns",
+	.num_args = 2,
+	.arg1name = "fd",
+	.arg1type = ARG_FD,
+	.arg2name = "nstype",
+};
