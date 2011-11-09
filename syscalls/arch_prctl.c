c878ab59819f977f7e0cc15eb23b04b469f026bf
Dave Jones <davej@redhat.com>
2011-11-09 16:01:33 -0500

    sanitise the arguments of arch_prctl

--- syscalls/arch_prctl.c
+++ syscalls/arch_prctl.c
@@ -1,12 +1,24 @@
-/*
-   long sys_arch_prctl(int code, unsigned long addr)
+/* (x86-64 only)
+ *  long sys_arch_prctl(int code, unsigned long addr)
+ *
+ * On success, arch_prctl() returns 0
+ * On error, -1 is returned, and errno is set to indicate the error.
  */
+
 #include "trinity.h"
 #include "sanitise.h"
+#include <asm/prctl.h>
+#include <sys/prctl.h>
 
 struct syscall syscall_arch_prctl = {
 	.name = "arch_prctl",
 	.num_args = 2,
 	.arg1name = "code",
+	.arg1type = ARG_LIST,
+	.arg1list = {
+		.num = 4,
+		.values = { ARCH_SET_FS, ARCH_GET_FS, ARCH_SET_GS, ARCH_GET_GS },
+	},
 	.arg2name = "addr",
+	.arg2type = ARG_ADDRESS,
 };
