e110db88939d2d549e6d4673c9cab71facd9ef23
Dave Jones <davej@redhat.com>
2012-02-21 23:52:18 -0500

    sanitise function for socket()

--- syscalls/socket.c
+++ syscalls/socket.c
@@ -1,8 +1,39 @@
 /*
  * SYSCALL_DEFINE3(socket, int, family, int, type, int, protocol)
  */
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
 #include "trinity.h"
 #include "sanitise.h"
+
+/* note: will eventually also be called from sanitise_socketcall, and generate_sockets() */
+void sanitise_socket(
+        unsigned long *family,
+        unsigned long *type,
+        unsigned long *protocol,
+        __unused__ unsigned long *a4,
+        __unused__ unsigned long *a5,
+        __unused__ unsigned long *a6)
+{
+	*family = rand() % PF_MAX;
+	*type = rand() % TYPE_MAX;
+	*protocol = rand() % PROTO_MAX;
+
+	switch (*family) {
+
+	case AF_X25:
+		*type = SOCK_SEQPACKET;
+		break;
+
+	case AF_INET6:
+		if (*type == SOCK_STREAM)
+			*protocol = 0;
+		break;
+	}
+}
 
 struct syscall syscall_socket = {
 	.name = "socket",
@@ -10,4 +41,5 @@ struct syscall syscall_socket = {
 	.arg1name = "family",
 	.arg2name = "type",
 	.arg3name = "protocol",
+	.sanitise = sanitise_socket,
 };
