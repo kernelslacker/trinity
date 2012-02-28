c2f58f284d39b23c49057c2b3774fc0c5c1ef5d8
Dave Jones <davej@redhat.com>
2012-02-28 18:30:11 -0500

    sanitiser for move_pages

--- syscalls/move_pages.c
+++ syscalls/move_pages.c
@@ -8,20 +8,53 @@
 #define MPOL_MF_MOVE    (1<<1)  /* Move pages owned by this process to conform to mapping */
 #define MPOL_MF_MOVE_ALL (1<<2) /* Move every page to conform to mapping */
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 #include "trinity.h"
 #include "sanitise.h"
+#include "arch.h"
+
+static void sanitise_move_pages(unsigned long *pid,
+	unsigned long *nr_pages, unsigned long *pages,
+	unsigned long *nodes,unsigned long *status, unsigned long *flags)
+{
+	unsigned long *page_alloc;
+	unsigned int i;
+
+	// Needs CAP_SYS_NICE to move pages in another process
+	if (getuid() != 0) {
+		*pid = 0;
+		*flags &= ~MPOL_MF_MOVE_ALL;
+	}
+
+	page_alloc = malloc(page_size);
+
+	*nr_pages = rand() % (page_size / sizeof(void *));
+
+	for (i = 0; i < *nr_pages; i++) {
+		page_alloc[i] = (unsigned long) malloc(page_size);
+		page_alloc[i] &= PAGE_MASK;
+	}
+
+	*pages = (unsigned long) page_alloc;
+
+	*nodes = (unsigned long) malloc(page_size);
+	for (i = 0; i < page_size; i++) {
+		*nodes = (int) rand() % 2;
+	}
+
+	*status = (unsigned long) malloc(page_size);
+}
 
 struct syscall syscall_move_pages = {
 	.name = "move_pages",
 	.num_args = 6,
 	.arg1name = "pid",
-	.arg1type = ARG_PID,
 	.arg2name = "nr_pages",
-	.arg2type = ARG_LEN,
 	.arg3name = "pages",
-	.arg3type = ARG_ADDRESS,
 	.arg4name = "nodes",
-	.arg4type = ARG_ADDRESS,
 	.arg5name = "status",
 	.arg5type = ARG_ADDRESS,
 	.arg6name = "flags",
@@ -31,4 +64,5 @@ struct syscall syscall_move_pages = {
 		.values = { MPOL_MF_MOVE, MPOL_MF_MOVE_ALL },
 	},
 	.group = GROUP_VM,
+	.sanitise = sanitise_move_pages,
 };
