5c74ff461ab5822c5833d52324deda85deee06a1
Dave Jones <davej@redhat.com>
2012-02-27 22:24:20 -0500

    sanitise mlock[all]
    Passing 0 to this just EINVAL's.

--- syscalls/mlockall.c
+++ syscalls/mlockall.c
@@ -5,8 +5,26 @@
 #define MCL_CURRENT     1
 #define MCL_FUTURE      2
 
+#include <stdlib.h>
 #include "trinity.h"
 #include "sanitise.h"
+
+static void sanitise_mlockall(unsigned long *flags,
+		__unused__ unsigned long *a2,
+		__unused__ unsigned long *a3,
+		__unused__ unsigned long *a4,
+		__unused__ unsigned long *a5,
+		__unused__ unsigned long *a6)
+{
+	if (*flags != 0)
+		return;
+
+	if ((rand() % 2) == 0)
+		*flags = MCL_CURRENT;
+	else
+		*flags = MCL_FUTURE;
+}
+
 
 struct syscall syscall_mlockall = {
 	.name = "mlockall",
@@ -18,4 +36,5 @@ struct syscall syscall_mlockall = {
 		.values = { MCL_CURRENT, MCL_FUTURE },
 	},
 	.group = GROUP_VM,
+	.sanitise = sanitise_mlockall,
 };
