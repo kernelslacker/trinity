6a0464270d4e95cc122e97e6244e9f72d57f0fac
Dave Jones <davej@redhat.com>
2012-02-27 22:44:24 -0500

    vm test script

---
+++ test-vm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ ! -d logs ]; then
+  mkdir logs
+fi
+
+if [ ! -d tmp ]; then
+  mkdir tmp
+fi
+chmod 755 tmp
+cd tmp
+
+NR_CPUS=`grep ^processor /proc/cpuinfo | /usr/bin/wc -l`
+NR_CPUS=$(($NR_CPUS-1))
+
+while [ 1 ];
+do
+  RND=$RANDOM
+  mkdir tmp.$RND
+  cd tmp.$RND
+  for i in `seq 0 $NR_CPUS`
+  do
+	taskset -c $i ../../trinity --mode=random --logfile=../../logs/trinity-rand-cpu$i.log    -i -F10 -g vm &
+  done
+  wait
+  cd ..
+  rm -rf tmp.$RND
+done
