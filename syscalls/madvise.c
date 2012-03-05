c4182f76b370c88a5efe5718336b0978696e4d87
Dave Jones <davej@redhat.com>
2012-03-05 16:26:03 -0500

    sanitise madvise flags

--- syscalls/madvise.c
+++ syscalls/madvise.c
@@ -4,6 +4,8 @@
 #include "trinity.h"
 #include "sanitise.h"
 
+#include <sys/mman.h>
+
 struct syscall syscall_madvise = {
 	.name = "madvise",
 	.num_args = 3,
@@ -12,5 +14,12 @@ struct syscall syscall_madvise = {
 	.arg2name = "len_in",
 	.arg2type = ARG_LEN,
 	.arg3name = "behaviour",
+	.arg3type = ARG_LIST,
+	.arg3list = {
+		.num = 12,
+		.values = { MADV_NORMAL, MADV_RANDOM, MADV_SEQUENTIAL, MADV_WILLNEED,
+			    MADV_DONTNEED, MADV_REMOVE, MADV_DONTFORK, MADV_DOFORK,
+			    MADV_MERGEABLE, MADV_UNMERGEABLE, MADV_HUGEPAGE, MADV_NOHUGEPAGE },
+	},
 	.group = GROUP_VM,
 };
