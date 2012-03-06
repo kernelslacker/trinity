a05cefd6fa6a0c7f49c2703b4fdb6d1cf9b4b12e
Dave Jones <davej@redhat.com>
2012-03-05 23:08:55 -0500

    remove -N1000 from test

--- test-random.sh
+++ test-random.sh
@@ -20,7 +20,7 @@ do
   cd tmp.$RND
   for i in `seq 0 $NR_CPUS`
   do
-	taskset -c $i ../../trinity --logfile=../../logs/trinity-rand-cpu$i.log -N 1000 &
+	taskset -c $i ../../trinity --logfile=../../logs/trinity-rand-cpu$i.log &
   done
   wait
   cd ..
