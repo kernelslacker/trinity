ac491b8fd1426010a58cf62cb010b54b7b9afbbe
Dave Jones <davej@redhat.com>
2012-03-13 16:16:28 -0400

    annotate unshare's flags

--- syscalls/unshare.c
+++ syscalls/unshare.c
@@ -1,6 +1,8 @@
 /*
  * SYSCALL_DEFINE1(unshare, unsigned long, unshare_flags)
  */
+#include <linux/sched.h>
+
 #include "trinity.h"
 #include "sanitise.h"
 
@@ -8,4 +10,11 @@ struct syscall syscall_unshare = {
 	.name = "unshare",
 	.num_args = 1,
 	.arg1name = "unshare_flags",
+	.arg1type = ARG_LIST,
+	.arg1list = {
+		.num = 7,
+		.values = { CLONE_FILES, CLONE_FS, CLONE_NEWIPC, CLONE_NEWNET,
+				CLONE_NEWNS, CLONE_SYSVSEM, CLONE_NEWUTS,
+		 },
+	},
 };
