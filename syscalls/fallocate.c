c7fff7ed995127816ed09bdba008605e7f1ad7db
Dave Jones <davej@redhat.com>
2012-03-28 18:26:17 -0400

    annotate mode argument of fallocate

--- syscalls/fallocate.c
+++ syscalls/fallocate.c
@@ -6,12 +6,20 @@
 #include "trinity.h"
 #include "sanitise.h"
 
+#define FALLOC_FL_KEEP_SIZE	0x01
+#define FALLOC_FL_PUNCH_HOLE	0x02
+
 struct syscall syscall_fallocate = {
 	.name = "fallocate",
 	.num_args = 4,
 	.arg1name = "fd",
 	.arg1type = ARG_FD,
 	.arg2name = "mode",
+	.arg2type = ARG_LIST,
+	.arg2list = {
+		.num = 2,
+		.values = { FALLOC_FL_KEEP_SIZE, FALLOC_FL_PUNCH_HOLE },
+	},
 	.arg3name = "offset",
 	.arg4name = "len",
 	.arg4type = ARG_LEN,
