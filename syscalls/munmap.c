1525658c2df52c03d4e58934c6ddd4af488155d8
Dave Jones <davej@redhat.com>
2012-05-23 11:27:49 -0400

    don't munmap 0

--- syscalls/munmap.c
+++ syscalls/munmap.c
@@ -4,6 +4,20 @@
 #include "trinity.h"
 #include "sanitise.h"
 
+void sanitise_munmap(unsigned long *addr,
+	__unused__ unsigned long *len,
+	__unused__ unsigned long *a3,
+	__unused__ unsigned long *a4,
+	__unused__ unsigned long *a5,
+	__unused__ unsigned long *a6)
+{
+retry:
+	if (*addr == 0) {
+		*addr = (unsigned long) get_address();
+		goto retry;
+	}
+}
+
 struct syscall syscall_munmap = {
 	.name = "munmap",
 	.num_args = 2,
@@ -12,4 +26,5 @@ struct syscall syscall_munmap = {
 	.arg2name = "len",
 	.arg2type = ARG_LEN,
 	.group = GROUP_VM,
+	.sanitise = sanitise_munmap,
 };
