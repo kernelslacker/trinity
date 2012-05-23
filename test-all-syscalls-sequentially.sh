388f374b1ab904e68f150760e04ed337daef5bfd
Dave Jones <davej@redhat.com>
2012-05-23 19:31:02 -0400

    whitespace

--- test-all-syscalls-sequentially.sh
+++ test-all-syscalls-sequentially.sh
@@ -11,5 +11,7 @@ do
   for syscall in $(../trinity -L | grep -v Trinity | grep -v 32bit | grep -v 64bit | awk '{ print $2 }' | sort -u)
   do
   	MALLOC_CHECK_=2 ../trinity -q -c $syscall -x mbind -N 10000 -D
+	echo
+	echo
   done
 done
