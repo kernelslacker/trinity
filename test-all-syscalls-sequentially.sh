bf5787eded8816853cc974125676579f660cb4fd
Dave Jones <davej@redhat.com>
2012-05-23 11:33:54 -0400

    sequential test harness.

---
+++ test-all-syscalls-sequentially.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -d tmp ]; then
+  mkdir tmp
+fi
+chmod 755 tmp
+cd tmp
+
+while [ 1 ];
+do
+  for syscall in $(../trinity -L | grep -v Trinity | grep -v 32bit | grep -v 64bit | awk '{ print $2 }' | sort -u)
+  do
+  	MALLOC_CHECK_=2 ../trinity -q -c $syscall -x mbind -N 10000 -D
+  done
+done
