70350cdcd5cf47bd28a9174d6ad5ff186ad88219
Dave Jones <davej@redhat.com>
2012-06-05 00:24:46 -0400

    ignore the xattr syscalls for now until the selinux oopses are fixed

--- test-random.sh
+++ test-random.sh
@@ -16,7 +16,7 @@ do
   cd tmp.$RND
   for i in `seq 1 $NR_PROCESSES`
   do
-	MALLOC_CHECK_=2 ../../trinity -q -l off -x mbind &
+	MALLOC_CHECK_=2 ../../trinity -q -l off -x lgetxattr -x fgetxattr -x getxattr &
   done
   wait
   cd ..
