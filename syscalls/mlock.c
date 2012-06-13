1d42dba81fa63dcbd57b8aaabe7eb91cf2c3f661
Dave Jones <davej@redhat.com>
2012-06-13 18:51:09 -0400

    There's no flags arg for mlock.
    Looks like I got carried away and copied mlockall

--- syscalls/mlock.c
+++ syscalls/mlock.c
@@ -5,25 +5,6 @@
 #include "trinity.h"
 #include "sanitise.h"
 
-#define MCL_CURRENT	1
-#define MCL_FUTURE	2
-
-static void sanitise_mlock(unsigned long *flags,
-		__unused__ unsigned long *a2,
-		__unused__ unsigned long *a3,
-		__unused__ unsigned long *a4,
-		__unused__ unsigned long *a5,
-		__unused__ unsigned long *a6)
-{
-	if (*flags != 0)
-		return;
-
-	if ((rand() % 2) == 0)
-		*flags = MCL_CURRENT;
-	else
-		*flags = MCL_FUTURE;
-}
-
 struct syscall syscall_mlock = {
 	.name = "mlock",
 	.num_args = 2,
@@ -32,5 +13,4 @@ struct syscall syscall_mlock = {
 	.arg2name = "len",
 	.arg2type = ARG_LEN,
 	.group = GROUP_VM,
-	.sanitise = sanitise_mlock,
 };
