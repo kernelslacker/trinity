dc6bc58559dbcf5e2d8eff3816897ee1dbba28ee
Mikko Rapeli <mikko.rapeli@iki.fi>
2012-06-19 17:36:24 +0200

    Copied i386.h to arm.h
    Content seems to be close enough.
    
    Signed-off-by: Mikko Rapeli <mikko.rapeli@iki.fi>

---
+++ arm.h
@@ -0,0 +1,15 @@
+#define KERNEL_ADDR	0xc0100220
+
+#define PAGE_OFFSET 0xC0000000
+#define TASK_SIZE (PAGE_OFFSET)
+/*
+ * Alternative possibilities for PAGE_OFFSET:
+ * default 0xB0000000 if VMSPLIT_3G_OPT
+ * default 0x78000000 if VMSPLIT_2G
+ * default 0x40000000 if VMSPLIT_1G
+ */
+
+#define PAGE_SHIFT 12
+
+#define PTE_FILE_MAX_BITS 31
+
