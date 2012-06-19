76d6343e23eea4d07c35dedbd072076f7b08d7e5
Jan Stancek <jstancek@redhat.com>
2012-06-19 07:00:48 -0400

    add sync_file_range2
    This is identical to sync_file_range with one exception,
    order of arguments is different. Define both in sync_file_range.c
    to use common sanitize code.
    
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- syscalls/sync_file_range.c
+++ syscalls/sync_file_range.c
@@ -1,5 +1,6 @@
 /*
  * SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes, unsigned int flags)
+ * SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags, loff_t offset, loff_t nbytes)
  */
 #include <linux/fs.h>
 #include <fcntl.h>
@@ -10,25 +11,33 @@
 #include "arch.h"
 #include "shm.h"
 
+struct syscall syscall_sync_file_range;
+
 static void sanitise_sync_file_range(int childno)
 {
+	unsigned int call = shm->syscallno[childno];
+	struct syscall *syscall_entry = syscalls[call].entry;
 	long endbyte;
-	loff_t off = shm->a2[childno];
+	loff_t nbytes;
+	loff_t off;
 
 retry:
 	off = rand() & 0xfffffff;
-	shm->a3[childno] = rand() & 0xfffffff;
-
-	endbyte = off + shm->a2[childno];
-
-
+	nbytes = rand() & 0xfffffff;
+	endbyte = off + nbytes;
 	if (endbyte < off)
 		goto retry;
 
 	if (off >= (0x100000000LL << PAGE_SHIFT))
 		goto retry;
 
-	shm->a2[childno] = off;
+	if (syscall_entry == &syscall_sync_file_range) {
+		shm->a2[childno] = off;
+		shm->a3[childno] = nbytes;
+	} else {
+		shm->a3[childno] = off;
+		shm->a4[childno] = nbytes;
+	}
 }
 
 struct syscall syscall_sync_file_range = {
@@ -46,4 +55,21 @@ struct syscall syscall_sync_file_range = {
 		.num = 3,
 		.values = { SYNC_FILE_RANGE_WAIT_BEFORE, SYNC_FILE_RANGE_WRITE, SYNC_FILE_RANGE_WAIT_AFTER },
         },
+};
+
+struct syscall syscall_sync_file_range2 = {
+	.name = "sync_file_range2",
+	.num_args = 4,
+	.sanitise = sanitise_sync_file_range,
+	.arg1name = "fd",
+	.arg1type = ARG_FD,
+	.arg2name = "flags",
+	.arg2type = ARG_LIST,
+        .arg2list = {
+		.num = 3,
+		.values = { SYNC_FILE_RANGE_WAIT_BEFORE, SYNC_FILE_RANGE_WRITE, SYNC_FILE_RANGE_WAIT_AFTER },
+	},
+	.arg3name = "offset",
+	.arg4name = "nbytes",
+	.arg4type = ARG_LEN,
 };
