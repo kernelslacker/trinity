a9b93353b7780594e08f65dfac3e8b7ede96bcc2
Dave Jones <davej@redhat.com>
2012-06-19 13:57:53 -0400

    argument moved

--- test-all-syscalls-parallel.sh
+++ test-all-syscalls-parallel.sh
@@ -8,7 +8,7 @@ cd tmp
 
 while [ 1 ];
 do
-  for syscall in $(../trinity -L | grep -v Trinity | grep -v 32bit | grep -v 64bit | awk '{ print $2 }' | sort -u)
+  for syscall in $(../trinity -L | grep -v Trinity | grep -v 32bit | grep -v 64bit | awk '{ print $4 }' | sort -u)
   do
 	MALLOC_CHECK_=2 ../trinity -q -c $syscall -D &
   done
