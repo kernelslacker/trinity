eb1134b68fa1e120ccb99025c67db80cd1773d60
Dave Jones <davej@redhat.com>
2012-09-05 17:32:53 -0400

    Just run through the list once.

--- test-all-syscalls-sequentially.sh
+++ test-all-syscalls-sequentially.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# This is a useful test to run occasionally, to see which syscalls are
+# causing trinity to segfault.
 
 if [ ! -d tmp ]; then
   mkdir tmp
@@ -6,12 +9,9 @@ fi
 chmod 755 tmp
 cd tmp
 
-while [ 1 ];
+for syscall in $(../trinity -L | grep -v Trinity | grep -v syscalls: | grep -v AVOID | grep 64-bit | awk '{ print $4 }' | sort -u)
 do
-  for syscall in $(../trinity -L | grep -v Trinity | grep -v syscalls: | grep -v AVOID | grep 64-bit | awk '{ print $4 }' | sort -u)
-  do
-	MALLOC_CHECK_=2 ../trinity -q -c $syscall -N 99999 -D -l off
+	MALLOC_CHECK_=2 ../trinity -q -c $syscall -N 99999 -D -l off -C 64
 	echo
 	echo
-  done
 done
