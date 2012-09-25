a393b33d1fc081e83464c70afe44a0ccda16f3c4
Dave Jones <davej@redhat.com>
2012-09-25 13:07:11 -0400

    a basic sanitise routine for connect()
    Based on Eric Dumazet's reproducer for the SO_KEEPALIVE/Raw socket bug.

--- syscalls/connect.c
+++ syscalls/connect.c
@@ -4,8 +4,29 @@
  * If the connection or binding succeeds, zero is returned.
  * On error, -1 is returned, and errno is set appropriately.
  */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
 #include "trinity.h"
 #include "sanitise.h"
+#include "shm.h"
+
+static void sanitise_connect(int childno)
+{
+	struct sockaddr_in *addr;
+
+	addr = malloc(sizeof(struct sockaddr_in));
+	if (addr == NULL)
+		return;
+
+	//TODO: Support more families
+	addr->sin_family = AF_INET;
+	addr->sin_addr.s_addr = htonl(0x7f000001);
+	addr->sin_port = rand() % 65535;
+	shm->a2[childno] = (unsigned long) addr;
+	shm->a3[childno] = sizeof(struct sockaddr_in);
+}
 
 struct syscall syscall_connect = {
 	.name = "connect",
@@ -17,4 +38,5 @@ struct syscall syscall_connect = {
 	.arg3name = "addrlen",
 	.arg3type = ARG_LEN,
 	.rettype = RET_ZERO_SUCCESS,
+	.sanitise = sanitise_connect,
 };
