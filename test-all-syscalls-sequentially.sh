c1ba13090678472c145b2b897ae7e25a7197d7e4
Dave Jones <davej@redhat.com>
2012-10-02 11:19:45 -0400

    go back to doing sequential test repeatedly for burn-in type testing

--- test-all-syscalls-sequentially.sh
+++ test-all-syscalls-sequentially.sh
@@ -9,9 +9,12 @@ fi
 chmod 755 tmp
 cd tmp
 
+while [ 1 ]
+do
 for syscall in $(../trinity -L | grep -v Trinity | grep -v syscalls: | grep -v AVOID | grep 64-bit | awk '{ print $4 }' | sort -u)
 do
 	MALLOC_CHECK_=2 ../trinity -q -c $syscall -N 99999 -D -l off -C 64
 	echo
 	echo
+done
 done
