038153d3c043b9ddcc46f6f8ed4416486199a91c
Tommi Rantala <tt.rantala@gmail.com>
2012-10-25 13:36:20 +0300

    Add missing PAGE_SHIFT and PTE_FILE_MAX_BITS for SPARC

--- arch-sparc.h
+++ arch-sparc.h
@@ -1,7 +1,11 @@
 #ifdef __arch64__
 #define KERNEL_ADDR	0xfffff80000000000
 #define TASK_SIZE ~0UL
+#define PAGE_SHIFT		13
+#define PTE_FILE_MAX_BITS	(64UL - PAGE_SHIFT - 1UL)
 #else
 #define KERNEL_ADDR	0xf0000000
 #define TASK_SIZE 0xF0000000UL
+#define PAGE_SHIFT		12
+#define PTE_FILE_MAX_BITS	24
 #endif
