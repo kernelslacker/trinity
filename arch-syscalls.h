378110cf38daae9d568bd35345b68f062263c8ed
Tommi Rantala <tt.rantala@gmail.com>
2012-10-25 13:36:13 +0300

    Remove unused `NR_SYSCALLS' defines from `arch-syscalls.h'

--- arch-syscalls.h
+++ arch-syscalls.h
@@ -4,15 +4,12 @@
 #ifdef __x86_64__
 #include "syscalls-x86_64.h"
 #include "syscalls-i386.h"
-#define NR_SYSCALLS NR_X86_64_SYSCALLS
 #endif
 #ifdef __i386__
 #include "syscalls-i386.h"
-#define NR_SYSCALLS NR_I386_SYSCALLS
 #endif
 #ifdef __powerpc__
 #include "syscalls-ppc.h"
-#define NR_SYSCALLS NR_PPC_SYSCALLS
 #endif
 #ifdef __ia64__
 #include "syscalls-ia64.h"
