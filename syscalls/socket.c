5b153ef3945747ec46a4e86f8fe21f05368454b5
Dave Jones <davej@redhat.com>
2012-12-11 18:01:44 -0500

    PF_AX25 support for sanitise_socket

--- syscalls/socket.c
+++ syscalls/socket.c
@@ -15,6 +15,23 @@
 #include "shm.h"
 #include "compat.h"
 
+#define NR_AX25_PROTOS 13
+static int ax25_protocols[NR_AX25_PROTOS] = {
+	0x01,	/* ROSE */
+	0x06,	/* Compressed TCP/IP packet   *//* Van Jacobsen (RFC 1144)    */
+	0x07,	/* Uncompressed TCP/IP packet *//* Van Jacobsen (RFC 1144)    */
+	0x08,	/* Segmentation fragment      */
+	0xc3,	/* TEXTNET datagram protocol  */
+	0xc4,	/* Link Quality Protocol      */
+	0xca,	/* Appletalk                  */
+	0xcb,	/* Appletalk ARP              */
+	0xcc,	/* ARPA Internet Protocol     */
+	0xcd,	/* ARPA Address Resolution    */
+	0xce,	/* FlexNet                    */
+	0xcf,	/* NET/ROM                    */
+	0xF0	/* No layer 3 protocol impl.  */
+};
+
 /* note: also called from generate_sockets() & sanitise_socketcall() */
 void sanitise_socket(int childno)
 {
@@ -30,6 +47,20 @@ void sanitise_socket(int childno)
 			protocol = 0;
 			break;
 		case 1:	type = SOCK_RAW;
+			break;
+		default:break;
+		}
+		break;
+
+	case AF_AX25:
+		switch (rand() % 3) {
+		case 0:	type = SOCK_DGRAM;
+			protocol = 0;
+			break;
+		case 1:	type = SOCK_SEQPACKET;
+			protocol = ax25_protocols[rand() % NR_AX25_PROTOS];
+			break;
+		case 2:	type = SOCK_RAW;
 			break;
 		default:break;
 		}
