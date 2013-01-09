283d7c52a626f5819dc3f347c0e3b5ba2d974998
Dave Jones <davej@redhat.com>
2013-01-09 11:13:22 -0500

    reenable the known ioctls
    might as well at least pass down some sensible ioctl numbers.

--- syscalls/ioctl.c
+++ syscalls/ioctl.c
@@ -9,37 +9,6 @@
 #include "sanitise.h"
 #include "shm.h"
 
-/*
-#include <linux/agpgart.h>
-#include <linux/atmioc.h>
-#include <linux/cciss_ioctl.h>
-#include <linux/cm4000_cs.h>
-#include <linux/dm-ioctl.h>
-#include <linux/dn.h>
-#include <linux/gigaset_dev.h>
-#include <linux/i2o-dev.h>
-#include <linux/ipmi.h>
-#include <linux/kvm.h>
-#include <linux/mmtimer.h>
-#include <linux/phantom.h>
-#include <linux/pktcdvd.h>
-#include <linux/ppdev.h>
-#include <linux/rfkill.h>
-#include <linux/spi/spidev.h>
-#include <linux/sonet.h>
-#include <linux/suspend_ioctls.h>
-#include <linux/synclink.h>
-#include <linux/usb/tmc.h>
-#include <linux/uinput.h>
-#include <linux/vhost.h>
-#include <linux/videodev2.h>
-#include <linux/watchdog.h>
-#include <mtd/ubi-user.h>
-#include <rdma/ib_user_mad.h>
-*/
-// msm_mdp.h not exported to glibc yet.
-#define MSMFB_IOCTL_MAGIC 'm'
-
 struct ioctl {
 	const char *name;
 	unsigned int request;
@@ -48,7 +17,6 @@ struct ioctl {
 
 const struct ioctl ioctllist[] = {
 #include "ioctls/scsi-generic.h"
-/*
 #include "ioctls/framebuffer.h"
 #include "ioctls/console.h"
 #include "ioctls/cdrom.h"
@@ -59,7 +27,6 @@ const struct ioctl ioctllist[] = {
 #include "ioctls/snd.h"
 #include "ioctls/mem.h"
 #include "ioctls/sisfb.h"
-*/
 };
 
 static void generic_sanitise_ioctl(int childno)
