eb2634e6f7dd4c740339f7e033429f120e3cc0eb
Dave Jones <davej@redhat.com>
2013-01-17 12:06:57 -0500

    check taint

--- test-all-syscalls-sequentially.sh
+++ test-all-syscalls-sequentially.sh
@@ -3,17 +3,28 @@
 # This is a useful test to run occasionally, to see which syscalls are
 # causing trinity to segfault.
 
+check_tainted()
+{
+    if [ "$(cat /proc/sys/kernel/tainted)" != $TAINT ]; then
+      echo ERROR: Taint flag changed $(cat /proc/sys/kernel/tainted)
+      exit
+    fi
+}
+
 if [ ! -d tmp ]; then
   mkdir tmp
 fi
 chmod 755 tmp
 cd tmp
 
+TAINT=$(cat /proc/sys/kernel/tainted)
+
 while [ 1 ]
 do
 for syscall in $(../trinity -L | grep -v Trinity | grep -v syscalls: | grep -v AVOID | grep 64-bit | awk '{ print $4 }' | sort -u)
 do
 	MALLOC_CHECK_=2 ../trinity -q -c $syscall -N 99999 -D -l off -C 64
+	check_tainted
 	echo
 	echo
 done
