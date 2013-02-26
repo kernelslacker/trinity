d9fe3c63b6c517bed4b3227aa66d700a241f4f78
Jiri Slaby <jslaby@suse.cz>
2013-02-27 00:52:40 +0100

    compat.h: add RDS constants
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- include/compat.h
+++ include/compat.h
@@ -158,6 +158,17 @@
 #define SYS_SENDMMSG 20
 #endif
 
+/* linux/rds.h */
+#ifndef RDS_CANCEL_SENT_TO
+#define RDS_CANCEL_SENT_TO              1
+#define RDS_GET_MR                      2
+#define RDS_FREE_MR                     3
+/* deprecated: RDS_BARRIER 4 */
+#define RDS_RECVERR                     5
+#define RDS_CONG_MONITOR                6
+#define RDS_GET_MR_FOR_DEST             7
+#endif
+
 /* asm/ptrace-abi.h */
 #ifndef PTRACE_SYSEMU
 #define PTRACE_SYSEMU		  31
