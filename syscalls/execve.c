d3d573e0408e540da65fb59cac89c9823b954675
Dave Jones <davej@redhat.com>
2013-03-06 17:19:14 -0500

    execve sanitise improvements

--- syscalls/execve.c
+++ syscalls/execve.c
@@ -1,7 +1,8 @@
 /*
-   long sys_execve(const char __user *name,
-	const char __user *const __user *argv,
-	const char __user *const __user *envp, struct pt_regs *regs)
+ * SYSCALL_DEFINE3(execve,
+ *                const char __user *, filename,
+ *                const char __user *const __user *, argv,
+ *                const char __user *const __user *, envp)
  *
  * On success, execve() does not return
  * on error -1 is returned, and errno is set appropriately.
@@ -9,24 +10,50 @@
  * TODO: Redirect stdin/stdout.
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include "sanitise.h"
+#include "shm.h"
+
+static unsigned long ** gen_ptrs_to_crap(void)
+{
+	unsigned long **ptr;
+	unsigned int i;
+	unsigned int count = rand() % 32;
+
+	/* Fabricate argv */
+	ptr = malloc(count * sizeof(void *));	// FIXME: LEAK
+	if (ptr == NULL)
+		return NULL;
+
+	for (i = 0; i < count; i++) {
+		ptr[i] = malloc(page_size);	// FIXME: LEAK
+		if (ptr[i] != NULL)
+			fabricate_onepage_struct((char *) ptr[i]);
+	}
+
+	return ptr;
+}
 
 void sanitise_execve(__unused__ int childno)
 {
 	/* we don't want to block if something tries to read from stdin */
 	fclose(stdin);
+
+	/* Fabricate argv */
+	shm->a2[childno] = (unsigned long) gen_ptrs_to_crap();
+
+	/* Fabricate envp */
+	shm->a3[childno] = (unsigned long) gen_ptrs_to_crap();
 }
 
 struct syscall syscall_execve = {
 	.name = "execve",
-	.num_args = 4,
+	.num_args = 3,
 	.arg1name = "name",
 	.arg1type = ARG_PATHNAME,
 	.arg2name = "argv",
 	.arg2type = ARG_ADDRESS,
 	.arg3name = "envp",
 	.arg3type = ARG_ADDRESS,
-	.arg4name = "regs",
-	.arg4type = ARG_ADDRESS,
 	.sanitise = sanitise_execve,
 };
