8380ac3db92db2bee58f488df8478c499511693c
Dave Jones <davej@redhat.com>
2013-03-06 19:50:00 -0500

    generate a page of unicode garbage

--- generic-sanitise.c
+++ generic-sanitise.c
@@ -251,10 +251,82 @@ void fabricate_onepage_struct(char *page)
 	}
 }
 
+static void gen_unicode_page(char *page)
+{
+	unsigned int i = 0, j, l;
+	unsigned int unilen;
+
+	char unicode1[4] = { 0xb8, 0xe0, 0xe0, 0xaa };
+	char unicode2[6] = { 0x89, 0xb9, 0xb9, 0xe0, 0xe0, 0x89 };
+	char unicode3[2] = { 0x89, 0xb9 };
+	char unicode4[18] = { 0xbb, 0xef, 0xd2, 0xa9, 0xd2, 0x88, 0x20, 0x88, 0x88, 0xd2, 0x88, 0xd2, 0xd2, 0x20, 0xd2, 0x88, 0x0a, 0x88 };
+	char unicode5[4] = { 0xd9, 0x20, 0xd2, 0x87 };
+	char unicode6[4] = { 0xcc, 0x88, 0xd2, 0xbf };
+	char unicode7[2] = { 0x0a, 0xbf };
+
+	char *ptr = page;
+
+	while (i < (page_size - 4)) {
+
+		j = rand() % 7;
+
+		switch (j) {
+
+		case 0:
+			strncpy(ptr, unicode1, 4);
+			ptr += 4;
+			i += 4;
+			break;
+
+		case 1: unilen = rand() % 10;
+			for (l = 0; l < unilen; l++) {
+				strncpy(ptr, unicode2, 6);
+				ptr += 6;
+				i += 6;
+				if ((i + 6) > page_size)
+					break;
+			}
+			break;
+
+		case 2:	strncpy(ptr, unicode3, 2);
+			i += 2;
+			ptr += 2;
+			break;
+		case 3:	strncpy(ptr, unicode4, 18);
+			i += 18;
+			ptr += 18;
+			break;
+
+		case 4:	strncpy(ptr, unicode5, 4);
+			i += 4;
+			ptr += 4;
+			break;
+
+		case 5: unilen = rand() % 10;
+			for (l = 0; l < unilen; l++) {
+				strncpy(ptr, unicode6, 4);
+				ptr += 4;
+				i += 4;
+				if ((i + 4) > page_size)
+					break;
+			}
+			break;
+
+		case 6:	strncpy(ptr, unicode7, 4);
+			i += 4;
+			ptr += 4;
+			break;
+
+		default:
+			break;
+		}
+	}
+}
+
 void regenerate_random_page(void)
 {
 	unsigned int i;
-	unsigned int type = rand() % 4;
+	unsigned int type = rand() % 5;
 
 	/* The rest of the time, return a page of complete trash */
 	switch (type) {
@@ -281,6 +353,10 @@ void regenerate_random_page(void)
 
 	case 3:	/* sometimes return a page that looks kinda like a struct */
 		fabricate_onepage_struct(page_rand);
+		return;
+
+	case 4:
+		gen_unicode_page(page_rand);
 		return;
 
 	default:
