86d95293150e55048aa33365d7c0ce9bc4d6c4f2
Dave Jones <davej@redhat.com>
2013-04-14 20:08:38 -0400

    split up shm init
    we need randomness in parse_args if we pass -r
    but we also need to handle -s, so post-setup the shm too if necessary

--- include/random.h
+++ include/random.h
@@ -5,5 +5,6 @@ extern unsigned int seed;
 unsigned int init_seed(unsigned int seed);
 void set_seed(unsigned int pidslot);
 void reseed(void);
+unsigned int new_seed(void);
 
 #endif	/* _RANDOM_H */
