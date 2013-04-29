3580faf996ded86ecce48cdaffe5f066db12e0ca
Dave Jones <davej@redhat.com>
2013-04-29 14:33:00 -0400

    symlink takes pathnames

--- syscalls/symlink.c
+++ syscalls/symlink.c
@@ -7,8 +7,8 @@ struct syscall syscall_symlink = {
 	.name = "symlink",
 	.num_args = 2,
 	.arg1name = "oldname",
-	.arg1type = ARG_ADDRESS,
+	.arg1type = ARG_PATHNAME,
 	.arg2name = "newname",
-	.arg2type = ARG_ADDRESS,
+	.arg2type = ARG_PATHNAME,
 	.group = GROUP_VFS,
 };
