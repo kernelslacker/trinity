b4219484a5205dc762fc3c29e12f0bcfb640bbad
Dave Jones <davej@redhat.com>
2013-05-13 19:14:27 -0400

    move decnet sockaddr code out to own file

--- sockaddr.c
+++ sockaddr.c
@@ -3,7 +3,6 @@
 #include <sys/un.h>
 #include <netinet/in.h>
 #include <linux/atm.h>
-#include <linux/dn.h>
 #include <linux/if.h>
 #include <linux/if_arp.h>
 #include <linux/llc.h>
@@ -21,28 +20,6 @@
 #include "maps.h"
 #include "config.h"
 #include "params.h"	// do_specific_proto
-
-static void gen_decnet(unsigned long *addr, unsigned long *addrlen)
-{
-	struct sockaddr_dn *dn;
-	unsigned int i;
-
-	dn = malloc(sizeof(struct sockaddr_dn));
-	if (dn == NULL)
-		return;
-
-	dn->sdn_family = PF_DECnet;
-	dn->sdn_flags = rand();
-	dn->sdn_objnum = rand();
-	dn->sdn_objnamel = rand() % 16;
-	for (i = 0; i < dn->sdn_objnamel; i++)
-		dn->sdn_objname[i] = rand();
-	dn->sdn_add.a_len = rand() % 2;
-	dn->sdn_add.a_addr[0] = rand();
-	dn->sdn_add.a_addr[1] = rand();
-	*addr = (unsigned long) dn;
-	*addrlen = sizeof(struct sockaddr_dn);
-}
 
 static void gen_llc(unsigned long *addr, unsigned long *addrlen)
 {
