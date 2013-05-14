6c56fd583659ddceb1b5d8098de0dc40acbba834
Dave Jones <davej@redhat.com>
2013-05-14 18:15:31 -0400

    rename hw to attr

--- syscalls/perf_event_open.c
+++ syscalls/perf_event_open.c
@@ -14,84 +14,84 @@
 
 static void sanitise_perf_event_open(int childno)
 {
-	struct perf_event_attr *hw;
+	struct perf_event_attr *attr;
 
 	shm->a1[childno] = (unsigned long) page_rand;
-	hw = (struct perf_event_attr *) shm->a1[childno];
+	attr = (struct perf_event_attr *) shm->a1[childno];
 
 	/* this makes sure we clear out the reserved fields. */
 	memset(page_rand, 0, sizeof(struct perf_event_attr));
 
 	switch(rand() % 6) {
-		case 0:	hw->type = PERF_TYPE_HARDWARE;
+		case 0:	attr->type = PERF_TYPE_HARDWARE;
 			switch(rand() % 10) {
-				case 0: hw->config=PERF_COUNT_HW_CPU_CYCLES;
+				case 0: attr->config=PERF_COUNT_HW_CPU_CYCLES;
 					break;
-				case 1: hw->config=PERF_COUNT_HW_INSTRUCTIONS;
+				case 1: attr->config=PERF_COUNT_HW_INSTRUCTIONS;
 					break;
-				case 2: hw->config=PERF_COUNT_HW_CACHE_REFERENCES;
+				case 2: attr->config=PERF_COUNT_HW_CACHE_REFERENCES;
 					break;
-				case 3: hw->config=PERF_COUNT_HW_BRANCH_INSTRUCTIONS;
+				case 3: attr->config=PERF_COUNT_HW_BRANCH_INSTRUCTIONS;
 					break;
-				case 4: hw->config=PERF_COUNT_HW_BRANCH_MISSES;
+				case 4: attr->config=PERF_COUNT_HW_BRANCH_MISSES;
 					break;
-				case 5: hw->config=PERF_COUNT_HW_BUS_CYCLES;
+				case 5: attr->config=PERF_COUNT_HW_BUS_CYCLES;
 					break;
-				case 6: hw->config=PERF_COUNT_HW_STALLED_CYCLES_FRONTEND;
+				case 6: attr->config=PERF_COUNT_HW_STALLED_CYCLES_FRONTEND;
 					break;
-				case 7: hw->config=PERF_COUNT_HW_STALLED_CYCLES_BACKEND;
+				case 7: attr->config=PERF_COUNT_HW_STALLED_CYCLES_BACKEND;
 					break;
-				case 8: hw->config=PERF_COUNT_HW_REF_CPU_CYCLES;
+				case 8: attr->config=PERF_COUNT_HW_REF_CPU_CYCLES;
 					break;
-				case 9: hw->config = rand();
+				case 9: attr->config = rand();
 					break;
 				default: break;
 			}
 			break;
-		case 1: hw->type = PERF_TYPE_SOFTWARE;
+		case 1: attr->type = PERF_TYPE_SOFTWARE;
 			switch(rand() % 10) {
-				case 0: hw->config=PERF_COUNT_SW_CPU_CLOCK;
+				case 0: attr->config=PERF_COUNT_SW_CPU_CLOCK;
 					break;
-				case 1: hw->config=PERF_COUNT_SW_TASK_CLOCK;
+				case 1: attr->config=PERF_COUNT_SW_TASK_CLOCK;
 					break;
-				case 2: hw->config=PERF_COUNT_SW_PAGE_FAULTS;
+				case 2: attr->config=PERF_COUNT_SW_PAGE_FAULTS;
 					break;
-				case 3: hw->config=PERF_COUNT_SW_CONTEXT_SWITCHES;
+				case 3: attr->config=PERF_COUNT_SW_CONTEXT_SWITCHES;
 					break;
-				case 4: hw->config=PERF_COUNT_SW_CPU_MIGRATIONS;
+				case 4: attr->config=PERF_COUNT_SW_CPU_MIGRATIONS;
 					break;
-				case 5: hw->config=PERF_COUNT_SW_PAGE_FAULTS_MIN;
+				case 5: attr->config=PERF_COUNT_SW_PAGE_FAULTS_MIN;
 					break;
-				case 6: hw->config=PERF_COUNT_SW_PAGE_FAULTS_MAJ;
+				case 6: attr->config=PERF_COUNT_SW_PAGE_FAULTS_MAJ;
 					break;
-				case 7: hw->config=PERF_COUNT_SW_ALIGNMENT_FAULTS;
+				case 7: attr->config=PERF_COUNT_SW_ALIGNMENT_FAULTS;
 					break;
-				case 8: hw->config=PERF_COUNT_SW_EMULATION_FAULTS;
+				case 8: attr->config=PERF_COUNT_SW_EMULATION_FAULTS;
 					break;
-				case 9: hw->config=rand();
+				case 9: attr->config=rand();
 				default: break;
 			}
 			break;
-		case 2: hw->type = PERF_TYPE_TRACEPOINT;
+		case 2: attr->type = PERF_TYPE_TRACEPOINT;
 			break;
-		case 3: hw->type = PERF_TYPE_HW_CACHE;
+		case 3: attr->type = PERF_TYPE_HW_CACHE;
 			break;
-		case 4: hw->type = PERF_TYPE_RAW;
+		case 4: attr->type = PERF_TYPE_RAW;
 			/* can be arbitrary 64-bit value */
 			/* there are some constraints we can add */
 			/* to make it more likely to be a valid event */
-			hw->config = rand();
+			attr->config = rand();
 
 			break;
-		case 5: hw->type = PERF_TYPE_BREAKPOINT;
+		case 5: attr->type = PERF_TYPE_BREAKPOINT;
 			break;
 		default: break;
 	}
 
 	switch(rand() % 2) {
-		case 0: hw->size = sizeof(struct perf_event_attr);
+		case 0: attr->size = sizeof(struct perf_event_attr);
 			break;
-		case 1: hw->size = get_len();
+		case 1: attr->size = get_len();
 		default: break;
 	}
 }
