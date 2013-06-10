8cb34132c06c7e2897f143982f127ad4162aec7b
Dave Jones <davej@redhat.com>
2013-06-10 16:21:54 -0400

    delete the old tmp dirs on each run

--- scripts/test-multi.sh
+++ scripts/test-multi.sh
@@ -23,8 +23,12 @@ while [ 1 ];
 do
   for i in `seq 1 $NR_PROCESSES`
   do
-    mkdir -p tmp.$i
+    if [ -d tmp.$i ]; then
+      chmod 755 tmp.$i
+      rm -rf tmp.$i
+    fi
     chmod 755 tmp.$i
+    mkdir -p tmp.$i
     pushd tmp.$i
 
     if [ ! -f ../../trinity ]; then
@@ -33,6 +37,7 @@ do
       exit
     fi
 
+    rm -f trinity.socketcache
     MALLOC_CHECK_=2 ../../trinity -qq -l off -x sendmsg -N 999999 &
 
     popd
