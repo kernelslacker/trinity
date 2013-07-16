505c6becd2b1a1613482552d25d655d653c43322
Dave Jones <davej@redhat.com>
2013-07-16 18:46:16 -0400

    change sockaddr generation to use a table of func ptrs instead of a huge switch.

--- net/sockaddr.c
+++ net/sockaddr.c
@@ -2,7 +2,6 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <netinet/in.h>
-
 #include <stdlib.h>
 #include "sanitise.h"
 #include "compat.h"
@@ -10,9 +9,63 @@
 #include "maps.h"
 #include "config.h"
 #include "params.h"	// do_specific_proto
+#include "trinity.h"	// ARRAY_SIZE
+
+struct sa_func_entry {
+	unsigned int pf;
+	void (*func)(unsigned long *addr, unsigned long *addrlen);
+};
+
+static const struct sa_func_entry sa_funcs[] = {
+//TODO	{ .pf = PF_UNSPEC, .func = &unspec_gen_sockaddr },
+	{ .pf = PF_UNIX, .func = &unix_gen_sockaddr },
+	{ .pf = PF_INET, .func = &ipv4_gen_sockaddr },
+	{ .pf = PF_AX25, .func = &ax25_gen_sockaddr },
+	{ .pf = PF_IPX, .func = &ipx_gen_sockaddr },
+	{ .pf = PF_APPLETALK, .func = &atalk_gen_sockaddr },
+//TODO	{ .pf = PF_NETROM, .func = &netrom_gen_sockaddr },
+//TODO	{ .pf = PF_BRIDGE, .func = &bridge_gen_sockaddr },
+	{ .pf = PF_ATMPVC, .func = &atmpvc_gen_sockaddr },
+	{ .pf = PF_X25, .func = &x25_gen_sockaddr },
+	{ .pf = PF_INET6, .func = &ipv6_gen_sockaddr },
+	{ .pf = PF_ROSE, .func = &rose_gen_sockaddr },
+	{ .pf = PF_DECnet, .func = &decnet_gen_sockaddr },
+	{ .pf = PF_NETBEUI, .func = &llc_gen_sockaddr },
+//TODO	{ .pf = PF_SECURITY, .func = &security_gen_sockaddr },
+//TODO	{ .pf = PF_KEY, .func = &key_gen_sockaddr },
+	{ .pf = PF_NETLINK, .func = &netlink_gen_sockaddr },
+	{ .pf = PF_PACKET, .func = &packet_gen_sockaddr },
+//TODO	{ .pf = PF_ASH, .func = &ash_gen_sockaddr },
+	{ .pf = PF_ECONET, .func = &econet_gen_sockaddr },
+	{ .pf = PF_ATMSVC, .func = &atmsvc_gen_sockaddr },
+//TODO	{ .pf = PF_RDS, .func = &rds_gen_sockaddr },
+//TODO	{ .pf = PF_SNA, .func = &sna_gen_sockaddr },
+	{ .pf = PF_IRDA, .func = &irda_gen_sockaddr },
+	{ .pf = PF_PPPOX, .func = &pppox_gen_sockaddr },
+//TODO	{ .pf = PF_WANPIPE, .func = &wanpipe_gen_sockaddr },
+	{ .pf = PF_LLC, .func = &llc_gen_sockaddr },
+	{ .pf = PF_CAN, .func = &can_gen_sockaddr },
+	{ .pf = PF_TIPC, .func = &tipc_gen_sockaddr },
+//TODO	{ .pf = PF_BLUETOOTH, .func = &bluetooth_gen_sockaddr },
+//TODO	{ .pf = PF_IUCV, .func = &iucv_gen_sockaddr },
+//TODO	{ .pf = PF_RXRPC, .func = &rxrpc_gen_sockaddr },
+//TODO	{ .pf = PF_ISDN, .func = &isdn_gen_sockaddr },
+	{ .pf = PF_PHONET, .func = &phonet_gen_sockaddr },
+//TODO	{ .pf = PF_IEEE802154, .func = &ieee802154_gen_sockaddr },
+#ifdef USE_CAIF
+	{ .pf = PF_CAIF, .func = &caif_gen_sockaddr },
+#endif
+#ifdef USE_IF_ALG
+	{ .pf = PF_ALG, .func = &alg_gen_sockaddr },
+#endif
+	{ .pf = PF_NFC, .func = &nfc_gen_sockaddr },
+//TODO	{ .pf = PF_VSOCK, .func = &vsock_gen_sockaddr },
+};
 
 void generate_sockaddr(unsigned long *addr, unsigned long *addrlen, int pf)
 {
+	unsigned int i;
+
 	/* If we want sockets of a specific type, we'll want sockaddrs that match. */
 	if (do_specific_proto == TRUE)
 		pf = specific_proto;
@@ -21,168 +74,8 @@ void generate_sockaddr(unsigned long *addr, unsigned long *addrlen, int pf)
 	if (pf == -1)
 		pf = rand() % TRINITY_PF_MAX;
 
-	switch (pf) {
-
-	case PF_UNSPEC:
-		//TODO
-		break;
-
-	case PF_UNIX:
-		unix_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_INET:
-		ipv4_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_AX25:
-		ax25_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_IPX:
-		ipx_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_APPLETALK:
-		atalk_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_NETROM:
-		//TODO
-		break;
-
-	case PF_BRIDGE:
-		//TODO
-		break;
-
-	case PF_ATMPVC:
-		atmpvc_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_X25:
-		x25_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_INET6:
-		ipv6_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_ROSE:
-		rose_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_DECnet:
-		decnet_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_NETBEUI:
-		llc_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_SECURITY:
-		//TODO
-		break;
-
-	case PF_KEY:
-		break;
-
-	case PF_NETLINK:
-		netlink_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_PACKET:
-		packet_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_ASH:
-		//TODO
-		break;
-
-	case PF_ECONET:
-		econet_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_ATMSVC:
-		atmsvc_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_RDS:
-		//TODO
-		break;
-
-	case PF_SNA:
-		//TODO
-		break;
-
-	case PF_IRDA:
-		irda_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_PPPOX:
-		pppox_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_WANPIPE:
-		//TODO
-		break;
-
-	case PF_LLC:
-		llc_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_CAN:
-		can_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_TIPC:
-		tipc_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_BLUETOOTH:
-		//TODO
-		break;
-
-	case PF_IUCV:
-		//TODO
-		break;
-
-	case PF_RXRPC:
-		//TODO
-		break;
-
-	case PF_ISDN:
-		//TODO
-		break;
-
-	case PF_PHONET:
-		phonet_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_IEEE802154:
-		//TODO
-		break;
-
-#ifdef USE_CAIF
-	case PF_CAIF:
-		caif_gen_sockaddr(addr, addrlen);
-		break;
-#endif
-
-#ifdef USE_IF_ALG
-	case PF_ALG:
-		alg_gen_sockaddr(addr, addrlen);
-		break;
-#endif
-
-	case PF_NFC:
-		nfc_gen_sockaddr(addr, addrlen);
-		break;
-
-	case PF_VSOCK:
-		//TODO
-		break;
-
-	default:
-		break;
+	for (i = 0; i < ARRAY_SIZE(sa_funcs); i++) {
+		if (sa_funcs[i].pf == (unsigned int) pf)
+			sa_funcs[i].func(addr, addrlen);
 	}
 }
