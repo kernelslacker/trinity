c8905567e4d199bf8d04d82eb94920d1d75624ea
Dave Jones <davej@redhat.com>
2013-07-16 13:05:50 -0400

    nfc: move random socket generation into per protocol files.

--- net/nfc.c
+++ net/nfc.c
@@ -6,6 +6,7 @@
 #include "config.h"
 #include "compat.h"
 #include "net.h"
+#include "random.h"
 
 void gen_nfc(unsigned long *addr, unsigned long *addrlen)
 {
@@ -22,4 +23,19 @@ void gen_nfc(unsigned long *addr, unsigned long *addrlen)
 	nfc->nfc_protocol = rand() % 5;
 	*addr = (unsigned long) nfc;
 	*addrlen = sizeof(struct sockaddr_nfc);
+}
+
+void nfc_rand_socket(struct proto_type *pt)
+{
+	if (rand_bool()) {
+		pt->protocol = NFC_SOCKPROTO_LLCP;
+		if (rand_bool())
+			pt->type = SOCK_DGRAM;
+		else
+			pt->type = SOCK_STREAM;
+		return;
+	}
+
+	pt->protocol = NFC_SOCKPROTO_RAW;
+	pt->type = SOCK_SEQPACKET;
 }
