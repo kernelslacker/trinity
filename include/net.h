3c2950d1c4d98d3856acc1610b6715acf2da340b
Dave Jones <davej@redhat.com>
2013-07-16 14:46:33 -0400

    move x25 setsockopt out to own file

--- include/net.h
+++ include/net.h
@@ -142,5 +142,6 @@ void atalk_setsockopt(struct sockopt *so);
 void netrom_setsockopt(struct sockopt *so);
 void rose_setsockopt(struct sockopt *so);
 void decnet_setsockopt(struct sockopt *so);
+void x25_setsockopt(struct sockopt *so);
 
 #endif	/* _NET_H */
