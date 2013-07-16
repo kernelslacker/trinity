aab3046027f6d3059bfde3961726e9fc76bd556d
Dave Jones <davej@redhat.com>
2013-07-16 12:54:52 -0400

    inet6: move random socket generation into per protocol files.

--- net/ipv6.c
+++ net/ipv6.c
@@ -7,6 +7,7 @@
 #include <linux/if_packet.h>
 #include <stdlib.h>
 #include "net.h"
+#include "random.h"
 
 void gen_ipv6(unsigned long *addr, unsigned long *addrlen)
 {
@@ -24,4 +25,26 @@ void gen_ipv6(unsigned long *addr, unsigned long *addrlen)
 	ipv6->sin6_port = rand() % 65535;
 	*addr = (unsigned long) ipv6;
 	*addrlen = sizeof(struct sockaddr_in6);
+}
+
+void inet6_rand_socket(struct proto_type *pt)
+{
+	switch (rand() % 3) {
+	case 0: pt->type = SOCK_STREAM;     // TCP
+		pt->protocol = 0;
+		break;
+
+	case 1: pt->type = SOCK_DGRAM;      // UDP
+		if (rand_bool())
+			pt->protocol = 0;
+		else
+			pt->protocol = IPPROTO_UDP;
+		break;
+
+	case 2: pt->type = SOCK_RAW;
+		pt->protocol = rand() % PROTO_MAX;
+		break;
+
+	default:break;
+	}
 }
