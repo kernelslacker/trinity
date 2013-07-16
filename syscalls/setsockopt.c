e571956d5f10d6ec54366181b89d7d2e383888fa
Dave Jones <davej@redhat.com>
2013-07-16 15:07:46 -0400

    move netlink setsockopt out to own file

--- syscalls/setsockopt.c
+++ syscalls/setsockopt.c
@@ -204,10 +204,12 @@ static void sanitise_setsockopt(int childno)
 		shm->a5[childno] = so.optlen;
 		break;
 
-	case 23: level = SOL_NETLINK;
-		shm->a2[childno] = level;
-		val = rand() % NR_SOL_NETLINK_OPTS;
-		shm->a3[childno] = netlink_opts[val];
+	case 23:
+		netlink_setsockopt(&so);
+		shm->a2[childno] = so.level;
+		shm->a3[childno] = so.optname;
+		shm->a4[childno] = so.optval;
+		shm->a5[childno] = so.optlen;
 		break;
 
 	case 24: level = SOL_TIPC;
