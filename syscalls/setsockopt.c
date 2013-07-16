191cdb17e8877def0454175ea7f270fc5f8fb39e
Dave Jones <davej@redhat.com>
2013-07-16 14:30:59 -0400

    move ax25 setsockopt out to own file

--- syscalls/setsockopt.c
+++ syscalls/setsockopt.c
@@ -102,10 +102,11 @@ static void sanitise_setsockopt(int childno)
 		shm->a5[childno] = so.optlen;
 		break;
 
-	case 10: level = SOL_AX25;
-		shm->a2[childno] = level;
-		val = rand() % NR_SOL_AX25_OPTS;
-		shm->a3[childno] = ax25_opts[val];
+	case 10: ax25_setsockopt(&so);
+		shm->a2[childno] = so.level;
+		shm->a3[childno] = so.optname;
+		shm->a4[childno] = so.optval;
+		shm->a5[childno] = so.optlen;
 		break;
 
 	case 11: level = SOL_ATALK;
