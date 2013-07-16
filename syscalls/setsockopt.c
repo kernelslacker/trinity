dd6eff724b18d5da828bb811154a09290601c5c3
Dave Jones <davej@redhat.com>
2013-07-16 15:02:58 -0400

    move llc setsockopt out to own file

--- syscalls/setsockopt.c
+++ syscalls/setsockopt.c
@@ -188,10 +188,12 @@ static void sanitise_setsockopt(int childno)
 		shm->a5[childno] = so.optlen;
 		break;
 
-	case 21: level = SOL_LLC;
-		shm->a2[childno] = level;
-		val = rand() % NR_SOL_LLC_OPTS;
-		shm->a3[childno] = llc_opts[val];
+	case 21:
+		llc_setsockopt(&so);
+		shm->a2[childno] = so.level;
+		shm->a3[childno] = so.optname;
+		shm->a4[childno] = so.optval;
+		shm->a5[childno] = so.optlen;
 		break;
 
 	case 22: level = SOL_DCCP;
