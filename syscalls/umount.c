a1312d382650c28b42e32ccea1cf5b82124d7d11
Dave Jones <davej@redhat.com>
2013-07-31 22:10:43 -0400

    annotate flags for umount

--- syscalls/umount.c
+++ syscalls/umount.c
@@ -1,6 +1,7 @@
 /*
  * SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
  */
+#include <sys/mount.h>
 #include "sanitise.h"
 
 struct syscall syscall_umount = {
@@ -9,5 +10,10 @@ struct syscall syscall_umount = {
 	.arg1name = "name",
 	.arg1type = ARG_PATHNAME,
 	.arg2name = "flags",
+	.arg2type = ARG_LIST,
+	.arg2list = {
+		.num = 4,
+		.values = { MNT_FORCE, MNT_DETACH, MNT_EXPIRE, UMOUNT_NOFOLLOW },
+	},
 	.group = GROUP_VFS,
 };
