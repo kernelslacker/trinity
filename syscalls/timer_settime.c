5cf997201ce2c634c11bb80cbcdb870735357861
Dave Jones <davej@redhat.com>
2013-07-31 22:14:17 -0400

    annotate flags for sys_timer_settime

--- syscalls/timer_settime.c
+++ syscalls/timer_settime.c
@@ -3,6 +3,7 @@
 	const struct itimerspec __user *, new_setting,
 	struct itimerspec __user *, old_setting)
  */
+#include <time.h>
 #include "sanitise.h"
 
 struct syscall syscall_timer_settime = {
@@ -10,6 +11,11 @@ struct syscall syscall_timer_settime = {
 	.num_args = 4,
 	.arg1name = "timer_id",
 	.arg2name = "flags",
+	.arg2type = ARG_LIST,
+	.arg2list = {
+		.num = 1,
+		.values = { TIMER_ABSTIME },
+	},
 	.arg3name = "new_setting",
 	.arg3type = ARG_ADDRESS,
 	.arg4name = "old_setting",
