8d0c7218bff1cf7064eecfd644b4baecb4c5e339
Ildar Muslukhov <ildarm@google.com>
2013-09-19 14:24:24 -0700

    Fix syscall32 to supply 6th arg to syscal
    This patch fixes missing 6th param for 32bit syscalls in i386 and x86_64
    archs.
    
    Note: I have to remove static from syscall32, because gcc messes up with
    inline asm becuase of that.
    
    Signed-off-by: Ildar Muslukhov <ildarm@google.com>

--- include/syscall.h
+++ include/syscall.h
@@ -134,6 +134,7 @@ void count_syscalls_enabled(void);
 void display_enabled_syscalls(void);
 void disable_non_net_syscalls(void);
 void init_syscalls(void);
+long syscall32(int num_args, unsigned int call, unsigned long a1, unsigned long a2, unsigned long a3, unsigned long a4, unsigned long a5, unsigned long a6);
 
 #define for_each_32bit_syscall(i) \
 	for (i = 0; i < max_nr_32bit_syscalls; i++)
