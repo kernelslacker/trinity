4501a885a1d7f81f8c2ff7cd31de33df37de2e91
Dave Jones <davej@redhat.com>
2013-11-15 12:39:50 -0500

    reduce ifdeffery in caif

--- net/caif_setsockopt.c
+++ net/caif_setsockopt.c
@@ -4,27 +4,27 @@
 #include "compat.h"
 #include "trinity.h"	// ARRAY_SIZE
 
-#ifdef USE_CAIF
-#include <linux/caif/caif_socket.h>
-#endif
-
 #define SOL_CAIF 278
 
 #ifdef USE_CAIF
+#include <linux/caif/caif_socket.h>
+
 #define NR_SOL_CAIF_OPTS ARRAY_SIZE(caif_opts)
 static const unsigned int caif_opts[] = { CAIFSO_LINK_SELECT, CAIFSO_REQ_PARAM };
-#endif
 
 void caif_setsockopt(struct sockopt *so)
 {
-#ifdef USE_CAIF
 	unsigned char val;
-#endif
 
 	so->level = SOL_CAIF;
 
-#ifdef USE_CAIF
 	val = rand() % NR_SOL_CAIF_OPTS;
 	so->optname = caif_opts[val];
-#endif
 }
+#else
+/* stub if we are built on something without RDS headers */
+void caif_setsockopt(struct sockopt *so)
+{
+	so->level = SOL_CAIF;
+}
+#endif
