498fb8bc2824325645749ead7345b319d40617e4
Dave Jones <davej@redhat.com>
2013-12-05 17:08:27 -0500

    mremap sanitise improvements.

--- syscalls/mremap.c
+++ syscalls/mremap.c
@@ -2,28 +2,53 @@
  * asmlinkage unsigned long sys_mremap(unsigned long addr,
  *   unsigned long old_len, unsigned long new_len,
  *   unsigned long flags, unsigned long new_addr)
- *
- * This syscall is a bit of a nightmare to fuzz as we -EINVAL all over the place.
- * It might be more useful once we start passing around valid maps instead of just
- * trying random addresses.
  */
 #include <linux/mman.h>
 #include <stdlib.h>
 #include "trinity.h"	// page_size
 #include "arch.h"
+#include "maps.h"
+#include "random.h"
 #include "sanitise.h"
 #include "shm.h"
 
+#define MB (1024 * 1024UL)
+#define GB (1024 * MB)
+
+static unsigned long rand_size(void)
+{
+	const unsigned long sizes[] = { 1 * MB, 2 * MB, 4 * MB, 10 * MB, 1 * GB, 2 * GB };
+
+	return sizes[rand() % ARRAY_SIZE(sizes)];
+}
+
 static void sanitise_mremap(int childno)
 {
+	struct map *map;
+	char *p;
+	unsigned int i;
+
 	shm->a1[childno] &= PAGE_MASK;
 
-	if (shm->a4[childno] & MREMAP_FIXED) {
-		// Can't be fixed, and maymove.
-		shm->a4[childno] &= ~MREMAP_MAYMOVE;
+	map = get_map();
 
-		shm->a3[childno] &= TASK_SIZE - shm->a3[childno];
+	shm->a1[childno] = (unsigned long) map->ptr;
+	shm->a2[childno] = map->size;
+	shm->a3[childno] = map->size;		//TODO: Munge this.
+
+	if (shm->a4[childno] & MREMAP_FIXED) {
+		shm->a5[childno] = rand_size();
+	} else {
+		shm->a5[childno] = 0;
 	}
+
+	/* Sometimes dirty the mapping first. */
+	if (rand_bool())
+		return;
+
+	p = map->ptr;
+	for (i = 0; i < map->size; i += page_size)
+		p[i] = 1;
 }
 
 struct syscall syscall_mremap = {
@@ -31,17 +56,14 @@ struct syscall syscall_mremap = {
 	.num_args = 5,
 	.sanitise = sanitise_mremap,
 	.arg1name = "addr",
-	.arg1type = ARG_NON_NULL_ADDRESS,
 	.arg2name = "old_len",
-	.arg2type = ARG_LEN,
 	.arg3name = "new_len",
-	.arg3type = ARG_LEN,
 	.arg4name = "flags",
-        .arg4type = ARG_LIST,
-        .arg4list = {
+	.arg4type = ARG_LIST,
+	.arg4list = {
 		.num = 2,
 		.values = { MREMAP_MAYMOVE, MREMAP_FIXED },
-        },
+	},
 	.arg5name = "new_addr",
 	.arg5type = ARG_ADDRESS,
 	.group = GROUP_VM,
