f10193c9d08911c0dcb439991fd15b07f7293ddf
Dave Jones <davej@redhat.com>
2013-12-09 13:27:52 -0500

    make msync aware of maps list

--- syscalls/msync.c
+++ syscalls/msync.c
@@ -1,19 +1,24 @@
 /*
  * SYSCALL_DEFINE3(msync, unsigned long, start, size_t, len, int, flags)
  */
+#include "maps.h"
 #include "sanitise.h"
 
 #define MS_ASYNC        1               /* Sync memory asynchronously.  */
 #define MS_SYNC         4               /* Synchronous memory sync.  */
 #define MS_INVALIDATE   2               /* Invalidate the caches.  */
 
+static void sanitise_msync(int childno)
+{
+       (void) common_set_mmap_ptr_len(childno);
+}
+
 struct syscall syscall_msync = {
 	.name = "msync",
 	.num_args = 3,
 	.arg1name = "start",
-	.arg1type = ARG_ADDRESS,
+	.arg1type = ARG_MMAP,
 	.arg2name = "len",
-	.arg2type = ARG_LEN,
 	.arg3name = "flags",
 	.arg3type = ARG_LIST,
 	.arg3list = {
@@ -21,4 +26,5 @@ struct syscall syscall_msync = {
 		.values = { MS_ASYNC, MS_INVALIDATE, MS_SYNC },
 	},
 	.group = GROUP_VM,
+	.sanitise = sanitise_msync,
 };
