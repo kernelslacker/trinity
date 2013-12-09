fd1a80fa89d6ebdb356e209b5ade1565fac43f54
Dave Jones <davej@redhat.com>
2013-12-09 13:13:21 -0500

    make mbind map-list aware

--- syscalls/mbind.c
+++ syscalls/mbind.c
@@ -5,42 +5,46 @@
  */
 
 #include <linux/mempolicy.h>
-#include "utils.h"	// page_size
 #include "arch.h"
+#include "maps.h"
 #include "random.h"
 #include "sanitise.h"
 #include "shm.h"
+#include "utils.h"	// page_size
 
 #define MPOL_F_STATIC_NODES     (1 << 15)
 #define MPOL_F_RELATIVE_NODES   (1 << 14)
 
 static void sanitise_mbind(int childno)
 {
+	struct map *map;
 	unsigned long maxnode;
 
-	shm->a2[childno] &= PAGE_MASK;
+	map = (struct map *) shm->a1[childno];
+	shm->scratch[childno] = (unsigned long) map;    /* Save this for ->post */
+
+	shm->a1[childno] = (unsigned long) map->ptr;
+	shm->a2[childno] = map->size;           //TODO: Munge this.
 
 retry_maxnode:
 	shm->a5[childno] &= ~((page_size * 8) - 1);
 
 	maxnode = shm->a5[childno];
 
-	if (maxnode < 2 || (maxnode) > (page_size * 8)) {
+	if (maxnode < 2 || maxnode > (page_size * 8)) {
 		shm->a5[childno] = rand32();
 		goto retry_maxnode;
 	}
 }
 
 
-
 struct syscall syscall_mbind = {
 	.name = "mbind",
 	.num_args = 6,
 	.arg1name = "start",
-	.arg1type = ARG_ADDRESS,
+	.arg1type = ARG_MMAP,
 
 	.arg2name = "len",
-	.arg2type = ARG_LEN,
 
 	.arg3name = "mode",
 	.arg3type = ARG_LIST,
