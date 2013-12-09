32234b0eac364cc0c4393ea40d0d0b5a4d197a77
Dave Jones <davej@redhat.com>
2013-12-09 13:24:55 -0500

    make mlock aware of maps list

--- syscalls/mlock.c
+++ syscalls/mlock.c
@@ -2,22 +2,21 @@
  * SYSCALL_DEFINE2(mlock, unsigned long, start, size_t, len)
  */
 #include <stdlib.h>
+#include "maps.h"
 #include "shm.h"
 #include "sanitise.h"
 
 static void sanitise_mlock(int childno)
 {
-	if (shm->a2[childno] == 0)
-		shm->a2[childno] = 1;	// must be non-null.
+	(void) common_set_mmap_ptr_len(childno);
 }
 
 struct syscall syscall_mlock = {
 	.name = "mlock",
 	.num_args = 2,
 	.arg1name = "addr",
-	.arg1type = ARG_ADDRESS,
+	.arg1type = ARG_MMAP,
 	.arg2name = "len",
-	.arg2type = ARG_LEN,
 	.group = GROUP_VM,
 	.sanitise = sanitise_mlock,
 };
