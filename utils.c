fe9fdc7a7791dfa60da581e94dd810dd167f77dc
Dave Jones <davej@redhat.com>
2013-12-12 18:27:02 -0500

    add a show_backtrace function for debugging.

--- utils.c
+++ utils.c
@@ -1,7 +1,30 @@
+#include <execinfo.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include "utils.h"
+
+void show_backtrace(void)
+{
+	int j, nptrs;
+#define SIZE 100
+	void *buffer[100];
+	char **strings;
+
+	nptrs = backtrace(buffer, SIZE);
+
+	strings = backtrace_symbols(buffer, nptrs);
+	if (strings == NULL) {
+		perror("backtrace_symbols");
+		exit(EXIT_FAILURE);
+	}
+
+	for (j = 0; j < nptrs; j++)
+		printf("%s\n", strings[j]);
+
+	free(strings);
+}
 
 void * zmalloc(size_t size)
 {
