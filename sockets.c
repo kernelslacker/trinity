70d7ce10d84567388bf4972086f02b660e45d967
Dave Jones <davej@redhat.com>
2014-01-08 19:34:22 -0500

    validate protocol when passing -P and handle accordingly.
    graceful exit means we have to pass failure upwards.

--- sockets.c
+++ sockets.c
@@ -124,7 +124,25 @@ static void unlock_cachefile(int cachefile)
 		output(2, "dropped lock for cachefile\n");
 }
 
-static void generate_sockets(void)
+static unsigned int valid_proto(unsigned int family)
+{
+	const char *famstr;
+
+	famstr = get_proto_name(family);
+
+	if (strncmp(famstr, "PF_UNSPEC", 9) == 0)	/* doesn't make sense */
+		return FALSE;
+
+	if (strncmp(famstr, "PF_BRIDGE", 9) == 0)	/* Not used for creating sockets */
+		return FALSE;
+
+	if (strncmp(famstr, "PF_NETBEUI", 10) == 0)	/* Not actually implemented. */
+		return FALSE;
+
+	return TRUE;
+}
+
+static int generate_sockets(void)
 {
 	int fd, n;
 	int cachefile;
@@ -135,7 +153,7 @@ static void generate_sockets(void)
 	if (cachefile < 0) {
 		outputerr("Couldn't open cachefile for writing! (%s)\n",
 			strerror(errno));
-		exit(EXIT_FAILURE);
+		return FALSE;
 	}
 
 	lock_cachefile(cachefile, F_WRLCK);
@@ -159,7 +177,7 @@ static void generate_sockets(void)
 
 		if (shm->exit_reason != STILL_RUNNING) {
 			close(cachefile);
-			return;
+			return FALSE;
 		}
 
 		for (st.family = 0; st.family < TRINITY_PF_MAX; st.family++) {
@@ -169,6 +187,15 @@ static void generate_sockets(void)
 
 			if (get_proto_name(st.family) == NULL)
 				goto skip;
+
+			if (valid_proto(st.family) == FALSE) {
+				if (do_specific_proto == TRUE) {
+					outputerr("Can't do protocol %s\n", get_proto_name(st.family));
+					return FALSE;
+				} else {
+					goto skip;
+				}
+			}
 
 			BUG_ON(st.family >= ARRAY_SIZE(no_protos));
 			if (no_protos[st.family])
@@ -187,7 +214,7 @@ static void generate_sockets(void)
 				n = write(cachefile, &buffer, sizeof(int) * 3);
 				if (n == -1) {
 					outputerr("something went wrong writing the cachefile!\n");
-					exit(EXIT_FAILURE);
+					return FALSE;
 				}
 
 				if (nr_to_create == 0)
@@ -199,7 +226,7 @@ skip:
 
 			/* check for ctrl-c */
 			if (shm->exit_reason != STILL_RUNNING)
-				return;
+				return FALSE;
 
 			//FIXME: If we've passed -P and we're spinning here without making progress
 			// then we should abort after a few hundred loops.
@@ -212,6 +239,8 @@ done:
 	output(1, "created %d sockets\n", nr_sockets);
 
 	close(cachefile);
+
+	return TRUE;
 }
 
 
@@ -254,19 +283,20 @@ void close_sockets(void)
 	nr_sockets = 0;
 }
 
-void open_sockets(void)
+unsigned int open_sockets(void)
 {
 	int cachefile;
 	unsigned int domain, type, protocol;
 	unsigned int buffer[3];
 	int bytesread=-1;
 	int fd;
+	int ret;
 
 	cachefile = open(cachefilename, O_RDONLY);
 	if (cachefile < 0) {
 		output(1, "Couldn't find socket cachefile. Regenerating.\n");
-		generate_sockets();
-		return;
+		ret = generate_sockets();
+		return ret;
 	}
 
 	lock_cachefile(cachefile, F_RDLCK);
@@ -289,8 +319,8 @@ regenerate:
 				unlock_cachefile(cachefile);	/* drop the reader lock. */
 				close(cachefile);
 				unlink(cachefilename);
-				generate_sockets();
-				return;
+				ret = generate_sockets();
+				return ret;
 		}
 
 		fd = open_socket(domain, type, protocol);
@@ -303,7 +333,7 @@ regenerate:
 		/* check for ctrl-c */
 		if (shm->exit_reason != STILL_RUNNING) {
 			close(cachefile);
-			return;
+			return FALSE;
 		}
 	}
 
@@ -316,5 +346,6 @@ regenerate:
 
 	unlock_cachefile(cachefile);
 	close(cachefile);
-}
 
+	return TRUE;
+}
