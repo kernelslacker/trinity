06da91c42a53fbf11af88028e3e91cafbe852202
Dave Jones <davej@redhat.com>
2014-01-21 11:08:50 -0500

    add sched_setattr for x86-32 too

--- include/syscalls-i386.h
+++ include/syscalls-i386.h
@@ -358,4 +358,5 @@ struct syscalltable syscalls_i386[] = {
 	{ .entry = &syscall_process_vm_writev },
 	{ .entry = &syscall_kcmp },
 	{ .entry = &syscall_finit_module },
+	{ .entry = &syscall_sched_setattr },
 };
