cf5764a20443119e34c77582ff3893409dc97fb3
Dave Jones <davej@redhat.com>
2014-01-21 12:45:53 -0500

    fill out policy field for sched_setscheduler,
    and add new SCHED_DEADLINE

--- syscalls/sched_setscheduler.c
+++ syscalls/sched_setscheduler.c
@@ -1,6 +1,8 @@
 /*
  * SYSCALL_DEFINE3(sched_setscheduler, pid_t, pid, int, policy, struct sched_param __user *, param)
  */
+#include <sched.h>
+#include "compat.h"
 #include "sanitise.h"
 
 struct syscallentry syscall_sched_setscheduler = {
@@ -9,6 +11,11 @@ struct syscallentry syscall_sched_setscheduler = {
 	.arg1name = "pid",
 	.arg1type = ARG_PID,
 	.arg2name = "policy",
+	.arg2type = ARG_OP,
+	.arg2list = {
+		.num = 6,
+		.values = { SCHED_OTHER, SCHED_FIFO, SCHED_RR, SCHED_BATCH, SCHED_IDLE, SCHED_DEADLINE, },
+	},
 	.arg3name = "param",
 	.arg3type = ARG_ADDRESS,
 };
