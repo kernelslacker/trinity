d19e57d0a5cceacefe26d0d35804c9c7930c026a
Dave Jones <davej@redhat.com>
2014-02-11 16:23:06 -0500

    Make mincore operate only on map structs
    And also alloc/free a struct for the vec argument instead of randomly
    scribbling over something.

--- syscalls/mincore.c
+++ syscalls/mincore.c
@@ -1,16 +1,37 @@
 /*
  * SYSCALL_DEFINE3(mincore, unsigned long, start, size_t, len, unsigned char __user *, vec)
  */
+#include <stdlib.h>
+#include "arch.h"
+#include "maps.h"
 #include "sanitise.h"
+#include "shm.h"
+
+static void sanitise_mincore(int childno)
+{
+	struct map *map;
+	unsigned long len;
+
+	map = common_set_mmap_ptr_len(childno);
+
+	len = map->size + (page_size - 1) / page_size;
+	shm->a3[childno] = (unsigned long) malloc(len);
+	shm->scratch[childno] = shm->a3[childno];
+}
+
+static void post_mincore(int childno)
+{
+	free((void *) shm->a3[childno]);
+}
 
 struct syscallentry syscall_mincore = {
 	.name = "mincore",
 	.num_args = 3,
 	.arg1name = "start",
-	.arg1type = ARG_ADDRESS,
+	.arg1type = ARG_MMAP,
 	.arg2name = "len",
-	.arg2type = ARG_LEN,
 	.arg3name = "vec",
-	.arg3type = ARG_ADDRESS,
 	.group = GROUP_VM,
+	.sanitise = sanitise_mincore,
+	.post = post_mincore,
 };
