ba7a9579f33529137f99bcd9a9c575c29408c207
Dave Jones <davej@redhat.com>
2014-02-13 17:44:12 -0500

    annotate option arg for sys_sysfs

--- syscalls/sysfs.c
+++ syscalls/sysfs.c
@@ -7,6 +7,11 @@ struct syscallentry syscall_sysfs = {
 	.name = "sysfs",
 	.num_args = 3,
 	.arg1name = "option",
+	.arg1type = ARG_OP,
+	.arg1list = {
+		.num = 3,
+		.values = { 1, 2, 3 },
+	},
 	.arg2name = "arg1",
 	.arg3name = "arg2",
 };
