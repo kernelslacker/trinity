9d8705b17e9014a2fe4c1820a2a9238a7616a0ee
Dave Jones <davej@redhat.com>
2014-02-18 21:05:36 -0500

    shm: split up create/init

--- shm.c
+++ shm.c
@@ -13,6 +13,8 @@
 #include "random.h"
 #include "shm.h"
 
+struct shm_s *shm;
+
 void * alloc_shared(unsigned int size)
 {
 	void *ret;
@@ -24,41 +26,12 @@ void * alloc_shared(unsigned int size)
 	return ret;
 }
 
-struct shm_s *shm;
-
-#define SHM_PROT_PAGES 30
-
-int create_shm(void)
+static int shm_init(void)
 {
-	void *p;
-	unsigned int shm_pages;
-
-	shm_pages = ((sizeof(struct shm_s) + page_size - 1) & ~(page_size - 1)) / page_size;
-
-	/* Waste some address space to set up some "protection" near the SHM location. */
-	p = alloc_shared((SHM_PROT_PAGES + shm_pages + SHM_PROT_PAGES) * page_size);
-	if (p == NULL) {
-		perror("mmap");
-		return -1;
-	}
-
-	mprotect(p, SHM_PROT_PAGES * page_size, PROT_NONE);
-	mprotect(p + (SHM_PROT_PAGES + shm_pages) * page_size,
-			SHM_PROT_PAGES * page_size, PROT_NONE);
-
-	shm = p + SHM_PROT_PAGES * page_size;
-	memset(shm, 0, sizeof(struct shm_s));
-
-	output(2, "shm is at %p\n", shm);
-
 	shm->total_syscalls_done = 1;
-	shm->regenerate = 0;
 
 	memset(shm->pids, EMPTY_PIDSLOT, sizeof(shm->pids));
 
-	shm->nr_active_syscalls = 0;
-	shm->nr_active_32bit_syscalls = 0;
-	shm->nr_active_64bit_syscalls = 0;
 	memset(shm->active_syscalls, 0, sizeof(shm->active_syscalls));
 	memset(shm->active_syscalls32, 0, sizeof(shm->active_syscalls32));
 	memset(shm->active_syscalls64, 0, sizeof(shm->active_syscalls64));
@@ -68,8 +41,40 @@ int create_shm(void)
 
 	/* Set seed in parent thread */
 	set_seed(0);
-
 	return 0;
+}
+
+#define SHM_PROT_PAGES 30
+
+int create_shm(void)
+{
+	void *p;
+	unsigned int shm_pages;
+	int ret;
+
+	/* round up shm to nearest page size */
+	shm_pages = ((sizeof(struct shm_s) + page_size - 1) & ~(page_size - 1)) / page_size;
+
+	/* Waste some address space to set up some "protection" near the SHM location. */
+	p = alloc_shared((SHM_PROT_PAGES + shm_pages + SHM_PROT_PAGES) * page_size);
+	if (p == NULL) {
+		perror("mmap");
+		return -1;
+	}
+
+	/* clear whole mapping, including the redzones. */
+	memset(p, 0, shm_pages * page_size);
+
+	/* set the redzones to PROT_NONE */
+	mprotect(p, SHM_PROT_PAGES * page_size, PROT_NONE);
+	mprotect(p + (SHM_PROT_PAGES + shm_pages) * page_size,
+			SHM_PROT_PAGES * page_size, PROT_NONE);
+
+	shm = p + SHM_PROT_PAGES * page_size;
+	output(2, "shm is at %p\n", shm);
+
+	ret = shm_init();
+	return ret;
 }
 
 void setup_shm_postargs(void)
