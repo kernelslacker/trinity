ebae6a3b3ace9dfbe0ef74bb064689846bfdbc2b
Dave Jones <davej@redhat.com>
2014-02-21 16:46:36 -0500

    update paths in analyse-dumps.sh
    also mask out childno to make hashing work better.

--- scripts/analyse-dumps.sh
+++ scripts/analyse-dumps.sh
@@ -2,10 +2,10 @@
 
 TRINITY_PATH=${TRINITY_PATH:-.}
 
-for core in $(find . -name "core.*")
+for core in tmp/trinity.*/tmp/core.*
 do
   gdb -batch -n -ex 'bt' $TRINITY_PATH/trinity $core > core.txt
-  SHA=$(cat core.txt|  grep -v New\ LWP | sha1sum)
+  SHA=$(cat core.txt| grep -v New\ LWP | grep -v childno | sha1sum | awk '{ print $1 }')
   cat core.txt > core-$SHA.txt
   rm -f core.txt
 done
