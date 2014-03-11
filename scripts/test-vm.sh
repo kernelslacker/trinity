b20ef2eb1b835b41d56f0892ab3edf2ad033262e
Dave Jones <davej@redhat.com>
2014-03-11 13:54:03 -0400

    make test-vm cycle through mmap + 1 other VM syscall.

--- scripts/test-vm.sh
+++ scripts/test-vm.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+OLDPATH=$(pwd)
+TRINITY_PATH=${TRINITY_PATH:-$OLDPATH}
+
+if [ -d tmp ]; then
+  TRINITY_TMP=$(mktemp -d $(pwd)/tmp/trinity.XXXXXX)
+else
+  TRINITY_TMP=$(mktemp -d /tmp/trinity.XXXXXX)
+fi
+
 TRINITY_PATH=${TRINITY_PATH:-.}
 TRINITY_TMP=$(mktemp -d /tmp/trinity.XXXXXX)
+
+check_tainted()
+{
+    if [ "$(cat /proc/sys/kernel/tainted)" != $TAINT ]; then
+      echo ERROR: Taint flag changed $(cat /proc/sys/kernel/tainted)
+      exit
+    fi
+}
+
+TAINT=$(cat /proc/sys/kernel/tainted)
 
 chmod 755 $TRINITY_TMP
 cd $TRINITY_TMP
@@ -11,14 +30,24 @@ NR_PROCESSES=$(($NR_CPUS * 2))
 
 while [ 1 ];
 do
-  RND=$RANDOM
-  mkdir tmp.$RND
-  cd tmp.$RND
-  for i in `seq 1 $NR_PROCESSES`
+  for syscall in madvise mbind migrate_pages mincore mlockall mlock move_pages mprotect mremap msync munlockall munlock munmap remap_file_pages vmsplice
   do
-	$TRINITY_PATH/trinity -q -g vm &
+	echo testing mmap + $syscall
+	chmod 755 $TRINITY_TMP
+	pushd $TRINITY_TMP > /dev/null
+
+	if [ ! -f $TRINITY_PATH/trinity ]; then
+		echo lost!
+		pwd
+		exit
+	fi
+
+	MALLOC_CHECK_=2 $TRINITY_PATH/trinity -q -l off -c mmap -c $syscall -N 99999 -C 64
+	popd > /dev/null
+
+	check_tainted
+	echo
+	echo
   done
-  wait
-  cd ..
-  rm -rf tmp.$RND
+  check_tainted
 done
