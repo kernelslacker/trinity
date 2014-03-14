78a148927823dc435d58e750da3ec3243407829e
Dave Jones <davej@redhat.com>
2014-03-13 20:25:38 -0400

    Add an owner pid to a lock so we can figure out who owns it.

--- include/locks.h
+++ include/locks.h
@@ -1,9 +1,14 @@
 #pragma once
 
-typedef volatile unsigned char lock_t;
+struct lock_struct {
+	volatile unsigned char lock;
+	pid_t owner;
+};
+
+typedef struct lock_struct lock_t;
 
 #define UNLOCKED 0
 #define LOCKED 1
 
-void lock(lock_t *lock);
-void unlock(lock_t *lock);
+void lock(lock_t *_lock);
+void unlock(lock_t *_lock);
