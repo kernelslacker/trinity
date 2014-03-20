05c6debd2264b7be8b0e1356091a0cab6ca02347
Dave Jones <davej@redhat.com>
2014-03-20 16:47:14 -0400

    make send() use the same flags as sendto()
    The kernel treats them both as the same function.

--- syscalls/send.c
+++ syscalls/send.c
@@ -3,6 +3,7 @@
                 unsigned, flags)
  */
 #include <sys/socket.h>
+#include "compat.h"
 #include "sanitise.h"
 
 struct syscallentry syscall_send = {
@@ -16,9 +17,13 @@ struct syscallentry syscall_send = {
 	.arg3type = ARG_LEN,
 	.arg4name = "flags",
         .arg4type = ARG_LIST,
-        .arg4list = {
-                .num = 7,
-                .values = { MSG_CONFIRM, MSG_DONTROUTE, MSG_DONTWAIT,
-			    MSG_EOR, MSG_MORE, MSG_NOSIGNAL, MSG_OOB },
+	.arg4list = {
+		.num = 20,
+		.values = { MSG_OOB, MSG_PEEK, MSG_DONTROUTE, MSG_CTRUNC,
+			MSG_PROBE, MSG_TRUNC, MSG_DONTWAIT, MSG_EOR,
+			MSG_WAITALL, MSG_FIN, MSG_SYN, MSG_CONFIRM,
+			MSG_RST, MSG_ERRQUEUE, MSG_NOSIGNAL, MSG_MORE,
+			MSG_WAITFORONE, MSG_FASTOPEN, MSG_CMSG_CLOEXEC, MSG_CMSG_COMPAT,
+		},
 	},
 };
