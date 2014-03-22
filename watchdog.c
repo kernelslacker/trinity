2d1c3985479f7f18764e4186e713cccc8b9e5034
Dave Jones <davej@redhat.com>
2014-03-22 17:54:20 -0400

    When we detect a taint, record the time it happened.
    This will be useful for comparing child timestamps when debugging.

--- watchdog.c
+++ watchdog.c
@@ -379,6 +379,8 @@ static void watchdog(void)
 		if (kernel_taint_mask != 0) {
 			ret = check_tainted();
 			if (((ret & kernel_taint_mask) & (~kernel_taint_initial)) != 0) {
+				gettimeofday(&shm->taint_tv, NULL);
+
 				output(0, "kernel became tainted! (%d/%d) Last seed was %u\n", ret, kernel_taint_initial, shm->seed);
 				shm->exit_reason = EXIT_KERNEL_TAINTED;
 			}
