06db7b35b4f631be34f7a99bb5ff560123288d04
Dave Jones <davej@redhat.com>
2014-03-23 17:26:40 -0400

    merge stat variants

--- syscalls/stat.c
+++ syscalls/stat.c
@@ -11,3 +11,18 @@ struct syscallentry syscall_stat = {
 	.arg2name = "statbuf",
 	.arg2type = ARG_ADDRESS,
 };
+
+
+/*
+ * SYSCALL_DEFINE2(stat64, const char __user *, filename,
+                 struct stat64 __user *, statbuf)
+ */
+
+struct syscallentry syscall_stat64 = {
+	.name = "stat64",
+	.num_args = 2,
+	.arg1name = "filename",
+	.arg1type = ARG_PATHNAME,
+	.arg2name = "statbuf",
+	.arg2type = ARG_ADDRESS,
+};
