833d7b41d345c1ce9e3762353bc2ae552c8f30f3
Dave Jones <davej@redhat.com>
2014-03-23 17:47:20 -0400

    merge symlink variants

--- syscalls/symlink.c
+++ syscalls/symlink.c
@@ -12,3 +12,21 @@ struct syscallentry syscall_symlink = {
 	.arg2type = ARG_PATHNAME,
 	.group = GROUP_VFS,
 };
+
+/*
+ * SYSCALL_DEFINE3(symlinkat, const char __user *, oldname,
+	 int, newdfd, const char __user *, newname)
+ */
+
+struct syscallentry syscall_symlinkat = {
+	.name = "symlinkat",
+	.num_args = 3,
+	.arg1name = "oldname",
+	.arg1type = ARG_PATHNAME,
+	.arg2name = "newdfd",
+	.arg2type = ARG_FD,
+	.arg3name = "newname",
+	.arg3type = ARG_PATHNAME,
+	.flags = NEED_ALARM,
+	.group = GROUP_VFS,
+};
