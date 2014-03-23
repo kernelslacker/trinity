dc27a5fbb38f1c8f888b9990abde061b360720db
Dave Jones <davej@redhat.com>
2014-03-23 17:33:44 -0400

    merge getresgid variants

--- syscalls/getresgid.c
+++ syscalls/getresgid.c
@@ -13,3 +13,20 @@ struct syscallentry syscall_getresgid = {
 	.arg3name = "sgid",
 	.arg3type = ARG_ADDRESS,
 };
+
+
+/*
+ * SYSCALL_DEFINE3(getresgid16, old_gid_t __user *, rgid, old_gid_t __user *, egid, old_gid_t __user *, sgid)
+ */
+#include "sanitise.h"
+
+struct syscallentry syscall_getresgid16 = {
+	.name = "getresgid16",
+	.num_args = 3,
+	.arg1name = "rgid",
+	.arg1type = ARG_ADDRESS,
+	.arg2name = "egid",
+	.arg2type = ARG_ADDRESS,
+	.arg3name = "sgid",
+	.arg3type = ARG_ADDRESS,
+};
