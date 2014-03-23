1be4d28a7a546a080ca618c84743e84e47e3d568
Dave Jones <davej@redhat.com>
2014-03-23 17:44:09 -0400

    merge mknod variants

--- syscalls/mknod.c
+++ syscalls/mknod.c
@@ -13,3 +13,21 @@ struct syscallentry syscall_mknod = {
 	.arg3name = "dev",
 	.group = GROUP_VFS,
 };
+
+
+/*
+ * SYSCALL_DEFINE4(mknodat, int, dfd, const char __user *, filename, int, mode, unsigned, dev)
+ */
+
+struct syscallentry syscall_mknodat = {
+	.name = "mknodat",
+	.num_args = 4,
+	.arg1name = "dfd",
+	.arg1type = ARG_FD,
+	.arg2name = "filename",
+	.arg2type = ARG_PATHNAME,
+	.arg3name = "mode",
+	.arg4name = "dev",
+	.flags = NEED_ALARM,
+	.group = GROUP_VFS,
+};
