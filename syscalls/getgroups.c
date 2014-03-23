3a873e2a0bedc5f3d68fb960c20b64ccad4c9147
Dave Jones <davej@redhat.com>
2014-03-23 17:33:13 -0400

    merge getgroups variants

--- syscalls/getgroups.c
+++ syscalls/getgroups.c
@@ -11,3 +11,17 @@ struct syscallentry syscall_getgroups = {
 	.arg2name = "grouplist",
 	.rettype = RET_BORING,
 };
+
+
+/*
+ * SYSCALL_DEFINE2(getgroups16, int, gidsetsize, old_gid_t __user *, grouplist)
+ */
+
+struct syscallentry syscall_getgroups16 = {
+	.name = "getgroups16",
+	.num_args = 2,
+	.arg1name = "gidsetsize",
+	.arg2type = ARG_ADDRESS,
+	.arg2name = "grouplist",
+	.rettype = RET_BORING,
+};
