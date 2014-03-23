ecbdee368d0e3040d3fabf6133de8a5e9a2c6aa4
Dave Jones <davej@redhat.com>
2014-03-23 17:46:52 -0400

    merge rename variants

--- syscalls/rename.c
+++ syscalls/rename.c
@@ -12,3 +12,23 @@ struct syscallentry syscall_rename = {
 	.arg2type = ARG_ADDRESS,
 	.group = GROUP_VFS,
 };
+
+/*
+ * SYSCALL_DEFINE4(renameat, int, olddfd, const char __user *, oldname,
+	 int, newdfd, const char __user *, newname)
+ */
+
+struct syscallentry syscall_renameat = {
+	.name = "renameat",
+	.num_args = 4,
+	.arg1name = "olddfd",
+	.arg1type = ARG_FD,
+	.arg2name = "oldname",
+	.arg2type = ARG_ADDRESS,
+	.arg3name = "newdfd",
+	.arg3type = ARG_FD,
+	.arg4name = "newname",
+	.arg4type = ARG_ADDRESS,
+	.flags = NEED_ALARM,
+	.group = GROUP_VFS,
+};
