6e81545911fb2b723ab1fc3a97143338c80572de
Dave Jones <davej@redhat.com>
2014-03-23 17:22:50 -0400

    merge ftruncate variants

--- syscalls/ftruncate.c
+++ syscalls/ftruncate.c
@@ -14,3 +14,23 @@ struct syscallentry syscall_ftruncate = {
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
 };
+
+
+/*
+ * SYSCALL_DEFINE(ftruncate64)(unsigned int fd, loff_t length)
+ *
+ * On success, zero is returned.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+struct syscallentry syscall_ftruncate64 = {
+	.name = "ftruncate64",
+	.num_args = 2,
+	.arg1name = "fd",
+	.arg1type = ARG_FD,
+	.arg2name = "length",
+	.arg2type = ARG_LEN,
+	.rettype = RET_ZERO_SUCCESS,
+	.flags = NEED_ALARM,
+	.group = GROUP_VFS,
+};
