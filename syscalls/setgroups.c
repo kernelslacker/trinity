3a7073c9502df4dfe946b82439c0b38a8da72529
Dave Jones <davej@redhat.com>
2014-03-23 17:37:35 -0400

    merge setgroups variants

--- syscalls/setgroups.c
+++ syscalls/setgroups.c
@@ -10,3 +10,16 @@ struct syscallentry syscall_setgroups = {
 	.arg2name = "grouplist",
 	.arg2type = ARG_ADDRESS,
 };
+
+
+/*
+ * SYSCALL_DEFINE2(getgroups16, int, gidsetsize, old_gid_t __user *, grouplist)
+ */
+
+struct syscallentry syscall_setgroups16 = {
+	.name = "setgroups16",
+	.num_args = 2,
+	.arg1name = "gidsetsize",
+	.arg2name = "grouplist",
+	.arg2type = ARG_ADDRESS,
+};
