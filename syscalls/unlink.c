59dea8ea3a79de469add0683ffc4ce0b123ba034
Dave Jones <davej@redhat.com>
2014-03-23 17:47:53 -0400

    merge unlink variants

--- syscalls/unlink.c
+++ syscalls/unlink.c
@@ -10,3 +10,19 @@ struct syscallentry syscall_unlink = {
 	.arg1type = ARG_PATHNAME,
 	.group = GROUP_VFS,
 };
+
+/*
+ * SYSCALL_DEFINE3(unlinkat, int, dfd, const char __user *, pathname, int, flag)
+ */
+
+struct syscallentry syscall_unlinkat = {
+	.name = "unlinkat",
+	.num_args = 3,
+	.arg1name = "dfd",
+	.arg1type = ARG_FD,
+	.arg2name = "pathname",
+	.arg2type = ARG_PATHNAME,
+	.arg3name = "flag",
+	.flags = NEED_ALARM,
+	.group = GROUP_VFS,
+};
