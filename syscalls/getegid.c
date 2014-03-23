66d242f569ca99bd1d274671fcbe1cac41922681
Dave Jones <davej@redhat.com>
2014-03-23 17:31:37 -0400

    merge getegid variants

--- syscalls/getegid.c
+++ syscalls/getegid.c
@@ -8,3 +8,13 @@ struct syscallentry syscall_getegid = {
 	.num_args = 0,
 	.rettype = RET_GID_T,
 };
+
+/*
+ * SYSCALL_DEFINE0(getegid16)
+ */
+
+struct syscallentry syscall_getegid16 = {
+	.name = "getegid16",
+	.num_args = 0,
+	.rettype = RET_GID_T,
+};
