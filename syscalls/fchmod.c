e4bebf2d2223fefb23839d733398d065fc073f3f
Dave Jones <davej@redhat.com>
2014-03-23 17:29:18 -0400

    merge fchmod variants

--- syscalls/fchmod.c
+++ syscalls/fchmod.c
@@ -17,3 +17,25 @@ struct syscallentry syscall_fchmod = {
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
 };
+
+
+/*
+ * SYSCALL_DEFINE3(fchmodat, int, dfd, const char __user *, filename, mode_t, mode)
+ *
+ * On success, fchmodat() returns 0.
+ * On error, -1 is returned and errno is set to indicate the error.
+ */
+
+struct syscallentry syscall_fchmodat = {
+	.name = "fchmodat",
+	.num_args = 3,
+	.arg1name = "dfd",
+	.arg1type = ARG_FD,
+	.arg2name = "filename",
+	.arg2type = ARG_PATHNAME,
+	.arg3name = "mode",
+	.arg3type = ARG_MODE_T,
+	.rettype = RET_ZERO_SUCCESS,
+	.flags = NEED_ALARM,
+	.group = GROUP_VFS,
+};
