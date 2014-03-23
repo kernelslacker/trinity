ff8cd6dd8ff5a2ac659216a62326a4554848a032
Dave Jones <davej@redhat.com>
2014-03-23 17:38:29 -0400

    merge setresgid variants

--- syscalls/setresgid.c
+++ syscalls/setresgid.c
@@ -10,3 +10,16 @@ struct syscallentry syscall_setresgid = {
 	.arg2name = "egid",
 	.arg3name = "sgid",
 };
+
+
+/*
+ * SYSCALL_DEFINE3(setresgid16, old_gid_t, rgid, old_gid_t, egid, old_gid_t, sgid)
+ */
+
+struct syscallentry syscall_setresgid16 = {
+	.name = "setresgid16",
+	.num_args = 3,
+	.arg1name = "rgid",
+	.arg2name = "egid",
+	.arg3name = "sgid",
+};
