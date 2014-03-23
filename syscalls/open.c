bc566a764c7c1c24bcf7fab6e2bfa099cbddca3b
Dave Jones <davej@redhat.com>
2014-03-23 17:46:01 -0400

    merge open variants

--- syscalls/open.c
+++ syscalls/open.c
@@ -27,3 +27,32 @@ struct syscallentry syscall_open = {
 	.arg3name = "mode",
 	.arg3type = ARG_MODE_T,
 };
+
+
+/*
+ * SYSCALL_DEFINE4(openat, int, dfd, const char __user *, filename, int, flags, int, mode)
+ */
+
+struct syscallentry syscall_openat = {
+	.name = "openat",
+	.num_args = 4,
+	.arg1name = "dfd",
+	.arg1type = ARG_FD,
+	.arg2name = "filename",
+	.arg2type = ARG_PATHNAME,
+	.arg3name = "flags",
+	.arg3type = ARG_LIST,
+	.arg3list = {
+		.num = 20,
+		.values = { O_RDONLY, O_WRONLY, O_RDWR,
+				O_CREAT, O_EXCL, O_NOCTTY,
+				O_TRUNC, O_APPEND, O_NONBLOCK,
+				O_SYNC, O_ASYNC,
+				O_DIRECTORY, O_NOFOLLOW, O_CLOEXEC,
+				O_DIRECT, O_NOATIME, O_PATH,
+				O_DSYNC, O_LARGEFILE, O_TMPFILE },
+	},
+	.arg4name = "mode",
+	.arg4type = ARG_MODE_T,
+	.flags = NEED_ALARM,
+};
