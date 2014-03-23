8a47c0c3a2b16c4b30850673e2afcbc87b98e59a
Dave Jones <davej@redhat.com>
2014-03-23 17:37:07 -0400

    merge setgid variants

--- syscalls/setgid.c
+++ syscalls/setgid.c
@@ -8,3 +8,14 @@ struct syscallentry syscall_setgid = {
 	.num_args = 1,
 	.arg1name = "gid",
 };
+
+
+/*
+ * SYSCALL_DEFINE1(setgid16, old_gid_t, gid)
+ */
+
+struct syscallentry syscall_setgid16 = {
+	.name = "setgid16",
+	.num_args = 1,
+	.arg1name = "gid",
+};
