c5d2ffee0ee87cf1c2a28e00c8a48669e3b54bea
Dave Jones <davej@redhat.com>
2014-03-23 17:27:34 -0400

    merge truncate variants

--- syscalls/truncate.c
+++ syscalls/truncate.c
@@ -12,3 +12,17 @@ struct syscallentry syscall_truncate = {
 	.arg2type = ARG_LEN,
 	.group = GROUP_VFS,
 };
+
+/*
+ * SYSCALL_DEFINE(truncate64)(const char __user * path, loff_t length)
+ */
+
+struct syscallentry syscall_truncate64 = {
+	.name = "truncate64",
+	.num_args = 2,
+	.arg1name = "path",
+	.arg1type = ARG_PATHNAME,
+	.arg2name = "length",
+	.arg2type = ARG_LEN,
+	.group = GROUP_VFS,
+};
