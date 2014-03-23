a8cc8dd742eaffb79d84d5fa3adafffce9d6d8ed
Dave Jones <davej@redhat.com>
2014-03-23 17:34:32 -0400

    merge getuid variants

--- syscalls/getuid.c
+++ syscalls/getuid.c
@@ -7,3 +7,13 @@ struct syscallentry syscall_getuid = {
 	.name = "getuid",
 	.num_args = 0,
 };
+
+
+/*
+ * SYSCALL_DEFINE0(getuid16)
+ */
+
+struct syscallentry syscall_getuid16 = {
+	.name = "getuid16",
+	.num_args = 0,
+};
--- syscalls/getuid16.c
+++
@@ -1,9 +0,0 @@
-/*
- * SYSCALL_DEFINE0(getuid16)
- */
-#include "sanitise.h"
-
-struct syscallentry syscall_getuid16 = {
-	.name = "getuid16",
-	.num_args = 0,
-};
