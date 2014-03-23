0619055a92b8e10811aea8c90ed6baaa1d4b0dee
Dave Jones <davej@redhat.com>
2014-03-23 17:35:04 -0400

    merge lchown variants

--- syscalls/lchown.c
+++ syscalls/lchown.c
@@ -12,3 +12,17 @@ struct syscallentry syscall_lchown = {
 	.arg3name = "group",
 	.group = GROUP_VFS,
 };
+
+/*
+ * SYSCALL_DEFINE3(lchown16, const char __user *, filename, old_uid_t, user, old_gid_t, group)
+ */
+
+struct syscallentry syscall_lchown16 = {
+	.name = "lchown16",
+	.num_args = 3,
+	.arg1name = "filename",
+	.arg1type = ARG_PATHNAME,
+	.arg2name = "user",
+	.arg3name = "group",
+	.group = GROUP_VFS,
+};
