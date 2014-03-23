cb47c094bdd7d683965b88d38401fdfd09659049
Dave Jones <davej@redhat.com>
2014-03-23 17:24:19 -0400

    merge lstat variants

--- syscalls/lstat.c
+++ syscalls/lstat.c
@@ -13,3 +13,19 @@ struct syscallentry syscall_lstat = {
 	.arg2type = ARG_ADDRESS,
 	.group = GROUP_VFS,
 };
+
+
+/*
+ * SYSCALL_DEFINE2(lstat64, const char __user *, filename,
+                 struct stat64 __user *, statbuf)
+ */
+
+struct syscallentry syscall_lstat64 = {
+	.name = "lstat64",
+	.num_args = 2,
+	.arg1name = "filename",
+	.arg1type = ARG_PATHNAME,
+	.arg2name = "statbuf",
+	.arg2type = ARG_ADDRESS,
+	.group = GROUP_VFS,
+};
