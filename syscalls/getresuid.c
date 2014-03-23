b6257e2ac325ebe9edb06b252b800a58101786e0
Dave Jones <davej@redhat.com>
2014-03-23 17:34:07 -0400

    merge getresuid variants

--- syscalls/getresuid.c
+++ syscalls/getresuid.c
@@ -13,3 +13,18 @@ struct syscallentry syscall_getresuid = {
 	.arg3name = "suid",
 	.arg3type = ARG_ADDRESS,
 };
+
+/*
+ * SYSCALL_DEFINE3(getresuid16, old_uid_t __user *, ruid, old_uid_t __user *, euid, old_uid_t __user *, suid)
+ */
+
+struct syscallentry syscall_getresuid16 = {
+	.name = "getresuid16",
+	.num_args = 3,
+	.arg1name = "ruid",
+	.arg1type = ARG_ADDRESS,
+	.arg2name = "euid",
+	.arg2type = ARG_ADDRESS,
+	.arg3name = "suid",
+	.arg3type = ARG_ADDRESS,
+};
