229cbc50433a5a7de0eddd9a8e1c1e7f4b90e495
Dave Jones <davej@redhat.com>
2014-03-23 17:39:17 -0400

    merge setreuid variants

--- syscalls/setreuid.c
+++ syscalls/setreuid.c
@@ -9,3 +9,15 @@ struct syscallentry syscall_setreuid = {
 	.arg1name = "ruid",
 	.arg2name = "euid",
 };
+
+
+/*
+ * SYSCALL_DEFINE2(setreuid16, old_uid_t, ruid, old_uid_t, euid)
+ */
+
+struct syscallentry syscall_setreuid16 = {
+	.name = "setreuid16",
+	.num_args = 2,
+	.arg1name = "ruid",
+	.arg2name = "euid",
+};
