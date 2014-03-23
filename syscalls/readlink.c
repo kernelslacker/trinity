7334c73eb304f9ac69e7c0b4164593f1dd713fe4
Dave Jones <davej@redhat.com>
2014-03-23 17:46:34 -0400

    merge readlink variants

--- syscalls/readlink.c
+++ syscalls/readlink.c
@@ -14,3 +14,24 @@ struct syscallentry syscall_readlink = {
 	.arg3type = ARG_LEN,
 	.group = GROUP_VFS,
 };
+
+
+/*
+ * SYSCALL_DEFINE4(readlinkat, int, dfd, const char __user *, pathname,
+	 char __user *, buf, int, bufsiz)
+ */
+
+struct syscallentry syscall_readlinkat = {
+	.name = "readlinkat",
+	.num_args = 4,
+	.arg1name = "dfd",
+	.arg1type = ARG_FD,
+	.arg2name = "pathname",
+	.arg2type = ARG_PATHNAME,
+	.arg3name = "buf",
+	.arg3type = ARG_ADDRESS,
+	.arg4name = "bufsiz",
+	.arg4type = ARG_LEN,
+	.flags = NEED_ALARM,
+	.group = GROUP_VFS,
+};
