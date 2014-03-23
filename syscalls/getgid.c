3bf4b21c3fde49ed2eb0ab1e519b8245047c7ac4
Dave Jones <davej@redhat.com>
2014-03-23 17:32:49 -0400

    merge getgid variants

--- syscalls/getgid.c
+++ syscalls/getgid.c
@@ -8,3 +8,14 @@ struct syscallentry syscall_getgid = {
 	.num_args = 0,
 	.rettype = RET_GID_T,
 };
+
+
+/*
+ * SYSCALL_DEFINE0(getgid)
+ */
+
+struct syscallentry syscall_getgid16 = {
+	.name = "getgid16",
+	.num_args = 0,
+	.rettype = RET_GID_T,
+};
