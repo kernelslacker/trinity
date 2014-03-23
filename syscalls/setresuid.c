f640266a08c07dcb51801590fbb8c45a2a5db02b
Dave Jones <davej@redhat.com>
2014-03-23 17:38:54 -0400

    merge setresuid variants

--- syscalls/setresuid.c
+++ syscalls/setresuid.c
@@ -10,3 +10,15 @@ struct syscallentry syscall_setresuid = {
 	.arg2name = "euid",
 	.arg3name = "suid",
 };
+
+/*
+ * SYSCALL_DEFINE3(setresuid16, old_uid_t, ruid, old_uid_t, euid, old_uid_t, suid)
+ */
+
+struct syscallentry syscall_setresuid16 = {
+	.name = "setresuid16",
+	.num_args = 3,
+	.arg1name = "ruid",
+	.arg2name = "euid",
+	.arg3name = "suid",
+};
