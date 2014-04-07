3dabc672182decad3e93e389ee5c3e6044ed0e98
Dave Jones <davej@redhat.com>
2014-04-07 11:14:38 -0400

    add new renameat2 syscall

--- syscalls/rename.c
+++ syscalls/rename.c
@@ -32,3 +32,32 @@ struct syscallentry syscall_renameat = {
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
 };
+
+/*
+ * SYSCALL_DEFINE5(renameat2, int, olddfd, const char __user *, oldname,
+		   int, newdfd, const char __user *, newname, unsigned int, flags)
+ */
+
+#define RENAME_NOREPLACE        (1 << 0)        /* Don't overwrite target */
+#define RENAME_EXCHANGE         (1 << 1)        /* Exchange source and dest */
+
+struct syscallentry syscall_renameat2 = {
+	.name = "renameat2",
+	.num_args = 5,
+	.arg1name = "olddfd",
+	.arg1type = ARG_FD,
+	.arg2name = "oldname",
+	.arg2type = ARG_ADDRESS,
+	.arg3name = "newdfd",
+	.arg3type = ARG_FD,
+	.arg4name = "newname",
+	.arg4type = ARG_ADDRESS,
+	.arg5name = "flags",
+	.arg5type = ARG_LIST,
+	.arg5list = {
+		.num = 2,
+		.values = { RENAME_NOREPLACE, RENAME_EXCHANGE },
+	},
+	.flags = NEED_ALARM,
+	.group = GROUP_VFS,
+};
