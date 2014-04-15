ec12189f43f1fc473f4b9e1783820e668ddd9f76
Dave Jones <davej@redhat.com>
2014-04-15 17:15:42 -0400

    divide open flags into ARG_OP stuff and ARG_LIST stuff.
    O_RDONLY and O_WRONLY etc don't make any sense together.
    By splitting out the not-exclusive stuff we could later change
    things up so that check_stat_file() can OR in random flags
    during the initial fd opening.
    
    (and any other places we end up doing fd opening)

--- syscalls/open.c
+++ syscalls/open.c
@@ -4,8 +4,43 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include "sanitise.h"
+#include "shm.h"
+#include "utils.h"
 #include "compat.h"
+
+static const unsigned long o_flags[] = {
+	O_EXCL, O_NOCTTY, O_TRUNC, O_APPEND,
+	O_NONBLOCK, O_SYNC, O_ASYNC, O_DIRECTORY,
+	O_NOFOLLOW, O_CLOEXEC, O_DIRECT, O_NOATIME,
+	O_PATH, O_DSYNC, O_LARGEFILE, O_TMPFILE,
+};
+
+static unsigned long get_o_flags(void)
+{
+	unsigned long mask = 0;
+	unsigned long bits;
+	unsigned int i, num;
+
+	num = ARRAY_SIZE(o_flags);
+
+	bits = rand() % (num + 1);      /* num of bits to OR */
+
+	for (i = 0; i < bits; i++)
+		mask |= o_flags[rand() % num];
+
+	return mask;
+}
+
+static void sanitise_open(int childno)
+{
+	unsigned long flags;
+
+	flags = get_o_flags();
+
+	shm->syscall[childno].a2 |= flags;
+}
 
 struct syscallentry syscall_open = {
 	.name = "open",
@@ -13,26 +48,28 @@ struct syscallentry syscall_open = {
 	.arg1name = "filename",
 	.arg1type = ARG_PATHNAME,
 	.arg2name = "flags",
-	.arg2type = ARG_LIST,
+	.arg2type = ARG_OP,
 	.arg2list = {
-		.num = 20,
-		.values = { O_RDONLY, O_WRONLY, O_RDWR,
-				O_CREAT, O_EXCL, O_NOCTTY,
-				O_TRUNC, O_APPEND, O_NONBLOCK,
-				O_SYNC, O_ASYNC,
-				O_DIRECTORY, O_NOFOLLOW, O_CLOEXEC,
-				O_DIRECT, O_NOATIME, O_PATH,
-				O_DSYNC, O_LARGEFILE, O_TMPFILE },
+		.num = 4,
+		.values = { O_RDONLY, O_WRONLY, O_RDWR, O_CREAT, },
 	},
 	.arg3name = "mode",
 	.arg3type = ARG_MODE_T,
+	.sanitise = sanitise_open,
 };
 
+static void sanitise_openat(int childno)
+{
+	unsigned long flags;
+
+	flags = get_o_flags();
+
+	shm->syscall[childno].a3 |= flags;
+}
 
 /*
  * SYSCALL_DEFINE4(openat, int, dfd, const char __user *, filename, int, flags, int, mode)
  */
-
 struct syscallentry syscall_openat = {
 	.name = "openat",
 	.num_args = 4,
@@ -41,18 +78,13 @@ struct syscallentry syscall_openat = {
 	.arg2name = "filename",
 	.arg2type = ARG_PATHNAME,
 	.arg3name = "flags",
-	.arg3type = ARG_LIST,
+	.arg3type = ARG_OP,
 	.arg3list = {
-		.num = 20,
-		.values = { O_RDONLY, O_WRONLY, O_RDWR,
-				O_CREAT, O_EXCL, O_NOCTTY,
-				O_TRUNC, O_APPEND, O_NONBLOCK,
-				O_SYNC, O_ASYNC,
-				O_DIRECTORY, O_NOFOLLOW, O_CLOEXEC,
-				O_DIRECT, O_NOATIME, O_PATH,
-				O_DSYNC, O_LARGEFILE, O_TMPFILE },
+		.num = 4,
+		.values = { O_RDONLY, O_WRONLY, O_RDWR, O_CREAT, },
 	},
 	.arg4name = "mode",
 	.arg4type = ARG_MODE_T,
 	.flags = NEED_ALARM,
+	.sanitise = sanitise_openat,
 };
