6489c80e9ac2fa9a12ad9b1df0bead4f2963f5d4
Dave Jones <davej@redhat.com>
2014-04-15 16:50:03 -0400

    merge recvmsg into recv

--- syscalls/recv.c
+++ syscalls/recv.c
@@ -64,6 +64,29 @@ struct syscallentry syscall_recvfrom = {
 
 
 /*
+ * SYSCALL_DEFINE3(recvmsg, int, fd, struct msghdr __user *, msg, unsigned int, flags)
+ */
+struct syscallentry syscall_recvmsg = {
+	.name = "recvmsg",
+	.num_args = 3,
+	.arg1name = "fd",
+	.arg1type = ARG_FD,
+	.arg2name = "msg",
+	.arg2type = ARG_ADDRESS,
+	.arg3name = "flags",
+	.arg3type = ARG_LIST,
+	.arg3list = {
+		.num = 20,
+		.values = { MSG_OOB, MSG_PEEK, MSG_DONTROUTE, MSG_CTRUNC,
+			    MSG_PROBE, MSG_TRUNC, MSG_DONTWAIT, MSG_EOR,
+			    MSG_WAITALL, MSG_FIN, MSG_SYN, MSG_CONFIRM,
+			    MSG_RST, MSG_ERRQUEUE, MSG_NOSIGNAL, MSG_MORE,
+			    MSG_WAITFORONE, MSG_CMSG_CLOEXEC, MSG_FASTOPEN, MSG_CMSG_COMPAT },
+	},
+	.flags = NEED_ALARM,
+};
+
+/*
  * SYSCALL_DEFINE5(recvmmsg, int, fd, struct mmsghdr __user *, mmsg,
 	 unsigned int, vlen, unsigned int, flags,
 	 struct timespec __user *, timeout)
