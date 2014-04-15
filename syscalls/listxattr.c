838ba9eba89f5c7a76cb9667900f5c82ad6f7659
Dave Jones <davej@redhat.com>
2014-04-15 16:50:36 -0400

    merge listxattr variants

--- syscalls/listxattr.c
+++ syscalls/listxattr.c
@@ -14,3 +14,19 @@ struct syscallentry syscall_listxattr = {
 	.arg3type = ARG_LEN,
 	.group = GROUP_VFS,
 };
+
+
+/*
+ * SYSCALL_DEFINE3(llistxattr, const char __user *, pathname, char __user *, list, size_t, size)
+ */
+struct syscallentry syscall_llistxattr = {
+	.name = "llistxattr",
+	.arg1name = "pathname",
+	.arg1type = ARG_PATHNAME,
+	.arg2name = "list",
+	.arg2type = ARG_ADDRESS,
+	.arg3name = "size",
+	.arg3type = ARG_LEN,
+	.num_args = 3,
+	.group = GROUP_VFS,
+};
