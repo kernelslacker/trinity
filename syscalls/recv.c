79ab0e51c7693f6093079858eea7dab104cc4997
Dave Jones <davej@redhat.com>
2014-04-15 16:45:36 -0400

    merge recv* syscalls

--- syscalls/recv.c
+++ syscalls/recv.c
@@ -3,6 +3,7 @@
                             unsigned flags)
 
  */
+#include <sys/types.h>
 #include <sys/socket.h>
 #include "compat.h"
 #include "sanitise.h"
@@ -20,10 +21,73 @@ struct syscallentry syscall_recv = {
 	.arg4type = ARG_LIST,
 	.arg4list = {
 		.num = 20,
-		.values = { MSG_DONTWAIT, MSG_ERRQUEUE, MSG_ERRQUEUE,
-			    MSG_ERRQUEUE, MSG_OOB, MSG_PEEK, MSG_TRUNC,
-			    MSG_WAITALL, MSG_EOR, MSG_TRUNC, MSG_CTRUNC,
-			    MSG_OOB, MSG_ERRQUEUE },
+		.values = { MSG_OOB, MSG_PEEK, MSG_DONTROUTE, MSG_CTRUNC,
+			    MSG_PROBE, MSG_TRUNC, MSG_DONTWAIT, MSG_EOR,
+			    MSG_WAITALL, MSG_FIN, MSG_SYN, MSG_CONFIRM,
+			    MSG_RST, MSG_ERRQUEUE, MSG_NOSIGNAL, MSG_MORE,
+			    MSG_WAITFORONE, MSG_FASTOPEN, MSG_CMSG_CLOEXEC, MSG_CMSG_COMPAT },
 	},
+	.flags = NEED_ALARM,
+};
+
+
+/*
+ * SYSCALL_DEFINE6(recvfrom, int, fd, void __user *, ubuf, size_t, size,
+	unsigned, flags, struct sockaddr __user *, addr,
+	int __user *, addr_len)
+ */
+struct syscallentry syscall_recvfrom = {
+	.name = "recvfrom",
+	.num_args = 6,
+	.arg1name = "fd",
+	.arg1type = ARG_FD,
+	.arg2name = "ubuf",
+	.arg2type = ARG_ADDRESS,
+	.arg3name = "size",
+	.arg3type = ARG_LEN,
+	.arg4name = "flags",
+	.arg4type = ARG_LIST,
+	.arg4list = {
+		.num = 20,
+		.values = { MSG_OOB, MSG_PEEK, MSG_DONTROUTE, MSG_CTRUNC,
+			    MSG_PROBE, MSG_TRUNC, MSG_DONTWAIT, MSG_EOR,
+			    MSG_WAITALL, MSG_FIN, MSG_SYN, MSG_CONFIRM,
+			    MSG_RST, MSG_ERRQUEUE, MSG_NOSIGNAL, MSG_MORE,
+			    MSG_WAITFORONE, MSG_FASTOPEN, MSG_CMSG_CLOEXEC, MSG_CMSG_COMPAT },
+	},
+	.arg5name = "addr",
+	.arg5type = ARG_SOCKADDR,
+	.arg6name = "addr_len",
+	.arg6type = ARG_SOCKADDRLEN,
+	.flags = NEED_ALARM,
+};
+
+
+/*
+ * SYSCALL_DEFINE5(recvmmsg, int, fd, struct mmsghdr __user *, mmsg,
+	 unsigned int, vlen, unsigned int, flags,
+	 struct timespec __user *, timeout)
+ */
+struct syscallentry syscall_recvmmsg = {
+	.name = "recvmmsg",
+	.num_args = 5,
+	.arg1name = "fd",
+	.arg1type = ARG_FD,
+	.arg2name = "mmsg",
+	.arg2type = ARG_ADDRESS,
+	.arg3name = "vlen",
+	.arg3type = ARG_LEN,
+	.arg4name = "flags",
+	.arg4type = ARG_LIST,
+	.arg4list = {
+		.num = 20,
+		.values = { MSG_OOB, MSG_PEEK, MSG_DONTROUTE, MSG_CTRUNC,
+			    MSG_PROBE, MSG_TRUNC, MSG_DONTWAIT, MSG_EOR,
+			    MSG_WAITALL, MSG_FIN, MSG_SYN, MSG_CONFIRM,
+			    MSG_RST, MSG_ERRQUEUE, MSG_NOSIGNAL, MSG_MORE,
+			    MSG_WAITFORONE, MSG_CMSG_CLOEXEC, MSG_FASTOPEN, MSG_CMSG_COMPAT },
+	},
+	.arg5name = "timeout",
+	.arg5type = ARG_ADDRESS,
 	.flags = NEED_ALARM,
 };
