43a46693b7803b6a4290d97a53474f215229f244
Dave Jones <davej@redhat.com>
2014-04-25 00:15:14 -0400

    remove unnecessary headers

--- include/trinity.h
+++ include/trinity.h
@@ -1,11 +1,6 @@
 #pragma once
 
-#include <unistd.h>
-#include <sys/types.h>
-
 extern unsigned int num_online_cpus;
-
-#define __unused__ __attribute((unused))
 
 extern char *progname;
 
@@ -13,6 +8,8 @@ void main_loop(void);
 
 void init_watchdog(void);
 unsigned int check_if_fd(unsigned int child);
+
+#define __unused__ __attribute((unused))
 
 #define FAIL 0
 #define SUCCESS 1
