0cb2b9c48a3f67908ab3fb6b5aa2aadbf8f7f538
Dave Jones <davej@redhat.com>
2014-05-01 18:02:13 -0400

    move the lock stealing code to the watchdog.
    Rather than have each child try to do this, which can't really work well.
    This does require that the watchdog needs to know about every lock we
    define, but that might not be a bad thing.

--- locks.c
+++ locks.c
@@ -1,28 +1,14 @@
 #include <signal.h>
 #include <unistd.h>
 #include "locks.h"
-#include "log.h"
-#include "pids.h"
-
-#define STEAL_THRESHOLD 100000
 
 void lock(lock_t *_lock)
 {
 	while (_lock->lock == LOCKED) {
 		_lock->contention++;
 		usleep(1);
-		if (_lock->contention > STEAL_THRESHOLD) {
-			pid_t pid = _lock->owner;
-
-			if (pid_alive(pid) == FALSE) {
-				output(0, "[%d] more than %d attempts to get lock. pid %d looks dead, stealing.\n",
-					getpid(), STEAL_THRESHOLD, pid);
-				goto steal;
-			}
-		}
 	}
 
-steal:
 	_lock->contention = 0;
 	_lock->owner = getpid();
 	_lock->lock = LOCKED;
