e6ce829a1777b4ad4fbc0df8cc97b90529a4da9b
Dave Jones <davej@redhat.com>
2014-05-06 18:37:29 -0400

    Move eventfd2 defines to compat.h
    Reported by Dave Miller on Sparc.

--- include/compat.h
+++ include/compat.h
@@ -1,5 +1,15 @@
 #pragma once
 
+/* bits/eventfd.h */
+#ifndef EFD_SEMAPHORE
+#define EFD_SEMAPHORE 1
+#endif
+#ifndef EFD_CLOEXEC
+#define EFD_CLOEXEC 02000000
+#endif
+#ifndef EFD_NONBLOCK
+#define EFD_NONBLOCK 04000
+#endif
 
 /* fcntl.h */
 #ifndef AT_EMPTY_PATH
