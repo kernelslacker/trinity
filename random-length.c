84f90a9f347b4da6658576ad80862919b2b0f353
Dave Jones <davej@redhat.com>
2014-05-08 13:59:57 -0400

    use rand_range in get_len

--- random-length.c
+++ random-length.c
@@ -37,7 +37,7 @@ unsigned long get_len(void)
 
 	/* we might get lucky if something is counting ints/longs etc. */
 	if (rand() % 100 < 25) {
-		int _div = 1 << ((rand() % 4) + 1);      /* 2,4,8 or 16 */
+		int _div = 1 << rand_range(1, 4);	/* 2,4,8 or 16 */
 		i /= _div;
 	}
 
