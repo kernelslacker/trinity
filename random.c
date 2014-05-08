2fdc21369de046e0414262efa7d8f2ae30250010
Dave Jones <davej@redhat.com>
2014-05-08 14:06:23 -0400

    add a missed rand_range conversion in rand32()

--- random.c
+++ random.c
@@ -180,7 +180,7 @@ unsigned int rand32(void)
 
 	/* we might get lucky if something is counting ints/longs etc. */
 	if (rand() % 100 < 25) {
-		int _div = 1 << ((rand() % 4) + 1);	/* 2,4,8 or 16 */
+		int _div = 1 << rand_range(1, 4);	/* 2,4,8 or 16 */
 		r /= _div;
 	}
 
