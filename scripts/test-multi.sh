d2c884ad022bacb480f62a701cee0bf5469f395f
Dave Jones <davej@redhat.com>
2014-05-15 17:27:41 -0400

    do the TRINITY_TMP chmod after a trinity run has completed, not before

--- scripts/test-multi.sh
+++ scripts/test-multi.sh
@@ -4,7 +4,6 @@
 . scripts/privs.sh
 . scripts/taint.sh
 
-chmod 755 $TRINITY_TMP
 cd $TRINITY_TMP
 
 NR_CPUS=`grep ^processor /proc/cpuinfo | /usr/bin/wc -l`
@@ -16,18 +15,19 @@ do
   cp $TRINITY_PATH/trinity .
   chmod -w trinity
 
-  chmod 755 $TRINITY_TMP
   if [ -d tmp ]; then
     chmod 755 tmp
     rm -rf tmp
   fi
   mkdir -p tmp
+
   pushd tmp > /dev/null
 
   rm -f trinity.socketcache
 
   MALLOC_CHECK_=2 ../trinity -q -l off -C $NR_PROCESSES $DROPPRIVS -N 1000000
 
+  chmod 755 $TRINITY_TMP
   popd > /dev/null
 
   check_tainted
