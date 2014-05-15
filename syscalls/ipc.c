baf2d2d53f995891cc2ce4a88813d1c0c0cfe231
Dave Jones <davej@redhat.com>
2014-05-14 21:05:41 -0400

    annotate ipc syscall 1st arg

--- syscalls/ipc.c
+++ syscalls/ipc.c
@@ -2,12 +2,23 @@
  * SYSCALL_DEFINE6(ipc, unsigned int, call, int, first, unsigned long, second,
                   unsigned long, third, void __user *, ptr, long, fifth)
  */
+#include <linux/ipc.h>
 #include "sanitise.h"
 
 struct syscallentry syscall_ipc = {
 	.name = "ipc",
 	.num_args = 6,
 	.arg1name = "call",
+	.arg1type = ARG_OP,
+	.arg1list = {
+		.num = 13,
+		.values = {
+			SEMOP, SEMGET, SEMCTL, SEMTIMEDOP,
+			MSGSND, MSGRCV, MSGGET, MSGCTL,
+			SHMAT, SHMDT, SHMGET, SHMCTL,
+			DIPC,
+		},
+	},
 	.arg2name = "first",
 	.arg3name = "second",
 	.arg4name = "third",
