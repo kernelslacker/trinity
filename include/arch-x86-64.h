c933ecd48996be1b02fbcbc40f748ba757e5837a
Dave Jones <davej@redhat.com>
2014-05-16 10:45:52 -0400

    move "do 32 bit syscall from 64 bit code" implentation to arch header

--- include/arch-x86-64.h
+++ include/arch-x86-64.h
@@ -16,3 +16,18 @@
 #define ARCH_IS_BIARCH 1
 #define SYSCALLS32 syscalls_i386
 #define SYSCALLS64 syscalls_x86_64
+
+#define DO_32_SYSCALL \
+	__asm__ volatile ( \
+		"pushq %%rbp\n\t" \
+		"pushq %%r10\n\t" \
+		"pushq %%r11\n\t" \
+		"movq %7, %%rbp\n\t" \
+		"int $0x80\n\t" \
+		"popq %%r11\n\t" \
+		"popq %%r10\n\t" \
+		"popq %%rbp\n\t" \
+		: "=a" (__res) \
+		: "0" (call),"b" ((long)(a1)),"c" ((long)(a2)),"d" ((long)(a3)), "S" ((long)(a4)),"D" ((long)(a5)), "g" ((long)(a6)) \
+		: "%rbp" /* mark EBP reg as dirty */ \
+	);
