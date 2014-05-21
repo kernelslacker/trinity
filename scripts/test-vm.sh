2bf8db04650e4db040bb0aff1eda282c9164de32
Dave Jones <davej@redhat.com>
2014-05-21 10:24:43 -0400

    test-vm.sh: small updates to match other recent test scripts

--- scripts/test-vm.sh
+++ scripts/test-vm.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# This script demonstrates hunting for a VM bug that involved mmap + some other
+# VM related syscall.
 
 . scripts/paths.sh
 . scripts/privs.sh
@@ -7,15 +10,11 @@
 chmod 755 $TRINITY_TMP
 cd $TRINITY_TMP
 
-NR_CPUS=`grep ^processor /proc/cpuinfo | /usr/bin/wc -l`
-NR_PROCESSES=$(($NR_CPUS * 2))
-
 while [ 1 ];
 do
   for syscall in madvise mbind migrate_pages mincore mlockall mlock move_pages mprotect mremap msync munlockall munlock munmap remap_file_pages vmsplice
   do
 	echo testing mmap + $syscall
-	chmod 755 $TRINITY_TMP
 	pushd $TRINITY_TMP > /dev/null
 
 	if [ ! -f $TRINITY_PATH/trinity ]; then
@@ -24,7 +23,9 @@ do
 		exit
 	fi
 
-	MALLOC_CHECK_=2 $TRINITY_PATH/trinity -q -l off -c mmap -c $syscall -N 99999 -C 64 $DROPPRIVS
+	MALLOC_CHECK_=2 $TRINITY_PATH/trinity -q -l off -c mmap -c $syscall -N 1000000 -C 64 $DROPPRIVS
+
+	chmod 755 $TRINITY_TMP
 	popd > /dev/null
 
 	check_tainted
