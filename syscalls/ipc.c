23afb71392f1b7761f72d0de7614b2ad5438003d
Dave Jones <davej@redhat.com>
2014-05-22 16:59:43 -0400

    remove DIPC as a possible op for sys_ipc
    The kernel will ENOSYS things it doesn't understand, and as this
    syscall is obsolete it's unlikely to be extended.

--- syscalls/ipc.c
+++ syscalls/ipc.c
@@ -11,12 +11,11 @@ struct syscallentry syscall_ipc = {
 	.arg1name = "call",
 	.arg1type = ARG_OP,
 	.arg1list = {
-		.num = 13,
+		.num = 12,
 		.values = {
 			SEMOP, SEMGET, SEMCTL, SEMTIMEDOP,
 			MSGSND, MSGRCV, MSGGET, MSGCTL,
 			SHMAT, SHMDT, SHMGET, SHMCTL,
-			DIPC,
 		},
 	},
 	.arg2name = "first",
