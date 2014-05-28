0d645b2f52598773cf7316ac2a0147baa6721b33
Dave Jones <davej@redhat.com>
2014-05-28 16:00:35 -0400

    skeleton of the dumping pidslot work
    ifdef'd out for now until I figure out how I'm going to make
    output_syscall_* output just to the post mortem file.

--- post-mortem.c
+++ post-mortem.c
@@ -1,9 +1,47 @@
 #include <syslog.h>
 #include <sys/time.h>
 #include "log.h"
+#include "pids.h"
 #include "shm.h"
 #include "taint.h"
 #include "post-mortem.h"
+
+#if 0
+static void dump_syscall_rec(int childno)
+{
+	struct syscallrecord *rec;
+
+	rec = &shm->syscall[childno];
+
+	switch (rec->state) {
+	case UNKNOWN:
+		/* new child, so nothing to dump. */
+		break;
+	case PREP:
+		/* haven't finished setting up, so don't dump. */
+		break;
+	case BEFORE:
+		output_syscall_prefix(childno);
+		break;
+	case AFTER:
+		output_syscall_prefix(childno);
+		output_syscall_postfix(childno);
+		break;
+	case GOING_AWAY:
+		output_syscall_prefix(childno);
+		break;
+	}
+}
+
+static void dump_syscall_records(void)
+{
+	unsigned int i;
+
+	for_each_pidslot(i) {
+		dump_syscall_rec(i);
+	}
+}
+#endif
 
 void tainted_postmortem(int taint)
 {
