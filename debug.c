9987d16e9b64ac575a92ac98e0127fb44a78e7a0
Dave Jones <davej@redhat.com>
2014-06-13 12:21:44 -0400

    move the backtrace code to debug.c

--- debug.c
+++ debug.c
@@ -2,10 +2,34 @@
  * Various routines useful for debugging.
  */
 
+#include <execinfo.h>
 #include <stdio.h>
 #include "config.h"
+#include "debug.h"
 #include "log.h"
 #include "shm.h"
+
+#define BACKTRACE_SIZE 100
+
+void show_backtrace(void)
+{
+	unsigned int j, nptrs;
+	void *buffer[BACKTRACE_SIZE];
+	char **strings;
+
+	nptrs = backtrace(buffer, BACKTRACE_SIZE);
+
+	strings = backtrace_symbols(buffer, nptrs);
+	if (strings == NULL) {
+		perror("backtrace_symbols");
+		return;
+	}
+
+	for (j = 0; j < nptrs; j++)
+		output(0, "%s\n", strings[j]);
+
+	free(strings);
+}
 
 void __BUG(const char *bugtxt, const char *filename, const char *funcname, unsigned int lineno)
 {
--- locks.c
+++ locks.c
@@ -2,6 +2,7 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "debug.h"
 #include "locks.h"
 #include "log.h"
 #include "pids.h"
