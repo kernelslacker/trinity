c7befb14e92f41f770273e6e2fbd336cfd2fa8e3
Dave Jones <davej@redhat.com>
2014-07-10 11:21:26 -0400

    don't regenerate version.h if it hasn't changed since last time

--- scripts/gen-versionh.sh
+++ scripts/gen-versionh.sh
@@ -1,20 +1,42 @@
 #!/bin/sh
 
+hdr()
+{
+ echo "#pragma once" > version.h
+ echo "/* This file is auto-generated */" >> version.h
+}
+
+
+if [ -f version.h ]; then
+OLD=$(grep VERSION version.h | head -n1 | sed 's/"//g' | awk '{ print $3 }')
+else
+OLD=""
+fi
+
 DEVEL=$(grep VERSION Makefile | head -n1 | grep pre | wc -l)
 
-echo "#pragma once" > version.h
-echo "/* This file is auto-generated */" >> version.h
+# if we don't have git installed, or we're a release version
+# get the version number from the makefile.
+makefilever()
+{
+VER=$(grep VERSION= Makefile| head -n1| sed 's/=/ /' | sed 's/"//g' | awk '{print $2}')
+  if [ "$OLD" != "$VER" ]; then
+    hdr
+    echo -n "#define " >> version.h
+    echo $VER >> version.h
+  fi
+}
 
 if [ "$DEVEL" == "1" ]; then
   if [ ! -f /usr/bin/git ]; then
-    echo -n "#define " >> version.h
-    grep VERSION= Makefile | sed 's/=/ /' >> version.h
+   makefilever
   else
     VER=$(git describe --always)
-    echo "#define VERSION \""$VER\" >> version.h
+    if [ "$OLD" != "$VER" ]; then
+      hdr
+      echo "#define VERSION \""$VER\" >> version.h
+    fi
   fi
 else
-  echo -n "#define " >> version.h
-  grep VERSION= Makefile | sed 's/=/ /' >> version.h
+  makefilever
 fi
-
