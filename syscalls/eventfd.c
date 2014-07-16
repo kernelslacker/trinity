9aed2a6f66a8b7191c078837dae55d55a99714e4
Dave Jones <davej@redhat.com>
2014-07-16 10:34:38 -0400

    merge both eventfd variants into same file

--- syscalls/eventfd.c
+++ syscalls/eventfd.c
@@ -3,6 +3,8 @@
  *
  * On success, eventfd() returns a new eventfd file descriptor.
  * On error, -1 is returned and errno is set to indicate the error.
+ *
+ * eventfd() calls eventfd2() with a zero'd flags arg.
  */
 #include "sanitise.h"
 
@@ -11,5 +13,29 @@ struct syscallentry syscall_eventfd = {
 	.num_args = 1,
 	.arg1name = "count",
 	.arg1type = ARG_LEN,
+	.rettype = RET_FD,
+};
+
+/*
+ * SYSCALL_DEFINE2(eventfd2, unsigned int, count, int, flags)
+ *
+ * On success, eventfd() returns a new eventfd file descriptor.
+ * On error, -1 is returned and errno is set to indicate the error.
+ */
+
+#include "sanitise.h"
+#include "compat.h"
+
+struct syscallentry syscall_eventfd2 = {
+	.name = "eventfd2",
+	.num_args = 2,
+	.arg1name = "count",
+	.arg1type = ARG_LEN,
+	.arg2name = "flags",
+	.arg2type = ARG_LIST,
+	.arg2list = {
+		.num = 3,
+		.values = { EFD_CLOEXEC, EFD_NONBLOCK, EFD_SEMAPHORE },
+	},
 	.rettype = RET_FD,
 };
