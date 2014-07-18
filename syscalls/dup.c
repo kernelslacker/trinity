9c59dafac1229d58c30c66126081816776a0573a
Dave Jones <davej@redhat.com>
2014-07-18 13:17:47 -0400

    merge all the dup variants into the same file

--- syscalls/dup.c
+++ syscalls/dup.c
@@ -1,16 +1,63 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include "sanitise.h"
+#include "compat.h"
+
 /*
  * SYSCALL_DEFINE1(dup, unsigned int, fildes)
  *
  * On success, returns the new descriptor.
  * On error, -1 is returned, and errno is set appropriately.
  */
-#include "sanitise.h"
 
 struct syscallentry syscall_dup = {
 	.name = "dup",
 	.num_args = 1,
 	.arg1name = "fildes",
 	.arg1type = ARG_FD,
+	.rettype = RET_FD,
+	.flags = NEED_ALARM,
+};
+
+/*
+ * SYSCALL_DEFINE2(dup2, unsigned int, oldfd, unsigned int, newfd)
+ *
+ * On success, returns the new descriptor.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+struct syscallentry syscall_dup2 = {
+	.name = "dup2",
+	.num_args = 2,
+	.arg1name = "oldfd",
+	.arg1type = ARG_FD,
+	.arg2name = "newfd",
+	.arg2type = ARG_FD,
+	.rettype = RET_FD,
+	.flags = NEED_ALARM,
+};
+
+
+/*
+ * SYSCALL_DEFINE3(dup3, unsigned int, oldfd, unsigned int, newfd, int, flags)
+ *
+ * On success, returns the new descriptor.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+struct syscallentry syscall_dup3 = {
+	.name = "dup3",
+	.num_args = 3,
+	.arg1name = "oldfd",
+	.arg1type = ARG_FD,
+	.arg2name = "newfd",
+	.arg2type = ARG_FD,
+	.arg3name = "flags",
+	.arg3type = ARG_LIST,
+	.arg3list = {
+		.num = 1,
+		.values = { O_CLOEXEC },
+	},
 	.rettype = RET_FD,
 	.flags = NEED_ALARM,
 };
