129e4f5ea95161b29636565aa1b962543441779b
Dave Jones <davej@redhat.com>
2014-07-25 12:22:34 -0400

    rename dev -> bdev

--- include/bdevs.h
+++ include/bdevs.h
@@ -1,8 +1,8 @@
 #ifndef _DEVS_H
 #define _DEVS_H 1
 
-void process_dev_param(char *optarg);
-void init_dev_list(void);
-void dump_dev_list(void);
+void process_bdev_param(char *optarg);
+void init_bdev_list(void);
+void dump_bdev_list(void);
 
 #endif	/* _DEVS_H */
