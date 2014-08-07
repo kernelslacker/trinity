76e36192ecc40c999ba5e4907b8975ec01fd65a5
Dave Jones <davej@redhat.com>
2014-08-07 12:08:22 -0400

    add the flags and special cases to seccomp sanitise

--- syscalls/seccomp.c
+++ syscalls/seccomp.c
@@ -8,6 +8,16 @@
 #define SECCOMP_SET_MODE_STRICT 0
 #define SECCOMP_SET_MODE_FILTER 1
 
+#define SECCOMP_FILTER_FLAG_TSYNC 1
+
+static void sanitise_seccomp(struct syscallrecord *rec)
+{
+	if (rec->a1 == SECCOMP_SET_MODE_STRICT) {
+		rec->a2 = 0;
+		rec->a3 = 0;
+	}
+}
+
 struct syscallentry syscall_seccomp = {
 	.name = "seccomp",
 	.num_args = 3,
@@ -18,6 +28,12 @@ struct syscallentry syscall_seccomp = {
 		.values = { SECCOMP_SET_MODE_STRICT, SECCOMP_SET_MODE_FILTER },
 	},
 	.arg2name = "flags",
+	.arg2type = ARG_LIST,
+	.arg2list = {
+		.num = 1,
+		.values= { SECCOMP_FILTER_FLAG_TSYNC },
+	},
 	.arg3name = "uargs",
 	.arg3type = ARG_ADDRESS,
+	.sanitise = sanitise_seccomp,
 };
