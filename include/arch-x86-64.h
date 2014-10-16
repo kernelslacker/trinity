10120f7daad6ad9e9236f60749e66f9e8c3b953e
Dave Jones <davej@redhat.com>
2014-10-16 12:45:54 -0400

    save r8/r9 across 32-bit syscall.

--- include/arch-x86-64.h
+++ include/arch-x86-64.h
@@ -20,12 +20,16 @@
 #define DO_32_SYSCALL \
 	__asm__ volatile ( \
 		"pushq %%rbp\n\t" \
+		"pushq %%r8\n\t" \
+		"pushq %%r9\n\t" \
 		"pushq %%r10\n\t" \
 		"pushq %%r11\n\t" \
 		"movq %7, %%rbp\n\t" \
 		"int $0x80\n\t" \
 		"popq %%r11\n\t" \
 		"popq %%r10\n\t" \
+		"popq %%r9\n\t" \
+		"popq %%r8\n\t" \
 		"popq %%rbp\n\t" \
 		: "=a" (__res) \
 		: "0" (call),"b" ((long)(a1)),"c" ((long)(a2)),"d" ((long)(a3)), "S" ((long)(a4)),"D" ((long)(a5)), "g" ((long)(a6)) \
