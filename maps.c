e7dfa27d32418cbaa0c51a82d5fd9dae05c04ef3
Dave Jones <davej@redhat.com>
2014-10-23 23:52:23 -0400

    fold the remnants of maps-fault back into maps.c

--- maps.c
+++ maps.c
@@ -98,3 +98,37 @@ struct map * common_set_mmap_ptr_len(void)
 
 	return map;
 }
+
+/*
+ * Routine to perform various kinds of write operations to a mapping
+ * that we created.
+ */
+void dirty_mapping(struct map *map)
+{
+	bool rw = rand_bool();
+
+	if (rw == TRUE) {
+		/* Check mapping is writable, or we'll segv.
+		 * TODO: Perhaps we should do that, and trap it, mark it writable,
+		 * then reprotect after we dirtied it ? */
+		if (!(map->prot & PROT_WRITE))
+			return;
+
+		random_map_writefn(map);
+		return;
+
+	} else {
+		if (!(map->prot & PROT_READ))
+			return;
+
+		random_map_readfn(map);
+	}
+}
+
+void dirty_random_mapping(void)
+{
+	struct map *map;
+
+	map = get_map();
+	dirty_mapping(map);
+}
