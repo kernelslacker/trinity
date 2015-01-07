98c979069f2fb58fcf635ffa2af8329460d73469
Dave Jones <davej@codemonkey.org.uk>
2015-01-07 11:18:05 -0500

    add sys_execveat support

--- include/syscalls-x86_64.h
+++ include/syscalls-x86_64.h
@@ -1,6 +1,6 @@
 #pragma once
 
-/* Syscalls from arch/x86/syscalls/syscall_64.tbl as of 3.17+ */
+/* Syscalls from arch/x86/syscalls/syscall_64.tbl */
 
 #include "sanitise.h"
 #include "syscall.h"
@@ -329,4 +329,5 @@ struct syscalltable syscalls_x86_64[] = {
 	{ .entry = &syscall_memfd_create },
 	{ .entry = &syscall_kexec_file_load },
 	{ .entry = &syscall_bpf },
+	{ .entry = &syscall_execveat },
 };
