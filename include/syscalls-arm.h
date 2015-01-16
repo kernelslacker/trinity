db62bf96edc6aa833bae9d90b3cd6deb655eec54
Dave Jones <djones@akamai.com>
2015-01-16 10:30:09 -0500

    wire up execveat for Arm

--- include/syscalls-arm.h
+++ include/syscalls-arm.h
@@ -397,4 +397,5 @@ struct syscalltable syscalls_arm[] = {
 		{ .entry = &syscall_getrandom },
 /* 385 */	{ .entry = &syscall_memfd_create },
 		{ .entry = &syscall_bpf },
+		{ .entry = &syscall_execveat },
 };
