8b082ef5a2aab3944ad692ecdbe6ddae347ea737
Dave Jones <davej@codemonkey.org.uk>
2015-04-28 19:08:56 -0400

    sort, and add placeholders for all socket families

--- syscalls/socket.c
+++ syscalls/socket.c
@@ -23,31 +23,52 @@ struct socket_ptr {
 	void (*func)(struct socket_triplet *st);
 };
 static const struct socket_ptr socketptrs[] = {
+	{ .family = AF_UNIX, .func = &unix_rand_socket },
+	{ .family = AF_INET, .func = &inet_rand_socket },
+	{ .family = AF_AX25, .func = &ax25_rand_socket },
+	{ .family = AF_IPX, .func = &ipx_rand_socket },
 #ifdef USE_APPLETALK
 	{ .family = AF_APPLETALK, .func = &atalk_rand_socket },
 #endif
-	{ .family = AF_AX25, .func = &ax25_rand_socket },
-#ifdef USE_CAIF
-	{ .family = AF_CAIF, .func = &caif_rand_socket },
-#endif
-	{ .family = AF_CAN, .func = &can_rand_socket },
-	{ .family = AF_DECnet, .func = &decnet_rand_socket },
-	{ .family = AF_INET, .func = &inet_rand_socket },
+//TODO	{ .family = AF_NETROM, .func = &netrom_rand_socket },
+//TODO	{ .family = AF_BRIDGE, .func = &bridge_rand_socket },
+//TODO	{ .family = AF_ATMPVC, .func = &atmpvc_rand_socket },
+	{ .family = AF_X25, .func = &x25_rand_socket },
 #ifdef USE_IPV6
 	{ .family = AF_INET6, .func = &inet6_rand_socket },
 #endif
-	{ .family = AF_IPX, .func = &ipx_rand_socket },
-	{ .family = AF_IRDA, .func = &irda_rand_socket },
-	{ .family = AF_LLC, .func = &llc_rand_socket },
+//TODO	{ .family = AF_ROSE, .func = &rose_rand_socket },
+	{ .family = AF_DECnet, .func = &decnet_rand_socket },
+//TODO	{ .family = AF_NETBEUI, .func = &netbeui_rand_socket },
+//TODO	{ .family = AF_SECURITY, .func = &security_rand_socket },
+//TODO	{ .family = AF_KEY, .func = &key_rand_socket },
 	{ .family = AF_NETLINK, .func = &netlink_rand_socket },
-	{ .family = AF_NFC, .func = &nfc_rand_socket },
-//TODO	{ .family = AF_IB, .func = &ib_rand_socket },
 	{ .family = AF_PACKET, .func = &packet_rand_socket },
-	{ .family = AF_PHONET, .func = &phonet_rand_socket },
+//TODO	{ .family = AF_ASH, .func = &ash_rand_socket },
+//DEAD	{ .family = AF_ECONET, .func = &econet_rand_socket },
+//TODO	{ .family = AF_ATMSVC, .func = &atmsvc_rand_socket },
 	{ .family = AF_RDS, .func = &rds_rand_socket },
+//TODO	{ .family = AF_SNA, .func = &sna_rand_socket },
+	{ .family = AF_IRDA, .func = &irda_rand_socket },
+//TODO	{ .family = AF_PPPOX, .func = &pppox_rand_socket },
+//TODO	{ .family = AF_WANPIPE, .func = &wanpipe_rand_socket },
+	{ .family = AF_LLC, .func = &llc_rand_socket },
+//TODO	{ .family = AF_IB, .func = &ib_rand_socket },
+//TODO	{ .family = AF_MPLS, .func = &mpls_rand_socket },
+	{ .family = AF_CAN, .func = &can_rand_socket },
 	{ .family = AF_TIPC, .func = &tipc_rand_socket },
-	{ .family = AF_UNIX, .func = &unix_rand_socket },
-	{ .family = AF_X25, .func = &x25_rand_socket },
+//TODO	{ .family = AF_BLUETOOTH, .func = &bluetooth_rand_socket },
+//TODO	{ .family = AF_IUCV, .func = &iucv_rand_socket },
+//TODO	{ .family = AF_RXRPC, .func = &rxrpc_rand_socket },
+//TODO	{ .family = AF_ISDN, .func = &isdn_rand_socket },
+//TODO	{ .family = AF_PHONET, .func = &phonet_rand_socket },
+//TODO	{ .family = AF_IEEE802154, .func = &ieee802154_rand_socket },
+#ifdef USE_CAIF
+	{ .family = AF_CAIF, .func = &caif_rand_socket },
+#endif
+//TODO	{ .family = AF_ALG, .func = &alg_rand_socket },
+	{ .family = AF_NFC, .func = &nfc_rand_socket },
+//TODO	{ .family = AF_VSOCK, .func = &vsock_rand_socket },
 };
 
 void rand_proto_type(struct socket_triplet *st)
