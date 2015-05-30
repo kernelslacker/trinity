a86ef0a025a60a19cd2342b36e115abd80858282
Dave Jones <davej@codemonkey.org.uk>
2015-05-29 23:01:33 -0400

    use ARRAY_RAND in proto netrom

--- net/proto-netrom.c
+++ net/proto-netrom.c
@@ -5,7 +5,7 @@
 #include <netrom/netrom.h>
 #include "net.h"
 #include "compat.h"
-#include "utils.h"	// ARRAY_SIZE
+#include "utils.h"	// RAND_ARRAY
 
 static const unsigned int netrom_opts[] = {
 	NETROM_T1, NETROM_T2, NETROM_N2, NETROM_T4, NETROM_IDLE
@@ -15,7 +15,7 @@ void netrom_setsockopt(struct sockopt *so)
 {
 	unsigned char val;
 
-	val = rand() % ARRAY_SIZE(netrom_opts);
+	val = RAND_ARRAY(netrom_opts);
 	so->optname = netrom_opts[val];
 }
 #endif
