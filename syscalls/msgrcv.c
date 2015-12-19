397961a569231c73259131572cba531a179033b0
Dave Jones <davej@codemonkey.org.uk>
2015-12-18 20:48:14 -0500

    improve msgrcv sanitise
    mark as NEED_ALARM, give it a non-null address, and set a smaller len.

--- syscalls/msgrcv.c
+++ syscalls/msgrcv.c
@@ -4,7 +4,13 @@
 #include <sys/types.h>
 #include <linux/msg.h>
 #include "compat.h"
+#include "random.h"
 #include "sanitise.h"
+
+static void sanitise_msgrcv(struct syscallrecord *rec)
+{
+	rec->a3 = rand() % MSGMAX;
+}
 
 static unsigned long msgrcv_flags[] = {
 	MSG_NOERROR, MSG_EXCEPT, MSG_COPY, IPC_NOWAIT,
@@ -15,12 +21,12 @@ struct syscallentry syscall_msgrcv = {
 	.num_args = 5,
 	.arg1name = "msqid",
 	.arg2name = "msgp",
-	.arg2type = ARG_ADDRESS,
+	.arg2type = ARG_NON_NULL_ADDRESS,
 	.arg3name = "msgsz",
-	.arg3type = ARG_LEN,
 	.arg4name = "msgtyp",
 	.arg5name = "msgflg",
 	.arg5type = ARG_LIST,
 	.arg5list = ARGLIST(msgrcv_flags),
-	.flags = IGNORE_ENOSYS,
+	.flags = IGNORE_ENOSYS | NEED_ALARM,
+	.sanitise = sanitise_msgrcv,
 };
