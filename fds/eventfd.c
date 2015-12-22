e6ef3e03e14f18499ceb6d1641b9e639b405f256
Dave Jones <davej@codemonkey.org.uk>
2015-12-22 11:25:49 -0500

    add a destructor for eventfd's

--- fds/eventfd.c
+++ fds/eventfd.c
@@ -11,13 +11,20 @@
 #include "fd.h"
 #include "files.h"
 #include "log.h"
+#include "objects.h"
 #include "random.h"
 #include "sanitise.h"
 #include "shm.h"
 #include "compat.h"
 
+static void eventfd_destructor(struct object *obj)
+{
+	close(obj->eventfd);
+}
+
 static int open_eventfd_fds(void)
 {
+	struct objhead *head;
 	unsigned int i;
 	unsigned int flags[] = {
 		0,
@@ -29,6 +36,9 @@ static int open_eventfd_fds(void)
 		EFD_CLOEXEC  | EFD_NONBLOCK | EFD_SEMAPHORE,
 		EFD_SEMAPHORE,
 	};
+
+	head = get_objhead(OBJ_GLOBAL, OBJ_FD_EVENTFD);
+	head->destroy = &eventfd_destructor;
 
 	for (i = 0; i < ARRAY_SIZE(flags); i++) {
 		struct object *obj;
