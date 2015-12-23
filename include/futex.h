0774c700ed4da79e876b78788c2d21ee7f1b72c5
Dave Jones <davej@codemonkey.org.uk>
2015-12-22 19:43:49 -0500

    argh, forgot to add include/futex.h

---
+++ include/futex.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <sys/types.h>
+#include <stdint.h>
+
+struct __lock {
+	uint32_t futex;
+	pid_t owner_pid;
+};
+
+void create_futexes(void);
+u32 * get_futex(void);
+struct __lock * get_random_lock(void);
