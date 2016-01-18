0347350352967f862e602912d6c98b9d792db496
Dave Jones <davej@codemonkey.org.uk>
2016-01-17 22:06:14 -0500

    merge generate_rand_bytes and generate_random_page
    More work to do here, probably broken in some cases, but it's a start

--- rand/random-page.c
+++ rand/random-page.c
@@ -7,62 +7,6 @@
 #include "random.h"
 #include "sanitise.h"
 
-void generate_rand_bytes(unsigned char *ptr, unsigned int len)
-{
-	unsigned int i;
-	unsigned int startoffset = 0, remain;
-	unsigned char separators[] = { ':', ',', '.', ' ', '-', '\0', };
-	unsigned char separator;
-
-	switch (rnd() % 3) {
-	case 0:
-		/* Complete garbage. */
-		for (i = 0; i < len; i++)
-			ptr[i] = RAND_BYTE();
-		break;
-	case 1:
-		/* printable text strings. */
-		for (i = 0; i < len; i++)
-			ptr[i] = 32 + rnd() % (0x7f - 32);
-		break;
-	case 2:
-		/* numbers (for now, decimal only) */
-
-		separator = separators[rnd() % sizeof(separators)];
-
-		remain = len;
-
-		while (remain > 0) {
-			unsigned int runlen;
-
-			/* Sometimes make the numbers be negative. */
-			if (RAND_BOOL()) {
-				ptr[startoffset++] = '-';
-				remain--;
-				if (remain == 0)
-					break;
-			}
-
-			/* At most make this run 10 chars. */
-			runlen = min(remain, (unsigned int) rnd() % 10);
-
-			for (i = startoffset; i < startoffset + runlen; i++)
-				ptr[i] = '0' + rnd() % 10;
-
-			startoffset += runlen;
-			remain -= runlen;
-
-			/* insert commas and/or spaces */
-			if (remain > 0) {
-				ptr[i++] = separator;
-				startoffset++;
-				remain--;
-			}
-		}
-		break;
-	}
-}
-
 static void fabricate_onepage_struct(char *page)
 {
 	unsigned int i;
@@ -105,57 +49,81 @@ static void fabricate_onepage_struct(char *page)
 	}
 }
 
-void generate_random_page(char *page)
+//TODO: Some of this code used to assume page_size. It needs auditting
+// to be sure we don't write past len.
+void generate_rand_bytes(unsigned char *ptr, unsigned int len)
 {
-	unsigned int i;
 	char *p;
+	unsigned int i;
+	unsigned int startoffset = 0, remain;
+	unsigned char separators[] = { ':', ',', '.', ' ', '-', '\0', };
+	unsigned char separator;
 
-	switch (rnd() % 8) {
-
+	switch (rnd() % 10) {
 	case 0:
-		memset(page, 0, page_size);
-		return;
-
+		/* Complete garbage. */
+		for (i = 0; i < len; i++)
+			ptr[i] = RAND_BYTE();
+		break;
 	case 1:
-		memset(page, 0xff, page_size);
+		memset(ptr, 0, len);
 		return;
-
 	case 2:
-		memset(page, RAND_BYTE(), page_size);
+		memset(ptr, 0xff, len);
 		return;
-
 	case 3:
-		for (i = 0; i < page_size; )
-			page[i++] = (unsigned char)rnd();
+		memset(ptr, RAND_BYTE(), len);
 		return;
-
 	case 4:
-		for (i = 0; i < page_size; )
-			page[i++] = (unsigned char)RAND_BOOL();
+		for (i = 0; i < len; i++)
+			ptr[i] = (unsigned char)RAND_BOOL();
 		return;
+	case 5:
+		/* printable text strings. */
+		for (i = 0; i < len; i++)
+			ptr[i] = 32 + rnd() % (0x7f - 32);
+		break;
 
-	/* return a page that looks kinda like a struct */
-	case 5:	fabricate_onepage_struct(page);
-		return;
+//FIXME: consolidate case 6 & 7
 
-	/* page full of format strings. */
 	case 6:
-		for (i = 0; i < page_size; i += 2) {
-			page[i] = '%';
-			switch (RAND_BOOL()) {
-			case 0:	page[i + 1] = 'd';
-				break;
-			case 1:	page[i + 1] = 's';
-				break;
+		/* numbers (for now, decimal only) */
+		separator = separators[rnd() % sizeof(separators)];
+
+		remain = len;
+
+		while (remain > 0) {
+			unsigned int runlen;
+
+			/* Sometimes make the numbers be negative. */
+			if (RAND_BOOL()) {
+				ptr[startoffset++] = '-';
+				remain--;
+				if (remain == 0)
+					break;
+			}
+
+			/* At most make this run 10 chars. */
+			runlen = min(remain, (unsigned int) rnd() % 10);
+
+			for (i = startoffset; i < startoffset + runlen; i++)
+				ptr[i] = '0' + rnd() % 10;
+
+			startoffset += runlen;
+			remain -= runlen;
+
+			/* insert commas and/or spaces */
+			if (remain > 0) {
+				ptr[i++] = separator;
+				startoffset++;
+				remain--;
 			}
 		}
-		page_size = getpagesize();	// Hack for clang 3.3 false positive.
-		page[rnd() % page_size] = 0;
-		return;
+		break;
 
 	/* ascii representation of a random number */
 	case 7:
-		p = page;
+		p = (char *) ptr;
 
 		if (RAND_BOOL()) {
 			/* hex */
@@ -183,8 +151,33 @@ void generate_random_page(char *page)
 				break;
 			}
 		}
-
 		*p = 0;
 		break;
+
+	/* return something that looks kinda like a struct */
+	case 8:
+		if (len >= page_size)
+			fabricate_onepage_struct((char *)ptr);
+		//FIXME: Do something better here if 'else'
+		return;
+
+	/* format strings. */
+	case 9:
+		for (i = 0; i < len; i += 2) {
+			ptr[i] = '%';
+			switch (RAND_BOOL()) {
+			case 0:	ptr[i + 1] = 'd';
+				break;
+			case 1:	ptr[i + 1] = 's';
+				break;
+			}
+		}
+		ptr[rnd() % len] = 0;
+		return;
 	}
+}
+
+void generate_random_page(char *page)
+{
+	generate_rand_bytes((void *)page, page_size);
 }
