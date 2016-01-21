3cf0e88a289e9757d953362f66414c70e726d026
Dave Jones <davej@codemonkey.org.uk>
2016-01-21 14:58:43 -0500

    fill in missing protocol ptrs from previous commits

--- net/protocols.c
+++ net/protocols.c
@@ -2,5 +2,9 @@
 #include "net.h"
 
 const struct protoptr net_protocols[PF_MAX] = {
+	[PF_UNIX] = { .proto = &proto_unix },
+	[PF_INET] = { .proto = &proto_ipv4 },
+	[PF_AX25] = { .proto = &proto_ax25 },
+	[PF_IPX] = { .proto = &proto_ipx },
 	[PF_NETLINK] = { .proto = &proto_netlink },
 };
