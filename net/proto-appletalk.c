cd5a452f05300bd0c23b8b9e3f8229f4eee7f3a9
Dave Jones <davej@codemonkey.org.uk>
2016-08-17 20:01:14 -0400

    ->generate for appletalk

--- net/proto-appletalk.c
+++ net/proto-appletalk.c
@@ -1,6 +1,7 @@
 #include "config.h"
 
 #ifdef USE_APPLETALK
+#include <errno.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,6 +9,7 @@
 #include <netinet/in.h>
 #include <netatalk/at.h>
 #include <linux/atalk.h>
+#include "log.h"
 #include "random.h"
 #include "net.h"
 #include "utils.h"
@@ -43,10 +45,36 @@ static void atalk_setsockopt(struct sockopt *so, __unused__ struct socket_triple
 	so->level = SOL_ATALK;
 }
 
+static void generate_atalk(unsigned int protocol, unsigned int type)
+{
+	struct socket_triplet st;
+	int fd;
+
+	st.family = PF_APPLETALK;
+	st.type = type;
+	st.protocol = protocol;
+
+	fd = open_socket(st.family, st.type, st.protocol);
+	if (fd > -1) {
+		write_socket_to_cache(&st);
+		return;
+	}
+	output(0, "Couldn't open socket PF_INET:%d:%d. %s\n", type, protocol, strerror(errno));
+}
+
+static void gen_atalk(void)
+{
+	generate_atalk(0, SOCK_DGRAM);
+
+	// Atalk will let us create 256 RAW sockets, but we only need one.
+	generate_atalk(0, SOCK_RAW);
+}
+
 const struct netproto proto_appletalk = {
 	.name = "appletalk",
 	.socket = atalk_rand_socket,
 	.setsockopt = atalk_setsockopt,
 	.gen_sockaddr = atalk_gen_sockaddr,
+	.generate = gen_atalk,
 };
 #endif
