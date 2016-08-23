5ce30649ea1c58f79a05d0389f490af48023a4c7
Chunyu Hu <chuhu@redhat.com>
2016-08-23 23:26:12 +0800

    config: a few updates for memo

---
+++ config/tag.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# generate tag file for lookupfile plugin
+echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > filenametags
+find . -not -regex '.*\.\(png\|gif\)' -type f -printf "%f\t%p\t1\n" | \
+        sort -f >> filenametags
