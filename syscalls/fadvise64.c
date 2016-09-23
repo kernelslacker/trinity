76aadaba3e48f73adecff456e595582fdc472916
Dave Jones <davej@codemonkey.org.uk>
2016-09-23 16:25:49 -0400

    annotate fadvise flags

--- syscalls/fadvise64.c
+++ syscalls/fadvise64.c
@@ -4,7 +4,17 @@
  * On success, zero is returned.
  * On error, an error number is returned.
  */
+#include <fcntl.h>
 #include "sanitise.h"
+
+static unsigned long fadvise_flags[] = {
+	POSIX_FADV_NORMAL,
+	POSIX_FADV_SEQUENTIAL,
+	POSIX_FADV_RANDOM,
+	POSIX_FADV_NOREUSE,
+	POSIX_FADV_WILLNEED,
+	POSIX_FADV_DONTNEED,
+};
 
 struct syscallentry syscall_fadvise64 = {
 	.name = "fadvise64",
@@ -15,6 +25,8 @@ struct syscallentry syscall_fadvise64 = {
 	.arg3name = "len",
 	.arg3type = ARG_LEN,
 	.arg4name = "advice",
+	.arg4type = ARG_OP,
+	.arg4list = ARGLIST(fadvise_flags),
 	.rettype = RET_ZERO_SUCCESS,
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
@@ -37,6 +49,8 @@ struct syscallentry syscall_fadvise64_64 = {
 	.arg3name = "len",
 	.arg3type = ARG_LEN,
 	.arg4name = "advice",
+	.arg4type = ARG_OP,
+	.arg4list = ARGLIST(fadvise_flags),
 	.rettype = RET_ZERO_SUCCESS,
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
@@ -54,6 +68,8 @@ struct syscallentry syscall_arm_fadvise64_64 = {
 	.arg1name = "fd",
 	.arg1type = ARG_FD,
 	.arg2name = "advice",
+	.arg2type = ARG_OP,
+	.arg2list = ARGLIST(fadvise_flags),
 	.arg3name = "offset",
 	.arg4name = "len",
 	.arg4type = ARG_LEN,
