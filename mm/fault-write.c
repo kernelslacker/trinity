40c86c5a30b36dd95307f5a6033c101252f28495
Dave Jones <davej@codemonkey.org.uk>
2016-10-14 14:09:35 -0400

    multi-page mprotect fixes for fault-write
    Multiple problems here:
    - In some cases we would pass down a multi-page mapping after mprotect'ing
    the first page as writable, and then try writing to a different page within
    that mapping, and segfault.
    - In some cases we would mprotect write a whole multi-page mapping, even when
    we were only going to write to a single page.

--- mm/fault-write.c
+++ mm/fault-write.c
@@ -17,55 +17,66 @@ static unsigned int nr_pages(struct map *map)
 static void dirty_one_page(struct map *map)
 {
 	char *p = map->ptr;
+	unsigned long offset = (rnd() % (map->size - 1)) & PAGE_MASK;
 
-	p[rnd() % (map->size - 1)] = rnd();
+	mprotect((void *) p + offset, page_size, PROT_READ|PROT_WRITE);
+	p[offset] = rnd();
 }
 
 static void dirty_whole_mapping(struct map *map)
 {
-	char *p = map->ptr;
 	unsigned int i, nr;
 
 	nr = nr_pages(map);
 
-	for (i = 0; i < nr; i++)
-		p[i * page_size] = rnd();
+	for (i = 0; i < nr; i++) {
+		char *p = map->ptr + (i * page_size);
+		mprotect((void *) p, page_size, PROT_READ|PROT_WRITE);
+		*p = rnd();
+	}
 }
 
 static void dirty_every_other_page(struct map *map)
 {
-	char *p = map->ptr;
 	unsigned int i, nr, first;
 
 	nr = nr_pages(map);
 
 	first = RAND_BOOL();
 
-	for (i = first; i < nr; i+=2)
-		p[i * page_size] = rnd();
+	for (i = first; i < nr; i+=2) {
+		char *p = map->ptr + (i * page_size);
+		mprotect((void *) p, page_size, PROT_READ|PROT_WRITE);
+		*p = rnd();
+	}
 }
 
 static void dirty_mapping_reverse(struct map *map)
 {
-	char *p = map->ptr;
 	unsigned int i, nr;
 
 	nr = nr_pages(map) - 1;
 
-	for (i = nr; i > 0; i--)
-		p[i * page_size] = rnd();
+	for (i = nr; i > 0; i--) {
+		char *p = map->ptr + (i * page_size);
+		mprotect((void *) p, page_size, PROT_READ|PROT_WRITE);
+		*p = rnd();
+	}
 }
 
 /* dirty a random set of map->size pages. (some may be faulted >once) */
 static void dirty_random_pages(struct map *map)
 {
-	char *p = map->ptr;
 	unsigned int i, nr;
 
 	nr = nr_pages(map);
 
-	for (i = 0; i < nr; i++)
-		p[(rnd() % nr) * page_size] = rnd();
+	for (i = 0; i < nr; i++) {
+		off_t offset = (rnd() % nr) * page_size;
+		char *p = map->ptr + offset;
+		mprotect((void *) p, page_size, PROT_READ|PROT_WRITE);
+		*p = rnd();
+	}
 }
 
 /*
@@ -74,6 +85,7 @@ static void dirty_first_page(struct map *map)
 {
 	char *p = map->ptr;
 
+	mprotect(map->ptr, page_size, PROT_READ|PROT_WRITE);
 	generate_random_page(p);
 }
 
@@ -84,6 +96,7 @@ static void dirty_last_page(struct map *map)
 {
 	char *p = map->ptr + map->size - page_size;
 
+	mprotect((void *) p, page_size, PROT_READ|PROT_WRITE);
 	memset((void *) p, 'A', page_size);
 }
 
@@ -103,14 +116,11 @@ static const struct faultfn write_faultfns[] = {
 void random_map_writefn(struct map *map)
 {
 	if (map->size == page_size) {
-		mprotect(map->ptr, page_size, PROT_READ|PROT_WRITE);
 		write_faultfns_single[rnd() % ARRAY_SIZE(write_faultfns_single)].func(map);
 	} else {
 		if (RAND_BOOL()) {
-			mprotect(map->ptr, map->size, PROT_READ|PROT_WRITE);
 			write_faultfns[rnd() % ARRAY_SIZE(write_faultfns)].func(map);
 		} else {
-			mprotect(map->ptr, page_size, PROT_READ|PROT_WRITE);
 			write_faultfns_single[rnd() % ARRAY_SIZE(write_faultfns_single)].func(map);
 		}
 	}
