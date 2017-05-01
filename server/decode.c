cf19ddb8dade1063cd1d11df479fea59da2c35fa
Dave Jones <davej@codemonkey.org.uk>
2017-05-01 16:29:17 -0400

    fix backwards ternary operators
    when I switched the meaning of ->global, I forgot to change all these.

--- server/decode.c
+++ server/decode.c
@@ -64,13 +64,13 @@ static void decode_obj_created_file(char *buf)
 
 	if (objmsg->fopened) {
 		printf("%s file object created at %p by pid %d: fd %d = fopen(\"%s\") ; fcntl(fd, 0x%x)\n",
-			objmsg->hdr.global ? "local" : "global",
+			objmsg->hdr.global ? "global" : "local",
 			objmsg->hdr.address, objmsg->hdr.pid,
 			objmsg->fd, objmsg->filename,
 			objmsg->fcntl_flags);
 	} else {
 		printf("%s file object created at %p by pid %d: fd %d = open(\"%s\", 0x%x)\n",
-			objmsg->hdr.global ? "local" : "global",
+			objmsg->hdr.global ? "global" : "local",
 			objmsg->hdr.address, objmsg->hdr.pid,
 			objmsg->fd, objmsg->filename, objmsg->flags);
 	}
@@ -87,7 +87,7 @@ static void decode_obj_created_map(char *buf)
 	objmsg = (struct msg_objcreatedmap *) buf;
 
 	printf("%s map object created at %p by pid %d: start:%p size:%ld name:%s prot:%x type:%s\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid,
 		objmsg->start, objmsg->size, objmsg->name, objmsg->prot, maptypes[objmsg->type - 1]);
 }
@@ -98,7 +98,7 @@ static void decode_obj_created_pipe(char *buf)
 	objmsg = (struct msg_objcreatedpipe *) buf;
 
 	printf("%s pipe object created at %p by pid %d: fd:%d flags:%x [%s]\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid,
 		objmsg->fd, objmsg->flags,
 		objmsg->reader ? "reader" : "writer");
@@ -112,7 +112,7 @@ static void decode_obj_created_perf(char *buf)
 
 	objmsg = (struct msg_objcreatedperf *) buf;
 	printf("%s perf object created at %p by pid %d: fd:%d pid:%d cpu:%d group_fd:%d flags:%lx eventattr len:%d\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid,
 		objmsg->fd, objmsg->pid, objmsg->cpu, objmsg->group_fd, objmsg->flags,
 		objmsg->eventattrsize);
@@ -131,7 +131,7 @@ static void decode_obj_created_epoll(char *buf)
 	objmsg = (struct msg_objcreatedepoll *) buf;
 
 	printf("%s epoll object created at %p by pid %d: fd:%d create1: %s flags:%x\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->fd,
 		objmsg->create1 ? "false" : "true",
 		objmsg->flags);
@@ -143,7 +143,7 @@ static void decode_obj_created_eventfd(char *buf)
 	objmsg = (struct msg_objcreatedeventfd *) buf;
 
 	printf("%s eventfd object created at %p by pid %d: fd:%d count: %d flags:%x\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->fd,
 		objmsg->count, objmsg->flags);
 }
@@ -155,7 +155,7 @@ static void decode_obj_created_timerfd(char *buf)
 	objmsg = (struct msg_objcreatedtimerfd *) buf;
 
 	printf("%s timerfd object created at %p by pid %d: fd:%d clockid: %d flags:%x\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->fd,
 		objmsg->clockid, objmsg->flags);
 }
@@ -168,13 +168,13 @@ static void decode_obj_created_testfile(char *buf)
 
 	if (objmsg->fopened) {
 		printf("%s testfile object created at %p by pid %d: fd %d = fopen(\"%s\") ; fcntl(fd, 0x%x)\n",
-			objmsg->hdr.global ? "local" : "global",
+			objmsg->hdr.global ? "global" : "local",
 			objmsg->hdr.address, objmsg->hdr.pid,
 			objmsg->fd, objmsg->filename,
 			objmsg->fcntl_flags);
 	} else {
 		printf("%s testfile object created at %p by pid %d: fd %d = open(\"%s\", 0x%x)\n",
-			objmsg->hdr.global ? "local" : "global",
+			objmsg->hdr.global ? "global" : "local",
 			objmsg->hdr.address, objmsg->hdr.pid,
 			objmsg->fd, objmsg->filename, objmsg->flags);
 	}
@@ -186,7 +186,7 @@ static void decode_obj_created_memfd(char *buf)
 	objmsg = (struct msg_objcreatedmemfd *) buf;
 
 	printf("%s memfd object created at %p by pid %d: fd:%d name: %s flags:%x\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->fd,
 		objmsg->name, objmsg->flags);
 }
@@ -197,7 +197,7 @@ static void decode_obj_created_drm(char *buf)
 	objmsg = (struct msg_objcreateddrm *) buf;
 
 	printf("%s drm object created at %p by pid %d: fd:%d\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->fd);
 }
 
@@ -207,7 +207,7 @@ static void decode_obj_created_inotify(char *buf)
 	objmsg = (struct msg_objcreatedinotify *) buf;
 
 	printf("%s inotify object created at %p by pid %d: fd:%d flags:%x\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->fd, objmsg->flags);
 }
 
@@ -217,7 +217,7 @@ static void decode_obj_created_userfault(char *buf)
 	objmsg = (struct msg_objcreateduserfault *) buf;
 
 	printf("%s userfault object created at %p by pid %d: fd:%d flags:%x\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->fd, objmsg->flags);
 }
 
@@ -227,7 +227,7 @@ static void decode_obj_created_fanotify(char *buf)
 	objmsg = (struct msg_objcreatedfanotify *) buf;
 
 	printf("%s fanotify object created at %p by pid %d: fd:%d flags:%x eventflags:%x\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->fd,
 		objmsg->flags, objmsg->eventflags);
 }
@@ -244,7 +244,7 @@ static void decode_obj_created_bpfmap(char *buf)
 	objmsg = (struct msg_objcreatedbpfmap *) buf;
 
 	printf("%s bpf map object created at %p by pid %d: fd:%d type:%s\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->map_fd,
 		bpfmaptypes[objmsg->map_type]);
 }
@@ -255,7 +255,7 @@ static void decode_obj_created_socket(char *buf)
 	objmsg = (struct msg_objcreatedsocket *) buf;
 
 	printf("%s socket object created at %p by pid %d: fd:%d family:%d type:%d protocol:%d\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid, objmsg->si.fd,
 		objmsg->si.triplet.family,
 		objmsg->si.triplet.type,
@@ -268,7 +268,7 @@ static void decode_obj_created_futex(char *buf)
 	objmsg = (struct msg_objcreatedfutex *) buf;
 
 	printf("%s futex object created at %p by pid %d: futex:%d owner:%d\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid,
 		objmsg->futex, objmsg->owner);
 }
@@ -279,7 +279,7 @@ static void decode_obj_created_shm(char *buf)
 	objmsg = (struct msg_objcreatedshm *) buf;
 
 	printf("%s shm object created at %p by pid %d: id:%u size:%zu flags:%x ptr:%p\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid,
 		objmsg->id, objmsg->size, objmsg->flags, objmsg->ptr);
 }
@@ -290,7 +290,7 @@ static void decode_obj_destroyed(char *buf)
 	objmsg = (struct msg_objdestroyed *) buf;
 
 	printf("%s object at %p destroyed by pid %d. type:%d\n",
-		objmsg->hdr.global ? "local" : "global",
+		objmsg->hdr.global ? "global" : "local",
 		objmsg->hdr.address, objmsg->hdr.pid,
 		objmsg->hdr.type);
 }
