b4a52de7b7f78cebe9e8bc648185a25c43aa4ab3
Marcin Nowakowski <marcin.nowakowski@imgtec.com>
2017-09-07 14:14:11 +0200

    mips: add n32 and 64 ABIs
    MIPS has a different set of syscalls for n32 and 64-bit ABIs.
    They are placed in a different order and start from a different offset,
    so add new syscall tables for these ABIs and define SYSCALL_OFFSET
    appropriately.
    
    Signed-off-by: Marcin Nowakowski <marcin.nowakowski@imgtec.com>

--- include/arch-mips.h
+++ include/arch-mips.h
@@ -6,6 +6,13 @@
 #define TASK_SIZE (PAGE_OFFSET)
 #define PAGE_SHIFT 12
 #define PTE_FILE_MAX_BITS 31
+
+#if _MIPS_SIM == _ABIO32
 #define SYSCALL_OFFSET 4000
+#elif _MIPS_SIM == _ABIN32
+#define SYSCALL_OFFSET 6000
+#elif _MIPS_SIM == _ABI64
+#define SYSCALL_OFFSET 5000
+#endif
 
 #define SYSCALLS syscalls_mips
--- include/syscalls-mips.h
+++ include/syscalls-mips.h
@@ -4,4 +4,12 @@
 #include "syscall.h"
 #include "syscalls/syscalls.h"
 
+#if _MIPS_SIM == _ABIO32
 #include "syscalls-mips-o32.h"
+#elif _MIPS_SIM == _ABIN32
+#include "syscalls-mips-n32.h"
+#elif _MIPS_SIM == _ABI64
+#include "syscalls-mips-64.h"
+#else
+#error Unknown MIPS ABI
+#endif
