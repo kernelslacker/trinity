7cef0554e1acfe2a5dd7f6ce36d36e1e6f3a682b
Dave Jones <davej@codemonkey.org.uk>
2017-10-06 13:58:50 -0400

    ignore possible error codes like ERESTARTSYS
    These are usually only seen by libc, and never passed on to apps,
    but Trinity is doing raw syscalls, so we have to worry about these.

--- syscall.c
+++ syscall.c
@@ -278,7 +278,13 @@ void handle_syscall_ret(struct syscallrecord *rec)
 			if (err < NR_ERRNOS) {
 				entry->errnos[err]++;
 			} else {
-				printf("errno out of range: %d:%s\n", err, strerror(err));
+				// "These should never be seen by user programs."
+				// But trinity isn't a 'normal' user program, we're doing
+				// stuff that libc hides from apps.
+				if (err < 512 || err > 530)
+					printf("errno out of range after doing %s: %d:%s\n",
+						entry->name,
+						err, strerror(err));
 			}
 			shm->stats.failures++;
 		}
