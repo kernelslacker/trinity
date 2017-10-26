c87e6af192dd7682dd7dd4883a52797ecedc8af5
Dave Jones <davej@codemonkey.org.uk>
2017-10-26 10:21:04 -0400

    update hashes

--- scripts/hashcheck.sh
+++ scripts/hashcheck.sh
@@ -20,12 +20,16 @@ check()
   fi
 }
 
+
 # new syscalls
-check include/uapi/asm-generic/unistd.h    f639f3e7152473778b30cbc59b55acb36b9970f4 sys_
+# Note: Commented out checks are likely out of date, but they're for architectures
+#  I don't have time to care about. Step up and contribute if you care.
+#
+check include/uapi/asm-generic/unistd.h    e1f35cf48a7ae6a07949ad3e875cb7937c2c6cf0 sys_
 #check arch/alpha/include/uapi/asm/unistd.h 51fa669a21d8f26a0c9de8280a3cfd9c257a0d28 _NR_
 #check arch/arm/include/uapi/asm/unistd.h   7c601d436a3ebbe05a9b6813c0ffcf8eedaf101b SYSCALL
 check arch/x86/entry/syscalls/syscall_32.tbl     1b9ed3050a88b2d84bdb7dbcfe2ab23b8234bee8 sys_
-check arch/x86/entry/syscalls/syscall_64.tbl     d96a3053b5267a3191df9a459274dbec738947f8 sys_
+check arch/x86/entry/syscalls/syscall_64.tbl     d52841da5ee801bfd7277fccf5b76d30566ace1a sys_
 check arch/x86/entry/syscalls/syscall_64.tbl     da39a3ee5e6b4b0d3255bfef95601890afd80709 stub_
 #check arch/ia64/kernel/entry.S             e6a21b973609ec08cd19b0b8c67830f8570e93ef "data8\ sys_"
 #check arch/mips/kernel/scall32-o32.S       b1501a675dd998fe2af68ae43cd797cf18d67b33 sys_
@@ -36,16 +40,16 @@ check arch/x86/entry/syscalls/syscall_64.tbl     da39a3ee5e6b4b0d3255bfef9560189
 #check arch/sparc/kernel/systbls_32.S       98100f9dedc82d82ac18a33fd68dc7e4852ffcb8 sys_
 
 # new setsockopt values
-check include/uapi/asm-generic/socket.h	07d5ff722694d6dbb02b9e176d5155a0bfb82dcb SO_
+check include/uapi/asm-generic/socket.h	848bfd5d1700324c48e1216da2abdda7515b230b SO_
 check include/uapi/linux/tcp.h          ed5bbfa500b64aad4d742b2eed0d35ce22ab36b8 \#define\ TCP_
 check include/uapi/linux/in.h           0116e6878df350e74ec730fd9f455efb95e510c8 \#define\ IP_
 
 # Check for new packet families
-check include/linux/socket.h 1d7b02dedfa31669e23495f3c854e6e1f0fc2ce2 SOL_
+check include/linux/socket.h 17b4779bb45a76825972f41d1fa1f8d2b908a21b SOL_
 check include/linux/socket.h 80c34154051e712d03e81577d494efe8f09139a8 AF_
 
 # MSG_ flags
-check include/linux/socket.h 8a16113605d79d8f280345857d40779a3b8b48cc \ MSG_
+check include/linux/socket.h 91b84d05363d32e88375567a9bb7a4e16d738ea8 \ MSG_
 
 # new netlink protos
 check include/uapi/linux/netlink.h 1f391f35d0443f432871cbb366d1150973eb2bcb "#define\ NETLINK_"
@@ -60,7 +64,7 @@ check include/uapi/asm-generic/fcntl.h 98cf236ce61466e272727683eba11493d41c6b27 
 check include/linux/splice.h fb753f99bf38f7c041427c442f199aa2049fa329 SPLICE_F_
 
 # new madvise flags
-check include/uapi/asm-generic/mman-common.h 2baddc86959839fbc367eff0c3fe830bce866d7c MADV_
+check include/uapi/asm-generic/mman-common.h c6e990af02fd65c13c5e25c2d4e7dffa32724b23 MADV_
 
 # new mremap flags
 check include/uapi/linux/mman.h 556bcea4a4581a03a600c2d383c462840f1c0e6c MREMAP_
