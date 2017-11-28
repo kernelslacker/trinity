6278fa908cc579ecbef97d4ee9d9af988066a46e
Chunyu Hu <chuhu@redhat.com>
2017-11-28 05:56:44 -0500

    hung_task: roll back test to origin
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>

--- kmods/hung_task/hung_task.c
+++ kmods/hung_task/hung_task.c
@@ -7,7 +7,7 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 
-static int hang_duration = 10;
+static int dummy_arg = 0;
 
 static struct test_hung_task_data {
         struct mutex dead_lock;
@@ -29,39 +29,33 @@ static enum hrtimer_restart test_hrtimer_free_mutex_func(struct hrtimer* hr)
 static int init_hung_task_test(void)
 {
         ktime_t ktime;  
-        ktime = ktime_set(hang_duration, 0);   //  10* 1000 ms  
+        ktime = ktime_set(20, 0);   //  10* 1000 ms  
 
         pr_info("loading module");
 
         pr_info("Init and take the mutex in init.");
 
         mutex_init(&test_data.dead_lock);
-		mutex_lock(&test_data.dead_lock);
+	mutex_lock(&test_data.dead_lock);
 
         pr_info("Start a alarm for 100s");
 
-		hrtimer_init(&test_data.hrtimer_release_mutex, CLOCK_MONOTONIC,
+	hrtimer_init(&test_data.hrtimer_release_mutex, CLOCK_MONOTONIC,
                         HRTIMER_MODE_REL);
-		test_data.hrtimer_release_mutex.function = test_hrtimer_free_mutex_func;
+	test_data.hrtimer_release_mutex.function = test_hrtimer_free_mutex_func;
         hrtimer_start(&test_data.hrtimer_release_mutex, ktime,
                         HRTIMER_MODE_REL);
-
-        pr_info("relock the mutex, waiting for unlock ...");
-		mutex_lock(&test_data.dead_lock);
-
+	mutex_lock(&test_data.dead_lock);
         return 0;
 }
 
 static void exit_hung_task_test(void)
 {
         ktime_t rem;
-        schedule_timeout_interruptible(10000);
         while (hrtimer_active(&test_data.hrtimer_release_mutex)) {
                 rem = hrtimer_get_remaining(&test_data.hrtimer_release_mutex);
-                printk("hrtimer is active, remaining %lld secs\n",
-                        rem / NSEC_PER_SEC);
-                set_current_state(TASK_INTERRUPTIBLE);
-                msleep(1000);
+                printk("hrtimer is active, remaining %lld secs",
+                        rem.tv64 / NSEC_PER_SEC);
         }
         pr_info("hrtimer has been done...");
         printk("unloading module\n");
@@ -71,6 +65,6 @@ module_init(init_hung_task_test);
 module_exit(exit_hung_task_test);
 
 MODULE_LICENSE("GPL");
-module_param(hang_duration, int, 0444);
-MODULE_PARM_DESC(hang_duration, "How long will the hung task block.");
+module_param(dummy_arg, int, 0444);
+MODULE_PARM_DESC(dummy_arg, "Test parameter...");
 
