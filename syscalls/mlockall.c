313d5a89bdd8694b6cd7582cb6873d53ebd69a47
Dave Jones <davej@codemonkey.org.uk>
2019-06-14 12:36:38 -0400

    cppcheck fixes

--- syscalls/mlockall.c
+++ syscalls/mlockall.c
@@ -18,19 +18,6 @@
 #define MCL_ONFAULT	4
 #endif
 
-static void sanitise_mlockall(struct syscallrecord *rec)
-{
-	if (rec->a1 != 0)
-		return;
-
-	/*
-	 * There are two invalid bit patterns for MCL flags, 0, and MCL_ONFAULT
-	 * alone.  All other combinations should be valid.
-	 */
-	while (rec->a1 == 0 || rec->a1 == MCL_ONFAULT)
-		rec->a1 = (RAND_BYTE() & 0x07);
-}
-
 static unsigned long mlockall_flags[] = {
 	MCL_CURRENT, MCL_FUTURE, MCL_ONFAULT,
 };
@@ -42,5 +29,4 @@ struct syscallentry syscall_mlockall = {
 	.arg1type = ARG_LIST,
 	.arg1list = ARGLIST(mlockall_flags),
 	.group = GROUP_VM,
-	.sanitise = sanitise_mlockall,
 };
