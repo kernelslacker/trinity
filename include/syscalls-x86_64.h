fcaf8297f02680e48044216c707d8960a0f59930
Dave Jones <davej@codemonkey.org.uk>
2019-06-27 12:39:31 -0400

    basic fsconfig support

--- include/syscalls-x86_64.h
+++ include/syscalls-x86_64.h
@@ -442,4 +442,5 @@ struct syscalltable syscalls_x86_64[] = {
 	{ .entry = &syscall_open_tree},
 	{ .entry = &syscall_move_mount},
 	{ .entry = &syscall_fsopen },
+	{ .entry = &syscall_fsconfig },
 };
