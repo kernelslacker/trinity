dd950fa33ed5d60758923ca182f4f71f78640208
wuruilong <wuruilong@loongson.cn>
2023-09-13 01:28:53 +0000

    add support for loongarch

---
+++ include/arch-loongarch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define PAGE_OFFSET             0x9000000000000000
+#define TASK_SIZE               (PAGE_OFFSET)
+#define MODULE_ADDR		0xffff800000000000
+#define KERNEL_ADDR 		0x9000000000000000
+
+#define PAGE_SHIFT    	 	14
+#define PTE_FILE_MAX_BITS       31
+
+#define PTRACE_GETREGS          0
+#define PTRACE_GETFPREGS        0
+#define PTRACE_SETREGS          0
+#define PTRACE_SETFPREGS        0
+
+#define SYSCALLS syscalls_loongarch
