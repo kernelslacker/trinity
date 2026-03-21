aa469edd8a165ac8e6a86ddf25b00b0666aa2ae7
Dave Jones <davej@codemonkey.org.uk>
2026-03-21 09:02:52 -0700

    Add ARG_FD_PIDFD typed fd support
    Add full pidfd typed fd infrastructure:
    - ARG_FD_PIDFD enum value in syscall.h
    - OBJ_FD_PIDFD object type and pidfdobj struct
    - pidfd fd provider (fds/pidfd.c) that opens pidfds for
      our own pid and pid 1
    - get_typed_fd() cases in fds.c and generate-args.c
    - post handler on pidfd_open to track dynamically created pidfds
    
    Convert all pidfd-consuming syscalls to use ARG_FD_PIDFD:
    - pidfd_getfd (arg1)
    - pidfd_send_signal (arg1)
    - process_madvise (arg1)
    - process_mrelease (arg1)
    
    Summary: Previously these syscalls received random fds which would
    almost always fail with EBADF. Now they get actual pidfds from the
    object pool, enabling meaningful coverage of the pidfd codepaths.

--- syscalls/pidfd_open.c
+++ syscalls/pidfd_open.c
@@ -1,7 +1,23 @@
 /*
  * SYSCALL_DEFINE2(pidfd_open, pid_t, pid, unsigned int, flags)
  */
+#include "objects.h"
 #include "sanitise.h"
+#include "utils.h"
+
+static void post_pidfd_open(struct syscallrecord *rec)
+{
+	struct object *new;
+	int fd = rec->retval;
+
+	if (fd == -1)
+		return;
+
+	new = alloc_object();
+	new->pidfdobj.fd = fd;
+	new->pidfdobj.pid = rec->a1;
+	add_object(new, OBJ_LOCAL, OBJ_FD_PIDFD);
+}
 
 static unsigned long pidfd_open_flags[] = {
 	0,
@@ -17,4 +33,5 @@ struct syscallentry syscall_pidfd_open = {
 	.arg2type = ARG_LIST,
 	.arg2list = ARGLIST(pidfd_open_flags),
 	.rettype = RET_FD,
+	.post = post_pidfd_open,
 };
