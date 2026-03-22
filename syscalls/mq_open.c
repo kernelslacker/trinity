9bc9ab2eb36439f37721af13de2302ad49aabef3
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 12:28:12 -0700

    mq_open: add oflag array
    Pass valid open flags (O_RDONLY, O_WRONLY, O_RDWR, O_CREAT,
    O_EXCL, O_NONBLOCK) instead of random values.

--- syscalls/mq_open.c
+++ syscalls/mq_open.c
@@ -1,7 +1,13 @@
 /*
- * SYSCALL_DEFINE4(mq_open, const char __user *, u_name, int, oflag, mode_t, mode, struct mq_attr __user *, u_a>
+ * SYSCALL_DEFINE4(mq_open, const char __user *, u_name, int, oflag, mode_t, mode, struct mq_attr __user *, u_attr)
  */
+#include <fcntl.h>
 #include "sanitise.h"
+
+static unsigned long mq_open_flags[] = {
+	O_RDONLY, O_WRONLY, O_RDWR,
+	O_CREAT, O_EXCL, O_NONBLOCK,
+};
 
 struct syscallentry syscall_mq_open = {
 	.name = "mq_open",
@@ -10,6 +16,8 @@ struct syscallentry syscall_mq_open = {
 	.arg1name = "u_name",
 	.arg1type = ARG_ADDRESS,
 	.arg2name = "oflag",
+	.arg2type = ARG_LIST,
+	.arg2list = ARGLIST(mq_open_flags),
 	.arg3name = "mode",
 	.arg3type = ARG_MODE_T,
 	.arg4name = "u_attr",
