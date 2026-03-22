ec56f397e5cac48fd8f4686c22f87173b89b34a8
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:37:14 -0700

    memfd_create: add MFD_HUGETLB, MFD_NOEXEC_SEAL, MFD_EXEC flags
    MFD_HUGETLB was already defined in memfd.h but missing from the
    syscall's flag list. Add it along with MFD_NOEXEC_SEAL (v6.3) and
    MFD_EXEC (v6.3) which control the executable permission defaults
    for memfd mappings.

--- include/memfd.h
+++ include/memfd.h
@@ -6,6 +6,14 @@
 #define MFD_HUGETLB		0x0004U
 #endif
 
+#ifndef MFD_NOEXEC_SEAL
+#define MFD_NOEXEC_SEAL		0x0008U
+#endif
+
+#ifndef MFD_EXEC
+#define MFD_EXEC		0x0010U
+#endif
+
 // FIXME: Keep all this here until glibc supports it.
 #ifndef SYS_memfd_create
 #ifdef __x86_64__
