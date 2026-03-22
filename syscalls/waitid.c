0b18ac52e2344a4edee95d05c7fd6d4670a0c520
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:57:51 -0700

    wait*: add WNOHANG/WUNTRACED/WCONTINUED option flags
    wait4 and waitpid get WNOHANG|WUNTRACED|WCONTINUED as ARG_LIST.
    waitid also gets WEXITED|WSTOPPED|WNOWAIT.

--- syscalls/waitid.c
+++ syscalls/waitid.c
@@ -2,7 +2,12 @@
  * SYSCALL_DEFINE5(waitid, int, which, pid_t, upid, struct siginfo __user *,
 	infop, int, options, struct rusage __user *, ru)
  */
+#include <sys/wait.h>
 #include "sanitise.h"
+
+static unsigned long waitid_options[] = {
+	WNOHANG, WEXITED, WSTOPPED, WCONTINUED, WNOWAIT,
+};
 
 struct syscallentry syscall_waitid = {
 	.name = "waitid",
@@ -14,6 +19,8 @@ struct syscallentry syscall_waitid = {
 	.arg3name = "infop",
 	.arg3type = ARG_ADDRESS,
 	.arg4name = "options",
+	.arg4type = ARG_LIST,
+	.arg4list = ARGLIST(waitid_options),
 	.arg5name = "ru",
 	.arg5type = ARG_ADDRESS,
 };
