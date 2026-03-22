d731019e3dd9ca19efebb400e9910b88358ed156
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:57:58 -0700

    sigprocmask: add SIG_BLOCK/UNBLOCK/SETMASK how values
    Both rt_sigprocmask and sigprocmask now pick from the three
    valid "how" values instead of passing random integers.

--- syscalls/rt_sigprocmask.c
+++ syscalls/rt_sigprocmask.c
@@ -12,12 +12,18 @@ static void sanitise_rt_sigprocmask(struct syscallrecord *rec)
 	rec->a4 = sizeof(sigset_t);
 }
 
+static unsigned long sigprocmask_how[] = {
+	SIG_BLOCK, SIG_UNBLOCK, SIG_SETMASK,
+};
+
 struct syscallentry syscall_rt_sigprocmask = {
 	.name = "rt_sigprocmask",
 	.group = GROUP_SIGNAL,
 	.num_args = 4,
 	.sanitise = sanitise_rt_sigprocmask,
 	.arg1name = "how",
+	.arg1type = ARG_OP,
+	.arg1list = ARGLIST(sigprocmask_how),
 	.arg2name = "set",
 	.arg2type = ARG_ADDRESS,
 	.arg3name = "oset",
