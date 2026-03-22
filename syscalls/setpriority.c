875d741754819fae9f31b72d0ca5eb29973c860a
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:58:33 -0700

    setpriority: add PRIO_PROCESS/PGRP/USER which values

--- syscalls/setpriority.c
+++ syscalls/setpriority.c
@@ -1,12 +1,19 @@
 /*
  * SYSCALL_DEFINE3(setpriority, int, which, int, who, int, niceval)
  */
+#include <sys/resource.h>
 #include "sanitise.h"
+
+static unsigned long setpriority_which[] = {
+	PRIO_PROCESS, PRIO_PGRP, PRIO_USER,
+};
 
 struct syscallentry syscall_setpriority = {
 	.name = "setpriority",
 	.num_args = 3,
 	.arg1name = "which",
+	.arg1type = ARG_OP,
+	.arg1list = ARGLIST(setpriority_which),
 	.arg2name = "who",
 	.arg3name = "niceval",
 };
