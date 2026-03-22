8f140de13c2f1f82eec4a89418f55499279b489e
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:58:16 -0700

    ioprio_get/set: add IOPRIO_WHO_PROCESS/PGRP/USER which values

--- syscalls/ioprio_set.c
+++ syscalls/ioprio_set.c
@@ -3,10 +3,22 @@
  */
 #include "sanitise.h"
 
+#ifndef IOPRIO_WHO_PROCESS
+#define IOPRIO_WHO_PROCESS	1
+#define IOPRIO_WHO_PGRP		2
+#define IOPRIO_WHO_USER		3
+#endif
+
+static unsigned long ioprio_who[] = {
+	IOPRIO_WHO_PROCESS, IOPRIO_WHO_PGRP, IOPRIO_WHO_USER,
+};
+
 struct syscallentry syscall_ioprio_set = {
 	.name = "ioprio_set",
 	.num_args = 3,
 	.arg1name = "which",
+	.arg1type = ARG_OP,
+	.arg1list = ARGLIST(ioprio_who),
 	.arg2name = "who",
 	.arg3name = "ioprio",
 };
