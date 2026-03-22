11a86d2ca73c1dcb907699123fec4f902a42eab7
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:58:10 -0700

    getitimer/setitimer: add ITIMER_REAL/VIRTUAL/PROF which values

--- syscalls/setitimer.c
+++ syscalls/setitimer.c
@@ -1,7 +1,12 @@
 /*
  * SYSCALL_DEFINE3(setitimer, int, which, struct itimerval __user *, value, struct itimerval __user *, ovalue)
  */
+#include <sys/time.h>
 #include "sanitise.h"
+
+static unsigned long setitimer_which[] = {
+	ITIMER_REAL, ITIMER_VIRTUAL, ITIMER_PROF,
+};
 
 struct syscallentry syscall_setitimer = {
 	.flags = AVOID_SYSCALL,		/* setitimer interferes with alarm() */
@@ -9,6 +14,8 @@ struct syscallentry syscall_setitimer = {
 	.group = GROUP_TIME,
 	.num_args = 3,
 	.arg1name = "which",
+	.arg1type = ARG_OP,
+	.arg1list = ARGLIST(setitimer_which),
 	.arg2name = "value",
 	.arg2type = ARG_ADDRESS,
 	.arg3name = "ovalue",
