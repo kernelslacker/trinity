909f5f5db8e14b65a2d8e15a28d14e6586703a99
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 15:28:16 -0700

    SysV IPC: add arg type annotations for all IPC syscalls
    - msg{ctl,get,snd,rcv}: ARG_RANGE for msqid/key (0-65535)
    - sem{ctl,op,timedop}: ARG_RANGE for semid (0-65535)
    - semctl: ARG_RANGE for semnum (0-250), ARG_ADDRESS for union semun arg
    - semop, semtimedop: ARG_LEN for nsops
    - shm{at,ctl,get}: ARG_RANGE for shmid/key (0-65535)

--- syscalls/semop.c
+++ syscalls/semop.c
@@ -8,7 +8,11 @@ struct syscallentry syscall_semop = {
 	.group = GROUP_IPC,
 	.num_args = 3,
 	.arg1name = "semid",
+	.arg1type = ARG_RANGE,
+	.low1range = 0,
+	.hi1range = 65535,
 	.arg2name = "tsops",
 	.arg2type = ARG_ADDRESS,
 	.arg3name = "nsops",
+	.arg3type = ARG_LEN,
 };
--- syscalls/semtimedop.c
+++ syscalls/semtimedop.c
@@ -9,9 +9,13 @@ struct syscallentry syscall_semtimedop = {
 	.group = GROUP_IPC,
 	.num_args = 4,
 	.arg1name = "semid",
+	.arg1type = ARG_RANGE,
+	.low1range = 0,
+	.hi1range = 65535,
 	.arg2name = "tsops",
 	.arg2type = ARG_ADDRESS,
 	.arg3name = "nsops",
+	.arg3type = ARG_LEN,
 	.arg4name = "timeout",
 	.arg4type = ARG_ADDRESS,
 };
