15f1f1bc7e1f00fced5daf1529ec8e8b9b86d32b
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 14:39:08 -0700

    nice, setpriority: add ARG_RANGE for niceness values
    Constrain nice increment and setpriority niceval to the valid
    range of -20 to 19 instead of random garbage values. These were
    bare skeleton args with no type information.

--- syscalls/nice.c
+++ syscalls/nice.c
@@ -7,5 +7,8 @@ struct syscallentry syscall_nice = {
 	.name = "nice",
 	.num_args = 1,
 	.arg1name = "increment",
+	.arg1type = ARG_RANGE,
+	.low1range = -20,
+	.hi1range = 19,
 	.group = GROUP_PROCESS,
 };
