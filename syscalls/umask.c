d03e86bdc05677e4d59555d4635d56b8026e78b8
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 14:39:10 -0700

    umask: add ARG_RANGE for permission mask
    Constrain umask's mask argument to 0-07777 (valid permission
    bits including setuid/setgid/sticky) instead of random values.

--- syscalls/umask.c
+++ syscalls/umask.c
@@ -7,5 +7,8 @@ struct syscallentry syscall_umask = {
 	.name = "umask",
 	.num_args = 1,
 	.arg1name = "mask",
+	.arg1type = ARG_RANGE,
+	.low1range = 0,
+	.hi1range = 07777,
 	.group = GROUP_PROCESS,
 };
