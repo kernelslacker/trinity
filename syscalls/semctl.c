57b59f10cbe817bb4cfa88b988010bc75e9051f8
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 12:28:07 -0700

    semctl: add IPC/SEM command array
    Pass actual semctl commands (IPC_RMID, GETVAL, SETVAL, etc.)
    instead of random integers.

--- syscalls/semctl.c
+++ syscalls/semctl.c
@@ -1,7 +1,16 @@
 /*
  * SYSCALL_DEFINE(semctl)(int semid, int semnum, int cmd, union semun arg)
  */
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include "sanitise.h"
+
+static unsigned long semctl_cmds[] = {
+	IPC_RMID, IPC_SET, IPC_STAT, IPC_INFO,
+	GETPID, GETVAL, GETALL, GETNCNT, GETZCNT,
+	SETVAL, SETALL,
+	SEM_STAT, SEM_INFO, SEM_STAT_ANY,
+};
 
 struct syscallentry syscall_semctl = {
 	.name = "semctl",
@@ -10,5 +19,7 @@ struct syscallentry syscall_semctl = {
 	.arg1name = "semid",
 	.arg2name = "semnum",
 	.arg3name = "cmd",
+	.arg3type = ARG_OP,
+	.arg3list = ARGLIST(semctl_cmds),
 	.arg4name = "arg",
 };
