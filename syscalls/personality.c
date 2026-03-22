69467155647cddd6fd20701c010e604df2a62c2f
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:58:35 -0700

    personality: add PER_LINUX through PER_LINUX32 values

--- syscalls/personality.c
+++ syscalls/personality.c
@@ -1,11 +1,20 @@
 /*
  * SYSCALL_DEFINE1(personality, unsigned int, personality
  */
+#include <sys/personality.h>
 #include "sanitise.h"
+
+static unsigned long personalities[] = {
+	PER_LINUX, PER_SVR4, PER_SVR3, PER_SCOSVR3,
+	PER_OSR5, PER_WYSEV386, PER_ISCR4, PER_BSD,
+	PER_LINUX32,
+};
 
 struct syscallentry syscall_personality = {
 	.name = "personality",
 	.group = GROUP_PROCESS,
 	.num_args = 1,
 	.arg1name = "personality",
+	.arg1type = ARG_OP,
+	.arg1list = ARGLIST(personalities),
 };
