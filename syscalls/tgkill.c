e5d507cd93bd7ac93caf43884bbc34b757e6cb5f
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 12:20:18 -0700

    kill/tgkill/tkill: add safe signal array
    Constrain sig argument to actual signal numbers, excluding
    SIGSTOP (unconditional freeze), SIGTSTP/SIGTTIN/SIGTTOU (job
    control stops), and SIGINT (would kill trinity main process).

--- syscalls/tgkill.c
+++ syscalls/tgkill.c
@@ -1,7 +1,16 @@
 /*
  * SYSCALL_DEFINE3(tgkill, pid_t, tgid, pid_t, pid, int, sig)
  */
+#include <signal.h>
 #include "sanitise.h"
+
+static unsigned long safe_signals[] = {
+	SIGHUP, SIGQUIT, SIGILL, SIGTRAP, SIGABRT,
+	SIGBUS, SIGFPE, SIGUSR1, SIGSEGV, SIGUSR2, SIGPIPE,
+	SIGALRM, SIGTERM, SIGCHLD, SIGCONT,
+	SIGURG, SIGXCPU, SIGXFSZ, SIGVTALRM,
+	SIGPROF, SIGWINCH, SIGIO, SIGSYS,
+};
 
 struct syscallentry syscall_tgkill = {
 	.name = "tgkill",
@@ -12,5 +21,7 @@ struct syscallentry syscall_tgkill = {
 	.arg2name = "pid",
 	.arg2type = ARG_PID,
 	.arg3name = "sig",
+	.arg3type = ARG_OP,
+	.arg3list = ARGLIST(safe_signals),
 	.flags = AVOID_SYSCALL,
 };
