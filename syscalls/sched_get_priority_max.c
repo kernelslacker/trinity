7f264b9e6a637da2b742ffdf20604861886c8413
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 12:27:56 -0700

    sched_get_priority_max/min: add SCHED_* policy array
    Pass actual scheduling policies instead of random integers.

--- syscalls/sched_get_priority_max.c
+++ syscalls/sched_get_priority_max.c
@@ -1,11 +1,19 @@
 /*
  * SYSCALL_DEFINE1(sched_get_priority_max, int, policy)
  */
+#include <sched.h>
 #include "sanitise.h"
+
+static unsigned long sched_policies[] = {
+	SCHED_OTHER, SCHED_FIFO, SCHED_RR,
+	SCHED_BATCH, SCHED_IDLE, SCHED_DEADLINE,
+};
 
 struct syscallentry syscall_sched_get_priority_max = {
 	.name = "sched_get_priority_max",
 	.group = GROUP_SCHED,
 	.num_args = 1,
 	.arg1name = "policy",
+	.arg1type = ARG_OP,
+	.arg1list = ARGLIST(sched_policies),
 };
