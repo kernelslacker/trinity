d59e21ac5c0d3747fb18e54220bf61e2a7ad0bcc
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 16:48:25 -0700

    prctl: add CAP_* values table for capability operations
    PR_CAPBSET_READ, PR_CAPBSET_DROP, and PR_CAP_AMBIENT all take
    capability values as arguments. Previously these were pure random
    noise. Now they select from the full set of CAP_CHOWN through
    CAP_CHECKPOINT_RESTORE (0-40), hitting actual kernel capability
    checking paths instead of always getting -EINVAL.

--- syscalls/prctl.c
+++ syscalls/prctl.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdlib.h>
+#include <linux/capability.h>
 #include <linux/filter.h>
 #ifdef USE_SECCOMP
 #include <linux/seccomp.h>
@@ -47,6 +48,20 @@ static int prctl_opts[] = {
 };
 #define NR_PRCTL_OPTS ARRAY_SIZE(prctl_opts)
 
+static unsigned long cap_values[] = {
+	CAP_CHOWN, CAP_DAC_OVERRIDE, CAP_DAC_READ_SEARCH, CAP_FOWNER,
+	CAP_FSETID, CAP_KILL, CAP_SETGID, CAP_SETUID,
+	CAP_SETPCAP, CAP_LINUX_IMMUTABLE, CAP_NET_BIND_SERVICE, CAP_NET_BROADCAST,
+	CAP_NET_ADMIN, CAP_NET_RAW, CAP_IPC_LOCK, CAP_IPC_OWNER,
+	CAP_SYS_MODULE, CAP_SYS_RAWIO, CAP_SYS_CHROOT, CAP_SYS_PTRACE,
+	CAP_SYS_PACCT, CAP_SYS_ADMIN, CAP_SYS_BOOT, CAP_SYS_NICE,
+	CAP_SYS_RESOURCE, CAP_SYS_TIME, CAP_SYS_TTY_CONFIG, CAP_MKNOD,
+	CAP_LEASE, CAP_AUDIT_WRITE, CAP_AUDIT_CONTROL, CAP_SETFCAP,
+	CAP_MAC_OVERRIDE, CAP_MAC_ADMIN, CAP_SYSLOG, CAP_WAKE_ALARM,
+	CAP_BLOCK_SUSPEND, CAP_AUDIT_READ, CAP_PERFMON, CAP_BPF,
+	CAP_CHECKPOINT_RESTORE,
+};
+
 
 #ifdef USE_SECCOMP
 static void do_set_seccomp(struct syscallrecord *rec)
@@ -76,6 +91,16 @@ static void sanitise_prctl(struct syscallrecord *rec)
 	switch (option) {
 	case PR_SET_SECCOMP:
 		do_set_seccomp(rec);
+		break;
+
+	case PR_CAPBSET_READ:
+	case PR_CAPBSET_DROP:
+		rec->a2 = RAND_ARRAY(cap_values);
+		break;
+
+	case PR_CAP_AMBIENT:
+		rec->a2 = RAND_RANGE(1, 4);
+		rec->a3 = RAND_ARRAY(cap_values);
 		break;
 
 	default:
