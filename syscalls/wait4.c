f9492dfec9d9d4c61649b58f67e571fbcaef24f0
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:57:51 -0700

    wait*: add WNOHANG/WUNTRACED/WCONTINUED option flags
    wait4 and waitpid get WNOHANG|WUNTRACED|WCONTINUED as ARG_LIST.
    waitid also gets WEXITED|WSTOPPED|WNOWAIT.

--- syscalls/wait4.c
+++ syscalls/wait4.c
@@ -2,7 +2,12 @@
  * SYSCALL_DEFINE4(wait4, pid_t, upid, int __user *, stat_addr,
 	 int, options, struct rusage __user *, ru)
  */
+#include <sys/wait.h>
 #include "sanitise.h"
+
+static unsigned long wait_options[] = {
+	WNOHANG, WUNTRACED, WCONTINUED,
+};
 
 struct syscallentry syscall_wait4 = {
 	.name = "wait4",
@@ -13,6 +18,8 @@ struct syscallentry syscall_wait4 = {
 	.arg2name = "stat_addr",
 	.arg2type = ARG_ADDRESS,
 	.arg3name = "options",
+	.arg3type = ARG_LIST,
+	.arg3list = ARGLIST(wait_options),
 	.arg4name = "ru",
 	.arg4type = ARG_ADDRESS,
 };
--- syscalls/waitpid.c
+++ syscalls/waitpid.c
@@ -1,7 +1,12 @@
 /*
  * SYSCALL_DEFINE3(waitpid, pid_t, pid, int __user *, stat_addr, int, options)
  */
+#include <sys/wait.h>
 #include "sanitise.h"
+
+static unsigned long wait_options[] = {
+	WNOHANG, WUNTRACED, WCONTINUED,
+};
 
 struct syscallentry syscall_waitpid = {
 	.name = "waitpid",
@@ -12,4 +17,6 @@ struct syscallentry syscall_waitpid = {
 	.arg2name = "stat_addr",
 	.arg2type = ARG_ADDRESS,
 	.arg3name = "options",
+	.arg3type = ARG_LIST,
+	.arg3list = ARGLIST(wait_options),
 };
