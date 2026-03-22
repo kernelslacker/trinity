0551ec3ab0ec208e88e4d3ae1eb3c7912a5bc7e6
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:58:36 -0700

    pidfd_send_signal: add signal number array
    Pick from actual signal numbers (SIGHUP through SIGSYS) instead
    of passing random values for the sig argument.
    
    Exclude SIGSTOP (unconditional freeze), SIGTSTP/SIGTTIN/SIGTTOU
    (job control stops), and SIGINT (would kill trinity main process).

--- syscalls/pidfd_send_signal.c
+++ syscalls/pidfd_send_signal.c
@@ -1,7 +1,16 @@
 /*
  *   SYSCALL_DEFINE4(pidfd_send_signal, int, pidfd, int, sig, siginfo_t __user *, info, unsigned int, flags)
  */
+#include <signal.h>
 #include "sanitise.h"
+
+static unsigned long pidfd_signals[] = {
+	SIGHUP, SIGQUIT, SIGILL, SIGTRAP, SIGABRT,
+	SIGBUS, SIGFPE, SIGUSR1, SIGSEGV, SIGUSR2, SIGPIPE,
+	SIGALRM, SIGTERM, SIGCHLD, SIGCONT,
+	SIGURG, SIGXCPU, SIGXFSZ, SIGVTALRM,
+	SIGPROF, SIGWINCH, SIGIO, SIGSYS,
+};
 
 static unsigned long pidfd_send_signal_flags[] = {
 	0,
@@ -14,6 +23,8 @@ struct syscallentry syscall_pidfd_send_signal = {
 	.arg1name = "pidfd",
 	.arg1type = ARG_FD_PIDFD,
 	.arg2name = "sig",
+	.arg2type = ARG_OP,
+	.arg2list = ARGLIST(pidfd_signals),
 	.arg3name = "info",
 	.arg3type = ARG_ADDRESS,
 	.arg4name = "flags",
