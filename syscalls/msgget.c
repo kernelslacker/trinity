5cd30510bbd8ca4ecbbed6d8abd42a78d2efbed9
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 12:28:02 -0700

    msgget/semget/shmget: add IPC_CREAT/IPC_EXCL flag arrays
    Constrain the flag argument to valid IPC creation flags instead
    of random values.

--- syscalls/msgget.c
+++ syscalls/msgget.c
@@ -1,7 +1,12 @@
 /*
  * SYSCALL_DEFINE2(msgget, key_t, key, int, msgflg)
  */
+#include <sys/ipc.h>
 #include "sanitise.h"
+
+static unsigned long ipc_flags[] = {
+	IPC_CREAT, IPC_EXCL,
+};
 
 struct syscallentry syscall_msgget = {
 	.name = "msgget",
@@ -9,4 +14,6 @@ struct syscallentry syscall_msgget = {
 	.num_args = 2,
 	.arg1name = "key",
 	.arg2name = "msgflg",
+	.arg2type = ARG_LIST,
+	.arg2list = ARGLIST(ipc_flags),
 };
