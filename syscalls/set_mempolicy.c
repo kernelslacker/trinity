49b8972375c1531634f08bd90ed8e7140d0e3531
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 11:44:11 -0700

    set_mempolicy: add mode values and VM group
    Replace bare integer mode with a proper array covering all MPOL_*
    modes up through MPOL_WEIGHTED_INTERLEAVE (kernel 6.9), and assign
    GROUP_VM.

--- syscalls/set_mempolicy.c
+++ syscalls/set_mempolicy.c
@@ -2,12 +2,32 @@
  * SYSCALL_DEFINE3(set_mempolicy, int, mode, unsigned long __user *, nmask, unsigned long, maxnode)
  */
 #include "sanitise.h"
+#include "compat.h"
+
+#ifndef MPOL_DEFAULT
+#define MPOL_DEFAULT	0
+#define MPOL_PREFERRED	1
+#define MPOL_BIND	2
+#define MPOL_INTERLEAVE	3
+#define MPOL_LOCAL	4
+#define MPOL_PREFERRED_MANY 5
+#define MPOL_WEIGHTED_INTERLEAVE 6
+#endif
+
+static unsigned long mempolicy_modes[] = {
+	MPOL_DEFAULT, MPOL_PREFERRED, MPOL_BIND,
+	MPOL_INTERLEAVE, MPOL_LOCAL, MPOL_PREFERRED_MANY,
+	MPOL_WEIGHTED_INTERLEAVE,
+};
 
 struct syscallentry syscall_set_mempolicy = {
 	.name = "set_mempolicy",
 	.num_args = 3,
 	.arg1name = "mode",
+	.arg1type = ARG_OP,
+	.arg1list = ARGLIST(mempolicy_modes),
 	.arg2name = "nmask",
 	.arg2type = ARG_ADDRESS,
 	.arg3name = "maxnode",
+	.group = GROUP_VM,
 };
