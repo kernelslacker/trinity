ff05ea83a4db9a6d77249e7d14888fe986cf9f4e
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 20:01:44 -0700

    listmount: generate valid struct mnt_id_req
    Fill mnt_id_req with proper size field and interesting mount IDs:
    LSMT_ROOT (list all), mount ID 1 (root), and random IDs. Allocate
    a properly-sized output buffer for mount ID results.

--- syscalls/listmount.c
+++ syscalls/listmount.c
@@ -3,6 +3,9 @@
  *		u64 __user *, mnt_ids, size_t, nr_mnt_ids,
  *		unsigned int, flags)
  */
+#include <linux/mount.h>
+#include <string.h>
+#include "random.h"
 #include "sanitise.h"
 #include "compat.h"
 
@@ -10,22 +13,49 @@
 #define LISTMOUNT_REVERSE	(1 << 0)
 #endif
 
+#ifndef LSMT_ROOT
+#define LSMT_ROOT 0xffffffffffffffff
+#endif
+
 static unsigned long listmount_flags[] = {
 	LISTMOUNT_REVERSE,
 };
+
+static void sanitise_listmount(struct syscallrecord *rec)
+{
+	struct mnt_id_req *req;
+	__u64 *mnt_ids;
+	unsigned int nr;
+
+	req = (struct mnt_id_req *) get_writable_address(sizeof(*req));
+	memset(req, 0, sizeof(*req));
+
+	req->size = MNT_ID_REQ_SIZE_VER0;
+
+	switch (rand() % 3) {
+	case 0: req->mnt_id = LSMT_ROOT; break;	/* list all mounts */
+	case 1: req->mnt_id = 1; break;		/* root mount */
+	default: req->mnt_id = rand32(); break;		/* random mount id */
+	}
+
+	nr = 1 + (rand() % 64);
+	mnt_ids = (__u64 *) get_writable_address(nr * sizeof(*mnt_ids));
+
+	rec->a1 = (unsigned long) req;
+	rec->a2 = (unsigned long) mnt_ids;
+	rec->a3 = nr;
+}
 
 struct syscallentry syscall_listmount = {
 	.name = "listmount",
 	.num_args = 4,
 	.arg1name = "req",
-	.arg1type = ARG_ADDRESS,
 	.arg2name = "mnt_ids",
-	.arg2type = ARG_ADDRESS,
 	.arg3name = "nr_mnt_ids",
-	.arg3type = ARG_LEN,
 	.arg4name = "flags",
 	.arg4type = ARG_LIST,
 	.arg4list = ARGLIST(listmount_flags),
 	.rettype = RET_ZERO_SUCCESS,
 	.group = GROUP_VFS,
+	.sanitise = sanitise_listmount,
 };
