e02abaf7077c135b346e3f9f7294557e5879db49
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 18:53:36 -0700

    vfork: add missing EXTRA_FORK flag
    vfork() creates a new child process like fork() and clone(), both of
    which have EXTRA_FORK set. Without this flag, vfork children aren't
    properly tracked by the process management code.

--- syscalls/vfork.c
+++ syscalls/vfork.c
@@ -7,6 +7,6 @@ struct syscallentry syscall_vfork = {
 	.name = "vfork",
 	.group = GROUP_PROCESS,
 	.num_args = 1,
-	.flags = AVOID_SYSCALL, // No args, confuses fuzzer
+	.flags = AVOID_SYSCALL | EXTRA_FORK, // No args, confuses fuzzer
 	.arg1name = "regs",
 };
