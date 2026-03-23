6e14a4c848128355e736233bacca11bb381ed309
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 20:01:37 -0700

    name_to_handle_at: generate valid struct file_handle
    Fill file_handle with boundary handle_bytes values: 0 (size query),
    MAX_HANDLE_SZ (typical), and random sizes. Allocate space for handle
    data and provide a writable mnt_id output buffer. Also added
    ARG_PATHNAME type for the name argument.

--- syscalls/name_to_handle_at.c
+++ syscalls/name_to_handle_at.c
@@ -4,13 +4,39 @@
  *	int, flag)
  */
 #include <fcntl.h>
+#include "random.h"
 #include "sanitise.h"
 #include "compat.h"
+
+#ifndef MAX_HANDLE_SZ
+#define MAX_HANDLE_SZ 128
+#endif
 
 static unsigned long name_to_handle_at_flags[] = {
 	AT_FDCWD, AT_SYMLINK_NOFOLLOW, AT_REMOVEDIR, AT_SYMLINK_FOLLOW,
 	AT_NO_AUTOMOUNT, AT_EMPTY_PATH,
 };
+
+static void sanitise_name_to_handle_at(struct syscallrecord *rec)
+{
+	struct file_handle *fh;
+	int *mnt_id;
+
+	/* Allocate enough for the handle struct plus max handle data. */
+	fh = (struct file_handle *) get_writable_address(sizeof(*fh) + MAX_HANDLE_SZ);
+
+	switch (rand() % 3) {
+	case 0: fh->handle_bytes = 0; break;		/* query size needed */
+	case 1: fh->handle_bytes = MAX_HANDLE_SZ; break;	/* typical */
+	default: fh->handle_bytes = rand() % 256; break;	/* boundary */
+	}
+
+	mnt_id = (int *) get_writable_address(sizeof(*mnt_id));
+
+	rec->a2 = (unsigned long) get_writable_address(256);	/* pathname */
+	rec->a3 = (unsigned long) fh;
+	rec->a4 = (unsigned long) mnt_id;
+}
 
 struct syscallentry syscall_name_to_handle_at = {
 	.name = "name_to_handle_at",
@@ -18,14 +44,13 @@ struct syscallentry syscall_name_to_handle_at = {
 	.arg1name = "fd",
 	.arg1type = ARG_FD,
 	.arg2name = "name",
-	.arg2type = ARG_ADDRESS,
+	.arg2type = ARG_PATHNAME,
 	.arg3name = "handle",
-	.arg3type = ARG_ADDRESS,
 	.arg4name = "mnt_id",
-	.arg4type = ARG_ADDRESS,
 	.arg5name = "flag",
 	.arg5type = ARG_LIST,
 	.arg5list = ARGLIST(name_to_handle_at_flags),
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
+	.sanitise = sanitise_name_to_handle_at,
 };
