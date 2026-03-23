49bc7adb79654298c537d9d7fb790e0f2e69e463
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 19:58:31 -0700

    sched_setaffinity/getaffinity: generate valid CPU masks
    sched_setaffinity generates cpu_set_t with realistic patterns:
    single CPU, first N CPUs, all CPUs set, or random sparse masks.
    Pairs len with the actual mask size.
    
    sched_getaffinity provides a writable output buffer and exercises
    various len values including too-small and oversized for boundary
    testing.

--- syscalls/sched_setaffinity.c
+++ syscalls/sched_setaffinity.c
@@ -2,7 +2,41 @@
  * SYSCALL_DEFINE3(sched_setaffinity, pid_t, pid, unsigned int, len,
 	 unsigned long __user *, user_mask_ptr)
  */
+#include <sched.h>
+#include "random.h"
 #include "sanitise.h"
+
+static void sanitise_sched_setaffinity(struct syscallrecord *rec)
+{
+	cpu_set_t *mask;
+	unsigned int i, ncpus;
+
+	mask = (cpu_set_t *) get_writable_address(sizeof(*mask));
+	CPU_ZERO(mask);
+
+	switch (rand() % 4) {
+	case 0: /* single CPU */
+		CPU_SET(rand() % CPU_SETSIZE, mask);
+		break;
+	case 1: /* first N CPUs (small, realistic) */
+		ncpus = 1 + (rand() % 8);
+		for (i = 0; i < ncpus; i++)
+			CPU_SET(i, mask);
+		break;
+	case 2: /* all CPUs set */
+		for (i = 0; i < CPU_SETSIZE; i++)
+			CPU_SET(i, mask);
+		break;
+	default: /* random sparse mask */
+		ncpus = 1 + (rand() % 16);
+		for (i = 0; i < ncpus; i++)
+			CPU_SET(rand() % CPU_SETSIZE, mask);
+		break;
+	}
+
+	rec->a2 = sizeof(*mask);
+	rec->a3 = (unsigned long) mask;
+}
 
 struct syscallentry syscall_sched_setaffinity = {
 	.name = "sched_setaffinity",
@@ -11,7 +45,6 @@ struct syscallentry syscall_sched_setaffinity = {
 	.arg1name = "pid",
 	.arg1type = ARG_PID,
 	.arg2name = "len",
-	.arg2type = ARG_LEN,
 	.arg3name = "user_mask_ptr",
-	.arg3type = ARG_ADDRESS,
+	.sanitise = sanitise_sched_setaffinity,
 };
