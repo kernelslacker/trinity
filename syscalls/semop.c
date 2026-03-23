a5c92d3e80d99df80ded61030d7e7473bacac1f1
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 19:58:38 -0700

    semop/semtimedop: generate valid struct sembuf arrays
    Fill sembuf arrays with realistic semaphore operations: V (+1),
    P (-1), wait-for-zero, and random values. Randomly set IPC_NOWAIT
    and SEM_UNDO flags. semtimedop also fills a short timeout to avoid
    blocking the fuzzer.

--- syscalls/semop.c
+++ syscalls/semop.c
@@ -1,7 +1,44 @@
 /*
  * SYSCALL_DEFINE3(semop, int, semid, struct sembuf __user *, tsops, unsigned, nsops)
  */
+#include <sys/sem.h>
+#include "random.h"
 #include "sanitise.h"
+
+#define MAX_SOPS 8
+
+static void fill_sembuf_array(struct sembuf *sops, unsigned int nsops)
+{
+	unsigned int i;
+
+	for (i = 0; i < nsops; i++) {
+		sops[i].sem_num = rand() % 32;
+		switch (rand() % 4) {
+		case 0: sops[i].sem_op = 1; break;		/* V (release) */
+		case 1: sops[i].sem_op = -1; break;		/* P (acquire) */
+		case 2: sops[i].sem_op = 0; break;		/* wait-for-zero */
+		default: sops[i].sem_op = (rand() % 20) - 10; break;
+		}
+		sops[i].sem_flg = 0;
+		if (RAND_BOOL())
+			sops[i].sem_flg |= IPC_NOWAIT;
+		if (RAND_BOOL())
+			sops[i].sem_flg |= SEM_UNDO;
+	}
+}
+
+static void sanitise_semop(struct syscallrecord *rec)
+{
+	struct sembuf *sops;
+	unsigned int nsops;
+
+	nsops = 1 + (rand() % MAX_SOPS);
+	sops = (struct sembuf *) get_writable_address(nsops * sizeof(*sops));
+	fill_sembuf_array(sops, nsops);
+
+	rec->a2 = (unsigned long) sops;
+	rec->a3 = nsops;
+}
 
 struct syscallentry syscall_semop = {
 	.name = "semop",
@@ -12,8 +49,7 @@ struct syscallentry syscall_semop = {
 	.low1range = 0,
 	.hi1range = 65535,
 	.arg2name = "tsops",
-	.arg2type = ARG_ADDRESS,
 	.arg3name = "nsops",
-	.arg3type = ARG_LEN,
 	.flags = NEED_ALARM,
+	.sanitise = sanitise_semop,
 };
