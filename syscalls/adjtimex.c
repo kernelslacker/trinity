e8c4ac9b35c5a8c99a6e1442fc78b95b47736bbf
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 20:24:26 -0700

    adjtimex/clock_adjtime: generate valid struct timex per mode
    Fill struct timex fields based on the selected ADJ_* mode: offset
    values within NTP range, frequency as scaled ppm, valid STA_* status
    bits, PLL time constant 0-10, tick in 9000-11000 range, etc.

--- syscalls/adjtimex.c
+++ syscalls/adjtimex.c
@@ -3,22 +3,67 @@
  *
  * On success, adjtimex() returns the clock state: */
 
-#define TIME_OK   0 /* clock synchronized */
-#define TIME_INS  1 /* insert leap second */
-#define TIME_DEL  2 /* delete leap second */
-#define TIME_OOP  3 /* leap second in progress */
-#define TIME_WAIT 4 /* leap second has occurred */
-#define TIME_BAD  5 /* clock not synchronized */
-
- /* On failure, adjtimex() returns -1 and sets errno. */
-
+#include <sys/timex.h>
+#include <string.h>
+#include "random.h"
 #include "sanitise.h"
+
+static unsigned long adj_modes[] = {
+	ADJ_OFFSET, ADJ_FREQUENCY, ADJ_MAXERROR, ADJ_ESTERROR,
+	ADJ_STATUS, ADJ_TIMECONST, ADJ_SETOFFSET, ADJ_MICRO,
+	ADJ_NANO, ADJ_TICK,
+	ADJ_OFFSET_SINGLESHOT, ADJ_OFFSET_SS_READ,
+};
+
+static void sanitise_adjtimex(struct syscallrecord *rec)
+{
+	struct timex *tx;
+
+	tx = (struct timex *) get_writable_address(sizeof(*tx));
+	memset(tx, 0, sizeof(*tx));
+
+	tx->modes = RAND_ARRAY(adj_modes);
+
+	switch (tx->modes) {
+	case ADJ_OFFSET:
+		/* NTP offset: +-512000 usec (or nsec with ADJ_NANO) */
+		tx->offset = (rand() % 1024001) - 512000;
+		break;
+	case ADJ_FREQUENCY:
+		/* Frequency: scaled ppm, +-512000 << 16 */
+		tx->freq = (rand32() % 67108865) - 33554432;
+		break;
+	case ADJ_MAXERROR:
+		tx->maxerror = rand() % 1000000;
+		break;
+	case ADJ_ESTERROR:
+		tx->esterror = rand() % 1000000;
+		break;
+	case ADJ_STATUS:
+		/* Only writable STA_* bits */
+		tx->status = rand() & 0xff;
+		break;
+	case ADJ_TIMECONST:
+		tx->constant = rand() % 11;	/* 0-10 */
+		break;
+	case ADJ_TICK:
+		/* Tick: 900000/HZ to 1100000/HZ usec, ~9000-11000 for HZ=100 */
+		tx->tick = 9000 + (rand() % 2001);
+		break;
+	case ADJ_SETOFFSET:
+		tx->time.tv_sec = (rand() % 3) - 1;
+		tx->time.tv_usec = rand() % 1000000;
+		break;
+	}
+
+	rec->a1 = (unsigned long) tx;
+}
 
 struct syscallentry syscall_adjtimex = {
 	.name = "adjtimex",
 	.group = GROUP_TIME,
 	.num_args = 1,
 	.arg1name = "txc_p",
-	.arg1type = ARG_ADDRESS,
 	.flags = NEEDS_ROOT,
+	.sanitise = sanitise_adjtimex,
 };
--- syscalls/clock_adjtime.c
+++ syscalls/clock_adjtime.c
@@ -2,8 +2,56 @@
  *SYSCALL_DEFINE2(clock_adjtime, const clockid_t, which_clock,
  *		struct timex __user *, utx)
  */
-
+#include <sys/timex.h>
+#include <string.h>
+#include "random.h"
 #include "sanitise.h"
+
+static unsigned long clock_adj_modes[] = {
+	ADJ_OFFSET, ADJ_FREQUENCY, ADJ_MAXERROR, ADJ_ESTERROR,
+	ADJ_STATUS, ADJ_TIMECONST, ADJ_SETOFFSET, ADJ_MICRO,
+	ADJ_NANO, ADJ_TICK,
+};
+
+static void sanitise_clock_adjtime(struct syscallrecord *rec)
+{
+	struct timex *tx;
+
+	tx = (struct timex *) get_writable_address(sizeof(*tx));
+	memset(tx, 0, sizeof(*tx));
+
+	tx->modes = RAND_ARRAY(clock_adj_modes);
+
+	switch (tx->modes) {
+	case ADJ_OFFSET:
+		tx->offset = (rand() % 1024001) - 512000;
+		break;
+	case ADJ_FREQUENCY:
+		tx->freq = (rand32() % 67108865) - 33554432;
+		break;
+	case ADJ_MAXERROR:
+		tx->maxerror = rand() % 1000000;
+		break;
+	case ADJ_ESTERROR:
+		tx->esterror = rand() % 1000000;
+		break;
+	case ADJ_STATUS:
+		tx->status = rand() & 0xff;
+		break;
+	case ADJ_TIMECONST:
+		tx->constant = rand() % 11;
+		break;
+	case ADJ_TICK:
+		tx->tick = 9000 + (rand() % 2001);
+		break;
+	case ADJ_SETOFFSET:
+		tx->time.tv_sec = (rand() % 3) - 1;
+		tx->time.tv_usec = rand() % 1000000;
+		break;
+	}
+
+	rec->a2 = (unsigned long) tx;
+}
 
 struct syscallentry syscall_clock_adjtime = {
 	.name = "clock_adjtime",
@@ -14,6 +62,6 @@ struct syscallentry syscall_clock_adjtime = {
 	.low1range = 0,
 	.hi1range = 16,
 	.arg2name = "utx",
-	.arg2type = ARG_ADDRESS,
 	.flags = NEEDS_ROOT,
+	.sanitise = sanitise_clock_adjtime,
 };
