a3443f767a303a4b992a4ec1f0d8fed74bffc25f
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 19:56:18 -0700

    sched_setparam/sched_setattr: generate valid scheduler parameters
    sched_setparam fills sched_priority with boundary values (0, 1, 99)
    and random valid priorities.
    
    sched_setattr fills struct sched_attr per-policy: nice values for
    SCHED_OTHER/BATCH, RT priorities for FIFO/RR, and runtime/deadline/
    period triplets for SCHED_DEADLINE. Also fixed num_args from 2 to 3
    (the flags argument was missing).

--- syscalls/sched_setattr.c
+++ syscalls/sched_setattr.c
@@ -1,14 +1,61 @@
 /*
- * SYSCALL_DEFINE2(sched_setattr, pid_t, pid, struct sched_attr __user *, uattr)
+ * SYSCALL_DEFINE3(sched_setattr, pid_t, pid, struct sched_attr __user *, uattr,
+ *		   unsigned int, flags)
  */
+#include <linux/sched/types.h>
+#include <string.h>
+#include "random.h"
 #include "sanitise.h"
+#include "compat.h"
+
+static void sanitise_sched_setattr(struct syscallrecord *rec)
+{
+	struct sched_attr *sa;
+
+	sa = (struct sched_attr *) get_writable_address(sizeof(*sa));
+	memset(sa, 0, sizeof(*sa));
+
+	sa->size = sizeof(*sa);
+
+	switch (rand() % 6) {
+	case 0: /* SCHED_OTHER */
+		sa->sched_policy = 0;
+		sa->sched_nice = (rand() % 40) - 20;	/* -20 to 19 */
+		break;
+	case 1: /* SCHED_FIFO */
+		sa->sched_policy = 1;
+		sa->sched_priority = 1 + (rand() % 99);
+		break;
+	case 2: /* SCHED_RR */
+		sa->sched_policy = 2;
+		sa->sched_priority = 1 + (rand() % 99);
+		break;
+	case 3: /* SCHED_BATCH */
+		sa->sched_policy = 3;
+		sa->sched_nice = (rand() % 40) - 20;
+		break;
+	case 4: /* SCHED_IDLE */
+		sa->sched_policy = SCHED_IDLE;
+		break;
+	default: /* SCHED_DEADLINE */
+		sa->sched_policy = SCHED_DEADLINE;
+		sa->sched_runtime  = 1000000ULL * (1 + (rand() % 10));	/* 1-10ms */
+		sa->sched_deadline = sa->sched_runtime * (1 + (rand() % 5));
+		sa->sched_period   = sa->sched_deadline * (1 + (rand() % 3));
+		break;
+	}
+
+	rec->a2 = (unsigned long) sa;
+}
 
 struct syscallentry syscall_sched_setattr = {
 	.name = "sched_setattr",
 	.group = GROUP_SCHED,
-	.num_args = 2,
+	.num_args = 3,
 	.arg1name = "pid",
 	.arg1type = ARG_PID,
 	.arg2name = "uattr",
-	.arg2type = ARG_ADDRESS,
+	.arg3name = "flags",
+	.rettype = RET_ZERO_SUCCESS,
+	.sanitise = sanitise_sched_setattr,
 };
--- syscalls/sched_setparam.c
+++ syscalls/sched_setparam.c
@@ -1,7 +1,25 @@
 /*
  * SYSCALL_DEFINE2(sched_setparam, pid_t, pid, struct sched_param __user *, param)
  */
+#include <sched.h>
+#include "random.h"
 #include "sanitise.h"
+
+static void sanitise_sched_setparam(struct syscallrecord *rec)
+{
+	struct sched_param *sp;
+
+	sp = (struct sched_param *) get_writable_address(sizeof(*sp));
+
+	switch (rand() % 4) {
+	case 0: sp->sched_priority = 0; break;			/* SCHED_OTHER/BATCH/IDLE */
+	case 1: sp->sched_priority = 1; break;			/* minimum RT */
+	case 2: sp->sched_priority = 99; break;			/* maximum RT */
+	default: sp->sched_priority = rand() % 100; break;	/* random valid */
+	}
+
+	rec->a2 = (unsigned long) sp;
+}
 
 struct syscallentry syscall_sched_setparam = {
 	.name = "sched_setparam",
@@ -10,5 +28,6 @@ struct syscallentry syscall_sched_setparam = {
 	.arg1name = "pid",
 	.arg1type = ARG_PID,
 	.arg2name = "param",
-	.arg2type = ARG_ADDRESS,
+	.rettype = RET_ZERO_SUCCESS,
+	.sanitise = sanitise_sched_setparam,
 };
