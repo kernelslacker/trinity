2e10ce170b42bdd8f55592df622061c9f32b65d4
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 20:24:35 -0700

    fsopen/fsconfig: generate valid filesystem names and per-cmd args
    fsopen: pick from a list of common filesystem types (ext4, btrfs,
    tmpfs, proc, sysfs, etc.) instead of random garbage addresses.
    
    fsconfig: generate proper key/value/aux args per command type.
    SET_FLAG gets a mount option key, SET_STRING gets key+value,
    SET_FD gets key+random fd, CMD_CREATE/RECONFIGURE clear all args.

--- syscalls/fsopen.c
+++ syscalls/fsopen.c
@@ -1,24 +1,44 @@
 /*
  *  SYSCALL_DEFINE2(fsopen, const char __user *, _fs_name, unsigned int, flags)
  */
+#include <string.h>
+#include "random.h"
 #include "sanitise.h"
-
-// TODO: construct arg1 from /proc/filesystems
 
 #define FSOPEN_CLOEXEC 0x00000001
 static unsigned long fsopen_flags[] = {
 	FSOPEN_CLOEXEC
 };
 
+static const char *fs_types[] = {
+	"ext4", "btrfs", "xfs", "tmpfs", "proc", "sysfs",
+	"devtmpfs", "devpts", "cgroup2", "overlay", "nfs",
+	"fuse", "hugetlbfs", "mqueue", "debugfs", "tracefs",
+	"securityfs", "pstore", "efivarfs", "bpf", "ramfs",
+};
+
+static void sanitise_fsopen(struct syscallrecord *rec)
+{
+	const char *fstype;
+	char *name;
+
+	fstype = fs_types[rand() % ARRAY_SIZE(fs_types)];
+	name = (char *) get_writable_address(32);
+	strncpy(name, fstype, 31);
+	name[31] = '\0';
+
+	rec->a1 = (unsigned long) name;
+}
+
 struct syscallentry syscall_fsopen = {
 	.name = "fsopen",
 	.num_args = 2,
 	.arg1name = "_fs_name",
-	.arg1type = ARG_ADDRESS,
 	.arg2name = "flags",
 	.arg2type = ARG_OP,
 	.arg2list = ARGLIST(fsopen_flags),
 	.rettype = RET_FD,
 	.group = GROUP_VFS,
 	.flags = NEEDS_ROOT,
+	.sanitise = sanitise_fsopen,
 };
