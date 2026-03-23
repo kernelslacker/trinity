6cfdd8335b2082f5b4f3dc01f5d0d46e4c99b272
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 19:58:38 -0700

    semop/semtimedop: generate valid struct sembuf arrays
    Fill sembuf arrays with realistic semaphore operations: V (+1),
    P (-1), wait-for-zero, and random values. Randomly set IPC_NOWAIT
    and SEM_UNDO flags. semtimedop also fills a short timeout to avoid
    blocking the fuzzer.

--- syscalls/semtimedop.c
+++ syscalls/semtimedop.c
@@ -2,7 +2,52 @@
  * SYSCALL_DEFINE4(semtimedop, int, semid, struct sembuf __user *, tsops,
 	 unsigned, nsops, const struct timespec __user *, timeout)
  */
+#include <sys/sem.h>
+#include <time.h>
+#include "random.h"
 #include "sanitise.h"
+
+#define MAX_SOPS 8
+
+static void fill_sembuf_array(struct sembuf *sops, unsigned int nsops)
+{
+	unsigned int i;
+
+	for (i = 0; i < nsops; i++) {
+		sops[i].sem_num = rand() % 32;
+		switch (rand() % 4) {
+		case 0: sops[i].sem_op = 1; break;
+		case 1: sops[i].sem_op = -1; break;
+		case 2: sops[i].sem_op = 0; break;
+		default: sops[i].sem_op = (rand() % 20) - 10; break;
+		}
+		sops[i].sem_flg = 0;
+		if (RAND_BOOL())
+			sops[i].sem_flg |= IPC_NOWAIT;
+		if (RAND_BOOL())
+			sops[i].sem_flg |= SEM_UNDO;
+	}
+}
+
+static void sanitise_semtimedop(struct syscallrecord *rec)
+{
+	struct sembuf *sops;
+	struct timespec *ts;
+	unsigned int nsops;
+
+	nsops = 1 + (rand() % MAX_SOPS);
+	sops = (struct sembuf *) get_writable_address(nsops * sizeof(*sops));
+	fill_sembuf_array(sops, nsops);
+
+	rec->a2 = (unsigned long) sops;
+	rec->a3 = nsops;
+
+	/* Short timeout to avoid blocking. */
+	ts = (struct timespec *) get_writable_address(sizeof(*ts));
+	ts->tv_sec = 0;
+	ts->tv_nsec = rand() % 1000000;	/* up to 1ms */
+	rec->a4 = (unsigned long) ts;
+}
 
 struct syscallentry syscall_semtimedop = {
 	.name = "semtimedop",
@@ -13,10 +58,8 @@ struct syscallentry syscall_semtimedop = {
 	.low1range = 0,
 	.hi1range = 65535,
 	.arg2name = "tsops",
-	.arg2type = ARG_ADDRESS,
 	.arg3name = "nsops",
-	.arg3type = ARG_LEN,
 	.arg4name = "timeout",
-	.arg4type = ARG_ADDRESS,
 	.flags = NEED_ALARM,
+	.sanitise = sanitise_semtimedop,
 };
