0bf1e7c903b85335bbcf747da1193e4ca0e3f245
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 19:42:07 -0700

    capget/capset: generate valid capability header and data structs
    Both syscalls were passing random garbage for the header struct, which
    always fails version validation in cap_validate_magic().  Fill in a
    valid _LINUX_CAPABILITY_VERSION and a plausible pid from get_pid().
    
    For capset, also fill the data struct with random capability bitmasks.
    v1 uses a single __user_cap_data_struct, v2/v3 use two (for 64-bit
    capability sets).

--- syscalls/capset.c
+++ syscalls/capset.c
@@ -4,15 +4,57 @@
  * On success, zero is returned.
  * On error, -1 is returned, and errno is set appropriately.
  */
+#include <linux/capability.h>
+#include "random.h"
 #include "sanitise.h"
+
+static const unsigned int cap_versions[] = {
+	_LINUX_CAPABILITY_VERSION_1,
+	_LINUX_CAPABILITY_VERSION_2,
+	_LINUX_CAPABILITY_VERSION_3,
+};
+
+/*
+ * Fill header with valid version and pid.
+ * Fill data with random capability bitmasks.
+ * v3 uses two __user_cap_data_struct entries (64-bit capability sets).
+ */
+static void sanitise_capset(struct syscallrecord *rec)
+{
+	struct __user_cap_header_struct *hdr;
+	struct __user_cap_data_struct *data;
+	unsigned int version;
+
+	hdr = (struct __user_cap_header_struct *) get_writable_address(sizeof(*hdr));
+	version = RAND_ARRAY(cap_versions);
+	hdr->version = version;
+	hdr->pid = get_pid();
+	rec->a1 = (unsigned long) hdr;
+
+	/* v1 uses 1 data struct, v2/v3 use 2. */
+	if (version == _LINUX_CAPABILITY_VERSION_1) {
+		data = (struct __user_cap_data_struct *) get_writable_address(sizeof(*data));
+		data->effective = rand32();
+		data->permitted = rand32();
+		data->inheritable = rand32();
+	} else {
+		data = (struct __user_cap_data_struct *) get_writable_address(2 * sizeof(*data));
+		data[0].effective = rand32();
+		data[0].permitted = rand32();
+		data[0].inheritable = rand32();
+		data[1].effective = rand32();
+		data[1].permitted = rand32();
+		data[1].inheritable = rand32();
+	}
+	rec->a2 = (unsigned long) data;
+}
 
 struct syscallentry syscall_capset = {
 	.name = "capset",
 	.num_args = 2,
 	.arg1name = "header",
-	.arg1type = ARG_ADDRESS,
 	.arg2name = "data",
-	.arg2type = ARG_ADDRESS,
 	.rettype = RET_ZERO_SUCCESS,
 	.group = GROUP_PROCESS,
+	.sanitise = sanitise_capset,
 };
