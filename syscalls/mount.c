87c18f63bec6eb1af8387377f6a5d710ff723d5f
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 20:24:42 -0700

    mount/mount_setattr: generate valid fs types and mount_attr struct
    mount: pick filesystem type from common fs list instead of random
    garbage. Removes the TODO comment.
    
    mount_setattr: fill struct mount_attr with random MOUNT_ATTR_*
    flag combinations in attr_set/attr_clr, pass MOUNT_ATTR_SIZE_VER0.

--- syscalls/mount.c
+++ syscalls/mount.c
@@ -4,10 +4,18 @@
  */
 
 #include <linux/fs.h>
+#include <linux/mount.h>
+#include <string.h>
+#include "random.h"
 #include "sanitise.h"
 #include "compat.h"
 
-//TODO: fill out 'type' with something random from /proc/filesystems
+static const char *fs_types[] = {
+	"ext4", "btrfs", "xfs", "tmpfs", "proc", "sysfs",
+	"devtmpfs", "devpts", "cgroup2", "overlay", "nfs",
+	"fuse", "hugetlbfs", "mqueue", "debugfs", "tracefs",
+	"securityfs", "pstore", "efivarfs", "bpf", "ramfs",
+};
 
 static unsigned long mount_flags[] = {
 	MS_RDONLY, MS_NOSUID, MS_NODEV, MS_NOEXEC,
@@ -20,6 +28,19 @@ static unsigned long mount_flags[] = {
 	MS_NOUSER,
 };
 
+static void sanitise_mount(struct syscallrecord *rec)
+{
+	const char *fstype;
+	char *type;
+
+	fstype = fs_types[rand() % ARRAY_SIZE(fs_types)];
+	type = (char *) get_writable_address(32);
+	strncpy(type, fstype, 31);
+	type[31] = '\0';
+
+	rec->a3 = (unsigned long) type;
+}
+
 struct syscallentry syscall_mount = {
 	.name = "mount",
 	.num_args = 5,
@@ -28,7 +49,6 @@ struct syscallentry syscall_mount = {
 	.arg2name = "dir_name",
 	.arg2type = ARG_PATHNAME,
 	.arg3name = "type",
-	.arg3type = ARG_ADDRESS,
 	.arg4name = "flags",
 	.arg4type = ARG_LIST,
 	.arg4list = ARGLIST(mount_flags),
@@ -36,19 +56,62 @@ struct syscallentry syscall_mount = {
 	.arg5type = ARG_ADDRESS,
 	.group = GROUP_VFS,
 	.flags = NEEDS_ROOT,
+	.sanitise = sanitise_mount,
 };
 
-#define AT_EMPTY_PATH           0x1000  /* Allow empty relative pathname */
+#ifndef MOUNT_ATTR_RDONLY
+#define MOUNT_ATTR_RDONLY	0x00000001
+#define MOUNT_ATTR_NOSUID	0x00000002
+#define MOUNT_ATTR_NODEV	0x00000004
+#define MOUNT_ATTR_NOEXEC	0x00000008
+#define MOUNT_ATTR_NOATIME	0x00000010
+#define MOUNT_ATTR_STRICTATIME	0x00000020
+#define MOUNT_ATTR_NODIRATIME	0x00000080
+#define MOUNT_ATTR_NOSYMFOLLOW	0x00200000
+#endif
 
-#define AT_STATX_SYNC_TYPE      0x6000  /* Type of synchronisation required from statx() */
-#define AT_STATX_SYNC_AS_STAT   0x0000  /* - Do whatever stat() does */
-#define AT_STATX_FORCE_SYNC     0x2000  /* - Force the attributes to be sync'd with the server */
-#define AT_STATX_DONT_SYNC      0x4000  /* - Don't sync attributes with the server */
-    
-#define AT_RECURSIVE            0x8000  /* Apply to the entire subtree */
+static unsigned long mount_attrs[] = {
+	MOUNT_ATTR_RDONLY, MOUNT_ATTR_NOSUID, MOUNT_ATTR_NODEV,
+	MOUNT_ATTR_NOEXEC, MOUNT_ATTR_NOATIME, MOUNT_ATTR_STRICTATIME,
+	MOUNT_ATTR_NODIRATIME, MOUNT_ATTR_NOSYMFOLLOW,
+};
+
+static void sanitise_mount_setattr(struct syscallrecord *rec)
+{
+	struct mount_attr *ma;
+	unsigned int i, nbits;
+	__u64 attrs;
+
+	ma = (struct mount_attr *) get_writable_address(sizeof(*ma));
+	memset(ma, 0, sizeof(*ma));
+
+	/* Build random attr_set (things to turn on). */
+	attrs = 0;
+	nbits = 1 + (rand() % ARRAY_SIZE(mount_attrs));
+	for (i = 0; i < nbits; i++)
+		attrs |= mount_attrs[rand() % ARRAY_SIZE(mount_attrs)];
+	ma->attr_set = attrs;
+
+	/* Build random attr_clr (things to turn off) — non-overlapping with attr_set. */
+	attrs = 0;
+	nbits = rand() % ARRAY_SIZE(mount_attrs);
+	for (i = 0; i < nbits; i++)
+		attrs |= mount_attrs[rand() % ARRAY_SIZE(mount_attrs)];
+	ma->attr_clr = attrs & ~ma->attr_set;
+
+	rec->a4 = (unsigned long) ma;
+	rec->a5 = MOUNT_ATTR_SIZE_VER0;
+}
+
+#define AT_STATX_SYNC_TYPE      0x6000
+#define AT_STATX_SYNC_AS_STAT   0x0000
+#define AT_STATX_FORCE_SYNC     0x2000
+#define AT_STATX_DONT_SYNC      0x4000
+#define AT_RECURSIVE_LOCAL       0x8000
 
 static unsigned long mount_setattr_flags[] = {
-	AT_EMPTY_PATH, AT_STATX_SYNC_TYPE, AT_STATX_SYNC_AS_STAT, AT_STATX_FORCE_SYNC, AT_STATX_DONT_SYNC, AT_RECURSIVE,
+	AT_EMPTY_PATH, AT_STATX_SYNC_TYPE, AT_STATX_SYNC_AS_STAT,
+	AT_STATX_FORCE_SYNC, AT_STATX_DONT_SYNC, AT_RECURSIVE_LOCAL,
 };
 
 struct syscallentry syscall_mount_setattr = {
@@ -62,8 +125,7 @@ struct syscallentry syscall_mount_setattr = {
 	.arg3type = ARG_LIST,
 	.arg3list = ARGLIST(mount_setattr_flags),
 	.arg4name = "uattr",
-	.arg4type = ARG_ADDRESS,
 	.arg5name = "usize",
-	.arg5type = ARG_LEN,
 	.group = GROUP_VFS,
+	.sanitise = sanitise_mount_setattr,
 };
