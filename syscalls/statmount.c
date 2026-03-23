db7c98fd0f30e77f9f052b9e30e5b2bd991a70c4
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 20:08:51 -0700

    statmount: generate valid struct mnt_id_req with STATMOUNT_* params
    Fill the mnt_id_req with proper mnt_id values (LSMT_ROOT, root mount,
    random) and a random combination of STATMOUNT_* request flags instead
    of passing raw garbage addresses. Sets a reasonable 4K output buffer.

--- syscalls/statmount.c
+++ syscalls/statmount.c
@@ -3,11 +3,60 @@
  *		struct statmount __user *, buf, size_t, bufsize,
  *		unsigned int, flags)
  */
+#include <linux/mount.h>
+#include <string.h>
+#include "random.h"
 #include "sanitise.h"
+#include "compat.h"
+
+#ifndef LSMT_ROOT
+#define LSMT_ROOT 0xffffffffffffffff
+#endif
+
+#ifndef STATMOUNT_SB_BASIC
+#define STATMOUNT_SB_BASIC		0x00000001U
+#define STATMOUNT_MNT_BASIC		0x00000002U
+#define STATMOUNT_PROPAGATE_FROM	0x00000004U
+#define STATMOUNT_MNT_ROOT		0x00000008U
+#define STATMOUNT_MNT_POINT		0x00000010U
+#define STATMOUNT_FS_TYPE		0x00000020U
+#define STATMOUNT_MNT_NS_ID		0x00000040U
+#define STATMOUNT_MNT_OPTS		0x00000080U
+#endif
+
+static unsigned long statmount_params[] = {
+	STATMOUNT_SB_BASIC, STATMOUNT_MNT_BASIC, STATMOUNT_PROPAGATE_FROM,
+	STATMOUNT_MNT_ROOT, STATMOUNT_MNT_POINT, STATMOUNT_FS_TYPE,
+	STATMOUNT_MNT_NS_ID, STATMOUNT_MNT_OPTS,
+};
 
 static void sanitise_statmount(struct syscallrecord *rec)
 {
-	rec->a4 = 0;	/* flags must be zero */
+	struct mnt_id_req *req;
+	unsigned int i, nbits;
+	__u64 param;
+
+	req = (struct mnt_id_req *) get_writable_address(sizeof(*req));
+	memset(req, 0, sizeof(*req));
+
+	req->size = MNT_ID_REQ_SIZE_VER0;
+
+	switch (rand() % 3) {
+	case 0: req->mnt_id = LSMT_ROOT; break;
+	case 1: req->mnt_id = 1; break;
+	default: req->mnt_id = rand32(); break;
+	}
+
+	/* Build a random combination of STATMOUNT_* request flags. */
+	param = 0;
+	nbits = 1 + (rand() % ARRAY_SIZE(statmount_params));
+	for (i = 0; i < nbits; i++)
+		param |= statmount_params[rand() % ARRAY_SIZE(statmount_params)];
+	req->param = param;
+
+	rec->a1 = (unsigned long) req;
+	rec->a3 = 4096;	/* reasonable output buffer size */
+	rec->a4 = 0;		/* flags must be zero */
 }
 
 struct syscallentry syscall_statmount = {
