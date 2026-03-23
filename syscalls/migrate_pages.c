b401cf0610ea854eca74ccf9d923436dbd79e0dd
Dave Jones <davej@codemonkey.org.uk>
2026-03-22 20:01:21 -0700

    set_mempolicy/migrate_pages: generate valid nodemask bitmaps
    Fill NUMA node masks with realistic patterns: node 0 only (common
    on non-NUMA systems), first few nodes, or random bitmaps. Pair
    maxnode with the actual mask allocation. migrate_pages generates
    separate old and new nodemasks.

--- syscalls/migrate_pages.c
+++ syscalls/migrate_pages.c
@@ -3,7 +3,46 @@
 	 const unsigned long __user *, old_nodes,
 	 const unsigned long __user *, new_nodes)
  */
+#include "random.h"
 #include "sanitise.h"
+
+#define MAX_NUMNODES 64
+
+static void fill_nodemask(unsigned long *mask)
+{
+	mask[0] = 0;
+	mask[1] = 0;
+
+	switch (rand() % 3) {
+	case 0: /* node 0 only */
+		mask[0] = 1;
+		break;
+	case 1: /* first few nodes */
+		mask[0] = (1UL << (1 + (rand() % 4))) - 1;
+		break;
+	default: /* random bits */
+		mask[0] = rand32();
+		break;
+	}
+}
+
+static void sanitise_migrate_pages(struct syscallrecord *rec)
+{
+	unsigned long *old_nodes, *new_nodes;
+	unsigned int maxnode;
+
+	maxnode = 1 + (rand() % MAX_NUMNODES);
+
+	old_nodes = (unsigned long *) get_writable_address(sizeof(unsigned long) * 2);
+	fill_nodemask(old_nodes);
+
+	new_nodes = (unsigned long *) get_writable_address(sizeof(unsigned long) * 2);
+	fill_nodemask(new_nodes);
+
+	rec->a2 = maxnode;
+	rec->a3 = (unsigned long) old_nodes;
+	rec->a4 = (unsigned long) new_nodes;
+}
 
 struct syscallentry syscall_migrate_pages = {
 	.name = "migrate_pages",
@@ -11,10 +50,8 @@ struct syscallentry syscall_migrate_pages = {
 	.arg1name = "pid",
 	.arg1type = ARG_PID,
 	.arg2name = "maxnode",
-	.arg2type = ARG_LEN,
 	.arg3name = "old_nodes",
-	.arg3type = ARG_ADDRESS,
 	.arg4name = "new_nodes",
-	.arg4type = ARG_ADDRESS,
 	.group = GROUP_VM,
+	.sanitise = sanitise_migrate_pages,
 };
--- syscalls/set_mempolicy.c
+++ syscalls/set_mempolicy.c
@@ -1,6 +1,7 @@
 /*
  * SYSCALL_DEFINE3(set_mempolicy, int, mode, unsigned long __user *, nmask, unsigned long, maxnode)
  */
+#include "random.h"
 #include "sanitise.h"
 #include "compat.h"
 
@@ -20,6 +21,35 @@ static unsigned long mempolicy_modes[] = {
 	MPOL_WEIGHTED_INTERLEAVE,
 };
 
+#define MAX_NUMNODES 64
+
+static void sanitise_set_mempolicy(struct syscallrecord *rec)
+{
+	unsigned long *mask;
+	unsigned int maxnode;
+
+	/* Nodemask is a bitmap, one bit per NUMA node. */
+	maxnode = 1 + (rand() % MAX_NUMNODES);
+	mask = (unsigned long *) get_writable_address(sizeof(unsigned long) * 2);
+	mask[0] = 0;
+	mask[1] = 0;
+
+	switch (rand() % 3) {
+	case 0: /* node 0 only (most common on non-NUMA) */
+		mask[0] = 1;
+		break;
+	case 1: /* first few nodes */
+		mask[0] = (1UL << (1 + (rand() % 4))) - 1;
+		break;
+	default: /* random bits */
+		mask[0] = rand32();
+		break;
+	}
+
+	rec->a2 = (unsigned long) mask;
+	rec->a3 = maxnode;
+}
+
 struct syscallentry syscall_set_mempolicy = {
 	.name = "set_mempolicy",
 	.num_args = 3,
@@ -27,8 +57,7 @@ struct syscallentry syscall_set_mempolicy = {
 	.arg1type = ARG_OP,
 	.arg1list = ARGLIST(mempolicy_modes),
 	.arg2name = "nmask",
-	.arg2type = ARG_ADDRESS,
 	.arg3name = "maxnode",
-	.arg3type = ARG_LEN,
 	.group = GROUP_VM,
+	.sanitise = sanitise_set_mempolicy,
 };
