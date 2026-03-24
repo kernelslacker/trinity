b44fe9743d54e63a790c7a9402042f7cf33a4d28
Dave Jones <davej@codemonkey.org.uk>
2026-03-24 08:51:04 -0700

    net: add structured payloads for TCP-AO, MD5SIG, FASTOPEN_KEY, REPAIR_OPTIONS
    TCP setsockopt previously generated random bytes for all options.
    Now generates properly structured payloads for:
    
    - TCP_MD5SIG/TCP_MD5SIG_EXT: struct tcp_md5sig with random key,
      prefix length, and extension flags
    - TCP_FASTOPEN_KEY: 16 or 32 byte keys (AES-128, 1 or 2 keys)
    - TCP_REPAIR_OPTIONS: 1-4 struct tcp_repair_opt entries
    - TCP_AO_ADD_KEY: struct tcp_ao_add with real algorithm names
      (hmac-sha1, cmac-aes, hmac-sha256), random key material,
      sndid/rcvid, maclen, keyflags, prefix
    - TCP_AO_DEL_KEY: struct tcp_ao_del with key identifiers and flags
    - TCP_AO_INFO: struct tcp_ao_info_opt with feature flags
    - TCP_AO_REPAIR: struct tcp_ao_repair with ISN/SNE values
    
    These structs exercise the kernel's TCP authentication and repair
    parsers instead of just hitting early -EINVAL on size checks.

--- net/proto-ip-tcp.c
+++ net/proto-ip-tcp.c
@@ -24,6 +24,8 @@ static const char *ulp_names[] = { "tls", "mptcp" };
 
 static const char *cc_algos[] = { "cubic", "reno", "bbr", "dctcp", "vegas", "westwood" };
 
+static const char *ao_algos[] = { "hmac(sha1)", "cmac(aes)", "hmac(sha256)" };
+
 void tcp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *triplet)
 {
 	char *ptr;
@@ -45,6 +47,108 @@ void tcp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *triple
 		so->optlen = strlen(str) + 1;
 		memcpy(ptr, str, so->optlen);
 		break;
+
+	case TCP_MD5SIG:
+	case TCP_MD5SIG_EXT: {
+		struct tcp_md5sig *md5 = (struct tcp_md5sig *) so->optval;
+
+		memset(md5, 0, sizeof(struct tcp_md5sig));
+		md5->tcpm_keylen = rand() % (TCP_MD5SIG_MAXKEYLEN + 1);
+		generate_rand_bytes(md5->tcpm_key, md5->tcpm_keylen);
+		if (so->optname == TCP_MD5SIG_EXT) {
+			md5->tcpm_flags = rand() & 0x3;
+			md5->tcpm_prefixlen = rand() % 129;
+		}
+		so->optlen = sizeof(struct tcp_md5sig);
+		break;
+	}
+
+	case TCP_FASTOPEN_KEY:
+		/* Key is 16 bytes (AES-128), kernel accepts 1 or 2 keys */
+		switch (rand() % 3) {
+		case 0: so->optlen = 16; break;
+		case 1: so->optlen = 32; break;
+		case 2: so->optlen = rand() % 48 + 1; break;
+		}
+		generate_rand_bytes((unsigned char *) so->optval, so->optlen);
+		break;
+
+	case TCP_REPAIR_OPTIONS: {
+		struct tcp_repair_opt *opt = (struct tcp_repair_opt *) so->optval;
+		unsigned int count = rand() % 4 + 1;
+		unsigned int i;
+
+		for (i = 0; i < count; i++) {
+			opt[i].opt_code = rand() % 16;
+			opt[i].opt_val = rand();
+		}
+		so->optlen = count * sizeof(struct tcp_repair_opt);
+		break;
+	}
+
+	case TCP_AO_ADD_KEY: {
+		struct tcp_ao_add *ao = (struct tcp_ao_add *) so->optval;
+
+		memset(ao, 0, sizeof(struct tcp_ao_add));
+		str = RAND_ARRAY(ao_algos);
+		strncpy(ao->alg_name, str, sizeof(ao->alg_name) - 1);
+		ao->sndid = rand();
+		ao->rcvid = rand();
+		ao->keylen = rand() % (TCP_AO_MAXKEYLEN + 1);
+		generate_rand_bytes(ao->key, ao->keylen);
+		ao->maclen = rand() % 2 ? 12 : 16;
+		ao->keyflags = rand() & 0x3;
+		ao->set_current = rand() & 1;
+		ao->set_rnext = rand() & 1;
+		ao->prefix = rand() % 129;
+		ao->ifindex = rand() % 4;
+		so->optlen = sizeof(struct tcp_ao_add);
+		break;
+	}
+
+	case TCP_AO_DEL_KEY: {
+		struct tcp_ao_del *del = (struct tcp_ao_del *) so->optval;
+
+		memset(del, 0, sizeof(struct tcp_ao_del));
+		del->sndid = rand();
+		del->rcvid = rand();
+		del->set_current = rand() & 1;
+		del->set_rnext = rand() & 1;
+		del->del_async = rand() & 1;
+		del->current_key = rand();
+		del->rnext = rand();
+		del->prefix = rand() % 129;
+		del->keyflags = rand() & 0x3;
+		del->ifindex = rand() % 4;
+		so->optlen = sizeof(struct tcp_ao_del);
+		break;
+	}
+
+	case TCP_AO_INFO: {
+		struct tcp_ao_info_opt *info = (struct tcp_ao_info_opt *) so->optval;
+
+		memset(info, 0, sizeof(struct tcp_ao_info_opt));
+		info->set_current = rand() & 1;
+		info->set_rnext = rand() & 1;
+		info->ao_required = rand() & 1;
+		info->set_counters = rand() & 1;
+		info->accept_icmps = rand() & 1;
+		info->current_key = rand();
+		info->rnext = rand();
+		so->optlen = sizeof(struct tcp_ao_info_opt);
+		break;
+	}
+
+	case TCP_AO_REPAIR: {
+		struct tcp_ao_repair *repair = (struct tcp_ao_repair *) so->optval;
+
+		repair->snt_isn = rand();
+		repair->rcv_isn = rand();
+		repair->snd_sne = rand();
+		repair->rcv_sne = rand();
+		so->optlen = sizeof(struct tcp_ao_repair);
+		break;
+	}
 
 	default:
 		break;
