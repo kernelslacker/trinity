e275c01425c2432f497167d6ce9fbfecce2a6784
Dave Jones <davej@codemonkey.org.uk>
2026-03-24 18:06:51 -0700

    ioctls/loop: use glob to discover loop block devices
    Replace the static "loop" device name with dynamic discovery via
    glob("/dev/loop[0-9]*").  At startup a constructor enumerates the
    actual loop block devices present and caches their dev_t values.
    The loop ioctl group now uses an fd_test function to match fds
    against discovered devices instead of relying on /proc/devices
    name matching.

--- ioctls/loop.c
+++ ioctls/loop.c
@@ -1,3 +1,8 @@
+#include <glob.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <linux/ioctl.h>
 #include <linux/loop.h>
 
@@ -28,7 +33,6 @@ static const struct ioctl loop_ioctls[] = {
 
 static const char *const loop_ctrl_devs[] = {
 	"loop-control",
-	//FIXME: Need to glob /dev/loop*
 };
 
 static const struct ioctl_group loop_ctrl_grp = {
@@ -42,14 +46,50 @@ static const struct ioctl_group loop_ctrl_grp = {
 
 REG_IOCTL_GROUP(loop_ctrl_grp)
 
-static const char *const loop_devs[] = {
-	"loop",
-};
+/* Discovered loop block devices from glob("/dev/loop*"). */
+static dev_t *loop_rdevs;
+static size_t nr_loop_rdevs;
+
+static void __attribute__((constructor)) discover_loop_block_devs(void)
+{
+	glob_t g;
+	struct stat st;
+	size_t i;
+
+	if (glob("/dev/loop[0-9]*", 0, NULL, &g) != 0)
+		return;
+
+	loop_rdevs = malloc(g.gl_pathc * sizeof(dev_t));
+	if (!loop_rdevs) {
+		globfree(&g);
+		return;
+	}
+
+	for (i = 0; i < g.gl_pathc; i++) {
+		if (stat(g.gl_pathv[i], &st) == 0 && S_ISBLK(st.st_mode))
+			loop_rdevs[nr_loop_rdevs++] = st.st_rdev;
+	}
+
+	globfree(&g);
+}
+
+static int loop_fd_test(int fd __attribute__((unused)), const struct stat *st)
+{
+	size_t i;
+
+	if (!S_ISBLK(st->st_mode))
+		return -1;
+
+	for (i = 0; i < nr_loop_rdevs; i++) {
+		if (loop_rdevs[i] == st->st_rdev)
+			return 0;
+	}
+
+	return -1;
+}
 
 static const struct ioctl_group loop_grp = {
-	.devtype = DEV_BLOCK,
-	.devs = loop_devs,
-	.devs_cnt = ARRAY_SIZE(loop_devs),
+	.fd_test = loop_fd_test,
 	.sanitise = pick_random_ioctl,
 	.ioctls = loop_ioctls,
 	.ioctls_cnt = ARRAY_SIZE(loop_ioctls),
