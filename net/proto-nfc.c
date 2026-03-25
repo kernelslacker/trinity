dfa2917a13621d20e2f6cae4aa584d8e804e58c2
Dave Jones <davej@codemonkey.org.uk>
2026-03-24 23:43:50 -0700

    net/proto-nfc: generate sockaddr_nfc_llcp addresses
    Randomly generate sockaddr_nfc_llcp in addition to sockaddr_nfc.
    The LLCP variant includes dsap, ssap, and a randomized service name,
    exercising the NFC LLCP socket address path.
    
    Add sockaddr_nfc_llcp and NFC_LLCP_MAX_SERVICE_NAME to compat.h for
    systems that don't have them in their kernel headers.

--- net/proto-nfc.c
+++ net/proto-nfc.c
@@ -9,17 +9,35 @@
 
 static void nfc_gen_sockaddr(struct sockaddr **addr, socklen_t *addrlen)
 {
-	struct sockaddr_nfc *nfc;
+	if (RAND_BOOL()) {
+		struct sockaddr_nfc_llcp *nfc_llcp;
+		unsigned int i;
 
-	// TODO: See also sockaddr_nfc_llcp
-	nfc = zmalloc(sizeof(struct sockaddr_nfc));
+		nfc_llcp = zmalloc(sizeof(struct sockaddr_nfc_llcp));
 
-	nfc->sa_family = PF_NFC;
-	nfc->dev_idx = rand();
-	nfc->target_idx = rand();
-	nfc->nfc_protocol = rand() % 5;
-	*addr = (struct sockaddr *) nfc;
-	*addrlen = sizeof(struct sockaddr_nfc);
+		nfc_llcp->sa_family = PF_NFC;
+		nfc_llcp->dev_idx = rand();
+		nfc_llcp->target_idx = rand();
+		nfc_llcp->nfc_protocol = rand() % 5;
+		nfc_llcp->dsap = rand();
+		nfc_llcp->ssap = rand();
+		nfc_llcp->service_name_len = rand() % NFC_LLCP_MAX_SERVICE_NAME;
+		for (i = 0; i < nfc_llcp->service_name_len; i++)
+			nfc_llcp->service_name[i] = 'a' + rand() % 26;
+		*addr = (struct sockaddr *) nfc_llcp;
+		*addrlen = sizeof(struct sockaddr_nfc_llcp);
+	} else {
+		struct sockaddr_nfc *nfc;
+
+		nfc = zmalloc(sizeof(struct sockaddr_nfc));
+
+		nfc->sa_family = PF_NFC;
+		nfc->dev_idx = rand();
+		nfc->target_idx = rand();
+		nfc->nfc_protocol = rand() % 5;
+		*addr = (struct sockaddr *) nfc;
+		*addrlen = sizeof(struct sockaddr_nfc);
+	}
 }
 
 #define SOL_NFC 280
