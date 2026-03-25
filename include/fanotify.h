7fa349571281f2a64cbba38be01a77895b17cf98
Dave Jones <davej@codemonkey.org.uk>
2026-03-24 18:00:23 -0700

    Remove glibc FIXME workarounds from local headers
    Modern glibc (and kernel headers) provide SYS_fanotify_init,
    SYS_userfaultfd, SYS_memfd_create via <sys/syscall.h>, and the MFD_*
    flags via <linux/memfd.h>.  Replace the hand-rolled fallback defines
    with proper system header includes.

--- include/fanotify.h
+++ include/fanotify.h
@@ -1,15 +1,7 @@
 #pragma once
 
+#include <sys/syscall.h>
+
 unsigned long get_fanotify_init_flags(void);
 
 unsigned long get_fanotify_init_event_flags(void);
-
-// FIXME: Keep all this here until glibc supports it.
-#ifndef SYS_fanotify_init
-#ifdef __x86_64__
-#define SYS_fanotify_init 300
-#endif
-#ifdef __i386__
-#define SYS_fanotify_init 338
-#endif
-#endif
