a1887e72feb5180b1c81f6d34bbf92164bdce42d
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 10:27:02 -0700

    utimensat/futimesat: add sanitise to allocate valid timespec/timeval arrays
    Passing a random ARG_ADDRESS for the times argument causes near-certain
    EFAULT. Allocate proper struct timespec[2] and struct timeval[2] with
    valid values; include UTIME_NOW and UTIME_OMIT as special cases for
    utimensat.

--- syscalls/utimensat.c
+++ syscalls/utimensat.c
@@ -3,11 +3,54 @@
 	 struct timespec __user *, utimes, int, flags)
  */
 #include <fcntl.h>
+#include <time.h>
+#include "random.h"
 #include "sanitise.h"
+
+/* From linux/stat.h - special nsec values for utimensat */
+#ifndef UTIME_NOW
+#define UTIME_NOW  ((1l << 30) - 1l)
+#endif
+#ifndef UTIME_OMIT
+#define UTIME_OMIT ((1l << 30) - 2l)
+#endif
 
 static unsigned long utimensat_flags[] = {
 	AT_SYMLINK_NOFOLLOW,
 };
+
+static void sanitise_utimensat(struct syscallrecord *rec)
+{
+	struct timespec *ts;
+	unsigned int i;
+
+	/* NULL utimes means "set both to current time" - allow that sometimes */
+	if (ONE_IN(8)) {
+		rec->a3 = 0;
+		return;
+	}
+
+	ts = (struct timespec *) get_writable_address(sizeof(*ts) * 2);
+
+	for (i = 0; i < 2; i++) {
+		switch (rand() % 4) {
+		case 0:
+			ts[i].tv_sec = 0;
+			ts[i].tv_nsec = UTIME_NOW;
+			break;
+		case 1:
+			ts[i].tv_sec = 0;
+			ts[i].tv_nsec = UTIME_OMIT;
+			break;
+		default:
+			ts[i].tv_sec = rand() % 2000000000;
+			ts[i].tv_nsec = rand() % 1000000000;
+			break;
+		}
+	}
+
+	rec->a3 = (unsigned long) ts;
+}
 
 struct syscallentry syscall_utimensat = {
 	.name = "utimensat",
@@ -16,5 +59,6 @@ struct syscallentry syscall_utimensat = {
 	.argtype = { [0] = ARG_FD, [1] = ARG_PATHNAME, [2] = ARG_ADDRESS, [3] = ARG_LIST },
 	.argname = { [0] = "dfd", [1] = "filename", [2] = "utimes", [3] = "flags" },
 	.arg4list = ARGLIST(utimensat_flags),
+	.sanitise = sanitise_utimensat,
 	.flags = NEED_ALARM,
 };
