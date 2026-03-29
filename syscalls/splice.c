c590fee4d0b2def3af7ad0146bd1c13b5906f0c5
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 09:46:07 -0700

    splice: use ARG_FD_PIPE for fd_out to match fd_in

--- syscalls/splice.c
+++ syscalls/splice.c
@@ -14,7 +14,7 @@ static unsigned long splice_flags[] = {
 struct syscallentry syscall_splice = {
 	.name = "splice",
 	.num_args = 6,
-	.argtype = { [0] = ARG_FD_PIPE, [1] = ARG_ADDRESS, [2] = ARG_FD, [3] = ARG_ADDRESS, [4] = ARG_LEN, [5] = ARG_LIST },
+	.argtype = { [0] = ARG_FD_PIPE, [1] = ARG_ADDRESS, [2] = ARG_FD_PIPE, [3] = ARG_ADDRESS, [4] = ARG_LEN, [5] = ARG_LIST },
 	.argname = { [0] = "fd_in", [1] = "off_in", [2] = "fd_out", [3] = "off_out", [4] = "len", [5] = "flags" },
 	.arg6list = ARGLIST(splice_flags),
 	.flags = NEED_ALARM,
