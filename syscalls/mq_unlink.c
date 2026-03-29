96c3c854bf6b0eda822b2cec1f8641fcb9f54a7c
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 10:27:52 -0700

    mq_unlink: generate POSIX-compliant queue names starting with '/'
    POSIX message queue names must begin with '/'. Without sanitise the
    fuzzer passes a random address, hitting EFAULT before the kernel ever
    validates the name. Generate /trinN names matching mq_open.

--- syscalls/mq_unlink.c
+++ syscalls/mq_unlink.c
@@ -1,7 +1,25 @@
 /*
  * SYSCALL_DEFINE1(mq_unlink, const char __user *, u_name)
  */
+#include <stdlib.h>
 #include "sanitise.h"
+
+static void sanitise_mq_unlink(struct syscallrecord *rec)
+{
+	char *name;
+
+	/* POSIX MQ names must start with '/' */
+	name = (char *) get_writable_address(8);
+	name[0] = '/';
+	name[1] = 't';
+	name[2] = 'r';
+	name[3] = 'i';
+	name[4] = 'n';
+	name[5] = '0' + (rand() % 10);
+	name[6] = '\0';
+
+	rec->a1 = (unsigned long) name;
+}
 
 struct syscallentry syscall_mq_unlink = {
 	.name = "mq_unlink",
@@ -9,4 +27,5 @@ struct syscallentry syscall_mq_unlink = {
 	.num_args = 1,
 	.argtype = { [0] = ARG_ADDRESS },
 	.argname = { [0] = "u_name" },
+	.sanitise = sanitise_mq_unlink,
 };
