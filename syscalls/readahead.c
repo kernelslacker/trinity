4971aca130f4da9f5b425772ec277f83334c4c9b
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 11:50:48 -0700

    readahead: add sanitise to clamp offset to non-negative loff_t
    ARG_LEN can produce ULONG_MAX via get_boundary_value(), which is
    negative when interpreted as loff_t.  readahead() returns EINVAL for
    negative offsets, same as fadvise64.  Add a sanitise that masks the
    offset to a non-negative 63-bit value.

--- syscalls/readahead.c
+++ syscalls/readahead.c
@@ -1,13 +1,21 @@
 /*
  * SYSCALL_DEFINE(readahead)(int fd, loff_t offset, size_t count)
  */
+#include "random.h"
 #include "sanitise.h"
+
+static void sanitise_readahead(struct syscallrecord *rec)
+{
+	/* Negative offsets produce EINVAL; mask to non-negative loff_t. */
+	rec->a2 = rand64() & 0x7fffffffffffffffULL;
+}
 
 struct syscallentry syscall_readahead = {
 	.name = "readahead",
 	.num_args = 3,
-	.argtype = { [0] = ARG_FD, [1] = ARG_LEN, [2] = ARG_LEN },
+	.argtype = { [0] = ARG_FD, [2] = ARG_LEN },
 	.argname = { [0] = "fd", [1] = "offset", [2] = "count" },
+	.sanitise = sanitise_readahead,
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
 };
