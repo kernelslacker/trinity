e086befe6a077176d30205211774b29c5ab34d17
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 10:26:28 -0700

    iopl: constrain level to valid range 0-3
    The I/O privilege level field is 2 bits wide; values above 3 are
    invalid and immediately fail. Use ARG_RANGE to generate useful inputs.

--- syscalls/x86/iopl.c
+++ syscalls/x86/iopl.c
@@ -6,7 +6,8 @@
 struct syscallentry syscall_iopl = {
 	.name = "iopl",
 	.num_args = 2,
-	.argtype = { [1] = ARG_ADDRESS },
+	.argtype = { [0] = ARG_RANGE, [1] = ARG_ADDRESS },
 	.argname = { [0] = "level", [1] = "regs" },
+	.low1range = 0, .hi1range = 3,
 	.flags = NEEDS_ROOT,
 };
