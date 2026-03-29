a18b69ab238248a6a0ea154a5ab4e0cbf02f5aec
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 10:26:51 -0700

    reboot: use valid magic constants instead of random garbage
    The kernel rejects reboot() immediately unless magic1 ==
    LINUX_REBOOT_MAGIC1 and magic2 is one of the four valid values.
    Fix magic1 in sanitise, use ARG_LIST for magic2 and cmd so we exercise
    actual shutdown/kexec paths rather than always hitting EINVAL.

--- syscalls/reboot.c
+++ syscalls/reboot.c
@@ -1,12 +1,33 @@
 /*
  * SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd, void __user *, arg)
  */
+#include <linux/reboot.h>
 #include "sanitise.h"
+
+static unsigned long reboot_magic2_vals[] = {
+	LINUX_REBOOT_MAGIC2, LINUX_REBOOT_MAGIC2A,
+	LINUX_REBOOT_MAGIC2B, LINUX_REBOOT_MAGIC2C,
+};
+
+static unsigned long reboot_cmds[] = {
+	LINUX_REBOOT_CMD_RESTART, LINUX_REBOOT_CMD_HALT,
+	LINUX_REBOOT_CMD_CAD_ON, LINUX_REBOOT_CMD_CAD_OFF,
+	LINUX_REBOOT_CMD_POWER_OFF, LINUX_REBOOT_CMD_RESTART2,
+	LINUX_REBOOT_CMD_SW_SUSPEND, LINUX_REBOOT_CMD_KEXEC,
+};
+
+static void sanitise_reboot(struct syscallrecord *rec)
+{
+	rec->a1 = LINUX_REBOOT_MAGIC1;
+}
 
 struct syscallentry syscall_reboot = {
 	.name = "reboot",
 	.num_args = 4,
-	.argtype = { [3] = ARG_ADDRESS },
+	.argtype = { [1] = ARG_LIST, [2] = ARG_LIST, [3] = ARG_ADDRESS },
 	.argname = { [0] = "magic1", [1] = "magic2", [2] = "cmd", [3] = "arg" },
+	.arg2list = ARGLIST(reboot_magic2_vals),
+	.arg3list = ARGLIST(reboot_cmds),
+	.sanitise = sanitise_reboot,
 	.group = GROUP_PROCESS,
 };
