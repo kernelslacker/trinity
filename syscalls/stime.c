9ea771fbd5629fe3b2012a0c0008e0554052d83a
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 11:51:39 -0700

    stime: add sanitise to allocate and populate a valid time_t pointer
    Passing ARG_ADDRESS could hand a random garbage pointer to the kernel.
    Allocate a proper time_t-sized buffer and fill it with a reasonable
    timestamp (near-current, epoch, or random), following the settimeofday
    pattern.  Also mark NEEDS_ROOT since stime() requires CAP_SYS_TIME.

--- syscalls/stime.c
+++ syscalls/stime.c
@@ -1,12 +1,36 @@
 /*
  * SYSCALL_DEFINE1(stime, time_t __user *, tptr)
  */
+#include <time.h>
+#include "random.h"
 #include "sanitise.h"
+
+static void sanitise_stime(struct syscallrecord *rec)
+{
+	time_t *t;
+
+	t = (time_t *) get_writable_address(sizeof(*t));
+
+	switch (rand() % 3) {
+	case 0:	/* near current time */
+		*t = time(NULL) + (rand() % 120) - 60;
+		break;
+	case 1:	/* epoch */
+		*t = 0;
+		break;
+	default:
+		*t = (time_t) rand32();
+		break;
+	}
+
+	rec->a1 = (unsigned long) t;
+}
 
 struct syscallentry syscall_stime = {
 	.name = "stime",
 	.group = GROUP_TIME,
 	.num_args = 1,
-	.argtype = { [0] = ARG_ADDRESS },
 	.argname = { [0] = "tptr" },
+	.flags = NEEDS_ROOT,
+	.sanitise = sanitise_stime,
 };
