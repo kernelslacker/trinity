34700c6695b64bfecb867b569559d7e5292fba9a
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 11:50:58 -0700

    sendfile/copy_file_range: sanitise offset pointer args
    Random pointers for off_t/loff_t arguments fault immediately. Add
    sanitise callbacks that point to static variables holding a valid file
    offset in [0, 1GB], letting the kernel actually reach the data path.

--- syscalls/sendfile.c
+++ syscalls/sendfile.c
@@ -1,13 +1,33 @@
 /*
  * SYSCALL_DEFINE4(sendfile, int, out_fd, int, in_fd, off_t __user *, offset, size_t, count)
  */
+#include <stdint.h>
+#include <sys/types.h>
+#include "random.h"
 #include "sanitise.h"
+#include "trinity.h"
+
+static off_t sendfile_offset;
+static off_t sendfile64_offset;
+
+static void sanitise_sendfile(struct syscallrecord *rec)
+{
+	sendfile_offset = RAND_RANGE(0, 1ULL << 30);
+	rec->a3 = (unsigned long) &sendfile_offset;
+}
+
+static void sanitise_sendfile64(struct syscallrecord *rec)
+{
+	sendfile64_offset = RAND_RANGE(0, 1ULL << 30);
+	rec->a3 = (unsigned long) &sendfile64_offset;
+}
 
 struct syscallentry syscall_sendfile = {
 	.name = "sendfile",
 	.num_args = 4,
 	.argtype = { [0] = ARG_FD, [1] = ARG_FD, [2] = ARG_ADDRESS, [3] = ARG_LEN },
 	.argname = { [0] = "out_fd", [1] = "in_fd", [2] = "offset", [3] = "count" },
+	.sanitise = sanitise_sendfile,
 	.flags = NEED_ALARM | IGNORE_ENOSYS,
 	.group = GROUP_VFS,
 };
@@ -21,6 +41,7 @@ struct syscallentry syscall_sendfile64 = {
 	.num_args = 4,
 	.argtype = { [0] = ARG_FD, [1] = ARG_FD, [2] = ARG_ADDRESS, [3] = ARG_LEN },
 	.argname = { [0] = "out_fd", [1] = "in_fd", [2] = "offset", [3] = "count" },
+	.sanitise = sanitise_sendfile64,
 	.flags = NEED_ALARM | IGNORE_ENOSYS,
 	.group = GROUP_VFS,
 };
