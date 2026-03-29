2e29ccfa5053372e934942515597d14476f21b15
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 00:36:29 -0700

    proto-netrom: add sockaddr_ax25 gen_sockaddr and SOCK_DGRAM triplet
    The netrom protocol handler had setsockopt coverage but was missing
    gen_sockaddr and only included SOCK_SEQPACKET. Add gen_sockaddr filling
    sockaddr_ax25 with AF_NETROM family and a random 7-byte AX.25 callsign,
    matching the addressing used by the NETROM kernel code. Add SOCK_DGRAM
    to the triplets to broaden socket type coverage.

--- net/proto-netrom.c
+++ net/proto-netrom.c
@@ -1,9 +1,23 @@
 
 #ifdef USE_NETROM
-#include <stdlib.h>
 #include <netrom/netrom.h>
+#include <netax25/ax25.h>
 #include "net.h"
+#include "random.h"
 #include "compat.h"
+
+static void netrom_gen_sockaddr(struct sockaddr **addr, socklen_t *addrlen)
+{
+	struct sockaddr_ax25 *ax25;
+
+	ax25 = zmalloc(sizeof(struct sockaddr_ax25));
+
+	ax25->sax25_family = PF_NETROM;
+	generate_rand_bytes((unsigned char *) ax25->sax25_call.ax25_call, 7);
+	ax25->sax25_ndigis = rand();
+	*addr = (struct sockaddr *) ax25;
+	*addrlen = sizeof(struct sockaddr_ax25);
+}
 
 static void netrom_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *triplet)
 {
@@ -17,11 +31,12 @@ static void netrom_setsockopt(struct sockopt *so, __unused__ struct socket_tripl
 
 static struct socket_triplet netrom_triplet[] = {
 	{ .family = PF_NETROM, .protocol = 0, .type = SOCK_SEQPACKET },
+	{ .family = PF_NETROM, .protocol = 0, .type = SOCK_DGRAM },
 };
 
 const struct netproto proto_netrom = {
 	.name = "netrom",
-//	.socket = netrom_rand_socket,
+	.gen_sockaddr = netrom_gen_sockaddr,
 	.setsockopt = netrom_setsockopt,
 	.valid_triplets = netrom_triplet,
 	.nr_triplets = ARRAY_SIZE(netrom_triplet),
