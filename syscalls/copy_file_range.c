7b88c8e8f43b6dc690cddae6bf67e6bfbfc0b1fa
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 11:50:58 -0700

    sendfile/copy_file_range: sanitise offset pointer args
    Random pointers for off_t/loff_t arguments fault immediately. Add
    sanitise callbacks that point to static variables holding a valid file
    offset in [0, 1GB], letting the kernel actually reach the data path.

--- syscalls/copy_file_range.c
+++ syscalls/copy_file_range.c
@@ -18,12 +18,24 @@ static unsigned long copy_file_range_flags[] = {
 	0,	// so far, no flags, MBZ.
 };
 
+static loff_t cfr_off_in;
+static loff_t cfr_off_out;
+
+static void sanitise_copy_file_range(struct syscallrecord *rec)
+{
+	cfr_off_in = RAND_RANGE(0, 1ULL << 30);
+	cfr_off_out = RAND_RANGE(0, 1ULL << 30);
+	rec->a2 = (unsigned long) &cfr_off_in;
+	rec->a4 = (unsigned long) &cfr_off_out;
+}
+
 struct syscallentry syscall_copy_file_range = {
 	.name = "copy_file_range",
 	.num_args = 6,
 	.argtype = { [0] = ARG_FD, [1] = ARG_ADDRESS, [2] = ARG_FD, [3] = ARG_ADDRESS, [4] = ARG_LEN, [5] = ARG_LIST },
 	.argname = { [0] = "fd_in", [1] = "off_in", [2] = "fd_out", [3] = "off_out", [4] = "len", [5] = "flags" },
 	.arg6list = ARGLIST(copy_file_range_flags),
+	.sanitise = sanitise_copy_file_range,
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
 };
