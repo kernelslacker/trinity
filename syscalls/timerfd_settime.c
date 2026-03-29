c580733555028859ae8ea0c7f1351d952a0414a6
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 09:43:43 -0700

    timerfd_settime: add sanitise to fill in a valid struct itimerspec

--- syscalls/timerfd_settime.c
+++ syscalls/timerfd_settime.c
@@ -3,7 +3,9 @@
 	 const struct itimerspec __user *, utmr,
 	 struct itimerspec __user *, otmr)
  */
+#include <time.h>
 #include <sys/timerfd.h>
+#include "random.h"
 #include "sanitise.h"
 
 #ifndef TFD_CLOEXEC
@@ -23,6 +25,23 @@ static unsigned long timerfd_settime_flags[] = {
 	TFD_TIMER_ABSTIME, TFD_TIMER_CANCEL_ON_SET,
 };
 
+static void sanitise_timerfd_settime(struct syscallrecord *rec)
+{
+	struct itimerspec *its;
+
+	its = (struct itimerspec *) get_writable_address(sizeof(*its));
+
+	/* interval: 1-10 seconds */
+	its->it_interval.tv_sec = 1 + (rand() % 10);
+	its->it_interval.tv_nsec = 0;
+
+	/* value: 1-10 seconds */
+	its->it_value.tv_sec = 1 + (rand() % 10);
+	its->it_value.tv_nsec = 0;
+
+	rec->a3 = (unsigned long) its;
+}
+
 struct syscallentry syscall_timerfd_settime = {
 	.name = "timerfd_settime",
 	.group = GROUP_TIME,
@@ -30,5 +49,6 @@ struct syscallentry syscall_timerfd_settime = {
 	.argtype = { [0] = ARG_FD_TIMERFD, [1] = ARG_LIST, [2] = ARG_ADDRESS, [3] = ARG_ADDRESS },
 	.argname = { [0] = "ufd", [1] = "flags", [2] = "utmr", [3] = "otmr" },
 	.arg2list = ARGLIST(timerfd_settime_flags),
+	.sanitise = sanitise_timerfd_settime,
 	.flags = NEED_ALARM,
 };
