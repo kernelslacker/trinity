9f2b5241f5c87a55b924e6c47abf69da1d7c5841
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 10:26:10 -0700

    pciconfig_read/write: constrain bus, dfn, off to valid ranges
    bus and dfn are 0-255 (8-bit PCI fields), off is 0-4095 (extended PCI
    config space). Random 32-bit values just produce EINVAL.

--- syscalls/pciconfig_read.c
+++ syscalls/pciconfig_read.c
@@ -7,7 +7,10 @@
 struct syscallentry syscall_pciconfig_read = {
 	.name = "pciconfig_read",
 	.num_args = 5,
-	.argtype = { [3] = ARG_LEN, [4] = ARG_ADDRESS },
+	.argtype = { [0] = ARG_RANGE, [1] = ARG_RANGE, [2] = ARG_RANGE, [3] = ARG_LEN, [4] = ARG_ADDRESS },
 	.argname = { [0] = "bus", [1] = "dfn", [2] = "off", [3] = "len", [4] = "buf" },
+	.low1range = 0, .hi1range = 255,
+	.low2range = 0, .hi2range = 255,
+	.low3range = 0, .hi3range = 4095,
 	.group = GROUP_PROCESS,
 };
