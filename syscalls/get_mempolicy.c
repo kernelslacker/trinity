bf06d1a3b68ec481ed74985f3c7e5f3c8264ad1e
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 11:53:17 -0700

    get_mempolicy: use ARG_MMAP for addr argument
    When MPOL_F_ADDR is set in flags, the kernel uses addr to look up the
    VMA's memory policy.  ARG_ADDRESS could pass an unmapped address and
    fail the lookup; ARG_MMAP provides a valid mapped address.

--- syscalls/get_mempolicy.c
+++ syscalls/get_mempolicy.c
@@ -17,7 +17,7 @@ static unsigned long get_mempolicy_flags[] = {
 struct syscallentry syscall_get_mempolicy = {
 	.name = "get_mempolicy",
 	.num_args = 5,
-	.argtype = { [0] = ARG_ADDRESS, [1] = ARG_ADDRESS, [2] = ARG_RANGE, [3] = ARG_ADDRESS, [4] = ARG_LIST },
+	.argtype = { [0] = ARG_ADDRESS, [1] = ARG_ADDRESS, [2] = ARG_RANGE, [3] = ARG_MMAP, [4] = ARG_LIST },
 	.argname = { [0] = "policy", [1] = "nmask", [2] = "maxnode", [3] = "addr", [4] = "flags" },
 	.low3range = 0,
 	.hi3range = 1 << 9,	/* 1 << CONFIG_NODES_SHIFT */
