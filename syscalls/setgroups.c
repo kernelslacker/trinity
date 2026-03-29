3493d8d0c53ca4541acc64fc9ab1f28c3c60aafb
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 09:45:23 -0700

    setgroups: add sanitise to allocate a gid_t array sized to gidsetsize

--- syscalls/setgroups.c
+++ syscalls/setgroups.c
@@ -1,7 +1,25 @@
 /*
  * SYSCALL_DEFINE2(setgroups, int, gidsetsize, gid_t __user *, grouplist)
  */
+#include <grp.h>
+#include "random.h"
 #include "sanitise.h"
+
+static void sanitise_setgroups(struct syscallrecord *rec)
+{
+	int count = (int) rec->a1;
+	gid_t *list;
+	int i;
+
+	if (count <= 0 || count > 65536)
+		return;
+
+	list = (gid_t *) get_writable_address(count * sizeof(gid_t));
+	for (i = 0; i < count; i++)
+		list[i] = (gid_t) rand();
+
+	rec->a2 = (unsigned long) list;
+}
 
 struct syscallentry syscall_setgroups = {
 	.name = "setgroups",
@@ -10,6 +28,7 @@ struct syscallentry syscall_setgroups = {
 	.argname = { [0] = "gidsetsize", [1] = "grouplist" },
 	.low1range = 0,
 	.hi1range = 65536,
+	.sanitise = sanitise_setgroups,
 	.group = GROUP_PROCESS,
 };
 
