c55d9c4e657be7316cf9d33d028b323acb064117
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 09:44:47 -0700

    rt_sigtimedwait: add sanitise for sigset_t and short struct timespec timeout

--- syscalls/rt_sigtimedwait.c
+++ syscalls/rt_sigtimedwait.c
@@ -3,7 +3,31 @@
 	 siginfo_t __user *, uinfo, const struct timespec __user *, uts,
 	 size_t, sigsetsize)
  */
+#include <signal.h>
+#include <time.h>
+#include "random.h"
 #include "sanitise.h"
+
+static void sanitise_rt_sigtimedwait(struct syscallrecord *rec)
+{
+	sigset_t *set;
+	struct timespec *ts;
+
+	set = (sigset_t *) get_writable_address(sizeof(*set));
+	sigemptyset(set);
+	sigaddset(set, SIGUSR1);
+	sigaddset(set, SIGUSR2);
+	sigaddset(set, SIGALRM);
+
+	/* short timeout: 0-1ms to avoid blocking */
+	ts = (struct timespec *) get_writable_address(sizeof(*ts));
+	ts->tv_sec = 0;
+	ts->tv_nsec = rand() % 1000000;
+
+	rec->a1 = (unsigned long) set;
+	rec->a3 = (unsigned long) ts;
+	rec->a4 = sizeof(sigset_t);
+}
 
 struct syscallentry syscall_rt_sigtimedwait = {
 	.name = "rt_sigtimedwait",
@@ -11,5 +35,6 @@ struct syscallentry syscall_rt_sigtimedwait = {
 	.num_args = 4,
 	.argtype = { [0] = ARG_ADDRESS, [1] = ARG_ADDRESS, [2] = ARG_ADDRESS, [3] = ARG_LEN },
 	.argname = { [0] = "uthese", [1] = "uinfo", [2] = "uts", [3] = "sigsetsize" },
+	.sanitise = sanitise_rt_sigtimedwait,
 	.flags = NEED_ALARM,
 };
