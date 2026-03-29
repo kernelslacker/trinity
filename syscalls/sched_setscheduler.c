ed54ef8e26ddf46796572ef9f449b54d1f4a6f4e
Dave Jones <davej@codemonkey.org.uk>
2026-03-29 09:44:18 -0700

    sched_setscheduler: add sanitise to fill in a valid struct sched_param

--- syscalls/sched_setscheduler.c
+++ syscalls/sched_setscheduler.c
@@ -3,12 +3,29 @@
  */
 #include <sched.h>
 #include "compat.h"
+#include "random.h"
 #include "sanitise.h"
 
 static unsigned long sched_setscheduler_policies[] = {
 	SCHED_OTHER, SCHED_FIFO, SCHED_RR, SCHED_BATCH,
 	SCHED_IDLE, SCHED_DEADLINE,
 };
+
+static void sanitise_sched_setscheduler(struct syscallrecord *rec)
+{
+	struct sched_param *sp;
+
+	sp = (struct sched_param *) get_writable_address(sizeof(*sp));
+
+	switch (rand() % 4) {
+	case 0: sp->sched_priority = 0; break;			/* SCHED_OTHER/BATCH/IDLE */
+	case 1: sp->sched_priority = 1; break;			/* minimum RT */
+	case 2: sp->sched_priority = 99; break;			/* maximum RT */
+	default: sp->sched_priority = rand() % 100; break;	/* random valid */
+	}
+
+	rec->a3 = (unsigned long) sp;
+}
 
 struct syscallentry syscall_sched_setscheduler = {
 	.name = "sched_setscheduler",
@@ -17,4 +34,5 @@ struct syscallentry syscall_sched_setscheduler = {
 	.argtype = { [0] = ARG_PID, [1] = ARG_OP, [2] = ARG_ADDRESS },
 	.argname = { [0] = "pid", [1] = "policy", [2] = "param" },
 	.arg2list = ARGLIST(sched_setscheduler_policies),
+	.sanitise = sanitise_sched_setscheduler,
 };
