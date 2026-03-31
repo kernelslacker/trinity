a1d5956ab8579a1282e8ce138196e04759ab7bda
Dave Jones <davej@codemonkey.org.uk>
2026-03-30 21:02:27 -0700

    syscalls/timerfd_settime: remove timerfd_create flags from settime flags
    TFD_NONBLOCK and TFD_CLOEXEC are only valid for timerfd_create(2), not
    timerfd_settime(2).  The valid flags for settime are TFD_TIMER_ABSTIME
    and TFD_TIMER_CANCEL_ON_SET.  Remove the invalid flags and the now-
    unused #ifndef guards for them.

--- syscalls/timerfd_settime.c
+++ syscalls/timerfd_settime.c
@@ -8,20 +8,11 @@
 #include "random.h"
 #include "sanitise.h"
 
-#ifndef TFD_CLOEXEC
-#define TFD_CLOEXEC 02000000
-#endif
-
-#ifndef TFD_NONBLOCK
-#define TFD_NONBLOCK 04000
-#endif
-
 #ifndef TFD_TIMER_CANCEL_ON_SET
 #define TFD_TIMER_CANCEL_ON_SET (1 << 1)
 #endif
 
 static unsigned long timerfd_settime_flags[] = {
-	TFD_NONBLOCK, TFD_CLOEXEC,
 	TFD_TIMER_ABSTIME, TFD_TIMER_CANCEL_ON_SET,
 };
 
