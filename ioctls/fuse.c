9a4c630523aada0f93e1d42b9e20fb39902bf402
Dave Jones <davej@codemonkey.org.uk>
2026-04-11 08:47:15 -0700

    Add FUSE ioctl sanitise functions
    Replace pick_random_ioctl with a custom sanitise for the FUSE device
    ioctls:
    
    - FUSE_DEV_IOC_BACKING_OPEN: populate a fuse_backing_map struct with a
      random fd from the fd pool, flags=0, padding=0.
    
    - FUSE_DEV_IOC_BACKING_CLOSE: pass a random uint32_t backing id.
    
    Other ioctls (CLONE, SYNC_INIT) fall through to the default random arg
    behavior from pick_random_ioctl.

--- ioctls/fuse.c
+++ ioctls/fuse.c
@@ -1,8 +1,47 @@
 #include <linux/ioctl.h>
 #include <linux/fuse.h>
 
+#include "fd.h"
 #include "ioctls.h"
+#include "random.h"
+#include "sanitise.h"
 #include "utils.h"
+
+static void sanitise_fuse_backing_open(struct syscallrecord *rec)
+{
+	struct fuse_backing_map *map;
+
+	map = (struct fuse_backing_map *) get_writable_address(sizeof(*map));
+	map->fd = get_random_fd();
+	map->flags = 0;
+	map->padding = 0;
+	rec->a3 = (unsigned long) map;
+}
+
+static void sanitise_fuse_backing_close(struct syscallrecord *rec)
+{
+	uint32_t *id;
+
+	id = (uint32_t *) get_writable_address(sizeof(*id));
+	*id = rand32();
+	rec->a3 = (unsigned long) id;
+}
+
+static void fuse_sanitise(const struct ioctl_group *grp, struct syscallrecord *rec)
+{
+	pick_random_ioctl(grp, rec);
+
+	switch (rec->a2) {
+	case FUSE_DEV_IOC_BACKING_OPEN:
+		sanitise_fuse_backing_open(rec);
+		break;
+	case FUSE_DEV_IOC_BACKING_CLOSE:
+		sanitise_fuse_backing_close(rec);
+		break;
+	default:
+		break;
+	}
+}
 
 static const struct ioctl fuse_ioctls[] = {
 	IOCTL(FUSE_DEV_IOC_CLONE),
@@ -21,7 +60,7 @@ static const struct ioctl_group fuse_grp = {
 	.devtype = DEV_MISC,
 	.devs = fuse_devs,
 	.devs_cnt = ARRAY_SIZE(fuse_devs),
-	.sanitise = pick_random_ioctl,
+	.sanitise = fuse_sanitise,
 	.ioctls = fuse_ioctls,
 	.ioctls_cnt = ARRAY_SIZE(fuse_ioctls),
 };
