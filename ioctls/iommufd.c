6dca9b14b16115eea1f86eb7a562b6eb8cb9cd82
Dave Jones <davej@codemonkey.org.uk>
2026-04-10 20:27:18 -0700

    Add IOMMUFD ioctl table
    IOMMUFD is the modern IOMMU interface (linux/iommufd.h) used by QEMU
    and cloud hypervisors to manage IOMMU page tables and device DMA.
    It was added in kernel 6.2 and exposes /dev/iommu as a misc device.
    
    Register all 15 ioctls present in the 6.12 kernel headers as
    unconditional entries, plus 6 post-6.12 ioctls behind individual
    #ifdef guards: IOMMU_IOAS_MAP_FILE, IOMMU_VIOMMU_ALLOC,
    IOMMU_VDEVICE_ALLOC, IOMMU_IOAS_CHANGE_PROCESS,
    IOMMU_VEVENTQ_ALLOC, and IOMMU_HW_QUEUE_ALLOC.
    
    Add a configure check for linux/iommufd.h -> USE_IOMMUFD.

---
+++ ioctls/iommufd.c
@@ -0,0 +1,58 @@
+
+#ifdef USE_IOMMUFD
+#include <linux/iommufd.h>
+
+#include "utils.h"
+#include "ioctls.h"
+
+static const struct ioctl iommufd_ioctls[] = {
+	IOCTL(IOMMU_DESTROY),
+	IOCTL(IOMMU_IOAS_ALLOC),
+	IOCTL(IOMMU_IOAS_IOVA_RANGES),
+	IOCTL(IOMMU_IOAS_ALLOW_IOVAS),
+	IOCTL(IOMMU_IOAS_MAP),
+	IOCTL(IOMMU_IOAS_COPY),
+	IOCTL(IOMMU_IOAS_UNMAP),
+	IOCTL(IOMMU_OPTION),
+	IOCTL(IOMMU_VFIO_IOAS),
+	IOCTL(IOMMU_HWPT_ALLOC),
+	IOCTL(IOMMU_GET_HW_INFO),
+	IOCTL(IOMMU_HWPT_SET_DIRTY_TRACKING),
+	IOCTL(IOMMU_HWPT_GET_DIRTY_BITMAP),
+	IOCTL(IOMMU_HWPT_INVALIDATE),
+	IOCTL(IOMMU_FAULT_QUEUE_ALLOC),
+#ifdef IOMMU_IOAS_MAP_FILE
+	IOCTL(IOMMU_IOAS_MAP_FILE),
+#endif
+#ifdef IOMMU_VIOMMU_ALLOC
+	IOCTL(IOMMU_VIOMMU_ALLOC),
+#endif
+#ifdef IOMMU_VDEVICE_ALLOC
+	IOCTL(IOMMU_VDEVICE_ALLOC),
+#endif
+#ifdef IOMMU_IOAS_CHANGE_PROCESS
+	IOCTL(IOMMU_IOAS_CHANGE_PROCESS),
+#endif
+#ifdef IOMMU_VEVENTQ_ALLOC
+	IOCTL(IOMMU_VEVENTQ_ALLOC),
+#endif
+#ifdef IOMMU_HW_QUEUE_ALLOC
+	IOCTL(IOMMU_HW_QUEUE_ALLOC),
+#endif
+};
+
+static const char *const iommufd_devs[] = {
+	"iommu",
+};
+
+static const struct ioctl_group iommufd_grp = {
+	.devtype = DEV_MISC,
+	.devs = iommufd_devs,
+	.devs_cnt = ARRAY_SIZE(iommufd_devs),
+	.sanitise = pick_random_ioctl,
+	.ioctls = iommufd_ioctls,
+	.ioctls_cnt = ARRAY_SIZE(iommufd_ioctls),
+};
+
+REG_IOCTL_GROUP(iommufd_grp)
+#endif
