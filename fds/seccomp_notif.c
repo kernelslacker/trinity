89cffbf5bafe076f4586f30591098da4b4aa3e94
Dave Jones <davej@codemonkey.org.uk>
2026-04-10 20:14:27 -0700

    seccomp: add notification fd provider
    The five seccomp notification ioctls in ioctls/seccomp.c were dead code
    because no fd provider existed to create the anonymous fds they operate
    on.
    
    Add fds/seccomp_notif.c which uses seccomp(SECCOMP_SET_MODE_FILTER,
    SECCOMP_FILTER_FLAG_NEW_LISTENER) to obtain notification fds.  The BPF
    program intercepts only getpid() with SECCOMP_RET_USER_NOTIF and allows
    everything else, so it doesn't interfere with normal fuzzer operation.
    
    Add OBJ_FD_SECCOMP_NOTIF to the object type enum and a corresponding
    seccomp_notifobj struct.  Add an fd_test callback to the seccomp ioctl
    group so ioctl fuzzing is directed at seccomp notification fds. Wrap
    both the fd provider and ioctl group in #ifdef USE_SECCOMP.

---
+++ fds/seccomp_notif.c
@@ -0,0 +1,127 @@
+/* seccomp notification FDs */
+
+#ifdef USE_SECCOMP
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <linux/filter.h>
+#include <linux/seccomp.h>
+
+#include "fd.h"
+#include "objects.h"
+#include "random.h"
+#include "sanitise.h"
+#include "shm.h"
+#include "compat.h"
+
+#ifndef SECCOMP_SET_MODE_FILTER
+#define SECCOMP_SET_MODE_FILTER 1
+#endif
+#ifndef SECCOMP_FILTER_FLAG_NEW_LISTENER
+#define SECCOMP_FILTER_FLAG_NEW_LISTENER (1UL << 3)
+#endif
+#ifndef SECCOMP_RET_USER_NOTIF
+#define SECCOMP_RET_USER_NOTIF 0x7fc00000U
+#endif
+#ifndef SECCOMP_RET_ALLOW
+#define SECCOMP_RET_ALLOW 0x7fff0000U
+#endif
+
+static void seccomp_notif_destructor(struct object *obj)
+{
+	close(obj->seccomp_notifobj.fd);
+}
+
+static void seccomp_notif_dump(struct object *obj, enum obj_scope scope)
+{
+	output(2, "seccomp_notif fd:%d scope:%d\n",
+		obj->seccomp_notifobj.fd, scope);
+}
+
+/*
+ * Build a minimal BPF program that returns SECCOMP_RET_USER_NOTIF for
+ * getpid() and SECCOMP_RET_ALLOW for everything else.  This gives us
+ * a notification fd without interfering with normal operation.
+ */
+static int create_seccomp_notif_fd(void)
+{
+	struct sock_filter filter[] = {
+		/* A = syscall number */
+		BPF_STMT(BPF_LD | BPF_W | BPF_ABS,
+			 offsetof(struct seccomp_data, nr)),
+		/* if (A == __NR_getpid) goto notify */
+		BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_getpid, 0, 1),
+		/* notify: return USER_NOTIF */
+		BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_USER_NOTIF),
+		/* allow: return ALLOW */
+		BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+	};
+	struct sock_fprog prog = {
+		.len = (unsigned short)(sizeof(filter) / sizeof(filter[0])),
+		.filter = filter,
+	};
+
+	return syscall(__NR_seccomp, SECCOMP_SET_MODE_FILTER,
+		       SECCOMP_FILTER_FLAG_NEW_LISTENER, &prog);
+}
+
+static int open_seccomp_notif(void)
+{
+	struct object *obj;
+	int fd;
+
+	fd = create_seccomp_notif_fd();
+	if (fd < 0)
+		return false;
+
+	obj = alloc_object();
+	obj->seccomp_notifobj.fd = fd;
+	add_object(obj, OBJ_GLOBAL, OBJ_FD_SECCOMP_NOTIF);
+	return true;
+}
+
+static int init_seccomp_notif_fds(void)
+{
+	struct objhead *head;
+
+	head = get_objhead(OBJ_GLOBAL, OBJ_FD_SECCOMP_NOTIF);
+	head->destroy = &seccomp_notif_destructor;
+	head->dump = &seccomp_notif_dump;
+
+	/* Create a small pool.  Each call installs a new seccomp filter,
+	 * so don't go overboard. */
+	open_seccomp_notif();
+	open_seccomp_notif();
+
+	return true;
+}
+
+static int get_rand_seccomp_notif_fd(void)
+{
+	struct object *obj;
+
+	if (objects_empty(OBJ_FD_SECCOMP_NOTIF) == true)
+		return -1;
+
+	obj = get_random_object(OBJ_FD_SECCOMP_NOTIF, OBJ_GLOBAL);
+	if (obj == NULL)
+		return -1;
+	return obj->seccomp_notifobj.fd;
+}
+
+static const struct fd_provider seccomp_notif_fd_provider = {
+	.name = "seccomp-notif",
+	.objtype = OBJ_FD_SECCOMP_NOTIF,
+	.enabled = true,
+	.init = &init_seccomp_notif_fds,
+	.get = &get_rand_seccomp_notif_fd,
+	.open = &open_seccomp_notif,
+};
+
+REG_FD_PROV(seccomp_notif_fd_provider);
+
+#endif /* USE_SECCOMP */
