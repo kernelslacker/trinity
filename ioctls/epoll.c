1f4c5848a3ec5ccf24616ef767ac85a2424a31ea
Dave Jones <davej@codemonkey.org.uk>
2026-04-11 08:44:33 -0700

    ioctls/epoll: add sanitise for EPIOCSPARAMS and EPIOCGPARAMS
    Replace pick_random_ioctl with a custom sanitise that always allocates
    a struct epoll_params and passes its address as the ioctl argument.
    For EPIOCSPARAMS the fields are filled with random-but-valid values;
    for EPIOCGPARAMS the zeroed allocation is sufficient as the kernel
    will fill it in.

--- ioctls/epoll.c
+++ ioctls/epoll.c
@@ -6,6 +6,8 @@
 
 #include "ioctls.h"
 #include "objects.h"
+#include "random.h"
+#include "sanitise.h"
 #include "shm.h"
 #include "utils.h"
 
@@ -36,6 +38,24 @@ struct epoll_params {
 #define EPIOCGPARAMS _IOR(EPOLL_IOC_TYPE, 0x02, struct epoll_params)
 #endif
 
+static void epoll_sanitise(const struct ioctl_group *grp, struct syscallrecord *rec)
+{
+	struct epoll_params *params;
+
+	pick_random_ioctl(grp, rec);
+
+	params = (struct epoll_params *) get_writable_address(sizeof(*params));
+
+	if (rec->a2 == EPIOCSPARAMS) {
+		params->busy_poll_usecs = rand() % 1000000;
+		params->busy_poll_budget = rand() % 256;
+		params->prefer_busy_poll = RAND_BOOL();
+		params->__pad = 0;
+	}
+
+	rec->a3 = (unsigned long) params;
+}
+
 static const struct ioctl epoll_ioctls[] = {
 	IOCTL(EPIOCSPARAMS),
 	IOCTL(EPIOCGPARAMS),
@@ -44,7 +64,7 @@ static const struct ioctl epoll_ioctls[] = {
 static const struct ioctl_group epoll_grp = {
 	.name = "epoll",
 	.fd_test = epoll_fd_test,
-	.sanitise = pick_random_ioctl,
+	.sanitise = epoll_sanitise,
 	.ioctls = epoll_ioctls,
 	.ioctls_cnt = ARRAY_SIZE(epoll_ioctls),
 };
