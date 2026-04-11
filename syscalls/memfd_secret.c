b89c2f4267cf9a2a0a90d0963f2d0fdf9d214827
Dave Jones <davej@codemonkey.org.uk>
2026-04-10 18:03:24 -0700

    signalfd/memfd_secret/pidfd_getfd/mq_open: close leaked fds
    These four syscalls all have RET_FD but had no post handler, so every
    successful call leaked an fd in the child's fd table.
    
    Add post handlers that close the returned fd immediately.  For mq_open,
    also call mq_unlink() to clean up the kernel IPC resource.

--- syscalls/memfd_secret.c
+++ syscalls/memfd_secret.c
@@ -3,6 +3,7 @@
  */
 
 #include <fcntl.h>
+#include <unistd.h>
 #include "sanitise.h"
 #include "memfd.h"
 #include "compat.h"
@@ -11,6 +12,14 @@ static unsigned long memfd_secret_flags[] = {
 	O_CLOEXEC,
 };
 
+static void post_memfd_secret(struct syscallrecord *rec)
+{
+	int fd = rec->retval;
+
+	if (fd != -1)
+		close(fd);
+}
+
 struct syscallentry syscall_memfd_secret = {
 	.name = "memfd_secret",
 	.num_args = 1,
@@ -18,5 +27,6 @@ struct syscallentry syscall_memfd_secret = {
 	.argname = { [0] = "flag" },
 	.arg_params[0].list = ARGLIST(memfd_secret_flags),
 	.rettype = RET_FD,
+	.post = post_memfd_secret,
 	.group = GROUP_VFS,
 };
--- syscalls/pidfd_getfd.c
+++ syscalls/pidfd_getfd.c
@@ -1,11 +1,20 @@
 /*
  * SYSCALL_DEFINE3(pidfd_getfd, int, pidfd, int, fd, unsigned int, flags)
  */
+#include <unistd.h>
 #include "sanitise.h"
 
 static unsigned long pidfd_getfd_flags[] = {
 	0,
 };
+
+static void post_pidfd_getfd(struct syscallrecord *rec)
+{
+	int fd = rec->retval;
+
+	if (fd != -1)
+		close(fd);
+}
 
 struct syscallentry syscall_pidfd_getfd = {
 	.name = "pidfd_getfd",
@@ -15,4 +24,5 @@ struct syscallentry syscall_pidfd_getfd = {
 	.argname = { [0] = "pidfd", [1] = "fd", [2] = "flags" },
 	.arg_params[2].list = ARGLIST(pidfd_getfd_flags),
 	.rettype = RET_FD,
+	.post = post_pidfd_getfd,
 };
