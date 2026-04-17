223d47e36ad55775f29dfef601e35623e2509195
Dave Jones <davej@codemonkey.org.uk>
2026-04-16 21:25:15 -0700

    net/alg: use per-type algorithm lists in alg_gen_sockaddr
    The shared algos[] array was used for aead, rng, skcipher, akcipher,
    and kpp types, but it contained a random mix of algorithm names from
    every type. Most bind() calls would fail with ENOENT because the kernel
    validates that the algorithm name matches the requested type.
    
    Replace algos[] with per-type lists: aead_algos, rng_algos,
    skcipher_algos, akcipher_algos, kpp_algos. The hash type already used
    the separate hashes[] array and is unchanged.

--- net/proto-alg.c
+++ net/proto-alg.c
@@ -17,204 +17,67 @@ static const char *hashes[] = {
 	"blake2b-256", "blake2b-512", "sm3",
 };
 
-static const char *algos[] = {
-	"__cbc-cast5-avx",
-	"__cbc-cast6-avx",
-	"__cbc-serpent-avx",
-	"__cbc-serpent-avx2",
-	"__cbc-serpent-sse2",
-	"__cbc-twofish-avx",
-	"__driver-cbc-aes-aesni",
-	"__driver-cbc-camellia-aesni",
-	"__driver-cbc-camellia-aesni-avx2",
-	"__driver-cbc-cast5-avx",
-	"__driver-cbc-cast6-avx",
-	"__driver-cbc-serpent-avx",
-	"__driver-cbc-serpent-avx2",
-	"__driver-cbc-serpent-sse2",
-	"__driver-cbc-twofish-avx",
-	"__driver-ecb-aes-aesni",
-	"__driver-ecb-camellia-aesni",
-	"__driver-ecb-camellia-aesni-avx2",
-	"__driver-ecb-cast5-avx",
-	"__driver-ecb-cast6-avx",
-	"__driver-ecb-serpent-avx",
-	"__driver-ecb-serpent-avx2",
-	"__driver-ecb-serpent-sse2",
-	"__driver-ecb-twofish-avx",
-	"__driver-gcm-aes-aesni",
-	"__ghash-pclmulqdqni",
-	"ansi_cprng",
-	"authenc(hmac(md5),ecb(cipher_null))",
-	"authenc(hmac(sha1),cbc(aes))",
-	"authenc(hmac(sha1),cbc(des))",
-	"authenc(hmac(sha1),cbc(des3_ede))",
-	"authenc(hmac(sha1),ctr(aes))",
-	"authenc(hmac(sha1),ecb(cipher_null))",
-	"authenc(hmac(sha224),cbc(des))",
-	"authenc(hmac(sha224),cbc(des3_ede))",
-	"authenc(hmac(sha256),cbc(aes))",
-	"authenc(hmac(sha256),cbc(des))",
-	"authenc(hmac(sha256),cbc(des3_ede))",
-	"authenc(hmac(sha256),ctr(aes))",
-	"authenc(hmac(sha384),cbc(des))",
-	"authenc(hmac(sha384),cbc(des3_ede))",
-	"authenc(hmac(sha384),ctr(aes))",
-	"authenc(hmac(sha512),cbc(aes))",
-	"authenc(hmac(sha512),cbc(des))",
-	"authenc(hmac(sha512),cbc(des3_ede))",
-	"authenc(hmac(sha512),ctr(aes))",
-	"cbc(aes)",
-	"cbc(anubis)",
-	"cbc(blowfish)",
-	"cbc(camellia)",
-	"cbc(cast5)",
-	"cbc(cast6)",
-	"cbc(des)",
-	"cbc(des3_ede)",
-	"cbc(serpent)",
-	"cbc(twofish)",
-	"ccm(aes)",
-	"chacha20",
-	"cmac(aes)",
-	"cmac(des3_ede)",
-	"compress_null",
-	"crc32",
-	"crc32c",
-	"crct10dif",
-	"cryptd(__driver-cbc-aes-aesni)",
-	"cryptd(__driver-cbc-camellia-aesni)",
-	"cryptd(__driver-cbc-camellia-aesni-avx2)",
-	"cryptd(__driver-cbc-serpent-avx2)",
-	"cryptd(__driver-ecb-aes-aesni)",
-	"cryptd(__driver-ecb-camellia-aesni)",
-	"cryptd(__driver-ecb-camellia-aesni-avx2)",
-	"cryptd(__driver-ecb-cast5-avx)",
-	"cryptd(__driver-ecb-cast6-avx)",
-	"cryptd(__driver-ecb-serpent-avx)",
-	"cryptd(__driver-ecb-serpent-avx2)",
-	"cryptd(__driver-ecb-serpent-sse2)",
-	"cryptd(__driver-ecb-twofish-avx)",
-	"cryptd(__driver-gcm-aes-aesni)",
-	"cryptd(__ghash-pclmulqdqni)",
-	"ctr(aes)",
-	"ctr(blowfish)",
-	"ctr(camellia)",
-	"ctr(cast5)",
-	"ctr(cast6)",
-	"ctr(des)",
-	"ctr(des3_ede)",
-	"ctr(serpent)",
-	"ctr(twofish)",
-	"cts(cbc(aes))",
-	"deflate",
-	"digest_null",
-	"drbg_nopr_ctr_aes128",
-	"drbg_nopr_ctr_aes192",
-	"drbg_nopr_ctr_aes256",
-	"drbg_nopr_hmac_sha1",
-	"drbg_nopr_hmac_sha256",
-	"drbg_nopr_hmac_sha384",
-	"drbg_nopr_hmac_sha512",
-	"drbg_nopr_sha1",
-	"drbg_nopr_sha256",
-	"drbg_nopr_sha384",
-	"drbg_nopr_sha512",
-	"drbg_pr_ctr_aes128",
-	"drbg_pr_ctr_aes192",
-	"drbg_pr_ctr_aes256",
-	"drbg_pr_hmac_sha1",
-	"drbg_pr_hmac_sha256",
-	"drbg_pr_hmac_sha384",
-	"drbg_pr_hmac_sha512",
-	"drbg_pr_sha1",
-	"drbg_pr_sha256",
-	"drbg_pr_sha384",
-	"drbg_pr_sha512",
-	"ecb(__aes-aesni)",
-	"ecb(aes)",
-	"ecb(anubis)",
-	"ecb(arc4)",
-	"ecb(blowfish)",
-	"ecb(camellia)",
-	"ecb(cast5)",
-	"ecb(cast6)",
-	"ecb(cipher_null)",
-	"ecb(des)",
-	"ecb(des3_ede)",
-	"ecb(fcrypt)",
-	"ecb(khazad)",
-	"ecb(seed)",
-	"ecb(serpent)",
-	"ecb(tea)",
-	"ecb(tnepres)",
-	"ecb(twofish)",
-	"ecb(xeta)",
-	"ecb(xtea)",
+static const char *aead_algos[] = {
 	"gcm(aes)",
-	"ghash",
-	"hmac(crc32)",
-	"hmac(md5)",
-	"hmac(rmd128)",
-	"hmac(rmd160)",
-	"hmac(sha1)",
-	"hmac(sha224)",
-	"hmac(sha256)",
-	"hmac(sha384)",
-	"hmac(sha512)",
-	"jitterentropy_rng",
-	"kw(aes)",
-	"lrw(aes)",
-	"lrw(camellia)",
-	"lrw(cast6)",
-	"lrw(serpent)",
-	"lrw(twofish)",
-	"lz4",
-	"lz4hc",
-	"lzo",
-	"md4",
-	"md5",
-	"michael_mic",
-	"ofb(aes)",
-	"pcbc(fcrypt)",
-	"poly1305",
-	"rfc3686(ctr(aes))",
+	"ccm(aes)",
 	"rfc4106(gcm(aes))",
 	"rfc4309(ccm(aes))",
 	"rfc4543(gcm(aes))",
 	"rfc7539(chacha20,poly1305)",
 	"rfc7539esp(chacha20,poly1305)",
-	"rmd128",
-	"rmd160",
-	"rmd256",
-	"rmd320",
-	"rsa",
-	"salsa20",
-	"sha1",
-	"sha224",
-	"sha256",
-	"sha384",
-	"sha512",
-	"tgr128",
-	"tgr160",
-	"tgr192",
-	"vmac(aes)",
-	"wp256",
-	"wp384",
-	"wp512",
-	"xcbc(aes)",
+	"authenc(hmac(sha1),cbc(aes))",
+	"authenc(hmac(sha1),cbc(des3_ede))",
+	"authenc(hmac(sha256),cbc(aes))",
+	"authenc(hmac(sha512),cbc(aes))",
+};
+
+static const char *rng_algos[] = {
+	"ansi_cprng",
+	"drbg_nopr_ctr_aes128",
+	"drbg_nopr_ctr_aes192",
+	"drbg_nopr_ctr_aes256",
+	"drbg_nopr_hmac_sha256",
+	"drbg_nopr_sha256",
+	"drbg_pr_ctr_aes128",
+	"drbg_pr_ctr_aes192",
+	"drbg_pr_ctr_aes256",
+	"drbg_pr_hmac_sha256",
+	"drbg_pr_sha256",
+	"jitterentropy_rng",
+};
+
+static const char *skcipher_algos[] = {
+	"cbc(aes)",
+	"cbc(des)",
+	"cbc(des3_ede)",
+	"cbc(camellia)",
+	"ecb(aes)",
+	"ecb(des)",
+	"ecb(des3_ede)",
+	"ecb(camellia)",
+	"ctr(aes)",
+	"ctr(camellia)",
 	"xts(aes)",
-	"xts(camellia)",
-	"xts(cast6)",
-	"xts(serpent)",
-	"xts(twofish)",
-	"zlib",
+	"lrw(aes)",
+	"cts(cbc(aes))",
+	"chacha20",
+	"salsa20",
+};
+
+static const char *akcipher_algos[] = {
+	"rsa",
+};
+
+static const char *kpp_algos[] = {
+	"dh",
+	"ecdh-nist-p192",
+	"ecdh-nist-p256",
 };
 
 static void alg_gen_sockaddr(struct sockaddr **addr, socklen_t *addrlen)
 {
 	struct sockaddr_alg *alg;
-	const char **algs = algos;
+	const char **algs = NULL;
 	unsigned int type;
 	const char *types[] = { "aead", "hash", "rng", "skcipher", "akcipher", "kpp", };
 	unsigned int algo;
@@ -228,23 +91,28 @@ static void alg_gen_sockaddr(struct sockaddr **addr, socklen_t *addrlen)
 
 	switch (type) {
 	// aead
-	case 0:	algo = rand() % ARRAY_SIZE(algos);
+	case 0:	algs = aead_algos;
+		algo = rand() % ARRAY_SIZE(aead_algos);
 		break;
 	// hash
-	case 1:	algo = rand() % ARRAY_SIZE(hashes);
-		algs = hashes;
+	case 1:	algs = hashes;
+		algo = rand() % ARRAY_SIZE(hashes);
 		break;
 	// rng
-	case 2:	algo = rand() % ARRAY_SIZE(algos);
+	case 2:	algs = rng_algos;
+		algo = rand() % ARRAY_SIZE(rng_algos);
 		break;
 	// skcipher
-	case 3:	algo = rand() % ARRAY_SIZE(algos);
+	case 3:	algs = skcipher_algos;
+		algo = rand() % ARRAY_SIZE(skcipher_algos);
 		break;
 	// akcipher
-	case 4:	algo = rand() % ARRAY_SIZE(algos);
+	case 4:	algs = akcipher_algos;
+		algo = rand() % ARRAY_SIZE(akcipher_algos);
 		break;
 	// kpp
-	case 5:	algo = rand() % ARRAY_SIZE(algos);
+	case 5:	algs = kpp_algos;
+		algo = rand() % ARRAY_SIZE(kpp_algos);
 		break;
 	default: unreachable();
 	}
@@ -297,7 +165,7 @@ static void alg_socket_setup(int fd)
 	unsigned int keylen;
 	const char *hash_types[] = { "hash", "skcipher", "aead", "rng" };
 	const char *hash_algos[] = { "sha1", "sha256", "md5", "sha512" };
-	const char *skcipher_algos[] = { "cbc(aes)", "ecb(aes)", "ctr(aes)" };
+	const char *setup_skcipher_algos[] = { "cbc(aes)", "ecb(aes)", "ctr(aes)" };
 	const char *type;
 	unsigned int type_idx;
 
@@ -317,7 +185,7 @@ static void alg_socket_setup(int fd)
 		break;
 	case 1: /* skcipher */
 		strncpy((char *)sa.salg_name,
-			skcipher_algos[rand() % ARRAY_SIZE(skcipher_algos)],
+			setup_skcipher_algos[rand() % ARRAY_SIZE(setup_skcipher_algos)],
 			sizeof(sa.salg_name) - 1);
 		break;
 	default:
