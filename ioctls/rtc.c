c1a146e78dd1fdcdae090f759ca0f80ba1312edc
Dave Jones <davej@codemonkey.org.uk>
2026-04-19 13:19:10 -0700

    ioctls/rtc: add per-ioctl sanitise functions
    Without sanitise, RTC ioctls receive garbage arg values.  The struct-arg
    ioctls (RTC_SET_TIME, RTC_ALM_SET, RTC_WKALM_SET, RTC_PLL_SET etc.)
    all get EFAULT immediately, and the _IOR output ioctls similarly need a
    valid buffer to write into.
    
    Add rtc_sanitise() covering all 22 ioctls.  A shared fill_rtc_time()
    helper builds rtc_time with bounded random field values (sec 0-59,
    min 0-59, hour 0-23, mday 1-31, mon 0-11, year 70-199).  Wakeup alarm
    ioctls use sanitise_rtc_wkalrm() which wraps fill_rtc_time() into
    rtc_wkalrm.  PLL ioctls get a rtc_pll_info with bounded signed values.
    The four unsigned-long ioctls (IRQP and EPOCH read/set) share a writable
    long pointer.  The eight flag-only _IO ioctls (AIE/UIE/PIE/WIE on/off)
    need no argument and are left as the default random value.

--- ioctls/rtc.c
+++ ioctls/rtc.c
@@ -2,7 +2,125 @@
 #include <linux/rtc.h>
 
 #include "ioctls.h"
+#include "random.h"
+#include "sanitise.h"
 #include "utils.h"
+
+static void fill_rtc_time(struct rtc_time *t)
+{
+	t->tm_sec = rand() % 60;
+	t->tm_min = rand() % 60;
+	t->tm_hour = rand() % 24;
+	t->tm_mday = rand() % 31 + 1;
+	t->tm_mon = rand() % 12;
+	t->tm_year = rand() % 130 + 70;	/* 1970-2099 relative to 1900 */
+	t->tm_wday = rand() % 7;
+	t->tm_yday = rand() % 366;
+	t->tm_isdst = RAND_BOOL();
+}
+
+static void sanitise_rtc_time(struct syscallrecord *rec)
+{
+	struct rtc_time *t;
+
+	t = (struct rtc_time *) get_writable_struct(sizeof(*t));
+	if (!t)
+		return;
+	fill_rtc_time(t);
+	rec->a3 = (unsigned long) t;
+}
+
+static void sanitise_rtc_wkalrm(struct syscallrecord *rec)
+{
+	struct rtc_wkalrm *alrm;
+
+	alrm = (struct rtc_wkalrm *) get_writable_struct(sizeof(*alrm));
+	if (!alrm)
+		return;
+	alrm->enabled = RAND_BOOL();
+	alrm->pending = RAND_BOOL();
+	fill_rtc_time(&alrm->time);
+	rec->a3 = (unsigned long) alrm;
+}
+
+static void sanitise_rtc_pll_info(struct syscallrecord *rec)
+{
+	struct rtc_pll_info *pll;
+
+	pll = (struct rtc_pll_info *) get_writable_struct(sizeof(*pll));
+	if (!pll)
+		return;
+	pll->pll_ctrl = rand();
+	pll->pll_value = (int)(rand() % 201) - 100;	/* -100 to 100 */
+	pll->pll_max = rand() % 1000;
+	pll->pll_min = -(int)(rand() % 1000);
+	pll->pll_posmult = rand() % 8 + 1;
+	pll->pll_negmult = rand() % 8 + 1;
+	pll->pll_clock = rand() % 1000000 + 1;
+	rec->a3 = (unsigned long) pll;
+}
+
+static void rtc_sanitise(const struct ioctl_group *grp, struct syscallrecord *rec)
+{
+	pick_random_ioctl(grp, rec);
+
+	switch (rec->a2) {
+	case RTC_ALM_SET:
+	case RTC_ALM_READ:
+	case RTC_RD_TIME:
+	case RTC_SET_TIME:
+		sanitise_rtc_time(rec);
+		break;
+
+	case RTC_WKALM_SET:
+	case RTC_WKALM_RD:
+		sanitise_rtc_wkalrm(rec);
+		break;
+
+	case RTC_PLL_GET:
+	case RTC_PLL_SET:
+		sanitise_rtc_pll_info(rec);
+		break;
+
+	case RTC_IRQP_READ:
+	case RTC_IRQP_SET:
+	case RTC_EPOCH_READ:
+	case RTC_EPOCH_SET: {
+		unsigned long *p = (unsigned long *) get_writable_struct(sizeof(unsigned long));
+		if (p) {
+			*p = rand();
+			rec->a3 = (unsigned long) p;
+		}
+		break;
+	}
+
+#ifdef RTC_VL_READ
+	case RTC_VL_READ: {
+		unsigned int *p = (unsigned int *) get_writable_struct(sizeof(unsigned int));
+		if (p)
+			rec->a3 = (unsigned long) p;
+		break;
+	}
+#endif
+
+	/* _IO ioctls: no pointer argument; leave default from pick_random_ioctl */
+	case RTC_AIE_ON:
+	case RTC_AIE_OFF:
+	case RTC_UIE_ON:
+	case RTC_UIE_OFF:
+	case RTC_PIE_ON:
+	case RTC_PIE_OFF:
+	case RTC_WIE_ON:
+	case RTC_WIE_OFF:
+#ifdef RTC_VL_CLR
+	case RTC_VL_CLR:
+#endif
+		break;
+
+	default:
+		break;
+	}
+}
 
 static const struct ioctl rtc_ioctls[] = {
 	IOCTL(RTC_AIE_ON),
@@ -41,7 +159,7 @@ static const struct ioctl_group rtc_grp = {
 	.devtype = DEV_CHAR,
 	.devs = rtc_devs,
 	.devs_cnt = ARRAY_SIZE(rtc_devs),
-	.sanitise = pick_random_ioctl,
+	.sanitise = rtc_sanitise,
 	.ioctls = rtc_ioctls,
 	.ioctls_cnt = ARRAY_SIZE(rtc_ioctls),
 };
