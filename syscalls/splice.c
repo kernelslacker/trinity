e44a12d6b9404b7e527085d12320baa2db1b0e86
Dave Jones <davej@codemonkey.org.uk>
2026-04-20 23:42:36 -0700

    syscalls/splice: sanitise off_in/off_out as valid loff_t pointers
    Both offset args were ARG_ADDRESS with no sanitise, so they got random
    addresses that EFAULT before splice() could touch the actual splice path.
    Mirror sendfile/copy_file_range: allocate writable storage and write a
    [0, 1G] offset into it.

--- syscalls/splice.c
+++ syscalls/splice.c
@@ -4,12 +4,25 @@
 	size_t, len, unsigned int, flags)
  */
 #include <fcntl.h>
+#include "random.h"
 #include "sanitise.h"
+#include "trinity.h"
 #include "compat.h"
 
 static unsigned long splice_flags[] = {
 	SPLICE_F_MOVE, SPLICE_F_NONBLOCK, SPLICE_F_MORE, SPLICE_F_GIFT,
 };
+
+static void sanitise_splice(struct syscallrecord *rec)
+{
+	loff_t *off_in = (loff_t *) get_writable_address(sizeof(loff_t));
+	loff_t *off_out = (loff_t *) get_writable_address(sizeof(loff_t));
+
+	*off_in = RAND_RANGE(0, 1ULL << 30);
+	*off_out = RAND_RANGE(0, 1ULL << 30);
+	rec->a2 = (unsigned long) off_in;
+	rec->a4 = (unsigned long) off_out;
+}
 
 struct syscallentry syscall_splice = {
 	.name = "splice",
@@ -17,6 +30,7 @@ struct syscallentry syscall_splice = {
 	.argtype = { [0] = ARG_FD_PIPE, [1] = ARG_ADDRESS, [2] = ARG_FD_PIPE, [3] = ARG_ADDRESS, [4] = ARG_LEN, [5] = ARG_LIST },
 	.argname = { [0] = "fd_in", [1] = "off_in", [2] = "fd_out", [3] = "off_out", [4] = "len", [5] = "flags" },
 	.arg_params[5].list = ARGLIST(splice_flags),
+	.sanitise = sanitise_splice,
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
 };
