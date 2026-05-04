81fb6af918f143b693c5261267c4b05a96f0a01d
Dave Jones <davej@codemonkey.org.uk>
2026-05-04 14:28:35 -0700

    tee: validate post-handler retval is in [0, len] or -1
    Bytes duplicated must be bounded by the requested len (rec->a3); on failure retval is -1.

--- syscalls/tee.c
+++ syscalls/tee.c
@@ -3,11 +3,23 @@
  */
 #include <fcntl.h>
 #include "sanitise.h"
+#include "trinity.h"
 #include "compat.h"
+#include "utils.h"
 
 static unsigned long tee_flags[] = {
 	SPLICE_F_MOVE, SPLICE_F_NONBLOCK, SPLICE_F_MORE, SPLICE_F_GIFT,
 };
+
+static void post_tee(struct syscallrecord *rec)
+{
+	long ret = (long) rec->retval;
+
+	if (ret == -1L)
+		return;
+	if (ret < 0 || (size_t) ret > (size_t) rec->a3)
+		post_handler_corrupt_ptr_bump(rec, NULL);
+}
 
 struct syscallentry syscall_tee = {
 	.name = "tee",
@@ -15,6 +27,7 @@ struct syscallentry syscall_tee = {
 	.argtype = { [0] = ARG_FD_PIPE, [1] = ARG_FD_PIPE, [2] = ARG_LEN, [3] = ARG_LIST },
 	.argname = { [0] = "fdin", [1] = "fdout", [2] = "len", [3] = "flags" },
 	.arg_params[3].list = ARGLIST(tee_flags),
+	.post = post_tee,
 	.flags = NEED_ALARM,
 	.group = GROUP_VFS,
 };
