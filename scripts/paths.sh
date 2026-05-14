19eaee9952845817daf3eecdc624b3a2cc449535
Dave Jones <davej@codemonkey.org.uk>
2026-05-14 00:51:19 -0700

    scripts: paths.sh: quote $(pwd) inside mktemp -d (SC2046)
    Whitespace in the working directory would otherwise word-split the path
    passed to mktemp. Same shape as the in-repo /tmp branch on the next line
    (which is already a literal absolute path).
    
    Author: Dave Jones <davej@codemonkey.org.uk>
    Committer: Dave Jones <davej@codemonkey.org.uk>

--- scripts/paths.sh
+++ scripts/paths.sh
@@ -3,7 +3,7 @@
 TRINITY_PATH=${TRINITY_PATH:-$(pwd)}
 
 if [ -d tmp ]; then
-  TRINITY_TMP=$(mktemp -d $(pwd)/tmp/trinity.XXXXXX)
+  TRINITY_TMP=$(mktemp -d "$(pwd)/tmp/trinity.XXXXXX")
 else
   TRINITY_TMP=$(mktemp -d /tmp/trinity.XXXXXX)
 fi
