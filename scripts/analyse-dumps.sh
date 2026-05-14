f40ebcd82bae1ce3502ade67054a52628facf680
Dave Jones <davej@codemonkey.org.uk>
2026-05-13 23:59:42 -0700

    scripts: analyse-dumps.sh: quote variable expansions (SC2086)
    The gdb invocation on line 7 and the cat redirection on line 9 left
    $TRINITY_PATH, $core, and $SHA unquoted. Quote them so a path or
    core filename containing whitespace would not word-split into
    multiple arguments.

--- scripts/analyse-dumps.sh
+++ scripts/analyse-dumps.sh
@@ -4,9 +4,9 @@ TRINITY_PATH=${TRINITY_PATH:-.}
 
 for core in tmp/trinity.*/tmp/core.*
 do
-  gdb -batch -n -ex 'bt' $TRINITY_PATH/trinity $core > core.txt
+  gdb -batch -n -ex 'bt' "$TRINITY_PATH/trinity" "$core" > core.txt
   SHA=$(grep -v New\ LWP core.txt | grep -v childno | sha1sum | awk '{ print $1 }')
-  cat core.txt > core-$SHA.txt
+  cat core.txt > "core-$SHA.txt"
   rm -f core.txt
 done
 
