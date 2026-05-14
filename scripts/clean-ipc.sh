c6a048120508c4dc5a5455abc8d48d559731a95f
Dave Jones <davej@codemonkey.org.uk>
2026-05-13 21:19:49 -0700

    scripts: clean-ipc.sh: delete commented-out dead code
    Lines 2-8 are two blocks of commented-out earlier attempts at IPC
    cleanup. Different cut/awk parsing strategies and a different ipcrm
    flag (-M vs -m); both predate the current working `awk '{ print $2 }'
    | ipcrm -m` form on the live line. They've been carried around
    commented for years and just clutter the file.

--- scripts/clean-ipc.sh
+++ scripts/clean-ipc.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-#for s in `ipcs -m | grep $(whoami) | cut -c12-19`
-#do
-#echo $s
-#ipcrm shm $s
-#done
-
-#for s in `ipcs -m | grep $(whoami) | grep -v ^0x00000000 | awk '{ print $1 }'`; do ipcrm -M $s ; done
 
 for s in `ipcs -m | grep $(whoami) | awk '{ print $2 }'`; do   ipcrm -m $s; done
 
