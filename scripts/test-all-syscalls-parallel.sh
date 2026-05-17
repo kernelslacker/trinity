8cd43c4c2e8c723ca601c81593892376ece946e0
Dave Jones <davej@codemonkey.org.uk>
2026-05-17 11:11:14 -0700

    [scripts] test-all-syscalls-parallel.sh: cap concurrent trinities at nproc
    Previously forked one trinity child per syscall simultaneously (~800
    processes on modern x86_64 builds, each with default child counts), an
    easy way to wedge a box. Throttle via xargs -P $(nproc) so no more than
    $(nproc) trinity instances run concurrently.

--- scripts/test-all-syscalls-parallel.sh
+++ scripts/test-all-syscalls-parallel.sh
@@ -7,13 +7,11 @@
 chmod 755 $TRINITY_TMP
 cd "$TRINITY_TMP" || exit 1
 
+NR_CPUS=$(nproc)
 
 while true;
 do
-  for syscall in $($TRINITY_PATH/trinity -L | grep entrypoint | grep -v AVOID | awk '{ print $3 }' | sort -u)
-  do
-	MALLOC_CHECK_=2 $TRINITY_PATH/trinity -c $syscall $DROPPRIVS &
-  done
-  wait
+  $TRINITY_PATH/trinity -L | grep entrypoint | grep -v AVOID | awk '{ print $3 }' | sort -u | \
+    xargs -P "$NR_CPUS" -I{} env MALLOC_CHECK_=2 $TRINITY_PATH/trinity -c {} $DROPPRIVS
   check_tainted
 done
