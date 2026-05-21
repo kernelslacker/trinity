09747d688e8efdfb52d8d28aa65916ddae2118b2
Dave Jones <davej@codemonkey.org.uk>
2026-05-21 11:05:22 -0700

    settimeofday: use rnd_u32() instead of libc rand()
    rand() showed up at ~5.82% of CPU in a perf top during a fuzz run.
    Switch to the inline splitmix64 helpers in rnd.h, which compile down to
    register-only multiplies and xor-shifts and take the libc PRNG out of
    the hot path.

--- syscalls/settimeofday.c
+++ syscalls/settimeofday.c
@@ -3,6 +3,7 @@
  */
 #include <sys/time.h>
 #include "random.h"
+#include "rnd.h"
 #include "sanitise.h"
 
 static void sanitise_settimeofday(struct syscallrecord *rec)
@@ -13,22 +14,22 @@ static void sanitise_settimeofday(struct syscallrecord *rec)
 	tv = (struct timeval *) get_writable_address(sizeof(*tv));
 	if (tv == NULL)
 		return;
-	switch (rand() % 4) {
+	switch (rnd_modulo_u32(4)) {
 	case 0:	/* epoch */
 		tv->tv_sec = 0;
 		tv->tv_usec = 0;
 		break;
 	case 1: /* near-current */
-		tv->tv_sec = time(NULL) + (rand() % 120) - 60;
-		tv->tv_usec = rand() % 1000000;
+		tv->tv_sec = time(NULL) + (rnd_modulo_u32(120)) - 60;
+		tv->tv_usec = rnd_modulo_u32(1000000);
 		break;
 	case 2: /* invalid usec (>= 1 million) */
 		tv->tv_sec = rand32();
-		tv->tv_usec = 1000000 + (rand() % 1000000);
+		tv->tv_usec = 1000000 + (rnd_modulo_u32(1000000));
 		break;
 	default:
 		tv->tv_sec = rand32();
-		tv->tv_usec = rand() % 1000000;
+		tv->tv_usec = rnd_modulo_u32(1000000);
 		break;
 	}
 	rec->a1 = (unsigned long) tv;
@@ -38,8 +39,8 @@ static void sanitise_settimeofday(struct syscallrecord *rec)
 		tz = (struct timezone *) get_writable_address(sizeof(*tz));
 		if (tz == NULL)
 			return;
-		tz->tz_minuteswest = (rand() % 1560) - 780;	/* -13h to +13h */
-		tz->tz_dsttime = rand() % 4;
+		tz->tz_minuteswest = (rnd_modulo_u32(1560)) - 780;	/* -13h to +13h */
+		tz->tz_dsttime = rnd_modulo_u32(4);
 		rec->a2 = (unsigned long) tz;
 	}
 }
