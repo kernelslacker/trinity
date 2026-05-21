0c8e871a6d0608ec8c91ba1114b54909f846aa5d
Dave Jones <davej@codemonkey.org.uk>
2026-05-21 11:06:28 -0700

    prlimit64: use rnd_u32() instead of libc rand()
    rand() showed up at ~5.82% of CPU in a perf top during a fuzz run.
    Switch to the inline splitmix64 helpers in rnd.h, which compile down to
    register-only multiplies and xor-shifts and take the libc PRNG out of
    the hot path.

--- syscalls/prlimit64.c
+++ syscalls/prlimit64.c
@@ -11,6 +11,7 @@
 #include "arch.h"
 #include "deferred-free.h"
 #include "random.h"
+#include "rnd.h"
 #include "sanitise.h"
 #include "shm.h"
 #include "trinity.h"
@@ -52,11 +53,11 @@ static unsigned long rlimit_resources[] = {
 
 static rlim64_t random_rlim64(void)
 {
-	switch (rand() % 5) {
+	switch (rnd_modulo_u32(5)) {
 	case 0: return RLIM64_INFINITY;
 	case 1: return 0;
-	case 2: return 1 + (rand() % 1024);
-	case 3: return (rlim64_t) page_size * (1 + (rand() % 256));
+	case 2: return 1 + (rnd_modulo_u32(1024));
+	case 3: return (rlim64_t) page_size * (1 + (rnd_modulo_u32(256)));
 	default: return rand32();
 	}
 }
