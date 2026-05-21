9459f8221df0526bcd9892bf258136bb9cc485f2
Dave Jones <davej@codemonkey.org.uk>
2026-05-21 11:05:20 -0700

    sched_setattr: use rnd_u32() instead of libc rand()
    rand() showed up at ~5.82% of CPU in a perf top during a fuzz run.
    Switch to the inline splitmix64 helpers in rnd.h, which compile down to
    register-only multiplies and xor-shifts and take the libc PRNG out of
    the hot path.

--- syscalls/sched_setattr.c
+++ syscalls/sched_setattr.c
@@ -5,6 +5,7 @@
 #include <linux/sched/types.h>
 #include <string.h>
 #include "random.h"
+#include "rnd.h"
 #include "sanitise.h"
 #include "compat.h"
 
@@ -19,31 +20,31 @@ static void sanitise_sched_setattr(struct syscallrecord *rec)
 
 	sa->size = sizeof(*sa);
 
-	switch (rand() % 6) {
+	switch (rnd_modulo_u32(6)) {
 	case 0: /* SCHED_OTHER */
 		sa->sched_policy = 0;
-		sa->sched_nice = (rand() % 40) - 20;	/* -20 to 19 */
+		sa->sched_nice = (rnd_modulo_u32(40)) - 20;	/* -20 to 19 */
 		break;
 	case 1: /* SCHED_FIFO */
 		sa->sched_policy = 1;
-		sa->sched_priority = 1 + (rand() % 99);
+		sa->sched_priority = 1 + (rnd_modulo_u32(99));
 		break;
 	case 2: /* SCHED_RR */
 		sa->sched_policy = 2;
-		sa->sched_priority = 1 + (rand() % 99);
+		sa->sched_priority = 1 + (rnd_modulo_u32(99));
 		break;
 	case 3: /* SCHED_BATCH */
 		sa->sched_policy = 3;
-		sa->sched_nice = (rand() % 40) - 20;
+		sa->sched_nice = (rnd_modulo_u32(40)) - 20;
 		break;
 	case 4: /* SCHED_IDLE */
 		sa->sched_policy = SCHED_IDLE;
 		break;
 	default: /* SCHED_DEADLINE */
 		sa->sched_policy = SCHED_DEADLINE;
-		sa->sched_runtime  = 1000000ULL * (1 + (rand() % 10));	/* 1-10ms */
-		sa->sched_deadline = sa->sched_runtime * (1 + (rand() % 5));
-		sa->sched_period   = sa->sched_deadline * (1 + (rand() % 3));
+		sa->sched_runtime  = 1000000ULL * (1 + (rnd_modulo_u32(10)));	/* 1-10ms */
+		sa->sched_deadline = sa->sched_runtime * (1 + (rnd_modulo_u32(5)));
+		sa->sched_period   = sa->sched_deadline * (1 + (rnd_modulo_u32(3)));
 		break;
 	}
 
