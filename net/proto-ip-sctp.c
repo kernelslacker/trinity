37dd4e2a94d7b0284ecd55f7edb2f8941fffa3aa
Dave Jones <davej@codemonkey.org.uk>
2026-05-21 11:01:58 -0700

    proto-ip-sctp: use rnd_u32() instead of libc rand()
    rand() showed up at about 5.82% of CPU in perf top during a fuzz run --
    the inline splitmix64 helpers in rnd.h compile down to a handful of
    register-only ops and are lockless, taking the libc PRNG out of the hot
    path while preserving the existing seed-reproducibility story.

--- net/proto-ip-sctp.c
+++ net/proto-ip-sctp.c
@@ -7,6 +7,7 @@
 #include "net.h"
 #include "random.h"
 #include "compat.h"
+#include "rnd.h"
 
 static const unsigned int sctp_opts[] = {
 	SCTP_RTOINFO, SCTP_ASSOCINFO, SCTP_INITMSG, SCTP_NODELAY,
@@ -48,30 +49,30 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	switch (so->optname) {
 	case SCTP_RTOINFO:
 		rtoinfo = (struct sctp_rtoinfo *) so->optval;
-		rtoinfo->srto_assoc_id = rand();
-		rtoinfo->srto_initial = rand() % 60000;
-		rtoinfo->srto_max = rand() % 60000;
-		rtoinfo->srto_min = rand() % 60000;
+		rtoinfo->srto_assoc_id = rnd_u32();
+		rtoinfo->srto_initial = rnd_modulo_u32(60000);
+		rtoinfo->srto_max = rnd_modulo_u32(60000);
+		rtoinfo->srto_min = rnd_modulo_u32(60000);
 		so->optlen = sizeof(struct sctp_rtoinfo);
 		break;
 
 	case SCTP_ASSOCINFO:
 		assocparams = (struct sctp_assocparams *) so->optval;
-		assocparams->sasoc_assoc_id = rand();
-		assocparams->sasoc_asocmaxrxt = rand();
-		assocparams->sasoc_number_peer_destinations = rand();
-		assocparams->sasoc_peer_rwnd = rand();
-		assocparams->sasoc_local_rwnd = rand();
-		assocparams->sasoc_cookie_life = rand();
+		assocparams->sasoc_assoc_id = rnd_u32();
+		assocparams->sasoc_asocmaxrxt = rnd_u32();
+		assocparams->sasoc_number_peer_destinations = rnd_u32();
+		assocparams->sasoc_peer_rwnd = rnd_u32();
+		assocparams->sasoc_local_rwnd = rnd_u32();
+		assocparams->sasoc_cookie_life = rnd_u32();
 		so->optlen = sizeof(struct sctp_assocparams);
 		break;
 
 	case SCTP_INITMSG:
 		initmsg = (struct sctp_initmsg *) so->optval;
-		initmsg->sinit_num_ostreams = rand();
-		initmsg->sinit_max_instreams = rand();
-		initmsg->sinit_max_attempts = rand();
-		initmsg->sinit_max_init_timeo = rand();
+		initmsg->sinit_num_ostreams = rnd_u32();
+		initmsg->sinit_max_instreams = rnd_u32();
+		initmsg->sinit_max_attempts = rnd_u32();
+		initmsg->sinit_max_init_timeo = rnd_u32();
 		so->optlen = sizeof(struct sctp_initmsg);
 		break;
 
@@ -87,25 +88,25 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 
 	case SCTP_AUTOCLOSE:
 		optval32 = (__u32 *) so->optval;
-		*optval32 = rand() % 3600;
+		*optval32 = rnd_modulo_u32(3600);
 		so->optlen = sizeof(__u32);
 		break;
 
 	case SCTP_FRAGMENT_INTERLEAVE:
 		optval32 = (__u32 *) so->optval;
-		*optval32 = rand() % 3;
+		*optval32 = rnd_modulo_u32(3);
 		so->optlen = sizeof(__u32);
 		break;
 
 	case SCTP_PARTIAL_DELIVERY_POINT:
 		optval32 = (__u32 *) so->optval;
-		*optval32 = 1024 * (1 + rand() % 128);
+		*optval32 = 1024 * (1 + rnd_modulo_u32(128));
 		so->optlen = sizeof(__u32);
 		break;
 
 	case SCTP_ADAPTATION_LAYER: {
 		struct sctp_setadaptation *adapt = (struct sctp_setadaptation *) so->optval;
-		adapt->ssb_adaptation_ind = rand();
+		adapt->ssb_adaptation_ind = rnd_u32();
 		so->optlen = sizeof(struct sctp_setadaptation);
 		break;
 	}
@@ -113,7 +114,7 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_SET_PEER_PRIMARY_ADDR: {
 		struct sctp_setpeerprim *pp = (struct sctp_setpeerprim *) so->optval;
 		memset(pp, 0, sizeof(*pp));
-		pp->sspp_assoc_id = rand();
+		pp->sspp_assoc_id = rnd_u32();
 		so->optlen = sizeof(struct sctp_setpeerprim);
 		break;
 	}
@@ -121,33 +122,33 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_PRIMARY_ADDR: {
 		struct sctp_prim *prim = (struct sctp_prim *) so->optval;
 		memset(prim, 0, sizeof(*prim));
-		prim->ssp_assoc_id = rand();
+		prim->ssp_assoc_id = rnd_u32();
 		so->optlen = sizeof(struct sctp_prim);
 		break;
 	}
 
 	case SCTP_DEFAULT_SEND_PARAM: {
 		struct sctp_sndrcvinfo *sinfo = (struct sctp_sndrcvinfo *) so->optval;
-		sinfo->sinfo_stream = rand() % 65535;
+		sinfo->sinfo_stream = rnd_modulo_u32(65535);
 		sinfo->sinfo_ssn = 0;
-		sinfo->sinfo_flags = rand() & 0x010f;
-		sinfo->sinfo_ppid = rand();
-		sinfo->sinfo_context = rand();
-		sinfo->sinfo_timetolive = rand() % 60000;
+		sinfo->sinfo_flags = rnd_u32() & 0x010f;
+		sinfo->sinfo_ppid = rnd_u32();
+		sinfo->sinfo_context = rnd_u32();
+		sinfo->sinfo_timetolive = rnd_modulo_u32(60000);
 		sinfo->sinfo_tsn = 0;
 		sinfo->sinfo_cumtsn = 0;
-		sinfo->sinfo_assoc_id = rand();
+		sinfo->sinfo_assoc_id = rnd_u32();
 		so->optlen = sizeof(struct sctp_sndrcvinfo);
 		break;
 	}
 
 	case SCTP_DEFAULT_SNDINFO: {
 		struct sctp_sndinfo *sndinfo = (struct sctp_sndinfo *) so->optval;
-		sndinfo->snd_sid = rand() % 65535;
-		sndinfo->snd_flags = rand() & 0x010f;
-		sndinfo->snd_ppid = rand();
-		sndinfo->snd_context = rand();
-		sndinfo->snd_assoc_id = rand();
+		sndinfo->snd_sid = rnd_modulo_u32(65535);
+		sndinfo->snd_flags = rnd_u32() & 0x010f;
+		sndinfo->snd_ppid = rnd_u32();
+		sndinfo->snd_context = rnd_u32();
+		sndinfo->snd_assoc_id = rnd_u32();
 		so->optlen = sizeof(struct sctp_sndinfo);
 		break;
 	}
@@ -160,19 +161,19 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_PEER_ADDR_PARAMS: {
 		struct sctp_paddrparams *pp = (struct sctp_paddrparams *) so->optval;
 		memset(pp, 0, sizeof(*pp));
-		pp->spp_assoc_id = rand();
-		pp->spp_hbinterval = rand() % 30000;
-		pp->spp_pathmaxrxt = rand() % 12;
-		pp->spp_pathmtu = rand() % 65536;
-		pp->spp_sackdelay = rand() % 500;
-		pp->spp_flags = rand() & (SPP_HB_ENABLE | SPP_PMTUD_ENABLE | SPP_SACKDELAY_ENABLE);
+		pp->spp_assoc_id = rnd_u32();
+		pp->spp_hbinterval = rnd_modulo_u32(30000);
+		pp->spp_pathmaxrxt = rnd_modulo_u32(12);
+		pp->spp_pathmtu = rnd_modulo_u32(65536);
+		pp->spp_sackdelay = rnd_modulo_u32(500);
+		pp->spp_flags = rnd_u32() & (SPP_HB_ENABLE | SPP_PMTUD_ENABLE | SPP_SACKDELAY_ENABLE);
 		so->optlen = sizeof(struct sctp_paddrparams);
 		break;
 	}
 
 	case SCTP_AUTH_CHUNK: {
 		struct sctp_authchunk *authchunk = (struct sctp_authchunk *) so->optval;
-		authchunk->sauth_chunk = rand() & 0xff;
+		authchunk->sauth_chunk = rnd_u32() & 0xff;
 		so->optlen = sizeof(struct sctp_authchunk);
 		break;
 	}
@@ -180,20 +181,20 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_HMAC_IDENT: {
 		static const __u16 hmac_ids[] = { SCTP_AUTH_HMAC_ID_SHA1, SCTP_AUTH_HMAC_ID_SHA256 };
 		struct sctp_hmacalgo *hmac = (struct sctp_hmacalgo *) so->optval;
-		unsigned int n = 1 + (rand() % 4);
+		unsigned int n = 1 + (rnd_modulo_u32(4));
 		unsigned int i;
 		hmac->shmac_num_idents = n;
 		for (i = 0; i < n; i++)
-			hmac->shmac_idents[i] = hmac_ids[rand() % 2];
+			hmac->shmac_idents[i] = hmac_ids[rnd_modulo_u32(2)];
 		so->optlen = sizeof(struct sctp_hmacalgo) + n * sizeof(__u16);
 		break;
 	}
 
 	case SCTP_AUTH_KEY: {
 		struct sctp_authkey *authkey = (struct sctp_authkey *) so->optval;
-		unsigned int keylen = rand() % 64;
-		authkey->sca_assoc_id = rand();
-		authkey->sca_keynumber = rand() % 8;
+		unsigned int keylen = rnd_modulo_u32(64);
+		authkey->sca_assoc_id = rnd_u32();
+		authkey->sca_keynumber = rnd_modulo_u32(8);
 		authkey->sca_keylength = keylen;
 		generate_rand_bytes(authkey->sca_key, keylen);
 		so->optlen = sizeof(struct sctp_authkey) + keylen;
@@ -204,17 +205,17 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_AUTH_DELETE_KEY:
 	case SCTP_AUTH_DEACTIVATE_KEY: {
 		struct sctp_authkeyid *keyid = (struct sctp_authkeyid *) so->optval;
-		keyid->scact_assoc_id = rand();
-		keyid->scact_keynumber = rand() % 8;
+		keyid->scact_assoc_id = rnd_u32();
+		keyid->scact_keynumber = rnd_modulo_u32(8);
 		so->optlen = sizeof(struct sctp_authkeyid);
 		break;
 	}
 
 	case SCTP_DELAYED_ACK_TIME: {
 		struct sctp_sack_info *sack = (struct sctp_sack_info *) so->optval;
-		sack->sack_assoc_id = rand();
-		sack->sack_delay = rand() % 500;
-		sack->sack_freq = 1 + (rand() % 8);
+		sack->sack_assoc_id = rnd_u32();
+		sack->sack_delay = rnd_modulo_u32(500);
+		sack->sack_freq = 1 + (rnd_modulo_u32(8));
 		so->optlen = sizeof(struct sctp_sack_info);
 		break;
 	}
@@ -232,41 +233,41 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_ECN_SUPPORTED:
 	case SCTP_EXPOSE_POTENTIALLY_FAILED_STATE:
 		av = (struct sctp_assoc_value *) so->optval;
-		av->assoc_id = rand();
-		av->assoc_value = rand();
+		av->assoc_id = rnd_u32();
+		av->assoc_value = rnd_u32();
 		so->optlen = sizeof(struct sctp_assoc_value);
 		break;
 
 	case SCTP_STREAM_SCHEDULER: {
 		av = (struct sctp_assoc_value *) so->optval;
-		av->assoc_id = rand();
-		av->assoc_value = rand() % (SCTP_SS_MAX + 1);
+		av->assoc_id = rnd_u32();
+		av->assoc_value = rnd_modulo_u32(SCTP_SS_MAX + 1);
 		so->optlen = sizeof(struct sctp_assoc_value);
 		break;
 	}
 
 	case SCTP_STREAM_SCHEDULER_VALUE: {
 		struct sctp_stream_value *sv = (struct sctp_stream_value *) so->optval;
-		sv->assoc_id = rand();
-		sv->stream_id = rand() % 1024;
-		sv->stream_value = rand() % 1024;
+		sv->assoc_id = rnd_u32();
+		sv->stream_id = rnd_modulo_u32(1024);
+		sv->stream_value = rnd_modulo_u32(1024);
 		so->optlen = sizeof(struct sctp_stream_value);
 		break;
 	}
 
 	case SCTP_DEFAULT_PRINFO: {
 		struct sctp_default_prinfo *prinfo = (struct sctp_default_prinfo *) so->optval;
-		prinfo->pr_assoc_id = rand();
-		prinfo->pr_value = rand();
-		prinfo->pr_policy = rand() & 0x3;
+		prinfo->pr_assoc_id = rnd_u32();
+		prinfo->pr_value = rnd_u32();
+		prinfo->pr_policy = rnd_u32() & 0x3;
 		so->optlen = sizeof(struct sctp_default_prinfo);
 		break;
 	}
 
 	case SCTP_EVENT: {
 		struct sctp_event *event = (struct sctp_event *) so->optval;
-		event->se_assoc_id = rand();
-		event->se_type = rand() % 16;
+		event->se_assoc_id = rnd_u32();
+		event->se_type = rnd_modulo_u32(16);
 		event->se_on = RAND_BOOL();
 		so->optlen = sizeof(struct sctp_event);
 		break;
@@ -275,8 +276,8 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_REMOTE_UDP_ENCAPS_PORT: {
 		struct sctp_udpencaps *encaps = (struct sctp_udpencaps *) so->optval;
 		memset(encaps, 0, sizeof(*encaps));
-		encaps->sue_assoc_id = rand();
-		encaps->sue_port = rand() % 65536;
+		encaps->sue_assoc_id = rnd_u32();
+		encaps->sue_port = rnd_modulo_u32(65536);
 		so->optlen = sizeof(struct sctp_udpencaps);
 		break;
 	}
@@ -284,9 +285,9 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_PEER_ADDR_THLDS: {
 		struct sctp_paddrthlds *thlds = (struct sctp_paddrthlds *) so->optval;
 		memset(thlds, 0, sizeof(*thlds));
-		thlds->spt_assoc_id = rand();
-		thlds->spt_pathmaxrxt = rand() % 12;
-		thlds->spt_pathpfthld = rand() % 12;
+		thlds->spt_assoc_id = rnd_u32();
+		thlds->spt_pathmaxrxt = rnd_modulo_u32(12);
+		thlds->spt_pathpfthld = rnd_modulo_u32(12);
 		so->optlen = sizeof(struct sctp_paddrthlds);
 		break;
 	}
@@ -294,32 +295,32 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_PEER_ADDR_THLDS_V2: {
 		struct sctp_paddrthlds_v2 *thlds = (struct sctp_paddrthlds_v2 *) so->optval;
 		memset(thlds, 0, sizeof(*thlds));
-		thlds->spt_assoc_id = rand();
-		thlds->spt_pathmaxrxt = rand() % 12;
-		thlds->spt_pathpfthld = rand() % 12;
-		thlds->spt_pathcpthld = rand() % 12;
+		thlds->spt_assoc_id = rnd_u32();
+		thlds->spt_pathmaxrxt = rnd_modulo_u32(12);
+		thlds->spt_pathpfthld = rnd_modulo_u32(12);
+		thlds->spt_pathcpthld = rnd_modulo_u32(12);
 		so->optlen = sizeof(struct sctp_paddrthlds_v2);
 		break;
 	}
 
 	case SCTP_ADD_STREAMS: {
 		struct sctp_add_streams *adds = (struct sctp_add_streams *) so->optval;
-		adds->sas_assoc_id = rand();
-		adds->sas_instrms = rand() % 16;
-		adds->sas_outstrms = rand() % 16;
+		adds->sas_assoc_id = rnd_u32();
+		adds->sas_instrms = rnd_modulo_u32(16);
+		adds->sas_outstrms = rnd_modulo_u32(16);
 		so->optlen = sizeof(struct sctp_add_streams);
 		break;
 	}
 
 	case SCTP_RESET_STREAMS: {
 		struct sctp_reset_streams *rstr = (struct sctp_reset_streams *) so->optval;
-		unsigned int n = rand() % 4;
+		unsigned int n = rnd_modulo_u32(4);
 		unsigned int i;
-		rstr->srs_assoc_id = rand();
-		rstr->srs_flags = rand() & 3;
+		rstr->srs_assoc_id = rnd_u32();
+		rstr->srs_flags = rnd_u32() & 3;
 		rstr->srs_number_streams = n;
 		for (i = 0; i < n; i++)
-			rstr->srs_stream_list[i] = rand() % 16;
+			rstr->srs_stream_list[i] = rnd_modulo_u32(16);
 		so->optlen = sizeof(struct sctp_reset_streams) + n * sizeof(uint16_t);
 		break;
 	}
@@ -327,8 +328,8 @@ void sctp_setsockopt(struct sockopt *so, __unused__ struct socket_triplet *tripl
 	case SCTP_PLPMTUD_PROBE_INTERVAL: {
 		struct sctp_probeinterval *pi = (struct sctp_probeinterval *) so->optval;
 		memset(pi, 0, sizeof(*pi));
-		pi->spi_assoc_id = rand();
-		pi->spi_interval = rand() % 60000;
+		pi->spi_assoc_id = rnd_u32();
+		pi->spi_interval = rnd_modulo_u32(60000);
 		so->optlen = sizeof(struct sctp_probeinterval);
 		break;
 	}
