e550a9170e4e3368cfec5c8ae1b3bcdefb06a777
Dave Jones <davej@codemonkey.org.uk>
2026-06-05 19:28:56 -0700

    scripts/find.sh: add set -uo pipefail, quote paths, check pushd/popd

--- scripts/find.sh
+++ scripts/find.sh
@@ -8,28 +8,31 @@
 # things down, and had already been ruled out.
 #
 
+set -uo pipefail
+
 . scripts/paths.sh
 . scripts/taint.sh
 
-chmod 755 $TRINITY_TMP
+chmod 755 "$TRINITY_TMP"
 cd "$TRINITY_TMP" || exit 1
 
 while true;
 do
-  pushd $(mktemp -d tmp.XXXXXX)
+  workdir=$(mktemp -d tmp.XXXXXX) || exit 1
+  pushd "$workdir" >/dev/null || exit 1
 
-  if [ ! -f $TRINITY_PATH/trinity ]; then
+  if [ ! -f "$TRINITY_PATH/trinity" ]; then
     echo lost!
     pwd
-    exit
+    exit 1
   fi
 
-  $TRINITY_PATH/trinity -a64 -c ftruncate -r20 -x execve -x execveat -x syncfs -x sync -x fsync -x fdatasync -C64 -N 1000000 --enable-fds=pseudo,testfile
+  "$TRINITY_PATH/trinity" -a64 -c ftruncate -r20 -x execve -x execveat -x syncfs -x sync -x fsync -x fdatasync -C64 -N 1000000 --enable-fds=pseudo,testfile
 
-  popd
+  popd >/dev/null || exit 1
 
   check_tainted
 
-  chmod 755 $TRINITY_TMP
+  chmod 755 "$TRINITY_TMP"
 
 done
