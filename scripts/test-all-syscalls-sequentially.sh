6357bc10bcc878a8391cefd5c8bc56eb9446a81d
Dave Jones <davej@codemonkey.org.uk>
2026-06-05 19:29:26 -0700

    test-all-syscalls-sequentially.sh: harden, quote paths, check popd

--- scripts/test-all-syscalls-sequentially.sh
+++ scripts/test-all-syscalls-sequentially.sh
@@ -3,25 +3,27 @@
 # This is a useful test to run occasionally, to see which syscalls are
 # causing trinity to segfault.
 
+set -uo pipefail
+
 . scripts/paths.sh
 . scripts/taint.sh
 
 while true
 do
-for syscall in $($TRINITY_PATH/trinity -L | grep entrypoint | grep -v AVOID | awk '{ print $3 }' | sort -u)
+for syscall in $("$TRINITY_PATH/trinity" -L | grep entrypoint | grep -v AVOID | awk '{ print $3 }' | sort -u)
 do
-	pushd "$TRINITY_TMP" || exit 1
+	pushd "$TRINITY_TMP" > /dev/null || exit 1
 
-	if [ ! -f $TRINITY_PATH/trinity ]; then
+	if [ ! -f "$TRINITY_PATH/trinity" ]; then
 		echo lost!
 		pwd
-		exit
+		exit 1
 	fi
 
-	MALLOC_CHECK_=2 $TRINITY_PATH/trinity -c $syscall -N 1000000 -C 64 -x execve -x execveat
+	MALLOC_CHECK_=2 "$TRINITY_PATH/trinity" -c "$syscall" -N 1000000 -C 64 -x execve -x execveat
 
-	chmod 755 $TRINITY_TMP
-	popd
+	chmod 755 "$TRINITY_TMP"
+	popd > /dev/null || exit 1
 
 	check_tainted
 	echo
