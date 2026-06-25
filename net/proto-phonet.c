fc5602ede018c12b3d6d225aca099ba2c5dbd60c
Dave Jones <davej@codemonkey.org.uk>
2026-06-24 21:29:16 -0700

    proto-phonet: drop unused netinet/in.h, sys/types.h includes
    These headers are included but the file references no symbol they
    provide. Drop them to shrink the include surface.

--- net/proto-phonet.c
+++ net/proto-phonet.c
@@ -1,8 +1,6 @@
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/uio.h>
 #include <sys/un.h>
-#include <netinet/in.h>
 #include <linux/phonet.h>
 #include <stdlib.h>
 #include <string.h>
