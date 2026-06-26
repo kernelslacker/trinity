6a81efa4b00581c64c20330a4707b812f61dc629
Dave Jones <davej@codemonkey.org.uk>
2026-06-25 17:10:09 -0700

    ipvs-sysctl-writer: drop unused string.h and sys/stat.h includes

--- childops/ipvs-sysctl-writer.c
+++ childops/ipvs-sysctl-writer.c
@@ -21,10 +21,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
