05aacef097f2bff4f9d14d1984f38fbc6d1a287f
Dave Jones <davej@codemonkey.org.uk>
2026-06-25 19:32:00 -0700

    sparse-files: drop unused string.h and sys/stat.h includes
    Unused leaf headers; no code references their symbols.

--- fds/sparse-files.c
+++ fds/sparse-files.c
@@ -15,10 +15,8 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 
 #include "arch.h"
 #include "deferred-free.h"
