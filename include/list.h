56f8dc437616d3814bd40d06c852cdf2f9eee7f8
Dave Jones <davej@codemonkey.org.uk>
2026-06-27 22:19:34 -0700

    list: drop unused helpers list_empty, list_move, __list_del_entry

--- include/list.h
+++ include/list.h
@@ -45,11 +45,6 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
         list->prev = list;
 }
 
-static inline bool list_empty(const struct list_head *head)
-{
-        return head->next == head;
-}
-
 static inline void __list_add(struct list_head *new,
                               struct list_head *prev,
                               struct list_head *next)
@@ -83,23 +78,6 @@ static inline void list_del(struct list_head *entry)
         __list_del(entry->prev, entry->next);
         entry->next = LIST_POISON1;
         entry->prev = LIST_POISON2;
-}
-
-static inline void __list_del_entry(struct list_head *entry)
-{
-	__list_del_entry_valid_or_die(entry, __FILE__, __func__, __LINE__);
-	__list_del(entry->prev, entry->next);
-}
-
-/**
- * list_move - delete from one list and add as another's head
- * @list: the entry to move
- * @head: the head that will precede our entry
- */
-static inline void list_move(struct list_head *list, struct list_head *head)
-{
-	__list_del_entry(list);
-	list_add(list, head);
 }
 
 #define list_for_each(pos, head) \
