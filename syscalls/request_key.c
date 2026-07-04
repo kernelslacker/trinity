1409be1ca4c2658263769b9d6b6ef0ece9c43a8a
Dave Jones <davej@codemonkey.org.uk>
2026-07-04 14:29:45 -0700

    request_key: drop dead duplicate keyspec ARGLIST

--- syscalls/request_key.c
+++ syscalls/request_key.c
@@ -155,19 +155,11 @@ desc_done:
 	rec->a4 = (unsigned long) request_key_pick_destring();
 }
 
-static unsigned long request_key_ids[] = {
-	KEY_SPEC_THREAD_KEYRING, KEY_SPEC_PROCESS_KEYRING,
-	KEY_SPEC_SESSION_KEYRING, KEY_SPEC_USER_KEYRING,
-	KEY_SPEC_USER_SESSION_KEYRING, KEY_SPEC_GROUP_KEYRING,
-	KEY_SPEC_REQKEY_AUTH_KEY, KEY_SPEC_REQUESTOR_KEYRING,
-};
-
 struct syscallentry syscall_request_key = {
 	.name = "request_key",
 	.num_args = 4,
-	.argtype = { [0] = ARG_ADDRESS, [1] = ARG_ADDRESS, [2] = ARG_ADDRESS, [3] = ARG_OP },
+	.argtype = { [0] = ARG_ADDRESS, [1] = ARG_ADDRESS, [2] = ARG_ADDRESS },
 	.argname = { [0] = "_type", [1] = "_description", [2] = "_callout_info", [3] = "destringid" },
-	.arg_params[3].list = ARGLIST(request_key_ids),
 	.rettype = RET_KEY_SERIAL_T,
 	.ret_objtype = OBJ_KEY_SERIAL,
 	.sanitise = sanitise_request_key,
