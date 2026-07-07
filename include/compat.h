33876de34dce1ab8d0e31cceb78010f2c42188f7
Dave Jones <davej@codemonkey.org.uk>
2026-07-07 13:03:06 -0400

    compat: expand rds fallback in include/kernel/rds.h
    Move RDS_* out of compat.h into existing include/kernel/rds.h.
    compat.h shrinks.
    
    Summary:
    Expand include/kernel/rds.h; remove from compat.h.
    
    Test Plan:
    make clean && make -j$(nproc)

--- include/compat.h
+++ include/compat.h
@@ -197,17 +197,6 @@
 #define FUTEX2_PRIVATE		0x80
 #endif
 
-/* linux/rds.h */
-#ifndef RDS_CANCEL_SENT_TO
-#define RDS_CANCEL_SENT_TO              1
-#define RDS_GET_MR                      2
-#define RDS_FREE_MR                     3
-/* deprecated: RDS_BARRIER 4 */
-#define RDS_RECVERR                     5
-#define RDS_CONG_MONITOR                6
-#define RDS_GET_MR_FOR_DEST             7
-#endif
-
 /* asm/ptrace-abi.h */
 #ifndef PTRACE_SYSEMU
 #define PTRACE_SYSEMU		  31
--- include/kernel/rds.h
+++ include/kernel/rds.h
@@ -8,3 +8,13 @@
 #ifndef SOL_RDS
 #define SOL_RDS			276
 #endif
+
+#ifndef RDS_CANCEL_SENT_TO
+#define RDS_CANCEL_SENT_TO              1
+#define RDS_GET_MR                      2
+#define RDS_FREE_MR                     3
+#define RDS_RECVERR                     5
+#define RDS_CONG_MONITOR                6
+#define RDS_GET_MR_FOR_DEST             7
+#endif
+
