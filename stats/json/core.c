56070dc1b2249085bdc573b53e6724e4212ba5de
Dave Jones <davej@codemonkey.org.uk>
2026-08-04 05:04:23 -0700

    ipv6-rpl-clone-fidelity: packet-fidelity oracle for RPL SRH receive

--- stats/json/core.c
+++ stats/json/core.c
@@ -244,6 +244,8 @@ void dump_stats_json_iouring_and_zombies(void)
 	putchar(',');
 	stat_category_emit_json(&inplace_crypto_category);
 	putchar(',');
+	stat_category_emit_json(&rpl_clone_fidelity_category);
+	putchar(',');
 	stat_category_emit_json(&fd_runtime_skipped_category);
 	putchar(',');
 	stat_category_emit_json(&child_category);
